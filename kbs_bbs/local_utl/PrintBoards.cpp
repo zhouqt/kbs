3a9053d0d4d16247383e5edce4f56afa88264aad
bbs <bbs@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2001-04-08 17:19:02 +0000

    print boards
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@36 de4889a5-8ad4-4bf3-bd87-57997c21d515

---
+++ kbs_bbs/local_utl/PrintBoards.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "RecordFile.h"
+#include "bbs.h"
+
+#ifndef AIX
+#define CONV_LONG(x) ( ((x)>>24) | (((x)&0x00ff0000)>>8) | (((x)&0x0000ff00)<<8) | (((x)&0x000000ff)<<24) )
+#endif
+int printboard(struct boardheader* bh)
+{
+  if (bh->filename[0]) {
+    unsigned long level = CONV_LONG(bh->level);
+    if (!((level&PERM_POSTMASK)||(level==0)))
+      printf("%-16s\n",bh->filename);
+  }
+  return 0;
+}
+
+main(int argc,char** argv)
+{
+  char* filename;
+  if (argc<=1) filename=".BOARDS";
+  else filename=argv[1];
+
+  CRecordFile file(filename);
+
+  if (file.ApplyRecord(sizeof(struct boardheader),(APPLY_FUNC)printboard)==-1)
+	perror("open boards");
+}
