c66e8a0fcdd14acdd1b49802b7eb3a94d44c84dc
flyriver <flyriver@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2002-04-10 16:44:38 +0000

    Updated bbstop10 & bbstopb10.
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@538 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/bbs2www/src/bbstop10.c
+++ kbs_bbs/bbs2www/src/bbstop10.c
@@ -1,21 +1,24 @@
 /*#include "bbslib.h"*/
 #include "bbslib.h"
 
-int main() {
+int main()
+{
 	FILE *fp;
 	int n;
 	char s1[256], s2[256], s3[256], s4[256],s5;
 	char brd[256], id[256], title[256], num[100];
+
 	init_all();
 	printf("<center>%s -- 今日十大热门话题\n<hr>\n", BBSNAME);
 	fp=fopen("etc/posts/day", "r");
-	if(fp==0) http_fatal("can't read data");
+	if(fp==0)
+		http_fatal("can't read data");
 	fgets(s1, 255, fp);
 	fgets(s1, 255, fp);
 	printf("<table border=\"1\" width=\"610\">\n");
-	printf("<tr><td>名次<td>讨论区<td>标题<td>作者<td>人数\n");
+	printf("<tr><td>名次</td><td>讨论区</td><td>标题</td><td>作者</td><td>人数</td></tr>\n");
 	for(n=1; n<=10; n++)
-       	{
+   	{
 		if(fgets(s1, 255, fp) == NULL)
 		       	break;
 		sscanf(s1+41, "%s", brd);
@@ -24,9 +27,9 @@ int main() {
 		if(fgets(s1, 255, fp) == NULL)
 		       	break;
 		strsncpy(title, s1+27, 60);
-		printf("<tr><td>第 %d 名<td><a href=\"bbsdoc?board=%s\">%s</a><td><a href='bbstfind?board=%s&title=%s'>%42.42s</a><td><a href=bbsqry?userid=%s>%12s</a><td>%s\n",
+		printf("<tr><td>第 %d 名<td><a href=\"bbsdoc?board=%s\">%s</a></td><td><a href=\"bbstfind?board=%s&title=%s\">%42.42s</a></td><td><a href=\"bbsqry?userid=%s\">%12s</a></td><td>%s</td></tr>\n",
    n, brd, brd, brd, nohtml(title), nohtml(title), id, id, num);
 	}
-		printf("</table><center>");
-		http_quit();
+	printf("</table><center>");
+	http_quit();
 }
