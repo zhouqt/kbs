afac804234cc65563167cd24cdb3c29edd75da43
flyriver <flyriver@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2002-04-28 11:18:14 +0000

    Moved the BADLOGINFILE macro to it.
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@726 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/src/bbs.h
+++ kbs_bbs/src/bbs.h
@@ -135,6 +135,7 @@ extern long ti ;
 #define THREAD_DIR ".THREAD"
 #define DIGEST_DIR  ".DIGEST"          /* Name of Directory file info */
 #define NAMEFILE "BoardName"     /* File containing site name of bbs */
+#define BADLOGINFILE    "logins.bad"
 
 #define QUIT 0x666               /* Return value to abort recursive functions */
 #define COUNT 0x2				/* count record */
