6c49ce2a70280d781dc1abf0cf7aab301405a766
flyriver <flyriver@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2002-05-02 06:01:17 +0000

    Announce library.
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@774 de4889a5-8ad4-4bf3-bd87-57997c21d515

---
+++ kbs_bbs/libBBS/libann.c
@@ -0,0 +1,274 @@
+#include "bbs.h"
+
+void ann_add_item(MENU *pm, ITEM *it)
+{
+	ITEM        *newitem;
+
+	if ( pm->num < MAXITEMS )
+	{
+		newitem = pm->item[pm->num];
+		strncpy( newitem->title, it->title, sizeof(newitem->title)-1 );
+		if (it->host != NULL)
+		{
+			newitem->host = (char *)malloc(sizeof(char)*(strlen(it->host)+1));
+			strcpy(newitem->host, it->host);
+		}
+		else
+			newitem->host = it->host;
+		newitem->port = it->port;
+		strncpy(newitem->fname, it->fname, sizeof(newitem->fname) - 1);
+		(pm->num)++;
+	}
+}
+
+int ann_load_names(MENU *pm)
+{
+	FILE        *fn;
+	ITEM        litem;
+	char        buf[PATHLEN];
+	char        *ptr;
+	char        hostname[STRLEN];
+
+	pm->num = 0;
+	snprintf(buf, sizeof(buf), "%s/.Names", pm->path);
+	if ( (fn = fopen( buf, "r" )) == NULL )
+		return 0;
+	hostname[0]='\0';
+	while ( fgets( buf, sizeof(buf), fn ) != NULL )
+	{
+		if ( (ptr = strchr( buf, '\n' )) != NULL )
+			*ptr = '\0';
+		if ( strncmp( buf, "Name=", 5 ) == 0 )
+		{
+			strncpy( litem.title, buf + 5, sizeof(litem.title)-1 );
+			litem.title[sizeof(litem.title)-1] = '\0';
+		}
+		else if ( strncmp( buf, "Path=", 5 ) == 0 )
+		{
+			if ( strncmp( buf, "Path=~/", 7 ) == 0 )
+				strncpy( litem.fname, buf + 7, sizeof(litem.fname)-1 );
+			else
+				strncpy( litem.fname, buf + 5, sizeof(litem.fname)-1 );
+			litem.fname[sizeof(litem.fname)-1] = '\0';
+			if ((!strstr(litem.title,"(BM: BMS)")||HAS_PERM(currentuser,PERM_BOARDS))&&
+				(!strstr(litem.title,"(BM: SYSOPS)")||HAS_PERM(currentuser,PERM_SYSOP))&&
+				(!strstr(litem.title,"(BM: ZIXIAs)")||HAS_PERM(currentuser,PERM_SECANC)))
+			{
+				if (strstr(litem.fname,"!@#$%")) /*? host & port */
+				{
+					char *ptr1,*ptr2,gtmp[STRLEN];
+					strncpy(gtmp, litem.fname, STRLEN-1);
+					/* Warning: NOT thread safe */
+					ptr1 = strtok(gtmp,"!#$%@");
+					strcpy(hostname, ptr1);
+					ptr2 = strtok(NULL,"@");
+					strncpy(litem.fname, ptr2, sizeof(litem.fname)-1);
+					litem.fname[sizeof(litem.fname)-1] = '\0';
+					litem.port = atoi(strtok(NULL,"@"));
+				}
+				ann_add_item(pm, &litem);
+			}
+			hostname[0] = '\0';
+		}
+		else if ( strncmp( buf, "# Title=", 8 ) == 0 )
+		{
+			if ( pm->mtitle[0] == '\0' )
+			{
+				strncpy( pm->mtitle, buf + 8, sizeof(pm->mtitle)-1 );
+				pm->mtitle[sizeof(pm->mtitle)-1] = '\0';
+			}
+		}
+		else if ( strncmp( buf, "Host=", 5 ) == 0 )
+		{
+			strncpy( hostname, buf + 5, sizeof(hostname)-1 );
+			hostname[sizeof(hostname)-1] = '\0';
+		}
+		else if ( strncmp( buf, "Port=", 5 ) == 0 )
+		{
+			litem.port = atoi(buf + 5);
+		}
+	}
+	fclose( fn );
+	return 1;
+}
+
+ITEM *ann_alloc_items(size_t num)
+{
+	ITEM *it;
+
+	it = (ITEM *)calloc(sizeof(ITEM)*num);
+	return it;
+}
+
+void ann_free_items(ITEM *it, size_t num)
+{
+	size_t i;
+
+	if (it != NULL)
+	{
+		for (i = 0; i < num; i++)
+		{
+			if (it[i].host != NULL)
+			{
+				free(it[i].host);
+				it[i].host = NULL;
+			}
+		}
+		free(it);
+	}
+}
+
+void ann_set_items(MENU *pm, ITEM *it, size_t num)
+{
+	size_t i;
+
+	for (i = 0; i < num; i++)
+		pm->item[i] = it + i;
+}
+
+/*
+ * Get a board name from an announce path.
+ * For high performance, we should drop .Search file into shared memory.
+*/
+int ann_get_board(char *path, char *board, size_t len)
+{
+	FILE *fp;
+	char buf[256];
+	char *ptr;
+	char *ptr2;
+	
+	ptr = path;
+	if(ptr[0] == '\0')
+		return -1;
+	if (ptr[0] == '/')
+		ptr++;
+	fp = fopen("0Announce/.Search", "r");
+	if(fp == NULL)
+		return -1;
+	while(fgets(buf, sizeof(buf), fp) != NULL)
+	{
+		if ((ptr2 = strchr(buf, ':')) != NULL)
+		{
+			*ptr2 = '\0';
+			if (strncmp(ptr+2, ptr, strlen(ptr+2)) == 0)
+			{
+				strncpy(board, buf, len-1);
+				board[len-1] = '\0';
+				fclose(fp);
+				return 0;
+			}
+		}
+	}
+	fclose(fp);
+	return -1;
+}
+
+/*
+ * Get an announce path from a board name.
+ * For high performance, we should drop .Search file into shared memory.
+*/
+int ann_get_path(char *board, char* path, size_t len)
+{
+	FILE *fp;
+	char buf[256];
+	char *ptr;
+	
+	fp = fopen("0Announce/.Search", "r");
+	if(fp == NULL)
+		return -1;
+	while(fgets(buf, sizeof(buf), fp) != NULL)
+	{
+		if ((ptr = strchr(buf, ':')) != NULL)
+		{
+			*ptr = '\0';
+			if (strcmp(buf, board) == 0)
+			{
+				strncpy(path, ptr+2, len-1);
+				path[len-1] = '\0';
+				fclose(fp);
+				return 0;
+			}
+		}
+	}
+	fclose(fp);
+	return -1;
+}
+
+int ann_traverse_check(char *path)
+{
+	char *ptr;
+	int i = 0;
+	char filename[256];
+	char buf[256];
+	char pathbuf[256];
+	char title[STRLEN];
+	char currpath[256];
+	FILE *fp;
+
+	/* path parameter can not have leading '/' character */
+	if (path[0] == '/')
+		return -1;
+	bzero(pathbuf, sizeof(pathbuf));
+	ptr = path;
+	while(*ptr != '\0')
+	{
+		if(pathbuf[0] == '\0')
+			snprintf(filename, sizeof(filename), "0Announce/.Names");
+		else if(*ptr == '/')
+			snprintf(filename, sizeof(filename), "0Announce/%s/.Names", pathbuf);
+		else
+		{
+			if (i < sizeof(pathbuf))
+				pathbuf[i] = *ptr;
+			ptr++;
+			i++;
+			continue;
+		}
+		if((fp = fopen(filename, "r")) == NULL)
+			return -1;
+		while(fgets(buf, sizeof(buf), fp) != NULL)
+		{
+			if ( strncmp( buf, "Name=", 5 ) == 0 )
+			{
+				strncpy( title, buf + 5, sizeof(title)-1 );
+				title[sizeof(title)-1] = '\0';
+			}
+			else if ( strncmp( buf, "Path=", 5 ) == 0 )
+			{
+				if ( strncmp( buf, "Path=~/", 7 ) == 0 )
+					snprintf(currpath, sizeof(currpath), "%s/%s", 
+							 pathbuf, buf+7);
+				else
+					snprintf(currpath, sizeof(currpath), "%s/%s", 
+							 pathbuf, buf+5);
+				/*litem.fname[sizeof(litem.fname)-1] = '\0';*/
+				if(strncmp(currpath, path, strlen(currpath)) != 0)
+				{
+					/* directory or file is not existed */
+					fclose(fp);
+					return -1;
+				}
+				if ((!strstr(title,"(BM: BMS)")||HAS_PERM(currentuser,PERM_BOARDS))&&
+					(!strstr(title,"(BM: SYSOPS)")||HAS_PERM(currentuser,PERM_SYSOP))&&
+					(!strstr(title,"(BM: ZIXIAs)")||HAS_PERM(currentuser,PERM_SECANC)))
+				{
+					/* directory can be accessed */
+					break;
+				}
+				else
+				{
+					/* diretory cannot be accessed */
+					fclose(fp);
+					return -1;
+				}
+			}
+		}
+		fclose(fp);
+		if (i < sizeof(pathbuf))
+			pathbuf[i] = *ptr;
+		ptr++;
+		i++;
+	}
+	return 0;
+}
+
