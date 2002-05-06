97002815b127ec714a48dab6914b716cb4f67b6a
flyriver <flyriver@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2002-05-06 06:53:15 +0000

    Added a macro named MAIN_PAGE
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@798 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/bbs2www/include/bbslib.h
+++ kbs_bbs/bbs2www/include/bbslib.h
@@ -20,6 +20,7 @@
 #endif
 
 #define FIRST_PAGE	"/index.html"
+#define MAIN_PAGE   "/frames.html"
 #define CSS_FILE 	"/bbs.css"
 #define CHARSET		"gb2312"
 #define UCACHE_SHMKEY	get_shmkey("UCACHE_SHMKEY")
