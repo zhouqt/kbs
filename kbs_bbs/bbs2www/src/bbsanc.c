d039ea4eb0558c64b9c7041db4abf6c4bf29d393
flyriver <flyriver@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2002-05-06 06:49:12 +0000

    Improved by using libann.c functions.
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@796 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/bbs2www/src/bbsanc.c
+++ kbs_bbs/bbs2www/src/bbsanc.c
@@ -3,30 +3,58 @@
  */
 #include "bbslib.h"
 
-int main() {
+void ann_show_file(char *path)
+{
 	FILE *fp;
-	char *board, path[512], buf[512], ch, tmp[80], *ptr; 
-	init_all();
-	strsncpy(path, getparm("path"), 511);
-	board=getbfroma(path);
-	buf[0]=0;
-	if(board[0]) sprintf(buf, "%s版", board);
-	printf("<center>%s -- %s精华区文章阅读<hr>\n", BBSNAME, board);
-	if(strstr(path, ".Search") || strstr(path, ".Names")|| strstr(path, "..")|| strstr(path, "SYSHome"))
+	char board[STRLEN];
+	char pathbuf[256];
+	char buf[STRLEN];
+	int len;
+
+	if(strstr(path, ".Search") || strstr(path, ".Names")
+			|| strstr(path, "..")|| strstr(path, "SYSHome"))
 		http_fatal("错误的文件名");
-	sprintf(buf, "0Announce%s", path);
-	printf("<table border=1 width=610>");
-	printf("<tr><td><pre>");
-	fp=fopen(buf, "r");
-	if(fp==0) http_fatal("错误的文件名");
-	while(1) {
-		if(fgets(buf, 256, fp)==0) break;
-		hhprintf("%s", void1(buf));
+	if (path[0] != '\0')
+	{
+		len = strlen(path);
+		if (path[len-1] == '/')
+			path[len-1] = '\0';
+		if (path[0] == '/')
+			snprintf(pathbuf, sizeof(pathbuf), "0Announce%s", path);
+		else
+			snprintf(pathbuf, sizeof(pathbuf), "0Announce/%s", path);
+		if (ann_traverse_check(pathbuf) < 0)
+			http_fatal("此目录不存在");
 	}
+	else
+		http_fatal("错误的文件名");
+	board[0] = '\0';
+	ann_get_board(path, board, sizeof(board));
+	if(board[0])
+		snprintf(buf, sizeof(buf), "%s版", board);
+	printf("<center>%s -- %s精华区文章阅读<hr class=\"default\">\n",
+			BBSNAME, board);
+	printf("<table class=\"default\" border=\"1\" width=\"610\">");
+	printf("<tr><td><pre>");
+	fp = fopen(pathbuf, "r");
+	if (fp == NULL)
+		http_fatal("错误的文件名");
+	while(fgets(buf, sizeof(buf), fp) != NULL)
+		hhprintf("%s", void1(buf));
 	fclose(fp);
    	printf("</pre></table><hr>\n");
-	printf("[<a href='javascript:history.go(-1)'>返回上一页</a>] ");
-   	if(board[0]) printf("[<a href=bbsdoc?board=%s>本讨论区</a>]", board);
+	printf("[<a href=\"javascript:history.go(-1)\">返回上一页</a>] ");
+   	if(board[0])
+		printf("[<a href=\"bbsdoc?board=%s\">本讨论区</a>]", board);
    	printf("</center>\n"); 
+}
+
+int main()
+{
+	char path[512]; 
+
+	init_all();
+	strsncpy(path, getparm("path"), 511);
+	ann_show_file(path);
 	http_quit();
 }
