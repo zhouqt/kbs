9a8d9d0c459486ef7fca600903fd1ffd91d0289f
czz <czz@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2002-05-07 07:18:24 +0000

    修复上站人次统计
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@803 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/local_utl/static.c
+++ kbs_bbs/local_utl/static.c
@@ -10,7 +10,6 @@ struct
   int sum[24];                  /* ×ÜºÏ */
 }      st;
 
-
 main(argc, argv)
   char *argv[];
 {
@@ -88,13 +87,20 @@ main(argc, argv)
     {
       if ((item * (i) > st.no[j]) && (item * (i-1) <= st.no[j]) && st.no[j])
       {
-        if(st.no[j]>=2000)
+		  /* Ã¿Ò»Ç§»»Ò»ÖÖÑÕÉ« Czz 020507 */
+        if(st.no[j]>=5000)
                 /*fprintf(fp, "[35m###[33m"); Leeward 97.12.08 */
+                fprintf(fp, "\033[1m[36m%-3d\033[m[33m", (st.no[j]) % 1000);
+        else if (st.no[j] >= 4000)
+                fprintf(fp, "\033[1m[35m%-3d\033[m[33m", (st.no[j]) % 1000);
+        else if (st.no[j] >= 3000)
+                fprintf(fp, "\033[1m[34m%-3d\033[m[33m", (st.no[j]) % 1000);
+        else if (st.no[j] >= 2000)
                 fprintf(fp, "\033[1m[33m%-3d\033[m[33m", (st.no[j]) % 1000);
-        else if (st.no[j] >= 1000) /* Leeward 98.02.27 */
-                fprintf(fp, "\033[1m[37m%-3d\033[m[33m", (st.no[j]) % 1000);
+        else if (st.no[j] >= 1000)
+                fprintf(fp, "\033[1m[32m%-3d\033[m[33m", (st.no[j]) % 1000);
         else
-                fprintf(fp, "[35m%-3d[33m", (st.no[j]));
+                fprintf(fp, "[31m%-3d[33m", (st.no[j]));
         continue;
       }
       if(st.no[j]-item*i<item && item*i<st.no[j])
@@ -110,7 +116,7 @@ main(argc, argv)
     " ©¸¡ª¡ª¡ª[37m   "NAME_BBS_CHINESE NAME_BBS_NICK"  ÉÏÕ¾ÈË´Î±í   [36m¡ª¡ª¡ª[37m%s[36m¡ª¡ª©¼\n"
     /*"    [34m  0  1  2  3  4  5  6  7  8  9  10 11 [31m12 13 14 15 16 17 18 19 20 21 22 23 \n\n"*/ /* Leeward 98.02.27 */
     "    [34m  1  2  3  4  5  6  7  8  9  10 11 12 [31m13 14 15 16 17 18 19 20 21 22 23 24\n\n"
-    "               [36m 1 [33m¡õ[36m = [37m%-5d [36m×Ü¹²ÉÏÕ¾ÈË´Î£º[37m%-9d[36mÆ½¾ùÊ¹ÓÃÊ±¼ä£º[37m%d[m \n\n", Ctime(&now),item,total, totaltime / total + 1); /* Leeward 98.09.24 add the 2nd \n for SHARE MEM in ../main.c */
+    "               [36m 1 [33m¨~[36m = [37m%-5d [36m×Ü¹²ÉÏÕ¾ÈË´Î£º[37m%-9d[36mÆ½¾ùÊ¹ÓÃÊ±¼ä£º[37m%d[m \n\n", Ctime(now),item,total, totaltime / total + 1); /* Leeward 98.09.24 add the 2nd \n for SHARE MEM in ../main.c */
 
   fclose(fp);
 }
