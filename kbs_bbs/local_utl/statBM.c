842ca4443183b15edecd69d10383795657a56452
bad <bad@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2002-08-16 17:07:19 +0000

    updated statBM tool
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@1338 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/local_utl/statBM.c
+++ kbs_bbs/local_utl/statBM.c
@@ -1,12 +1,61 @@
 #include "bbs.h"
 
 char *curuserid;
+int type=-1, flag=0, timed=0;
 
 #define DATALEN 100
 
+char suffix[4][10]={"", "week", "month", "year"};
+
+struct libtruct {
+	char boardname[STRLEN];
+	char id[STRLEN];
+	int data[DATALEN];
+} *lib;
+int total=0;
+
+int order[10000];
+
+int show(int i)
+{
+    if(lib[i].data[0]>=3600)
+    	printf("版面名称: %-15s  版主: %-15s  停留时间: %d 小时 %d 分钟 %d 秒\n", lib[i].boardname, lib[i].id, lib[i].data[0]/3600, lib[i].data[0]/60%60, lib[i].data[0]%60);
+    else if(lib[i].data[0]>=60)
+    	printf("版面名称: %-15s  版主: %-15s  停留时间: %d 分钟 %d 秒\n", lib[i].boardname, lib[i].id, lib[i].data[0]/60, lib[i].data[0]%60);
+    else
+    	printf("版面名称: %-15s  版主: %-15s  停留时间: %d 秒\n", lib[i].boardname, lib[i].id, lib[i].data[0]);
+    printf("   进版次数: %-4d    版内发文: %-4d    收入文摘: %-4d    去掉文摘: %-4d\n", lib[i].data[1], lib[i].data[2], lib[i].data[3], lib[i].data[4]);
+    printf("   区段次数: %-4d    标记文章: %-4d    去掉标记: %-4d    删除文章: %-4d\n", lib[i].data[5], lib[i].data[6], lib[i].data[7], lib[i].data[8]);
+    printf("   恢复删除: %-4d    封禁人数: %-4d    解封人数: %-4d    收入精华: %-4d\n", lib[i].data[9], lib[i].data[10], lib[i].data[11], lib[i].data[12]);
+    printf("   整理精华: %-4d    相同主题: %-4d\n", lib[i].data[13], lib[i].data[14]);
+    printf("\n");
+    return 0;
+}
+
+void sort()
+{
+	int i,j,k;
+	for(i=0;i<total;i++)
+		order[i]=i;
+	for(i=0;i<total;i++)
+		for(j=i+1;j<total;j++)
+		if(lib[order[i]].data[0]<lib[order[j]].data[0]){
+			k=order[i];
+			order[i]=order[j];
+			order[j]=k;
+		}
+}
+
+void showall()
+{
+	int i;
+	for(i=0;i<total;i++)
+		show(order[i]);
+}
+
 int check_BM(struct boardheader *bptr)
 {
-    int fd, data[DATALEN];
+    int i, fd, data[DATALEN];
     struct flock ldata;
     struct stat buf;
     char direct[PATHLEN];
@@ -14,27 +63,54 @@ int check_BM(struct boardheader *bptr)
     if ((bptr->level != 0) && !(bptr->level & PERM_POSTMASK))
         return 0;
     if (!chk_BM_instr(bptr->BM, curuserid)) return 0;
-    sprintf(direct, "boards/%s/.bm.%s", bptr->filename, curuserid);
-    	if ((fd = open(direct, O_RDWR | O_CREAT, 0644)) == -1) return 0;
-       ldata.l_type = F_RDLCK;
-       ldata.l_whence = 0;
-       ldata.l_len = 0;
-       ldata.l_start = 0;
-       if (fcntl(fd, F_SETLKW, &ldata) == -1) {
-            close(fd);
-            return 0;
-       }
-       fstat(fd, &buf);
-       if(buf.st_size<DATALEN*sizeof(int)){
+    sprintf(direct, "boards/%s/.bm.%s%s", bptr->filename, curuserid, suffix[timed]);
+    if ((fd = open(direct, O_RDWR | O_CREAT, 0644)) == -1) return 0;
+    ldata.l_type = F_RDLCK;
+    ldata.l_whence = 0;
+    ldata.l_len = 0;
+    ldata.l_start = 0;
+    if (fcntl(fd, F_SETLKW, &ldata) == -1) {
+        close(fd);
+        return 0;
+    }
+    fstat(fd, &buf);
+    if(buf.st_size<DATALEN*sizeof(int)){
        	memset(data, 0, sizeof(int)*DATALEN);
-       }
-       else
+    }
+    else
        	read(fd, data, sizeof(int)*DATALEN);
+    if (flag==1){
+    	for(i=0;i<DATALEN;i++)
+    		data[i]+=lib[total].data[i];
+    	total++;
+        lseek(fd, 0, SEEK_SET);
+        write(fd, data, sizeof(int)*DATALEN);
+    }
+    else if (flag==3) {
+    	for(i=0;i<DATALEN;i++)
+    		data[i]=lib[total].data[i];
+    	total++;
+        lseek(fd, 0, SEEK_SET);
+        write(fd, data, sizeof(int)*DATALEN);
+    }
    	ldata.l_type = F_UNLCK;
    	fcntl(fd, F_SETLKW, &ldata);
    	close(fd);
+   	
+   	if (flag==-1) {
+   		unlink(direct);
+   	}
+   	else if (flag==0)
+   		total++;
+   	else if (flag==1){
+   	}
+   	else if (flag==2){
+   		strcpy(lib[total].boardname, bptr->filename);
+   		strcpy(lib[total].id, curuserid);
+   		memcpy(lib[total].data, data, sizeof(int)*DATALEN);
+   		total++;
+   	}
     
-    printf("版面名称: %s        版主: %s        停留时间: %d 秒", bptr->filename, curuserid, data[0]);
     return 0;
 }
 
@@ -42,16 +118,92 @@ int query_BM(struct userec *user, char *arg)
 {
     if (!(user->userlevel & PERM_BOARDS))
         return 0;
-    printf("%s: ", user->userid);
     curuserid = user->userid;
     apply_boards(check_BM);
-    printf("\n");
 }
 
-main()
+main(int argc, char ** argv)
 {
+	time_t t;
+	struct tm res;
+	if (argc<=1) {
+		printf("usage: statBM day|week|month|year|update|clear\n");
+		return;
+	}
+	if (!strcasecmp(argv[1],"clear")) type=0;
+	if (!strcasecmp(argv[1],"day")) type=1;
+	if (!strcasecmp(argv[1],"week")) type=2;
+	if (!strcasecmp(argv[1],"month")) type=3;
+	if (!strcasecmp(argv[1],"year")) type=4;
+	if (!strcasecmp(argv[1],"update")) type=5;
+	if (type==-1) {
+		printf("usage: statBM day|week|month|year|update|clear\n");
+		return;
+	}
     chdir(BBSHOME);
     resolve_ucache();
     resolve_boards();
-    apply_users(query_BM, NULL);
+    switch(type) {
+    	case 0:
+    		timed=0;
+    		flag=-1;
+		    apply_users(query_BM, NULL);
+    		timed=1;
+    		flag=-1;
+		    apply_users(query_BM, NULL);
+    		timed=2;
+    		flag=-1;
+		    apply_users(query_BM, NULL);
+    		timed=3;
+    		flag=-1;
+		    apply_users(query_BM, NULL);
+    		break;
+    	case 1:
+    	case 2:
+    	case 3:
+    	case 4:
+    		timed=type-1;
+    		flag=0;
+		    apply_users(query_BM, NULL);
+		    lib = malloc(sizeof(struct libtruct)*total);
+		    total=0;
+    		flag=2;
+		    apply_users(query_BM, NULL);
+    		sort();
+    		showall();
+    		break;
+    	case 5:
+    		t=time(0);
+    		gmtime_r(&t,&res);
+    		flag=0;
+		    apply_users(query_BM, NULL);
+		    lib = malloc(sizeof(struct libtruct)*total);
+		    total=0;
+    		flag=2;
+		    apply_users(query_BM, NULL);
+		    
+		    timed=1;
+		    total=0;
+		    if (!res.tm_wday) flag=3;
+		    else flag=1;
+		    apply_users(query_BM, NULL);
+		    
+		    timed=2;
+		    total=0;
+		    if (!res.tm_mday) flag=3;
+		    else flag=1;
+		    apply_users(query_BM, NULL);
+		    
+		    timed=3;
+		    total=0;
+		    if (!res.tm_yday) flag=3;
+		    else flag=1;
+		    apply_users(query_BM, NULL);
+		    
+		    timed=0;
+		    total=0;
+		    flag=-1;
+		    apply_users(query_BM, NULL);
+		    break;
+    }
 }
