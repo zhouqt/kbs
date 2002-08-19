47e531745c61c64750d232fab141cf54ee183566
kcn <kcn@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2002-08-19 08:16:01 +0000

    add a local_utility for generate title
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@1373 de4889a5-8ad4-4bf3-bd87-57997c21d515

---
+++ kbs_bbs/local_utl/gen_title.c
@@ -0,0 +1,140 @@
+#include "bbs.h"
+
+static int simple_digest(char *str, int maxlen)
+{
+    char x[sizeof(int)];
+    char *p;
+    bzero(x, sizeof(int));
+    for (p = str; *p && ((p - str) < maxlen); p++)
+        x[(p - str) % sizeof(int)] += *p;
+    return (int) x;
+}
+
+int generate_board_title(struct boardheader *bh)
+{
+    int fd2, size = sizeof(fileheader), total, i, count = 0;
+    struct boardheader btmp;
+    char olddirect[PATHLEN];
+    char *ptr, *t, *t2;
+    struct fileheader * ptr1;
+    struct search_temp {
+        bool has_pre;
+        int digest;
+        int thread_id;
+        int id;
+        int next;
+    } *index;
+    struct stat buf;
+    int gen_threadid;
+
+    setbdir(0, olddirect, board,bh->filename);
+    
+	gen_threadid = 0;
+    if ((fd2 = open(olddirect, O_RDWR, 0664)) == -1) {
+        report("recopen err");
+        continue;
+    }
+
+    BBS_TRY {
+        if (safe_mmapfile_handle(fd2, O_RDWR, PROT_READ | PROT_WRITE, MAP_SHARED, (void **) &ptr, &buf.st_size) == 0) {
+            close(fd2);
+            continue;
+        }
+        total = buf.st_size / size;
+        index = (int *) malloc(sizeof(*index) * total);
+        ptr1 = (struct fileheader *) ptr;
+        for (i = 0; i < total; i++, ptr1++) {
+            int j;
+
+            t2 = ptr1->title;
+            if (!strncmp(t2, "Re:", 3)) {
+                index[i].has_pre = true;
+                t2 += 4;
+            } else
+                index[i].has_pre = false;
+            index[i].thread_id = ptr1->thread_id;
+            index[i].digest = simple_digest(t2, STRLEN);
+            for (j = i - 1; j >= 0; j--) {
+                struct fileheader *tmppost;
+
+                if (index[j].digest != index[i].digest)
+                    continue;
+                tmppost = ((struct fileheader *) (ptr + j * size));
+                t = tmppost->title;
+                if (index[j].has_pre)
+                    t += 4;
+                if (!strcmp(t, t2)) {
+                    index[j].next = i;
+                    index[i].thread_id = index[j].thread_id;
+                    index[i].next = 0;
+                    break;
+                }
+            }
+            if (index[i].thread_id == 0) {
+                index[i].thread_id = getnextid(board);
+                index[i].id = gen_threadid;
+                index[i].next = 0;
+                gen_threadid++;
+            } else {
+                index[i].id = gen_threadid;
+                gen_threadid++;
+            }
+        }
+        ptr1 = (struct fileheader *) ptr;
+        for (i = 0; i < total; i++, ptr1++) {
+            ptr1->id = index[i].id;
+            ptr1->groupid = index[i].thread_id;
+            ptr1->reid = index[i].thread_id;
+        }
+    }
+    BBS_CATCH {
+    }
+    BBS_END free(index);
+    end_mmapfile((void *) ptr, buf.st_size, -1);
+    close(fd2);
+    memcpy(&btmp,getbcache(bh->filename),sizeof(btmp));
+    btmp.nowid = gen_threadid + 1;
+    set_board(getbnum(bh->filename, &btmp));
+}
+
+int generate_all_title()
+{
+	apply_boards(generate_board_title);
+    return 0;
+}
+
+int main(int argc,char** argv)
+{
+	int allflag;
+	struct boardheader bh;
+    while (1) {
+    	int c;
+    	c = getopt(argc, argv, "a");
+    	if (c == -1)
+            break;
+        switch (c) {
+        case 'a':
+        	allflag = 0;
+        	break;
+        case 'h':
+        	printf("%s [-a|boardname]\n  generatate board thread index",argv[0]);
+        	return 0;
+        case '?':
+        	printf("%s:Unknown argument.\nTry `%s -h' for more information.\n", argv[0], argv[0]);
+        	return 0;
+        }
+    }
+    if (optind < argc) {
+    	struct stat st;
+    	name = argv[optind++];
+    	if (optind < argc) {
+            printf("%s:Too many arguments.\nTry `%s -h' for more information.\n", argv[0], argv[0]);
+            return 0;
+        }
+    	strncpy(bh.filename,name,STRLEN);
+    	generate_board_title(&bh);
+    }
+    if (allflag) {
+    	generate_all_title();
+    }
+}
--- kbs_bbs/src/bbs.c
+++ kbs_bbs/src/bbs.c
@@ -1013,16 +1013,6 @@ int generate_mark()
     return 0;
 }
 
-static int simple_digest(char *str, int maxlen)
-{
-    char x[sizeof(int)];
-    char *p;
-    bzero(x, sizeof(int));
-    for (p = str; *p && ((p - str) < maxlen); p++)
-        x[(p - str) % sizeof(int)] += *p;
-    return (int) x;
-}
-
 int generate_title()
 {
     struct fileheader mkpost, *ptr1, *ptr2;
@@ -1185,103 +1175,6 @@ int generate_title()
     ldata.l_type = F_UNLCK;
     fcntl(fd, F_SETLKW, &ldata);        // ÍË³ö»¥³âÇøÓò
     close(fd);
-    return 0;
-}
-
-int generate_all_title()
-{
-    struct fileheader mkpost, *ptr1;
-    int fd2, size = sizeof(fileheader), total, i, count = 0;
-    int boardt;
-    struct boardheader btmp;
-    char olddirect[PATHLEN];
-    char *ptr, *t, *t2;
-    struct search_temp {
-        bool has_pre;
-        int digest;
-        int thread_id;
-        int id;
-        int next;
-    } *index;
-    struct stat buf;
-    int gen_threadid;
-
-    if (!HAS_PERM(currentuser, PERM_SYSOP))
-        return 0;
-
-
-    for (boardt = 1; boardt <= get_boardcount(); boardt++) {
-        memcpy(&btmp, &bcache[boardt - 1], sizeof(struct boardheader));
-        strcpy(currboard, btmp.filename);
-        setbdir(digestmode, currdirect, currboard);
-
-        gen_threadid = 1;
-
-        if ((fd2 = open(currdirect, O_RDWR, 0664)) == -1) {
-            report("recopen err");
-            continue;
-        }
-
-        BBS_TRY {
-            if (safe_mmapfile_handle(fd2, O_RDWR, PROT_READ | PROT_WRITE, MAP_SHARED, (void **) &ptr, &buf.st_size) == 0) {
-                close(fd2);
-                continue;
-            }
-            total = buf.st_size / size;
-            index = (int *) malloc(sizeof(*index) * total);
-            ptr1 = (struct fileheader *) ptr;
-            for (i = 0; i < total; i++, ptr1++) {
-                int j;
-
-                t2 = ptr1->title;
-                if (!strncmp(t2, "Re:", 3)) {
-                    index[i].has_pre = true;
-                    t2 += 4;
-                } else
-                    index[i].has_pre = false;
-                index[i].thread_id = 0;
-                index[i].digest = simple_digest(t2, STRLEN);
-                for (j = i - 1; j >= 0; j--) {
-                    struct fileheader *tmppost;
-
-                    if (index[j].digest != index[i].digest)
-                        continue;
-                    tmppost = ((struct fileheader *) (ptr + j * size));
-                    t = tmppost->title;
-                    if (index[j].has_pre)
-                        t += 4;
-                    if (!strcmp(t, t2)) {
-                        index[j].next = i;
-                        index[i].thread_id = index[j].thread_id;
-                        index[i].next = 0;
-                        break;
-                    }
-                }
-                if (index[i].thread_id == 0) {
-                    index[i].thread_id = gen_threadid;
-                    index[i].id = gen_threadid;
-                    index[i].next = 0;
-                    gen_threadid++;
-                } else {
-                    index[i].id = gen_threadid;
-                    gen_threadid++;
-                }
-            }
-            ptr1 = (struct fileheader *) ptr;
-            for (i = 0; i < total; i++, ptr1++) {
-                ptr1->id = index[i].id;
-                ptr1->groupid = index[i].thread_id;
-                ptr1->reid = index[i].thread_id;
-            }
-        }
-        BBS_CATCH {
-        }
-        BBS_END free(index);
-        end_mmapfile((void *) ptr, buf.st_size, -1);
-        close(fd2);
-        btmp.nowid = gen_threadid + 1;
-        set_board(boardt, &btmp);
-    }
     return 0;
 }
 
