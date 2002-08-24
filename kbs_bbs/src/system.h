308d348d0a2171b4eba1f0d48b53f83e70f0be1d
kcn <kcn@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2002-08-24 07:29:07 +0000

    fix bug
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@1434 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/src/system.h
+++ kbs_bbs/src/system.h
@@ -34,24 +34,24 @@ int lock_reg(int fd,int cmd,int type,off_t offset,int whence,off_t len);
 pid_t lock_test(int fd,int cmd,int type,off_t offset,int whence,off_t len);
 
 /* some marco from APUE*/
-#define read_lock(fd,offset,whence.len)\
+#define read_lock(fd,offset,whence,len)\
 	lock_reg(fd,F_SETLK,F_RDLCK,offset,whence,len)
 
-#define readw_lock(fd,offset,whence.len)\
+#define readw_lock(fd,offset,whence,len)\
 	lock_reg(fd,F_SETLKW,F_RDLCK,offset,whence,len)
 
-#define write_lock(fd,offset,whence.len)\
+#define write_lock(fd,offset,whence,len)\
 	lock_reg(fd,F_SETLK,F_WRLCK,offset,whence,len)
 
-#define writew_lock(fd,offset,whence.len)\
+#define writew_lock(fd,offset,whence,len)\
 	lock_reg(fd,F_SETLKW,F_WRLCK,offset,whence,len)
 
-#define un_lock(fd,offset,whence.len)\
+#define un_lock(fd,offset,whence,len)\
 	lock_reg(fd,F_SETLKW,F_UNLCK,offset,whence,len)
 
-#define is_read_lockable(fd,offset,whence.len)\
+#define is_read_lockable(fd,offset,whence,len)\
 	!lock_test(fd,F_GETLK,F_RDLCK,offset,whence,len)
 
-#define is_write_lockable(fd,offset,whence.len)\
+#define is_write_lockable(fd,offset,whence,len)\
 	!lock_test(fd,F_GETLK,F_WRLCK,offset,whence,len)
 
