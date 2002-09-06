7c54b3149c49af9c2e6feae44208615c2aabef40
flyriver <flyriver@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2002-09-06 03:04:09 +0000

    Bug fixed.
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@1564 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/src/bm.c
+++ kbs_bbs/src/bm.c
@@ -239,12 +239,8 @@ int addtodeny(char *uident)
             my_flag = 0;
             fprintf(fn, "寄信人: SYSOP (System Operator) \n");
             fprintf(fn, "标  题: %s\n", buffer);
-            fprintf(fn, "发信站: %s (%24.24s)\n", "BBS " NAME_BBS_CHINESE "站", ctime(&now));
-#ifdef NINE_BUILD
-            fprintf(fn, "来  源: smth.org\n");
-#else
-            fprintf(fn, "来  源: bbs.net9.org\n");
-#endif
+            fprintf(fn, "发信站: %s (%24.24s)\n", BBS_FULL_NAME, ctime(&now));
+            fprintf(fn, "来  源: %s\n", NAME_BBS_ENGLISH);
             fprintf(fn, "\n");
             fprintf(fn, "由于您在 \x1b[4m%s\x1b[m 版 \x1b[4m%s\x1b[m，我很遗憾地通知您， \n", currboard, denymsg);
             if (denyday)
