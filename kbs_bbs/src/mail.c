10bd69cf33882cdac08be53fa5c46c02f1d0e03f
bad <bad@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2002-09-06 04:56:44 +0000

    oh oh
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@1565 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/src/mail.c
+++ kbs_bbs/src/mail.c
@@ -1239,7 +1239,6 @@ int mail_move(int ent, struct fileheader* fileinfo, char* direct)
     char menu_char[3][10]={"I) 收件箱", "J) 垃圾箱", "Q) 退出"};
 
     load_mail_list();
-    if (!mail_list_t) return DONOTHING;
     clear();
     move(5, 3);
     prints("请选择移动到哪个邮箱");
