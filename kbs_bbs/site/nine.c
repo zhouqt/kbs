f770f59f5b2dc203970cadedab6ac4c35f83b8cc
bad <bad@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2002-09-06 12:09:47 +0000

    ft
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@1571 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/site/nine.c
+++ kbs_bbs/site/nine.c
@@ -192,6 +192,7 @@ int     mode;
     case LOOKMSGS:  return "察看讯息";
     case WFRIEND:   return "寻人名册";
     case LOCKSCREEN:return "屏幕锁定";
+    case GIVEUPNET: return "戒网中..";
     default: return "去了那里!?" ;
     }
 }
