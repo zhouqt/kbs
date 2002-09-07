0958730d755b2765313500ec7767297b2d9b2149
bad <bad@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2002-09-07 06:18:18 +0000

    ohoh
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@1579 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/service/friendtest.c
+++ kbs_bbs/service/friendtest.c
@@ -14,8 +14,8 @@
 
 struct usertype {
     char userid[IDLEN+2];
-    unsigned int tried;
-    unsigned int got;
+    int tried;
+    int got;
     time_t create;
 } * users;
 int userst=0;
@@ -26,6 +26,10 @@ struct questype {
     int value[5];
 } * quests;
 int questst=0;
+
+typedef char statinfo[20];
+statinfo* stats;
+int statst=0;
 
 struct userec* lookuser;
 
@@ -115,7 +119,7 @@ void load_quests(char* filename)
         ldata.l_start=0;
         if(fcntl(fd, F_SETLKW, &ldata)!=-1){
             read(fd, &questst, sizeof(questst));
-            quests=(struct questype*)malloc(sizeof(struct questype)*questst);
+            quests=(struct questype*)malloc(sizeof(struct questype)*20);
             read(fd, quests, sizeof(struct questype)*questst);
             	
             ldata.l_type = F_UNLCK;
@@ -145,7 +149,7 @@ void save_quests(char* filename)
     }
 }
 
-void save_stats(char* filename, char a[20])
+void save_stats(char* filename, statinfo a)
 {
     int fd;
     struct flock ldata;
@@ -157,6 +161,29 @@ void save_stats(char* filename, char a[20])
         if(fcntl(fd, F_SETLKW, &ldata)!=-1){
             lseek(fd, 0, SEEK_END);
             write(fd, a, 20);
+            	
+            ldata.l_type = F_UNLCK;
+            fcntl(fd, F_SETLKW, &ldata);
+        }
+        close(fd);
+    }
+}
+
+void load_stats(char* filename)
+{
+    int fd;
+    struct flock ldata;
+    struct stat st;
+    if((fd = open(filename, O_RDONLY, 0644))!=-1) {
+        ldata.l_type=F_RDLCK;
+        ldata.l_whence=0;
+        ldata.l_len=0;
+        ldata.l_start=0;
+        if(fcntl(fd, F_SETLKW, &ldata)!=-1){
+            fstat(fd, &st);
+            statst = st.st_size/20;
+            stats = malloc(statst*20);
+            read(fd, stats, statst*20);
             	
             ldata.l_type = F_UNLCK;
             fcntl(fd, F_SETLKW, &ldata);
@@ -207,36 +234,68 @@ int usermenu()
     prints("作者: ");
     setfcolor(RED,0);
     prints("bad@smth.org");
-    move(5,28);
+    move(5,13);
     setfcolor(WHITE,1);
-    prints("===用户测试次数排行===");
-    move(6,30);
+    prints("===人气最旺指数排行===");
+    move(6,15);
     setfcolor(RED,1);
     prints("╭───────╮");
     for(i=0;i<10;i++){
-        move(7+i, 30);
+        move(7+i, 15);
         setfcolor(RED,1);
         prints("│");
         if (i>=userst) {
-            move(7+i, 35+7);
+            move(7+i, 20+7);
             setfcolor(GREEN,1);
             prints("--空--");
         }
         else {
-            move(7+i, 38-strlen(users[i].userid)/2+7);
+            move(7+i, 23-strlen(users[i].userid)/2+7);
             setfcolor(GREEN,1);
             prints(users[i].userid);
         }
-        move(7+i, 46+14);
+        move(7+i, 31+14);
         setfcolor(RED,1);
         prints("│");
     }
-    move(17,30);
+    move(17,15);
+    setfcolor(RED,1);
+    prints("╰───────╯");
+
+    load_users(FRIENDTOP);
+    move(5,53);
+    setfcolor(WHITE,1);
+    prints("===最新加入用户列表===");
+    move(6,55);
+    setfcolor(RED,1);
+    prints("╭───────╮");
+    for(i=0;i<10;i++){
+        move(7+i, 55+7+7);
+        setfcolor(RED,1);
+        prints("│");
+        if (userst-i-1<0) {
+            move(7+i, 60+7+7+7);
+            setfcolor(GREEN,1);
+            prints("--空--");
+        }
+        else {
+            move(7+i, 63-strlen(users[userst-i-1].userid)/2+7+7+7);
+            setfcolor(GREEN,1);
+            prints(users[userst-i-1].userid);
+        }
+        move(7+i, 71+14+7+7);
+        setfcolor(RED,1);
+        prints("│");
+    }
+    move(17,55);
     setfcolor(RED,1);
     prints("╰───────╯");
     setfcolor(WHITE,0);
+
     move(19,22);
-    prints("要创建或更改自己的题,请输入a<回车>");
+    prints("创建或更改题["); setfcolor(GREEN,1); prints("a"); setfcolor(WHITE,0); prints("] ");
+    prints("统计结果["); setfcolor(GREEN,1); prints("s"); setfcolor(WHITE,0); prints("] ");
+    prints("管理题["); setfcolor(GREEN,1); prints("m"); setfcolor(WHITE,0); prints("] ");
     move(20,22);
     prints("要做别人的题,请输入他的id");
 //    setfcolor(WHITE,1);
@@ -245,6 +304,10 @@ int usermenu()
         getdata(22,26,"请选择:",buf,IDLEN+1,1,NULL,true);
         if(toupper(buf[0])=='A'&&!buf[1])
               return 1;
+        if(toupper(buf[0])=='S'&&!buf[1])
+              return 3;
+        if(toupper(buf[0])=='M'&&!buf[1])
+              return 4;
         else if(buf[0]){
             tuid = getuser(buf, &lookuser);
             if(tuid) {
@@ -379,7 +442,7 @@ again:
     save_users(FRIENDTOP);
     sethometop(direct, currentuser->userid);
     unlink(direct);
-    sethomestat(direct, lookuser->userid);
+    sethomestat(direct, currentuser->userid);
     unlink(direct);
     move(3,0);
     prints("恭喜你，你已经创建了账号，赶快邀请你的好友来参加吧");
@@ -403,7 +466,8 @@ void do_test()
     char direct[PATHLEN];
     struct stat st;
     int i, j, k, l;
-    char ans[3], buf[122], buf2[10], answer[20];
+    char ans[3], buf[122], buf2[10];
+    statinfo answer;
     time_t span;
 
     do_test_reset();
@@ -419,6 +483,46 @@ void do_test()
     sethometop(direct, lookuser->userid);
     load_users(direct);
     sort_users();
+    move(2,0);
+    prints("欢迎进入 %s 的友谊测试\n", lookuser->userid);
+    prints("本测试最高可能得分为%d\n", possible_high());
+    if(userst) {
+        prints("已经有 %d 人参加过测试\n", userst);
+        prints("其中最高分 %d ,  最低分 %d\n", users[0].tried, users[userst-1].tried);
+        prints("\n=======TOP 10========\n");
+        for(i=0;i<10;i++)
+        if(i<userst)
+            prints(" %-12s %-3d\n", users[i].userid, users[i].tried);
+        getdata(22,0,"按d显示详细结果: ", ans, 2, true, NULL, true);
+        if (toupper(ans[0])=='D') {
+            do_test_reset();
+            move(1,0);
+            prints("详细测试结果\n");
+            move(3,0);
+            prints(" 账号         分数      测试次数");
+            j=4;
+            for(i=0;i<userst;i++) {
+                move(j,0);
+                prints(" %-12s %-3d        %-3d", users[i].userid, users[i].tried, users[i].got);
+                if(j>=20&&i<userst-1) {
+                    pressreturn();
+                    do_test_reset();
+                    move(1,0);
+                    prints("详细测试结果\n");
+                    move(3,0);
+                    prints(" 账号         分数      测试次数");
+                    j=3;
+                }
+                j++;
+            }
+            pressreturn();
+            do_test_reset();
+        }
+    }
+    else {
+        pressreturn();
+        do_test_reset();
+    }
     for(i=0;i<userst;i++)
     if(!strcmp(users[i].userid, currentuser->userid)){
         span = time(0)-users[i].create;
@@ -430,21 +534,10 @@ void do_test()
             return;
         }
     }
-    move(2,0);
-    prints("欢迎进入 %s 的友谊测试\n", lookuser->userid);
-    prints("本测试最高可能得分为%d\n", possible_high());
-    if(userst) {
-        prints("已经有 %d 人参加过测试\n", userst);
-        prints("其中最高分 %d ,  最低分 %d\n", users[0].tried, users[userst-1].tried);
-        prints("\n=======TOP 10========\n");
-        for(i=0;i<10;i++)
-        if(i<userst)
-            prints(" %-12s %-3d\n", users[i].userid, users[i].tried);
-    }
-    pressreturn();
     
     k=0;
     for(i=0;i<questst;i++){
+doitagain:
         do_test_reset();
         move(1,0);
         prints("第%d题(共%d题)\n\n", i+1, questst);
@@ -459,6 +552,12 @@ void do_test()
             clrtoeol();
             getdata(5+j, 0, buf, ans, 2, true, NULL, true);
             l=atoi(ans);
+            if(!l) {
+                do_test_reset();
+                getdata(1,0,"是否退出测试?[y/N]", ans, 2, true, NULL, true);
+                if(toupper(ans[0])=='Y') return;
+                goto doitagain;
+            }
         }while(l<1||l>j);
         answer[i]=l;
         k+=quests[i].value[l-1];
@@ -505,6 +604,278 @@ void do_test()
     pressreturn();
 }
 
+void show_stat_reset()
+{
+    resetcolor();
+    clear();
+    move(0,0);
+    setfcolor(YELLOW,1);
+    setbcolor(BLUE);
+    prints("   FRIENDTEST      统计结果 %s                                                 ", currentuser->userid);
+    resetcolor();
+    prints("\n");
+}
+
+void show_stat()
+{
+    char direct[PATHLEN];
+    struct stat st;
+    int i, j, k, l, m;
+    char ans[3], buf[122], buf2[10], answer[20];
+    time_t span;
+
+    do_test_reset();
+    
+    sethome(direct, currentuser->userid);
+    if(stat(direct, &st) == -1) {
+        move(1,0);
+        prints("你还没有建立账号!");
+        pressreturn();
+        return;
+    }
+    load_quests(direct);
+    sethometop(direct, currentuser->userid);
+    load_users(direct);
+    sort_users();
+    sethomestat(direct, currentuser->userid);
+    load_stats(direct);
+
+    move(2,0);
+    j=0;
+    for(i=0;i<userst;i++)
+        j+=users[i].tried;
+    if(userst) j/=userst;
+    prints("统计信息:\n共 %d 人做过你的测试\n平均分: %d\n\n按回车键开始详细资料",
+        userst, j);
+    pressreturn();
+    
+    for(i=0;i<questst;i++){
+        do_test_reset();
+        move(1,0);
+        prints("第%d题(共%d题)\n\n", i+1, questst);
+        prints("%s\n", quests[i].topic);
+        for(j=0;j<5;j++)
+            if(!quests[i].quest[j][0]) break;
+        for(k=0;k<j;k++){
+            m=0;
+            for(l=0;l<statst;l++)
+                if(stats[l][i]==k+1) m++;
+            prints("%d) %s (分值:%d  选择人次:%d)\n", k+1, quests[i].quest[k], quests[i].value[k], m);
+        }
+        getdata(10,0,"按q退出:",ans,2,true,NULL,true);
+        if(toupper(ans[0])=='Q') break;
+    }
+}
+
+void admin_reset()
+{
+    resetcolor();
+    clear();
+    move(0,0);
+    setfcolor(YELLOW,1);
+    setbcolor(BLUE);
+    prints("   FRIENDTEST      管理选单 %s                                                 ", currentuser->userid);
+    resetcolor();
+    prints("\n");
+}
+
+void admin_stat()
+{
+    char direct[PATHLEN];
+    struct stat st;
+    int i, j, k, l, m;
+    char ans[3], buf[122], buf2[10], answer[20];
+    time_t span;
+
+    sethome(direct, currentuser->userid);
+    if(stat(direct, &st) == -1) {
+        move(1,0);
+        prints("你还没有建立账号!");
+        pressreturn();
+        return;
+    }
+    load_quests(direct);
+    sethometop(direct, currentuser->userid);
+    load_users(direct);
+    sort_users();
+    sethomestat(direct, currentuser->userid);
+    load_stats(direct);
+
+    while(1) {
+        admin_reset();
+    
+        move(2,0);
+        j=0;
+        for(i=0;i<userst;i++)
+            j+=users[i].tried;
+        if(userst) j/=userst;
+        prints("统计信息:\n共 %d 人做过你的测试\n平均分: %d",
+        userst, j);
+
+        getdata(7,0,"选择[q-退出,d-删除,e-修改,a-添加,r-重置,s-察看] ",ans,2,true,NULL,true);
+        switch(toupper(ans[0])) {
+            case 'Q': return;
+            case 'D':
+                sprintf(buf,"输入删除项(1--%d, all全删):",questst);
+                do{
+                    move(8,0); clrtoeol();
+                    getdata(8,0,buf,buf2,4,true,NULL,true);
+                    i=atoi(buf2);
+                } while((i<1||i>questst)&&strcasecmp(buf2,"all"));
+                if(!strcasecmp(buf2,"all")) {
+                    free(quests);
+                    questst=0;
+                }
+                else {
+                    i--;
+                    for(j=i;j<questst-1;j++)
+                        memcpy(&quests[j],&quests[j+1],sizeof(quests[j]));
+                    questst--;
+                    if(!questst) free(quests);
+                }
+                sethome(direct, currentuser->userid);
+                if(!questst) {
+                    unlink(direct);
+                    return;
+                }
+                else save_quests(direct);
+                break;
+            case 'A':
+                i=questst;
+                if(i>=20){
+                    move(8,0);
+                    prints("题目不能超过20个!\n");
+                    pressreturn();
+                    break;
+                }
+againa:
+                new_friendtest_reset();
+                prints("第%d题(共%d题)\n", i+1, questst+1);
+                getdata(2,0,"请输入题目(40个汉字内): ", quests[i].topic, 80, true, NULL, true);
+                if (!quests[i].topic[0]) {
+                    new_friendtest_reset();
+                    getdata(1,0,"取消? [Y-退出/N-继续]: ", ans, 2, true, NULL, true);
+                    if(toupper(ans[0])=='Y') break;
+                    goto againa;
+                }
+                for(j=0;j<5;j++) quests[i].quest[j][0]=0;
+                do{
+                    move(3,0); clrtoeol();
+                    getdata(3,0,"请输入选项个数[1--5]: ", buf, 2, true, NULL, true);
+                    j=atoi(buf);
+                }while(j<1||j>5);
+                for(k=0;k<j;k++) {
+                    do{
+                        move(4+k*2,0); clrtoeol();
+                        sprintf(buf, "请输入第%d个选择内容:", k+1);
+                        getdata(4+k*2,0,buf,quests[i].quest[k],40,true,NULL,true);
+                    } while(!quests[i].quest[k][0]);
+                    do{
+                        move(5+k*2,0); clrtoeol();
+                        sprintf(buf, "请输入第%d个选择分值[-10--10]:", k+1);
+                        getdata(5+k*2,0,buf,buf2,4,true,NULL,true);
+                        quests[i].value[k]=atoi(buf2);
+                    } while(quests[i].value[k]<-10||quests[i].value[k]>10);
+                }
+                questst++;
+                sethome(direct, currentuser->userid);
+                save_quests(direct);
+                break;
+
+            case 'E':
+                sprintf(buf,"输入编辑项(1--%d):",questst);
+                do{
+                    move(8,0); clrtoeol();
+                    getdata(8,0,buf,buf2,4,true,NULL,true);
+                    i=atoi(buf2);
+                } while((i<1||i>questst));
+                i--;
+againb:
+                new_friendtest_reset();
+                prints("第%d题(共%d题)\n", i+1, questst);
+                getdata(2,0,"请输入题目(40个汉字内): ", quests[i].topic, 80, true, NULL, false);
+                if (!quests[i].topic[0]) {
+                    new_friendtest_reset();
+                    getdata(1,0,"取消? [Y-退出/N-继续]: ", ans, 2, true, NULL, true);
+                    if(toupper(ans[0])=='Y') break;
+                    goto againb;
+                }
+                for(k=0;k<5;k++) if(!quests[i].quest[k][0]) break;
+                do{
+                    move(3,0); clrtoeol();
+                    sprintf(buf, "%d", k);
+                    getdata(3,0,"请输入选项个数[1--5]: ", buf, 2, true, NULL, false);
+                    j=atoi(buf);
+                }while(j<1||j>5);
+                if(j<5) quests[i].quest[j][0]=0;
+                for(k=0;k<j;k++) {
+                    do{
+                        move(4+k*2,0); clrtoeol();
+                        sprintf(buf, "请输入第%d个选择内容:", k+1);
+                        getdata(4+k*2,0,buf,quests[i].quest[k],40,true,NULL,false);
+                    } while(!quests[i].quest[k][0]);
+                    do{
+                        move(5+k*2,0); clrtoeol();
+                        sprintf(buf, "请输入第%d个选择分值[-10--10]:", k+1);
+                        sprintf(buf2,"%d",quests[i].value[k]);
+                        getdata(5+k*2,0,buf,buf2,4,true,NULL,false);
+                        quests[i].value[k]=atoi(buf2);
+                    } while(quests[i].value[k]<-10||quests[i].value[k]>10);
+                }
+                sethome(direct, currentuser->userid);
+                save_quests(direct);
+                break;
+            case 'R':
+                load_users(FRIENDTOP);
+                j=1;
+                for(i=0;i<userst;i++)
+                if(!strcmp(users[i].userid, currentuser->userid)) {
+                    users[i].tried = 0;
+                    users[i].got = 0;
+                    users[i].create = time(0);
+                    j=0;
+                    break;
+                }
+                if(j) {
+                    if(!userst)
+                        users=(struct usertype*)malloc(sizeof(struct usertype)*(userst+1));
+                    strcpy(users[userst].userid, currentuser->userid);
+                    users[userst].tried = 0;
+                    users[userst].got = 0;
+                    users[userst].create = time(0);
+                    userst++;
+                }
+                save_users(FRIENDTOP);
+                sethometop(direct, currentuser->userid);
+                unlink(direct);
+                sethomestat(direct, currentuser->userid);
+                unlink(direct);
+                move(8,0);
+                prints("题目已经被重置!\n");
+                pressreturn();
+                break;
+            case 'S':
+                for(i=0;i<questst;i++){
+                    do_test_reset();
+                    move(1,0);
+                    prints("第%d题(共%d题)\n\n", i+1, questst);
+                    prints("%s\n", quests[i].topic);
+                    for(j=0;j<5;j++)
+                        if(!quests[i].quest[j][0]) break;
+                    for(k=0;k<j;k++){
+                        m=0;
+                        for(l=0;l<statst;l++)
+                            if(stats[l][i]==k+1) m++;
+                        prints("%d) %s (分值:%d  选择人次:%d)\n", k+1, quests[i].quest[k], quests[i].value[k], m);
+                    }
+                    getdata(10,0,"按q退出:",ans,2,true,NULL,true);
+                    if(toupper(ans[0])=='Q') break;
+                }
+                break;
+        }
+    }
+}
+
 int friend_main()
 {
     int i;
@@ -517,6 +888,12 @@ int friend_main()
                 break;
             case 2:
                 do_test();
+                break;
+            case 3:
+                show_stat();
+                break;
+            case 4:
+                admin_stat();
                 break;
         }
     }
