8585dc4f97ea7363be9a2f58f99035b7d1e8735a
kcn <kcn@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2002-09-16 10:16:39 +0000

    add IFS check
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@1664 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/bbs2www/html/bbstop10.php
+++ kbs_bbs/bbs2www/html/bbstop10.php
@@ -7,11 +7,28 @@
 		html_nologin();
 	else
 	{
-		html_init("gb2312");
 		$top_file = get_bbsfile("/etc/posts/day");
 		$fp = fopen($top_file, "r");
-		if ($fp == FALSE)
+		if ($fp == FALSE) {
+		        html_init("gb2312");
 			html_error_quit("无法读取十大话题数据");
+                }
+                $modifytime=filemtime($top_file);
+	session_cache_limiter("public");
+	$oldmidofied=$_SERVER["HTTP_IF_MODIFIED_SINCE"];
+	if ($oldmidofied!="") {
+		list($dayobweek,$day,$month,$year,$hour,$minute,$second)=
+			sscanf($oldmidofied,"%s, %d %d %d %d:%d:%d");
+		$oldtime=gmmktime($hour,$minute,$second,$month,$day,$year);
+	} else $oldtime=0;
+	if ($oldtime==$modifytime) {
+		header("HTTP/1.1 304 Not Modified");
+		return;
+	}
+	header("Last-Modified: " . gmdate("D, d M Y H:i:s", $modifytime) . "GMT");
+	header("Expires: " . gmdate("D, d M Y H:i:s", $modifytime+300) . "GMT");
+	header("Cache-Control: max-age=300, must-revalidate");
+	html_init("gb2312");
 ?>
 <body>
 <center><?php echo $BBS_FULL_NAME; ?> -- 今日十大热门话题
