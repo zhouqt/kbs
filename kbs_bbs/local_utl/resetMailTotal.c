028bc632a518551c212897e0af577300943d8163
flyriver <flyriver@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2002-09-17 03:45:59 +0000

    Improved for reset a specified userid's mail space.
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@1680 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/local_utl/resetMailTotal.c
+++ kbs_bbs/local_utl/resetMailTotal.c
@@ -9,10 +9,36 @@ int query_BM(struct userec *user, char *arg)
     return 1;
 }
 
+static void 
+usage()
+{
+	fprintf(stderr, "Usage: resetMailTotal [userid]\n\n");
+	fprintf(stderr, "    If userid is missing, this program will reset all userids' mail space,\n");
+	fprintf(stderr, "    else only reset the specified userid's mail space.\n");
+}
+
+int 
 main(int argc, char ** argv)
 {
+	struct userec *user = NULL;
+
     chdir(BBSHOME);
     resolve_ucache();
     resolve_boards();
-    apply_users(query_BM, NULL);
+	if (argc == 1)
+    	apply_users(query_BM, NULL);
+	else if (argc == 2)
+	{
+		getuser(argv[1], &user);
+		if (user == NULL)
+		{
+			fprintf(stderr, "User %s not found.\n", argv[1]);
+			return -1;
+		}
+		user->usedspace = 0xFFFF;
+	}
+	else
+		usage();
+
+	return 0;
 }
