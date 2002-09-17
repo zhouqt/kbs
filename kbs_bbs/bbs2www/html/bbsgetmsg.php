b0aaeae096dc61a41973066216b1e61ae0ea1a30
kcn <kcn@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2002-09-17 06:29:39 +0000

    bbsgetmsg to php
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@1684 de4889a5-8ad4-4bf3-bd87-57997c21d515

---
+++ kbs_bbs/bbs2www/html/bbsgetmsg.php
@@ -0,0 +1,40 @@
+<?php
+	/**
+	 * This file lists articles to user.
+	 * $Id$
+	 */
+	require("funcs.php");
+	if ($loginok != 1)
+		html_nologin();
+	else
+	{
+		html_init("gb2312");
+		$ret=bbs_getwebmsg($srcid,$msgbuf,$srcutmpnum);
+?>
+<meta http-equiv='Refresh' content='60; url=bbsgetmsg.php'>
+<meta http-equiv="pragma" content="no-cache"><style type="text/css">
+A {color: #0000FF}
+</style>
+<?php
+        if ($ret)
+		{
+?>
+<bgsound src="/sound/msg.wav">
+<body style="BACKGROUND-COLOR: #f0ffd0">
+<table width="100%">
+<tr><td><?php echo $msgbuf; ?></td>
+<td align="right"><a target="f3" href="/cgi-bin/bbs/bbssendmsg?destid=<?php 
+echo $srcid; ?>&destutmp=<?php 
+echo $srcutmpnum; ?>">[»ØÑ¶Ï¢]</a> <a href="bbsgetmsg.php">[ºöÂÔ]</a></td></tr></table>
+<script language=javascript>if (parent.viewfrm)	parent.viewfrm.rows = "18,*,20";
+</script>
+<?php
+        } else {
+		//no msg
+?><script language=javascript>if (parent.viewfrm)	parent.viewfrm.rows = "0,*,20";
+</script>
+<?php
+		}
+		html_normal_quit();
+    }
+?>
