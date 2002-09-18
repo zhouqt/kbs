f44dc42a3f17fb38d1a896e3a0b2154c3b30a2ee
kcn <kcn@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2002-09-18 08:30:49 +0000

    bbssig.php
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@1703 de4889a5-8ad4-4bf3-bd87-57997c21d515

---
+++ kbs_bbs/bbs2www/html/bbssig.php
@@ -0,0 +1,38 @@
+<?php
+	require("funcs.php");
+	if ($loginok != 1)
+		html_nologin();
+	else
+	{
+		html_init("gb2312");
+		$filename=bbs_sethomefile($currentuser["userid"],"signatures");
+		if ($_GET["type"]=="1") {
+		    $fp=fopen($filename,"w+");
+		    fwrite($_POST["text"]);
+			fclose($fp);
+		}
+?>
+<body>
+<center><?php echo $BBS_FULL_NAME; ?> -- 设置签名档 [使用者: <?php echo $currentuser["userid"]; ?>]</center><hr />
+<form method="post" action="bbssig?type=1">
+签名档每6行为一个单位, 可设置多个签名档.<table width="610" border="1"><tr><td><textarea name="text" rows="20" cols="100" wrap="physical">
+<?php
+if ($_GET["type"]!="1") {
+    $fp = fopen ($filename, "r");
+    while (!feof ($fp)) {
+        $buffer = fgets($fp, 300);
+        echo $buffer;
+    }
+    fclose ($fp);
+else {
+    echo $_POST["text"];
+}
+?>
+</textarea></table>
+<input type="submit" value="存盘" /> <input type="reset" value="复原" />
+</form><hr />
+</body>
+</html>
+<?php
+	}
+?>
