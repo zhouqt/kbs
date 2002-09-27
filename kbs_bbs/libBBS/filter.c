85c801071df764893cdb77f44f3b59376f7f84b1
kcn <kcn@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2002-09-27 16:50:55 +0000

    首先把文件改成mmap的
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@1768 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/libBBS/filter.c
+++ kbs_bbs/libBBS/filter.c
@@ -6,36 +6,41 @@
 
 extern int prepf(int fp);
 extern int mgrep(int fp);
+
 //³ÌÐòÃû
 
-int WORDBOUND,WHOLELINE,NOUPPER,INVERSE,FILENAMEONLY,SILENT,FNAME;
-int COUNT,num_of_matched,total_line;
-char CurrentFileName[255];
-char* Progname="";
+int WORDBOUND, WHOLELINE, NOUPPER, INVERSE, FILENAMEONLY, SILENT, FNAME;
+int COUNT, num_of_matched, total_line;
+char *CurrentFileName;
 
-int check_filter(char* patternfile,char* checkfile)
+int check_filter(char *patternfile, char *checkfile,int defaultval)
 {
-	int fp,fd;
-	WHOLELINE=0;
-	NOUPPER=0;
-	INVERSE=0;
-	FILENAMEONLY=1;
-	WORDBOUND=0;
-	SILENT=1;
-	FNAME=1;
-	COUNT=0;
+    int fp;
+    char *ptr;
+    int size, retv;
 
-	num_of_matched=0;
-	fp=open(patternfile,O_RDONLY);
-	if (fp==-1) return 0;
-	fd=open(checkfile,O_RDONLY);
-	if (fd==-1) {
-            close(fp);
-	    return 0;
-	};
-	prepf(fp);
-	mgrep(fd);
-	close(fp);
-	close(fd);
-	return num_of_matched;
+    WHOLELINE = 0;
+    NOUPPER = 0;
+    INVERSE = 0;
+    FILENAMEONLY = 1;
+    WORDBOUND = 0;
+    SILENT = 1;
+    FNAME = 1;
+    COUNT = 0;
+
+    CurrentFileName = checkfile;
+    num_of_matched = 0;
+    fp = open(patternfile, O_RDONLY);
+    prepf(fp);
+    BBS_TRY {
+        if (safe_mmapfile(checkfile, O_RDONLY, PROT_READ, MAP_SHARED, (void **) &ptr, &size, NULL) == 0)
+            BBS_RETURN(0);
+        retv = mgrep_str(ptr, size);
+    }
+    BBS_CATCH {
+    	retv=defaultval;
+    }
+    BBS_END end_mmapfile((void *) ptr, size, -1);
+    close(fp);
+    return retv;
 }
