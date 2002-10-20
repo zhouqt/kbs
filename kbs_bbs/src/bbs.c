3793429c3a98132e5d09ec14e60f87b3098e6bc6
kcn <kcn@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2002-10-20 13:23:09 +0000

    修正有些Re文没有Re开头的问题
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@1941 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/src/bbs.c
+++ kbs_bbs/src/bbs.c
@@ -604,7 +604,7 @@ char *readdoent(char *buf, int num, struct fileheader *ent)
 
     if (uinfo.mode != RMAIL && digestmode != 1 && digestmode != 4 && digestmode != 5) { // 新方法比较
         if (FFLL == 0) {
-            if (ent->groupid != ent->id)
+            if ((ent->groupid != ent->id)&&(!strncasecmp(TITLE,"Re:",3)||!strncmp(TITLE,"回复:",5))
                 sprintf(buf, " %4d %s%c%s %-12.12s %s  %-47.47s ", num, typeprefix, type, typesufix, ent->owner, date, TITLE);
             else                /* 非Re的文章 */
                 sprintf(buf, " %4d %s%c%s %-12.12s %s  ● %-44.44s ", num, typeprefix, type, typesufix, ent->owner, date, TITLE);
