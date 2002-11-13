6aa12b69ce498d347be5ad83b8d814d171177681
kcn <kcn@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2002-11-13 03:48:41 +0000

    fix duplite #ifdef
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@1990 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/filter.sh
+++ kbs_bbs/filter.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 awk 'BEGIN { definenum=0;infilter=0; } \
-  $0~/#ifdef[ \t]*FILTER/ { infilter=1;definenum=0;} \
-  $0~/#ifdef[ \t]*SMTH/ { infilter=1;definenum=0;} \
+  $0~/#ifdef[ \t]*FILTER/ { if (infilter==0) {infilter=1;definenum=0;} } \
+  $0~/#ifdef[ \t]*SMTH/ { if (infilter==0) {infilter=1;definenum=0;} } \
   { if (infilter==1) {  \
       if (match($0,"#endif")) {\
          definenum--; \
