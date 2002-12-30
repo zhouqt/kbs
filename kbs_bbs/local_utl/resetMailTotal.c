9020da80c70f3d2f14993137a59240c7e342fe91
flyriver <flyriver@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2002-12-30 08:56:53 +0000

    Changed command parameter.
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@2035 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/local_utl/resetMailTotal.c
+++ kbs_bbs/local_utl/resetMailTotal.c
@@ -12,8 +12,8 @@ int query_BM(struct userec *user, char *arg)
 static void 
 usage()
 {
-	fprintf(stderr, "Usage: resetMailTotal [userid]\n\n");
-	fprintf(stderr, "    If userid is missing, this program will reset all userids' mail space,\n");
+	fprintf(stderr, "Usage: resetMailTotal <-a|-u userid>\n\n");
+	fprintf(stderr, "    If -a parameter is provided, this program will reset all userids' mail space,\n");
 	fprintf(stderr, "    else only reset the specified userid's mail space.\n");
 }
 
@@ -25,11 +25,11 @@ main(int argc, char ** argv)
     chdir(BBSHOME);
     resolve_ucache();
     resolve_boards();
-	if (argc == 1)
+	if (argc == 2 && !strcmp(argv[1], "-a"))
     	apply_users(query_BM, NULL);
-	else if (argc == 2)
+	else if (argc == 3 && !strcmp(argv[1], "-u"))
 	{
-		getuser(argv[1], &user);
+		getuser(argv[2], &user);
 		if (user == NULL)
 		{
 			fprintf(stderr, "User %s not found.\n", argv[1]);
