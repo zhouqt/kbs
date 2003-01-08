ee7b48834d35d29fd24f2c897a3ca2f165aea992
kcn <kcn@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2003-01-08 03:17:20 +0000

    add post to bbslists
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@2044 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/local_utl/static.c
+++ kbs_bbs/local_utl/static.c
@@ -3,24 +3,28 @@
 #include <time.h>
 #include <stdio.h>
 #define MAX_LINE        15
+#define STATIC_COUNT_FILE "etc/posts/stat_count"
 #include "bbs.h"
 struct {
     int no[24];                 /* ´ÎÊý */
     int sum[24];                /* ×ÜºÏ */
 } st;
 
-main(argc, argv)
-    char *argv[];
+
+int main(int argc,char* argv[])
 {
     char *progmode;
     FILE *fp;
     char buf[256], *p;
     char date[80];
-    int now;
+    time_t now;
     int hour, max = 0, item, total = 0;
     int totaltime = 0;
     int i, j;
     struct tm *date_tm;
+    int fd;
+    int change;
+    struct tm now_tm,last_tm;
     char *blk[10] = {
         /* ·½¿òÌ«ÄÑ¿´ÁË modified by Czz */
         "£ß", "£ß", "¨x", "¨y", "¨z",
@@ -28,7 +32,20 @@ main(argc, argv)
         /* modified end */
     };
 
-    if ((fp = fopen(BBSHOME "/usies", "r")) == NULL) {
+    struct {
+    	int month_count[12]; /*Ã¿ÔÂÍ³¼Æ*/
+    	int day_count[31]; /*Ã¿ÌìÍ³¼Æ*/
+    	time_t counttime; /*ÉÏ´ÎÍ³¼ÆÊ±¼ä*/
+    } total_st,old_st;
+    chdir(BBSHOME);
+    resolve_boards();
+
+    //¶ÁÈëÔ­À´µÄÍ³¼ÆÊý¾Ý
+    bzero(&total_st,sizeof(total_st));
+    fd=open(STATIC_COUNT_FILE,O_RDONLY);
+    if (fd!=-1) 
+    	close(fd);
+    if ((fp = fopen("usies", "r")) == NULL) {
         printf("cann't open usies\n");
         return 1;
     }
@@ -65,30 +82,116 @@ main(argc, argv)
 
     item = max / MAX_LINE + 1;
 
-    if ((fp = fopen(BBSHOME "/0Announce/bbslists/countlogins", "w")) == NULL) {
+    /*ÀÛ¼ÆÒÔÇ°µÄÍ³¼ÆÊý¾Ý*/
+    old_st=total_st;
+    change=1;
+    if (total_st.counttime==0) {
+    	/*µÚÒ»´ÎÍ³¼Æ*/
+    	total_st.day_count[date_tm->tm_mday]=total;
+    	total_st.month_count[date_tm->tm_mon]=total;
+    	total_st.counttime=now;
+    } else {
+      now_tm=*date_tm;
+      date_tm=localtime(&total_st.counttime);
+      last_tm=*date_tm;
+      if (last_tm.tm_year!=now_tm.tm_year) {
+	      	/*¹ýÁËÒ»ÄêÁË*/
+           bzero(&total_st,sizeof(total_st));
+           total_st.month_count[date_tm->tm_mon]=total;
+      }
+      else if (last_tm.tm_mon!=now_tm.tm_mon) {
+      /*	»»ÔÂ·ÝÁË£¬Çå³ýÃ¿ÈÕÍ³¼Æ*/
+          int i;
+          for (i=0;i<31;i++)
+          	total_st.day_count[i]=0;
+      }
+      if (last_tm.tm_mday==now_tm.tm_mday) {
+      	/*µ±Ìì¶à´ÎÖ´ÐÐ£¬ÄÇÃ´µ±ÔÂÍ³¼Æ¼õÈ¥ÉÏ´Î¼ÓµÄ£¬¼ÓÉÏÕâ´ÎµÄ*/
+           total_st.month_count[date_tm->tm_mon]=total_st.month_count[date_tm->tm_mon]-
+           	total_st.day_count[date_tm->tm_mday-1]+total;
+      	    change=0;
+      	}
+      total_st.day_count[date_tm->tm_mday-1]=total;
+      total_st.counttime=now;
+    }
+
+    /*±£´æÀÛ¼ÆÊý¾Ý*/
+    substitute_record(STATIC_COUNT_FILE,&total_st,sizeof(total_st),0);
+    if (now_tm.tm_hour==23) {
+    	/*ÍíÉÏ12µãÏÔÊ¾ÀÛ¼ÆÊý¾Ý*/
+	    if ((fp = fopen("0Announce/bbslists/count", "w")) == NULL) {
+	        printf("Cann't open count\n");
+	    }
+	    /*ÏÔÊ¾Í³¼ÆÊý¾Ý*/
+	    if (fp!=NULL) {
+	    	int count,total,total2,count2;
+	    	int i;
+	       fprintf(fp, "\n\x1b[36m                  %sÉÏÕ¾ÈËÊýÍ³¼Æ\n",BBS_FULL_NAME);
+/*ÔÂÍ³¼Æ*/
+	       fprintf(fp, "\n\x1b[36m                  ÔÂÍ³¼ÆÊý¾Ý\n");
+	       
+		fprintf(fp,"\n       1        2        3        4        5        6|×Ü      Êý Æ½    ¾ù\n");
+		count=0;
+		total=0;
+		for (i=0;i<6;i++) {
+			if (total_st.month_count[i]!=0) {
+				total+=total_st.month_count[i];
+				count++;
+			}
+			fprintf(fp,"%10d",total_st.month_count[i]);
+	        }
+		total2=total;
+		count2=count;
+		count=0;
+		total=0;
+		
+		fprintf(fp," %11d %10d\n",total,total/count);
+		fprintf(fp,"\n       7        8        9       10       11       12|×Ü      Êý Æ½    ¾ù\n");
+		count=0;
+		total=0;
+		for (i=7;i<12;i++) {
+			if (total_st.month_count[i]!=0) {
+				total+=total_st.month_count[i];
+				count++;
+			}
+			fprintf(fp,"%10d",total_st.month_count[i]);
+	       }
+		fprintf(fp," %11d %10d\n",total,total/count);
+
+		fprintf(fp,"×ÜÊý: %d\nÃ¿ÔÂ×ÜÆ½¾ù: %d\n",
+			total+total2,(total+total2)/(count+count2));
+/*TODO:Ã¿ÌìÍ³¼Æ*/		
+		fclose(fp);
+	    } else {
+		 post_file(NULL, "", "0Announce/bbslists/countlogins", "BBSLists", "ÎÞ·¨´ò¿ªÎÄ¼þ", 0, 1);
+	    }
+    }
+    
+   /*ÏÔÊ¾ÏÖÔÚµÄÊý¾Ý*/
+    if ((fp = fopen("0Announce/bbslists/countlogins", "w")) == NULL) {
         printf("Cann't open countlogins\n");
         return 1;
     }
 
-    fprintf(fp, "\n[36m    ©°¡ª¡ª¡ª¡ª¡ª¡ª¡ª¡ª¡ª¡ª¡ª ³¬¹ý \033[01m\033[37m1000\033[00m\033[36m ½«²»ÏÔÊ¾Ç§Î»Êý×Ö ¡ª¡ª¡ª¡ª¡ª¡ª¡ª¡ª¡ª¡ª¡ª©´\n");
+    fprintf(fp, "\n\x1b[36m    ©°¡ª¡ª¡ª¡ª¡ª¡ª¡ª¡ª¡ª¡ª¡ª ³¬¹ý \033[01m\033[37m1000\033[00m\033[36m ½«²»ÏÔÊ¾Ç§Î»Êý×Ö ¡ª¡ª¡ª¡ª¡ª¡ª¡ª¡ª¡ª¡ª¡ª©´\n");
     for (i = max / item + 1; i >= 0; i--) {
-        fprintf(fp, "[34m%4d[36m©¦[33m", (i) * item);
+        fprintf(fp, "\x1b[34m%4d\x1b[36m©¦\x1b[33m", (i) * item);
         for (j = 0; j < 24; j++) {
             if ((item * (i) > st.no[j]) && (item * (i - 1) <= st.no[j]) && st.no[j]) {
                 /* Ã¿Ò»Ç§»»Ò»ÖÖÑÕÉ« Czz 020507 */
                 if (st.no[j] >= 5000)
-                    /*fprintf(fp, "[35m###[33m"); Leeward 97.12.08 */
-                    fprintf(fp, "\033[1m[36m%-3d\033[m[33m", (st.no[j]) % 1000);
+                    /*fprintf(fp, "\x1b[35m###\x1b[33m"); Leeward 97.12.08 */
+                    fprintf(fp, "\033[1m\x1b[36m%-3d\033[m\x1b[33m", (st.no[j]) % 1000);
                 else if (st.no[j] >= 4000)
-                    fprintf(fp, "\033[1m[35m%-3d\033[m[33m", (st.no[j]) % 1000);
+                    fprintf(fp, "\033[1m\x1b[35m%-3d\033[m\x1b[33m", (st.no[j]) % 1000);
                 else if (st.no[j] >= 3000)
-                    fprintf(fp, "\033[1m[34m%-3d\033[m[33m", (st.no[j]) % 1000);
+                    fprintf(fp, "\033[1m\x1b[34m%-3d\033[m\x1b[33m", (st.no[j]) % 1000);
                 else if (st.no[j] >= 2000)
-                    fprintf(fp, "\033[1m[33m%-3d\033[m[33m", (st.no[j]) % 1000);
+                    fprintf(fp, "\033[1m\x1b[33m%-3d\033[m\x1b[33m", (st.no[j]) % 1000);
                 else if (st.no[j] >= 1000)
-                    fprintf(fp, "\033[1m[32m%-3d\033[m[33m", (st.no[j]) % 1000);
+                    fprintf(fp, "\033[1m\x1b[32m%-3d\033[m\x1b[33m", (st.no[j]) % 1000);
                 else
-                    fprintf(fp, "[31m%-3d[33m", (st.no[j]));
+                    fprintf(fp, "\x1b[31m%-3d\x1b[33m", (st.no[j]));
                 continue;
             }
             if (st.no[j] - item * i < item && item * i < st.no[j])
@@ -98,11 +201,21 @@ main(argc, argv)
             else
                 fprintf(fp, "   ");
         }
-        fprintf(fp, "[36m©¦\n");
+        fprintf(fp, "\x1b[36m©¦\n");
     }
-    fprintf(fp, "   [36m" " ©¸¡ª¡ª¡ª[37m   " NAME_BBS_CHINESE NAME_BBS_NICK "  ÉÏÕ¾ÈË´Î±í   [36m¡ª¡ª¡ª[37m%s[36m¡ª¡ª©¼\n"
-/*"    [34m  0  1  2  3  4  5  6  7  8  9  10 11 [31m12 13 14 15 16 17 18 19 20 21 22 23 \n\n" *//* Leeward 98.02.27 */
-            "    [34m  1  2  3  4  5  6  7  8  9  10 11 12 [31m13 14 15 16 17 18 19 20 21 22 23 24\n\n" "               [36m 1 [33m¨~[36m = [37m%-5d [36m×Ü¹²ÉÏÕ¾ÈË´Î£º[37m%-9d[36mÆ½¾ùÊ¹ÓÃÊ±¼ä£º[37m%d[m \n\n", Ctime(now), item, total, totaltime / total + 1);    /* Leeward 98.09.24 add the 2nd \n for SHARE MEM in ../main.c */
+    fprintf(fp, "   \x1b[36m" " ©¸¡ª¡ª¡ª\x1b[37m   " NAME_BBS_CHINESE NAME_BBS_NICK "  ÉÏÕ¾ÈË´Î±í   \x1b[36m¡ª¡ª¡ª\x1b[37m%s\x1b[36m¡ª¡ª©¼\n"
+/*"    \x1b[34m  0  1  2  3  4  5  6  7  8  9  10 11 \x1b[31m12 13 14 15 16 17 18 19 20 21 22 23 \n\n" *//* Leeward 98.02.27 */
+            "    \x1b[34m  1  2  3  4  5  6  7  8  9  10 11 12 \x1b[31m13 14 15 16 17 18 19 20 21 22 23 24\n\n" "               \x1b[36m 1 \x1b[33m¨~\x1b[36m = \x1b[37m%-5d \x1b[36m×Ü¹²ÉÏÕ¾ÈË´Î£º\x1b[37m%-9d\x1b[36mÆ½¾ùÊ¹ÓÃÊ±¼ä£º\x1b[37m%d\x1b[m \n\n", 
+            Ctime(now), item, total, totaltime / total + 1);    /* Leeward 98.09.24 add the 2nd \n for SHARE MEM in ../main.c */
 
     fclose(fp);
+
+    if (now_tm.tm_hour==23) {
+    	sprintf(buf,"%dÄê%2dÔÂ%2dÈÕÈËÊýÍ³¼Æ",now_tm.tm_year+1900,now_tm.tm_mon+1,now_tm.tm_mday);
+    	post_file(NULL, "", "0Announce/bbslists/countlogins", "BBSLists", buf, 0, 1);
+    	sprintf(buf,"%dÄê%2dÔÂ%2dÈÕÀÛ¼ÆÈËÊýÍ³¼Æ",now_tm.tm_year+1900,now_tm.tm_mon+1,now_tm.tm_mday);
+	post_file(NULL, "", "0Announce/bbslists/count", "BBSLists", buf, 0, 1);
+    }
+    return 0;
 }
+
