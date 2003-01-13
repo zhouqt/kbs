0221696e95cfc4a62e4d113d976570a40b90f4cc
kcn <kcn@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2003-01-13 07:26:08 +0000

    看备忘录
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@2081 de4889a5-8ad4-4bf3-bd87-57997c21d515

---
+++ kbs_bbs/bbs2www/html/bbsnot.php
@@ -0,0 +1,59 @@
+<?php
+	/**
+	 * $Id$ 
+	 */
+	require("funcs.php");
+	if ($loginok !=1 )
+		html_nologin();
+	else
+	{
+		if (isset($_GET["board"]))
+			$board = $_GET["board"];
+		else 
+			html_error_quit("错误的讨论区");
+		if (bbs_checkreadperm($usernum,$board)==0)
+			html_error_quit("错误的讨论区");
+		$top_file="vote/" . $board . "/notes";
+		$fp = fopen($top_file, "r");
+		if ($fp == FALSE) {
+		        html_init("gb2312");
+			html_error_quit("现在没有留言");
+                }
+                
+                $modifytime=filemtime($top_file);
+	session_cache_limiter("public");
+	$oldmodified=$_SERVER["HTTP_IF_MODIFIED_SINCE"];
+	if ($oldmodified!="") {
+/*
+		list($dayobweek,$day,$month,$year,$hour,$minute,$second)=
+			sscanf($oldmidofied,"%s, %d %s %d %d:%d:%d");
+		$oldtime=gmmktime($hour,$minute,$second,$month,$day,$year);
+*/
+                $oldtime=strtotime($oldmodified);
+	} else $oldtime=0;
+	if ($oldtime>=$modifytime) {
+		header("HTTP/1.1 304 Not Modified");
+	        header("Cache-Control: max-age=300");
+		return;
+	}
+	header("Last-Modified: " . gmdate("D, d M Y H:i:s", $modifytime) . "GMT");
+	header("Expires: " . gmdate("D, d M Y H:i:s", $modifytime+300) . "GMT");
+	header("Cache-Control: max-age=300");
+	html_init("gb2312");
+?>
+<body>
+<center><?php echo $BBS_FULL_NAME; ?>水木清华 -- 备忘录 [讨论区: <?php echo $board; ?>]<hr color=green>
+<table border=1 width=610><tr><td><pre>
+<? 
+	bbs_printansifile($top_file);
+?></tr></td>
+</pre></table>
+[<a href=bbsdoc.php?board=<?php echo $board; ?>>本讨论区</a>]<?
+    if (bbs_is_bm($board,$usernum))
+	echo "[<a href=/cgi-bin/bbs/bbsmnote?board=" . $board . ">编辑进版画面</a>]";
+?> 
+</center>
+<?php
+		html_normal_quit();
+	}
+?>
