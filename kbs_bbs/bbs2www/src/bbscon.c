e24b55a71bea78c5aaca06393e97705a09de069a
flyriver <flyriver@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2003-02-04 18:29:32 +0000

    Improved with the newly get_records_from_id() function.
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@2133 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/bbs2www/src/bbscon.c
+++ kbs_bbs/bbs2www/src/bbscon.c
@@ -3,43 +3,51 @@
  */
 #include "bbslib.h"
 
+#define RECOREDS_NUM 3
+
 int main()
 {
     FILE *fp;
-    char buf[512], board[80], dir[80], file[80], filename[80], *ptr;
+    char buf[512], board[80], dir[80], filename[80], *ptr;
     char buf2[512],buf3[512];
-    struct fileheader x;
-    struct fileheader oldx;
+    fileheader_t records[RECORDS_NUM];
     int num, tmp, total;
     struct userec *user = NULL;
     struct boardheader *bp;
+	int rid;
+	int fd;
 
     init_all();
     strsncpy(board, getparm("board"), 32);
-    strsncpy(file, getparm("file"), 32);
+    rid = atoi(getparm("id"));
     num = atoi(getparm("num"));
     printf("<center>\n");
     bp = getbcache(board);
     if (!check_read_perm(currentuser, bp))
         http_fatal("错误的讨论区");
+	if (rid > bp->nowid)
+		http_fatal("本文不存在");
     strcpy(board, bp->filename);
-    if (VALID_FILENAME(file) < 0)
-        http_fatal("错误的参数");
-    if (get_file_ent(board, file, &x) <= 0)
-        http_fatal("错误的文件名");
-    memcpy(&oldx, &x, sizeof(x));
+	setbdir(DIR_MODE_NORMAL, dir, board);
+    total = file_size(dir) / sizeof(fileheader_t);
+    /*if (VALID_FILENAME(file) < 0)
+        http_fatal("错误的参数");*/
+    /* 需要修改 if (get_file_ent(board, file, &x) <= 0)
+        http_fatal("错误的文件名"); */
     printf("%s -- 文章阅读 [讨论区: %s]<hr color=\"green\">", BBSNAME, board);
-    sprintf(dir, "boards/%s/.DIR", board);
-    total = file_size(dir) / sizeof(x);
     if (total <= 0)
         http_fatal("此讨论区不存在或者为空");
+	fd = open(dir, O_RDONLY, 0644);
+	if (get_records_from_id(fd, rid, &records, RECOREDS_NUM) == 0)
+		http_fatal("本文不存在");
     printf("<table width=\"610\" border=\"1\">\n");
     printf("<tr><td>\n<pre>");
-    sprintf(filename, "boards/%s/%s", board, file);
+	setbfile(filename, board, records[1].filename);
     fp = fopen(filename, "r");
     if (fp == 0)
         http_fatal("本文不存在或者已被删除");
-    while (1) {
+    while (1)
+	{
         char *id, *s;
 
         if (fgets(buf, 512, fp) == 0)
@@ -70,43 +78,38 @@ int main()
 
     if ((loginok)&&strcmp(currentuser->userid,"guest")) {
         brc_initial(currentuser->userid, board);
-        brc_add_read(x.id);
+        brc_add_read(records[1].id);
         brc_update(currentuser->userid);
     }
     encode_url(buf2, board, sizeof(buf2));
     printf("</pre></td></tr>\n</table><hr>\n");
-    printf("[<a href=\"bbsfwd?board=%s&file=%s\">转寄/推荐</a>]", buf2, file);
-    printf("[<a href=\"bbsccc?board=%s&file=%s\">转贴</a>]", buf2, file);
-    if (can_delete_post(bp, &x, getcurrusr()))
-        printf("[<a onclick=\"return confirm('你真的要删除本文吗?')\" href=\"bbsdel?board=%s&file=%s\">删除文章</a>]", buf2, file);
-    if (can_edit_post(bp, &x, getcurrusr())) {
-        printf("[<a href=\"bbsedit?board=%s&file=%s\">修改文章</a>]", buf2, file);
+    printf("[<a href=\"bbsfwd?board=%s&file=%s\">转寄/推荐</a>]", buf2, records[1].filename);
+    printf("[<a href=\"bbsccc?board=%s&file=%s\">转贴</a>]", buf2, records[1].filename);
+    if (can_delete_post(bp, &records[1], getcurrusr()))
+        printf("[<a onclick=\"return confirm('你真的要删除本文吗?')\" href=\"bbsdel?board=%s&file=%s\">删除文章</a>]", buf2, records[1].filename);
+    if (can_edit_post(bp, &records[1], getcurrusr())) {
+        printf("[<a href=\"bbsedit?board=%s&file=%s\">修改文章</a>]", buf2, records[1].filename);
     }
-    /*
-     * TODO: 下面需要两次读操作，应该减为一个。
-     */
-    fp = fopen(dir, "r+");
-    if (fp == 0)
-        http_fatal("dir error2");
-    if (num > 1) {
-        fseek(fp, sizeof(x) * ((num-1) - 1), SEEK_SET);
-        fread(&x, sizeof(x), 1, fp);
-        printf("[<a href=\"bbscon?board=%s&file=%s&num=%d\">上一篇</a>]", buf2, x.filename, num - 1);
+    if (records[0].id != 0) {
+        printf("[<a href=\"bbscon?board=%s&id=%s&num=%d\">上一篇</a>]", buf2, 
+			   records[0].id, num - 1);
     }
     printf("[<a href=\"/bbsdoc.php?board=%s&page=%d\">本讨论区</a>]", 
 			buf2, (num + BBS_PAGE_SIZE - 1) / BBS_PAGE_SIZE);
-    if (num < total) {
-        fseek(fp, sizeof(x) * ((num-1) + 1), SEEK_SET);
-        fread(&x, sizeof(x), 1, fp);
-        printf("[<a href=\"bbscon?board=%s&file=%s&num=%d\">下一篇</a>]", buf2, x.filename, num + 1);
+    if (records[2].id != 0) {
+        printf("[<a href=\"bbscon?board=%s&file=%s&num=%d\">下一篇</a>]", buf2, 
+			   records[2].id, num + 1);
     }
-    fclose(fp);
-    ptr = oldx.title;
+    ptr = records[1].title;
     if (!strncmp(ptr, "Re: ", 4))
         ptr += 4;
     ptr[60] = 0;
-    if ((oldx.accessed[1] & FILE_READ) == 0)
-        printf("[<a href=\"bbspst?board=%s&file=%s&userid=%s&title=Re: %s&refilename=%s\">回文章</a>]", buf2, file, oldx.owner, encode_url(buf, void1(ptr), sizeof(buf)), oldx.filename);
+    if ((records[1].accessed[1] & FILE_READ) == 0)
+	{
+        printf("[<a href=\"bbspst?board=%s&file=%s&userid=%s&title=Re: %s&refilename=%s\">回文章</a>]", 
+			   buf2, records[1].filename, records[1].owner, 
+			   encode_url(buf, void1(ptr), sizeof(buf)), records[1].filename);
+	}
     printf("[<a href=\"bbstfind?board=%s&title=%s\">同主题阅读</a>]", buf2, encode_url(buf, void1(ptr), sizeof(buf)));
     printf("[<a href=\"javascript:history.go(-1)\">快速返回</a>]\n");
     printf("</center>\n");
