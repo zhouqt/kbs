2014ded46952b90076ea0dbf398210cebc1e6feb
bad <bad@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2003-02-09 21:27:42 +0000

    sort from top to low
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@2165 de4889a5-8ad4-4bf3-bd87-57997c21d515

---
+++ kbs_bbs/local_utl/statguy.c
@@ -0,0 +1,97 @@
+#include "bbs.h"
+
+char *curuserid;
+
+struct libstruct {
+	char id[STRLEN];
+	char nick[NAMELEN];
+	unsigned int data;
+} *lib;
+int total=0,flag=0;
+
+int show()
+{
+    int i;
+    printf("            ");
+    switch(flag){
+        case 0:
+	    printf("========== \033[1;36m上站次数排行榜\033[37;0m ==========\n\n");
+	    break;
+	case 1:
+	    printf("========== \033[1;36m发文次数排行榜\033[37;0m ==========\n\n");
+	    break;
+	case 2:
+	    printf("========== \033[1;36m上站时间排行榜\033[37;0m ==========\n\n");
+	    break;
+	case 3:
+	    printf("========== \033[1;36m总表现积分排行榜\033[37;0m ==========\n\n");
+	    break;
+    }
+    printf("名次 代号            昵称                           ");
+    if(flag==0||flag==1) printf("次数");
+    else if(flag==2) printf("总时数");
+    else printf("积分");
+    printf("\n");
+    printf("==== ======================================================\n");
+    for(i=0; i<total; i++) {
+        printf("[%2d] %-15s %-30s %d\n", i+1, lib[i].id, lib[i].nick, lib[i].data);
+    }
+    return 0;
+}
+
+int statit(struct userec *user, char *arg)
+{
+    int i,j,d;
+    if(!strcmp(user->userid,"guest")) return 0;
+    if(user->userid[0]<'A'||user->userid[0]>'z') return 0;
+    switch(flag){
+	case 0:
+	    d = user->numlogins;
+	    break;
+	case 1:
+	    d = user->numposts;
+	    break;
+	case 2:
+	    d = user->stay;
+	    break;
+	case 3:
+	    d = countexp(user);
+	    break;
+    }
+    for(i=0; i<total; i++)
+    if(lib[i].data<d) {
+        for(j=total-1;j>i;j--)
+	    memcpy(&lib[j],&lib[j-1],sizeof(struct libstruct));
+	strcpy(lib[i].id, user->userid);
+	strcpy(lib[i].nick, user->username);
+	lib[i].data = d;
+	break;
+    }
+    return 1;
+}
+
+main(int argc, char ** argv)
+{
+    time_t t;
+    struct tm res;
+    if (argc<=2) {
+        printf("usage: statguy login|post|stay|all <total>\n");
+	return;
+    }
+    if (!strcmp(argv[1], "login")) flag = 0;
+    if (!strcmp(argv[1], "post")) flag = 1;
+    if (!strcmp(argv[1], "stay")) flag = 2;
+    if (!strcmp(argv[1], "all")) flag = 3;
+    total=atoi(argv[2]);
+    if(!total) {
+        printf("error: <total> must be an integer and greater than zero!\n");
+        return;
+    }
+    chdir(BBSHOME);
+    resolve_ucache();
+    resolve_boards();
+    lib = malloc(sizeof(struct libstruct)*total);
+    memset(lib, 0, sizeof(struct libstruct)*total);
+    apply_users(statit, NULL);
+    show();
+}
