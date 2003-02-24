966b7c5e75fc60c3f2a305161c33515430860971
flyriver <flyriver@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2003-02-24 12:25:41 +0000

    Added a posttime field to fileheader struct.
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@2419 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/site/smth.h
+++ kbs_bbs/site/smth.h
@@ -270,7 +270,8 @@ typedef struct fileheader {     /* This structure is used to hold data in */
 #endif
     char innflag[2];
     char owner[OWNER_LEN];
-    char unused2[46];
+    char unused2[46-sizeof(time_t)];
+	time_t posttime;
     long attachment;
     char title[STRLEN];
     unsigned level;
@@ -279,9 +280,9 @@ typedef struct fileheader {     /* This structure is used to hold data in */
 
 typedef struct fileheader fileheader_t;
 
-#define GET_POSTFILENAME(x,y) get_postfilename(x,y,0)
+#define GET_POSTFILENAME(x,y) get_postfilename(x,y,1)
 #define GET_MAILFILENAME(x,y) get_postfilename(x,y,0)
-#define VALID_FILENAME(x) valid_filename(x,0)
+#define VALID_FILENAME(x) valid_filename(x,1)
 
 // WWW²¿·Ö
 
