c70c9de1148d5540e5aa09ca4c823a16b18d225e
bad <bad@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2003-02-26 17:29:26 +0000

    new dictionary
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@2495 de4889a5-8ad4-4bf3-bd87-57997c21d515

---
+++ kbs_bbs/service/dict.c
@@ -0,0 +1,35 @@
+#define BBSMAIN
+#include "bbs.h"
+#include "/usr/include/mysql/mysql.h"
+
+void main()
+{
+    MYSQL s;
+    MYSQL_RES* res;
+    MYSQL_ROW row;
+    int i;
+    char sql[600], word[300];
+    clear();
+    mysql_init(&s);
+    if(!mysql_real_connect(&s, "166.111.8.235", "smth", "3s4m5t9h", "ciba2k", 9527, 0, 0))
+    {
+        prints("%s\n", mysql_error(&s));
+        return;
+    }
+    good_move(2,0);
+    prints("«Î ‰»Îµ•¥ :  ");
+    sprintf(sql, "SELECT * FROM ecdict WHERE word='%s'", word);
+    if(mysql_real_query(&s, sql, strlen(sql))) {
+        prints("%s\n", mysql_error(&s));
+        return;
+    }
+    res = mysql_store_result(&s);
+    row = mysql_fetch_row(res);
+    while(row!=NULL) {
+        prints("%s\n", row[2]);
+        row = mysql_fetch_row(res);
+    }
+    i = igetkey();
+
+    mysql_close(&s);
+}
