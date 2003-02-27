e951aca6bd87616644c808c568177b0ac494e119
kcn <kcn@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2003-02-27 03:56:36 +0000

    增加处理。
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@2503 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/service/dict.c
+++ kbs_bbs/service/dict.c
@@ -16,7 +16,9 @@ int dict_main()
         pressanykey();
         return;
     }
+    while (1) {
     getdata(2, 0, "请输入单词:  ", word, 70, true, NULL, true);
+    if (word[0]==0) break;
     sprintf(sql, "SELECT * FROM ecdict WHERE word='%s'", word);
     if (mysql_real_query(&s, sql, strlen(sql))) {
         prints("%s\n", mysql_error(&s));
@@ -24,10 +26,54 @@ int dict_main()
     }
     res = mysql_store_result(&s);
     row = mysql_fetch_row(res);
+    if (row==NULL)
+        prints("没有这个词！");
+    else{
+        prints("%s\n", row[1]);
+        prints("----------------------------\n");
     while (row != NULL) {
-        prints("%s\n", row[2]);
+	char* p;
+	int insound=0;
+	for (p=row[2];*p!=0;p++) {
+            if (insound) {
+                if (*p==25) insound=0;
+		else prints("%c",*p);
+            } else
+            switch (*p) {
+            case 1: prints("n.");break;
+            case 2: prints("v.");break;
+	    case 3: prints("a.");break;
+	    case 4: prints("ad.");break;
+	    case 5: prints("vi.");break;
+	    case 6: prints("vt.");break;
+	    case 8: prints("vbl.");break;
+	    case 9: prints("int.");break;
+	    case 10: prints("art.");break;
+	    case 11: prints("aux.");break;
+	    case 12: prints("num.");break;
+	    case 13: prints("conj.");break;
+	    case 14: prints("prep.");break;
+	    case 15: prints("pron.");break;
+	    case 16: prints("st.");break;
+	    case 17: prints("adj.");break;
+	    case 18: prints("adv.");break;
+	    case 19: prints("pr.");break;
+	    case 20: prints("abbr.");break;
+            case 21: break;
+            case 22: break;
+            case 25: insound=1; break;
+            case 26: break;
+            case 30: break;
+            case 31: p++; if (*p!=0) p++; break;
+            case 59: prints("\n"); break;
+            default: prints("%c",*p);
+            }
+	}
+	prints("\n");
         row = mysql_fetch_row(res);
     }
-    i = igetkey();
+    }
+    //i = igetkey();
+    }
     mysql_close(&s);
 }
