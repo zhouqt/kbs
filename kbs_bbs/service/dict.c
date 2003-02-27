f25c717442c5944e6124fc0b406f6ceb933aa28a
kcn <kcn@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2003-02-27 01:36:11 +0000

    fix bug
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@2502 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/service/dict.c
+++ kbs_bbs/service/dict.c
@@ -1,35 +1,33 @@
 #define BBSMAIN
 #include "bbs.h"
 #include "/usr/include/mysql/mysql.h"
-
 int dict_main()
 {
     MYSQL s;
-    MYSQL_RES* res;
+    MYSQL_RES *res;
     MYSQL_ROW row;
     int i;
     char sql[600], word[300];
+
     clear();
     mysql_init(&s);
-    if(!mysql_real_connect(&s, "166.111.8.235", "smth", "3s4m5t9h", "ciba2k", 9527, 0, 0))
-    {
+    if (!mysql_real_connect(&s, "166.111.8.235", "smth", "3s4m5t9h", "ciba2k", 9527, 0, 0)) {
         prints("%s\n", mysql_error(&s));
         pressanykey();
         return;
     }
     getdata(2, 0, "«Î ‰»Îµ•¥ :  ", word, 70, true, NULL, true);
     sprintf(sql, "SELECT * FROM ecdict WHERE word='%s'", word);
-    if(mysql_real_query(&s, sql, strlen(sql))) {
+    if (mysql_real_query(&s, sql, strlen(sql))) {
         prints("%s\n", mysql_error(&s));
         return;
     }
     res = mysql_store_result(&s);
     row = mysql_fetch_row(res);
-    while(row!=NULL) {
+    while (row != NULL) {
         prints("%s\n", row[2]);
         row = mysql_fetch_row(res);
     }
     i = igetkey();
-
     mysql_close(&s);
 }
