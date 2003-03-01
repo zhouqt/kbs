0898b4612fcf475456dcba91402acd7ad5beea5f
flyriver <flyriver@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2003-03-01 04:19:31 +0000

    Added a new library function named output_ansi_html() and its helper routines.
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@2567 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/bbs2www/lib/bbslib.c
+++ kbs_bbs/bbs2www/lib/bbslib.c
@@ -2444,3 +2444,261 @@ char *unix_string(char *str)
 
     return str;
 }
+
+static void print_font_style(unsigned int style, buffered_output_t *output)
+{
+	char font_class[8];
+	char font_style[STRLEN];
+	char font_str[256];
+	unsigned int bg;
+
+	if (STYLE_ISSET(style, FONT_BG_SET))
+	{
+		bg = 8;
+	}
+	else
+		bg = STYLE_GET_BG(style);
+	sprintf(font_class, "f%01d%02d", bg, STYLE_GET_FG(style));
+	font_style[0] = '\0';
+	if (STYLE_ISSET(style, FONT_STYLE_UL))
+		strcat(font_style, "text-decoration: underline; ");
+	if (STYLE_ISSET(style, FONT_STYLE_ITALIC))
+		strcat(font_style, "font-style: italic; ");
+	if (font_style[0] != '\0')
+		sprintf(font_str, "<font class=\"%s\" style=\"%s\">", 
+				font_class, font_style);
+	else
+		sprintf(font_str, "<font class=\"%s\">", font_class);
+	output->output(font_str, strlen(font_str), output);
+}
+
+static void html_output(char *buf, size_t buflen, buffered_output_t *output)
+{
+	size_t i;
+	
+	for (i = 0; i < buflen; i++)
+	{
+		switch (buf[i])
+		{
+		case '&':
+			output->output("&amp;", 5, output);
+			break;
+		case '<':
+			output->output("&lt;", 4, output);
+			break;
+		case '>':
+			output->output("&gt;", 4, output);
+			break;
+		case ' ':
+			output->output("&nbsp;", 6, output);
+			break;
+		default:
+			output->output(&buf[i], 1, output);
+		}
+	}
+}
+
+static void print_raw_ansi(char *buf, size_t buflen, buffered_output_t *output)
+{
+	size_t i;
+	
+	for (i = 0; i < buflen; i++)
+	{
+		if (buf[i] == 0x1b)
+			html_output("*", 1, output);
+		else
+			html_output(&buf[i], 1, output);
+	}
+}
+
+static void generate_font_style(unsigned int *style, unsigned int *ansi_val,
+								size_t len)
+{
+	size_t i;
+	unsigned int color;
+
+	for (i = 0; i < len; i++)
+	{
+		if (ansi_val[i] == 0)
+			STYLE_ZERO(*style);
+		else if (ansi_val[i] == 1)
+			STYLE_SET(*style, FONT_FG_BOLD);
+		else if (ansi_val[i] == 4)
+			STYLE_SET(*style, FONT_STYLE_UL);
+		else if (ansi_val[i] == 5)
+			STYLE_SET(*style, FONT_STYLE_BLINK);
+		else if (ansi_val[i] >= 30 && ansi_val[i] <= 37)
+		{
+			if (ansi_val[i] == 30)
+				color = 7;
+			else if (ansi_val[i] == 37)
+				color = 0;
+			else
+				color = ansi_val[i] - 30;
+			STYLE_SET_FG(*style, color);
+		}
+		else if (ansi_val[i] >= 40 && ansi_val[i] <= 47)
+		{
+			/* user explicitly specify background color */
+			/* STYLE_SET(*style, FONT_BG_SET); */
+			color = ansi_val[i] - 40;
+			STYLE_SET_BG(*style, color);
+		}
+	}
+}
+
+void output_ansi_html(char *buf, size_t buflen, buffered_output_t *output)
+{
+	unsigned int font_style = 0;
+	unsigned int ansi_state;
+	unsigned int ansi_val[STRLEN];
+	int ival = 0;
+	size_t i;
+	char *ptr = buf;
+	char *ansi_begin;
+	char *ansi_end;
+	
+	if (ptr == NULL)
+		return;
+	STATE_ZERO(ansi_state);
+	bzero(ansi_val, sizeof(ansi_val));
+	for (i = 0; i < buflen; i++)
+	{
+		if (STATE_ISSET(ansi_state, STATE_NEW_LINE))
+		{
+			STATE_CLR(ansi_state, STATE_NEW_LINE);
+			if (i < (buflen - 1) && (buf[i] == ':' && buf[i+1] == ' '))
+			{
+				STATE_SET(ansi_state, STATE_QUOTE_LINE);
+				if (STATE_ISSET(ansi_state, STATE_FONT_SET))
+					output->output("</font>", 7, output);
+				/* set quoted line styles */
+				STYLE_SET(font_style, FONT_STYLE_QUOTE);
+				STYLE_SET_FG(font_style, FONT_COLOR_QUOTE);
+				STYLE_CLR_BG(font_style);
+				print_font_style(font_style, output);
+				output->output(&buf[i], 1, output);
+				STATE_SET(ansi_state, STATE_FONT_SET);
+				STATE_CLR(ansi_state, STATE_ESC_SET);
+				/* clear ansi_val[] array */
+				bzero(ansi_val, sizeof(ansi_val));
+				ival = 0;
+				continue;
+			}
+			else
+				STATE_CLR(ansi_state, STATE_QUOTE_LINE);
+		}
+		if (i < (buflen - 1) && (buf[i] == 0x1b && buf[i+1] == '['))
+		{
+			if (STATE_ISSET(ansi_state, STATE_ESC_SET))
+			{
+				/* *[*[ or *[13;24*[ */
+				size_t len;
+				ansi_end = &buf[i - 1];
+				len = ansi_end - ansi_begin + 1;
+				print_raw_ansi(ansi_begin, len, output);
+			}
+			STATE_SET(ansi_state, STATE_ESC_SET);
+			ansi_begin = &buf[i];
+			i++; /* skip the next '[' character */
+		}
+		else if (buf[i] == '\n')
+		{
+			if (STATE_ISSET(ansi_state, STATE_ESC_SET))
+			{
+				/* *[\n or *[13;24\n */
+				size_t len;
+				ansi_end = &buf[i - 1];
+				len = ansi_end - ansi_begin + 1;
+				print_raw_ansi(ansi_begin, len, output);
+				STATE_CLR(ansi_state, STATE_ESC_SET);
+			}
+			if (STATE_ISSET(ansi_state, STATE_QUOTE_LINE))
+			{
+				/* end of a quoted line */
+				output->output("</font>", 7, output);
+				STYLE_CLR(font_style, FONT_STYLE_QUOTE);
+				STATE_CLR(ansi_state, STATE_FONT_SET);
+			}
+			output->output("<br />\n", 7, output);
+			STATE_CLR(ansi_state, STATE_QUOTE_LINE);
+			STATE_SET(ansi_state, STATE_NEW_LINE);
+		}
+		else
+		{
+			if (STATE_ISSET(ansi_state, STATE_ESC_SET))
+			{
+				if (buf[i] == 'm')
+				{
+					/* *[0;1;4;31m */
+					if (STATE_ISSET(ansi_state, STATE_FONT_SET))
+					{
+						output->output("</font>", 7, output);
+						STATE_CLR(ansi_state, STATE_FONT_SET);
+					}
+					if (i < buflen - 1)
+					{
+						generate_font_style(&font_style, ansi_val, ival+1);
+						if (STATE_ISSET(ansi_state, STATE_QUOTE_LINE))
+							STYLE_SET(font_style, FONT_STYLE_QUOTE);
+						print_font_style(font_style, output);
+						STATE_SET(ansi_state, STATE_FONT_SET);
+						STATE_CLR(ansi_state, STATE_ESC_SET);
+						/*STYLE_ZERO(font_style);*/
+						/* clear ansi_val[] array */
+						bzero(ansi_val, sizeof(ansi_val));
+						ival = 0;
+					}
+				}
+				else if (isalpha(buf[i]))
+				{
+					/* *[23;32H */
+					/* ignore it */
+					STATE_CLR(ansi_state, STATE_ESC_SET);
+					STYLE_ZERO(font_style);
+					/* clear ansi_val[] array */
+					bzero(ansi_val, sizeof(ansi_val));
+					ival = 0;
+					continue;
+				}
+				else if (buf[i] == ';')
+				{
+					if (ival < sizeof(ansi_val) - 1)
+					{
+						ival ++; /* go to next ansi_val[] element */
+						ansi_val[ival] = 0;
+					}
+				}
+				else if (buf[i] >= '0' && buf[i] <= '9')
+				{
+					ansi_val[ival] *= 10;
+					ansi_val[ival] += (buf[i] - '0');
+				}
+				else
+				{
+					/* *[1;32/XXXX or *[* or *[[ */
+					/* not a valid ANSI string, just output it */
+					size_t len;
+					
+					ansi_end = &buf[i];
+					len = ansi_end - ansi_begin + 1;
+					print_raw_ansi(ansi_begin, len, output);
+					STATE_CLR(ansi_state, STATE_ESC_SET);
+					/* clear ansi_val[] array */
+					bzero(ansi_val, sizeof(ansi_val));
+					ival = 0;
+				}
+				
+			}
+			else
+				print_raw_ansi(&buf[i], 1, output);
+		}
+	}
+	if (STATE_ISSET(ansi_state, STATE_FONT_SET))
+	{
+		output->output("</font>", 7, output);
+		STATE_CLR(ansi_state, STATE_FONT_SET);
+	}
+	flush_buffer(output);
+}
+
