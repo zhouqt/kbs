226a6b1c30fa922068599548efc6981e8a0d7c95
czz <czz@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2003-03-02 13:16:57 +0000

    test
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@2587 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/local_utl/passf30k2s.c
+++ kbs_bbs/local_utl/passf30k2s.c
@@ -1,5 +1,5 @@
 /*
- * This program converts .PASSWDS file from Firebird 3.0K beta6 to SMTH_BBS.
+ * This program converts .PASSWDS file from Firebird 3.0K beta6 to SMTH_BBS
  */
 
 #include "bbs.h"
