3a79c31a1cca795a20bdd86f36d6c6f4cc160d16
kcn <kcn@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2003-03-04 15:12:42 +0000

    用自己的mime判断
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@2654 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/bbs2www/html/bbscon.php
+++ kbs_bbs/bbs2www/html/bbscon.php
@@ -4,6 +4,49 @@
 	 * $Id$
 	 */
 	require("funcs.php");
+function get_mimetype($name)
+{
+	$dot = strrchr($name, '.');
+	if ($dot == $name)
+		return "text/plain; charset=gb2312";
+	if (strcasecmp($dot, ".html") == 0 || strcasecmp($dot, ".htm") == 0)
+		return "text/html; charset=gb2312";
+	if (strcasecmp($dot, ".jpg") == 0 || strcasecmp($dot, ".jpeg") == 0)
+		return "image/jpeg";
+	if (strcasecmp($dot, ".gif") == 0)
+		return "image/gif";
+	if (strcasecmp($dot, ".png") == 0)
+		return "image/png";
+	if (strcasecmp($dot, ".pcx") == 0)
+		return "image/pcx";
+	if (strcasecmp($dot, ".css") == 0)
+		return "text/css";
+	if (strcasecmp($dot, ".au") == 0)
+		return "audio/basic";
+	if (strcasecmp($dot, ".wav") == 0)
+		return "audio/wav";
+	if (strcasecmp($dot, ".avi") == 0)
+		return "video/x-msvideo";
+	if (strcasecmp($dot, ".mov") == 0 || strcasecmp($dot, ".qt") == 0)
+		return "video/quicktime";
+	if (strcasecmp($dot, ".mpeg") == 0 || strcasecmp($dot, ".mpe") == 0)
+		return "video/mpeg";
+	if (strcasecmp($dot, ".vrml") == 0 || strcasecmp($dot, ".wrl") == 0)
+		return "model/vrml";
+	if (strcasecmp($dot, ".midi") == 0 || strcasecmp($dot, ".mid") == 0)
+		return "audio/midi";
+	if (strcasecmp($dot, ".mp3") == 0)
+		return "audio/mpeg";
+	if (strcasecmp($dot, ".pac") == 0)
+		return "application/x-ns-proxy-autoconfig";
+	if (strcasecmp($dot, ".txt") == 0)
+		return "text/plain; charset=gb2312";
+	if (strcasecmp($dot, ".xht") == 0 || strcasecmp($dot, ".xhtml") == 0)
+		return "application/xhtml+xml";
+	if (strcasecmp($dot, ".xml") == 0)
+		return "text/xml";
+	return "application/octet-stream";
+}
 	if ($loginok != 1)
 		html_nologin();
 	else
@@ -65,9 +108,7 @@
 				$str=fread($file,4);
 				$array=unpack('Nsize',$str);
 				$attachsize=$array["size"];
-				@$mimetype=mime_content_type($attachname);
-				if ($mimetype!='')
-					Header("Content-type: " . $mimetype);
+				Header("Content-type: " . get_mimetype($attachname));
 				Header("Accept-Ranges: bytes");
 				Header("Accept-Length: " . $attachsize);
 				Header("Content-Disposition: attachment; filename=" . $attachname);
