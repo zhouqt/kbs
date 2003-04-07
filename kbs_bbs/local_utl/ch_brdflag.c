6144c7c4785e5a422a18de41d423bbe894d4a684
flyriver <flyriver@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2003-04-07 15:41:16 +0000

    Added ch_brdflag.c program to reposity.
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@2933 de4889a5-8ad4-4bf3-bd87-57997c21d515

---
+++ kbs_bbs/local_utl/ch_brdflag.c
@@ -0,0 +1,38 @@
+#include "bbs.h"
+
+#undef printf
+#undef perror
+#undef time
+#undef fprintf
+
+int main(int argc, char **argv)
+{
+	struct boardheader fh, newfh;
+	int pos;
+
+	if (argc != 3)
+	{
+		fprintf(stderr, "Usage: %s --force <board>\n", argv[0]);
+		exit(-1);
+	}
+	if (strcmp(argv[1], "--force"))
+	{
+		fprintf(stderr, "Usage: %s --force <board>\n", argv[0]);
+		exit(-1);
+	}
+	chdir(BBSHOME);
+	resolve_boards();
+
+    pos = getboardnum(argv[2], &fh);
+	if (pos == 0)
+	{
+		fprintf(stderr, "%s not found.\n", argv[2]);
+		exit(-1);
+	}
+	memcpy(&newfh, &fh, sizeof(newfh));
+	newfh.flag |= BOARD_SUPER_CLUB;
+	set_board(pos, &newfh, &fh);
+
+	return 0;
+}
+
