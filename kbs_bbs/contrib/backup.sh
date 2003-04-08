108b5e55edab5ba0f9c5f2c8e5f526a5559e49fb
czz <czz@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2003-04-08 13:18:24 +0000

    考虑不周
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@2942 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/contrib/backup.sh
+++ kbs_bbs/contrib/backup.sh
@@ -6,12 +6,16 @@ BBSHOME=/home/bbs
 mkdir -p $BACKUPHOME
 cd $BBSHOME/0Announce/groups
 for i in `for i in $(grep Path .Names); do echo ${i##*/}; done`; do
-	cd $i
-	for j in `for j in $(grep Path .Names); do echo ${j##*/}; done`; do
-		echo "Backup 0Announce/$i/$j..."
-		tar -zcf $BACKUPHOME/$j.tar.gz $j
-	done
-	cd ..
+	if [ -d "$i" ]; then
+		cd $i
+		for j in `for j in $(grep Path .Names); do echo ${j##*/}; done`; do
+			if [ -d "$j" ]; then
+				echo "Backup 0Announce/$i/$j..."
+				tar -zcf $BACKUPHOME/$j.tar.gz $j
+			fi
+		done
+		cd ..
+	fi
 done
 
-#find $BACKUPHOME -maxdepth 1 -mtime +30 | xargs rm -rf
+find $BACKUPHOME -maxdepth 1 -mtime +30 | xargs rm -rf
