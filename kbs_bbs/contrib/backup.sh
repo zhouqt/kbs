a3396676b9f4e90f734ec37b3c12a98a895fd679
czz <czz@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2003-04-08 17:34:43 +0000

    autoftp
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@2948 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/contrib/backup.sh
+++ kbs_bbs/contrib/backup.sh
@@ -3,19 +3,34 @@ DAY=`date +%Y-%m-%d`
 BACKUPHOME=/backup/bbs/Announce/$DAY
 BBSHOME=/home/bbs
 
-mkdir -p $BACKUPHOME
-cd $BBSHOME/0Announce/groups
-for i in `for i in $(grep Path .Names); do echo ${i##*/}; done`; do
-	if [ -d "$i" ]; then
-		cd $i
-		for j in `for j in $(grep Path .Names); do echo ${j##*/}; done`; do
-			if [ -d "$j" ]; then
-				echo "Backup 0Announce/$i/$j..."
-				tar -zcf $BACKUPHOME/$j.tar.gz $j
-			fi
-		done
-		cd ..
-	fi
-done
+if [ ! -d "$BACKUPHOME" ]; then
+	mkdir -p $BACKUPHOME
+	cd $BBSHOME/0Announce/groups
+	for i in `for i in $(grep Path .Names); do echo ${i##*/}; done`; do
+		if [ -d "$i" ]; then
+			cd $i
+			for j in `for j in $(grep Path .Names); do echo ${j##*/}; done`; do
+				if [ -d "$j" ]; then
+					echo "Backup 0Announce/$i/$j..."
+					tar -zcf $BACKUPHOME/$j.tar.gz $j
+				fi
+			done
+			cd ..
+		fi
+	done
 
-find $BACKUPHOME -maxdepth 1 -mtime +30 | xargs rm -rf
+	find ${BACKUPHOME%/*} -maxdepth 1 -mtime +30 | xargs rm -rf
+fi
+
+cd $BACKUPHOME
+ftp -n <<end
+open $1
+quote user $2
+quote pass $3
+mkdir $DAY
+cd $DAY
+bin
+prompt
+mput *
+quit
+end
