60662fb5d3a44f593e32119c0fb80579027ea424
czz <czz@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2003-04-08 12:45:18 +0000

    改改日期格式
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@2940 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/contrib/backup.sh
+++ kbs_bbs/contrib/backup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-DAY=`date +%A`
-BACKUPHOME=/backup/bbs/Announce/`date +%A`
+DAY=`date date +%Y-%m-%d`
+BACKUPHOME=/backup/bbs/Announce/$DAY
 BBSHOME=/home/bbs
 
 mkdir -p $BACKUPHOME
@@ -8,7 +8,7 @@ cd $BBSHOME/0Announce/groups
 for i in `for i in $(grep Path .Names); do echo ${i##*/}; done`; do
 	cd $i
 	for j in `for j in $(grep Path .Names); do echo ${j##*/}; done`; do
-		echo -n "Backup 0Announce/$i/$j..."
+		echo "Backup 0Announce/$i/$j..."
 		tar -zcf $BACKUPHOME/$j.tar.gz $j
 	done
 	cd ..
