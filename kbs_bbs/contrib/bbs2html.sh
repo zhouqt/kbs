9ac0acd6348b5ac8acbd90d8a6665ce78089d194
czz <czz@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2003-04-10 00:38:02 +0000

    好像没改什么
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@2956 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/contrib/bbs2html.sh
+++ kbs_bbs/contrib/bbs2html.sh
@@ -1,19 +1,21 @@
 #!/bin/sh
 DAY=`date +%Y-%m-%d`
 SOURCE=/backup/Announce/$DAY
+EXTRACT=/backup/temp
 BBSHOME=/home/bbs
 DEST=$BBSHOME/announce/$DAY
 TEMP=$BBSHOME/temp
 
 if [ ! -d "$DEST" ]; then
 	mkdir -p $DEST
-	cd $TEMP
-	for i in $SOURCE/*.tar.gz; do
-		gzip -dc $i | tar -xf -
-		$BBSHOME/bin/Xhtml -o $DEST/ -w $TEMP/ $SOURCE/${i%%.*}
-		rm -rf $TEMP/${i%%.*}
+	cd $SOURCE
+	for i in *.tar.gz; do
+		cd $EXTRACT
+		gzip -dc $SOURCE/$i | tar -xf -
+		$BBSHOME/bin/Xhtml -o $DEST/ -w $TEMP/ $EXTRACT/${i%%.*}
+		rm -rf $EXTRACT/${i%%.*}
 		rm -f $DEST/${i%%.*}.bbs.tgz
 	done
 fi
 
-find ${DEST%/*} -maxdepth 1 -mtime +30 | xargs rm -rf
+find ${DEST%/*} -mtime +30 | xargs rm -rf
