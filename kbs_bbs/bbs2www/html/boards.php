10872768b381a8af97095ad082f3ba1fad13e7cf
flyriver <flyriver@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2003-04-13 03:57:19 +0000

    Added a new PHP library file for operating boards.
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@3006 de4889a5-8ad4-4bf3-bd87-57997c21d515

---
+++ kbs_bbs/bbs2www/html/boards.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ * This source file is part of SMTH BBS PHP Library Project.
+ * Copyright (c) 2002-2003 SMTH BBS
+ * 
+ * This file contains PHP library functions for operating boards,
+ * such as checking read-only board, checking anonymouse, etc.
+ * 
+ * $Id$
+ */
+if (!define('_BBS_BOARDS_PHP_'))
+{
+define('_BBS_BOARDS_PHP_', 1);
+
+require("funcs.php");
+
+/**
+ * Checking whether a board is set with some specific flags or not.
+ * 
+ * @param $board the board object to be checked
+ * @param $flag the flags to check
+ * @return TRUE  the board is set with the flags
+ *         FALSE the board is not set with the flags
+ * @author flyriver
+ */
+function bbs_check_board_flag($board,$flag)
+{
+	if ($board["FLAG"] & $flag)
+		return TRUE;
+	else
+		return FALSE;
+}
+
+/**
+ * Checking whether a board is an anonymous board or not.
+ * 
+ * @param $board the board object to be checked
+ * @return TRUE  the board is an anonymous board
+ *         FALSE the board is not an anonymous board
+ * @author flyriver
+ */
+function bbs_is_anony_board($board)
+{
+	return bbs_check_board_flag($board, $BOARD_FLAGS["ANONY"]);
+}
+
+/**
+ * Checking whether a board is an outgo board or not.
+ * 
+ * @param $board the board object to be checked
+ * @return TRUE  the board is an outgo board
+ *         FALSE the board is not an outgo board
+ * @author flyriver
+ */
+function bbs_is_outgo_board($board)
+{
+	return bbs_check_board_flag($board, $BOARD_FLAGS["OUTGO"]);
+}
+
+/**
+ * Checking whether a board is a junk board or not.
+ * 
+ * @param $board the board object to be checked
+ * @return TRUE  the board is a junk board
+ *         FALSE the board is not a junk board
+ * @author flyriver
+ */
+function bbs_is_junk_board($board)
+{
+	return bbs_check_board_flag($board, $BOARD_FLAGS["JUNK"]);
+}
+
+/**
+ * Checking whether a board is an attachment board or not.
+ * 
+ * @param $board the board object to be checked
+ * @return TRUE  the board is an attachment board
+ *         FALSE the board is not an attachment board
+ * @author flyriver
+ */
+function bbs_is_attach_board($board)
+{
+	return bbs_check_board_flag($board, $BOARD_FLAGS["ATTACH"]);
+}
+
+/**
+ * Checking whether a board is a readonly board or not.
+ * 
+ * @param $board the board object to be checked
+ * @return TRUE  the board is a readnoly board
+ *         FALSE the board is not a readonly board
+ * @author flyriver
+ */
+function bbs_is_readonly_board($board)
+{
+	return bbs_check_board_flag($board, $BOARD_FLAGS["READONLY"]);
+}
+
+} // !define ('_BBS_BOARDS_PHP_')
+?>
