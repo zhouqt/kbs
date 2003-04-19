bc6765b6b5ca25b4b950093a66a51e135df9bd54
czz <czz@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2003-04-19 02:59:54 +0000

    用标准的
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@3147 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- innbbsd/clibrary.h
+++ innbbsd/clibrary.h
@@ -136,7 +136,7 @@ extern long atol();
 extern time_t time();
 extern unsigned long inet_addr();
 extern FREEVAL free();
-extern POINTER malloc();
+extern void *malloc();
 extern POINTER realloc();
 
 #if	defined(ACT_MMAP)
