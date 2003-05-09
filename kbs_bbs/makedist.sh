3eae3129057052dbbaf2d334af77edf6c12fc210
kcn <kcn@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2003-05-09 03:26:27 +0000

    加入对dist目录生成的支持
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@3473 de4889a5-8ad4-4bf3-bd87-57997c21d515

---
+++ kbs_bbs/makedist.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+cpfile() {
+if [ ! -f $1/$2 ]; then
+  /bin/cp  $2 $1/$2
+fi
+}
+
+md() {
+if [ ! -d $1/$2 ]; then
+  mkdir $1/$2
+fi
+}
+
+cd bbshome
+for i in `find . -type d`; do
+md $1 $i
+done
+for i in `find . -type f`; do
+cpfile $1 $i
+done
+md $1/home
+md $1/mail
+for i in A B C D E F G H I J K L M N O P R S T U V W X Y Z; do
+md $1/home $i
+md $1/mail $i
+done
