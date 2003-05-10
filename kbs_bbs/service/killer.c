b703dd575e08b0bf7cb95ed0c84c1ad70702c076
bad <bad@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2003-05-10 07:17:04 +0000

    改一下select界面
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@3514 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/service/killer.c
+++ kbs_bbs/service/killer.c
@@ -1325,7 +1325,7 @@ static int room_list_refresh(struct _select_def *conf)
     docmdtitle("[ÓÎÏ·ÊÒÑ¡µ¥]",
               "  ÍË³ö[\x1b[1;32m¡û\x1b[0;37m,\x1b[1;32me\x1b[0;37m] ½øÈë[\x1b[1;32mEnter\x1b[0;37m] Ñ¡Ôñ[\x1b[1;32m¡ü\x1b[0;37m,\x1b[1;32m¡ý\x1b[0;37m] Ìí¼Ó[\x1b[1;32ma\x1b[0;37m] ¼ÓÈë[\x1b[1;32mJ\x1b[0;37m] \x1b[m");
     move(2, 0);
-    prints("[0;1;37;44m    %4s %-14s %-12s %4s %4s %4s %4s %-20s", "±àºÅ", "ÓÎÏ·ÊÒÃû³Æ", "´´½¨Õß", "ÀàÐÍ", "ÈËÊý", "×î¶à", "Ëø¶¨", "»°Ìâ");
+    prints("[0;1;37;44m    %4s %-14s %-12s %4s %4s %6s %-20s", "±àºÅ", "ÓÎÏ·ÊÒÃû³Æ", "´´½¨Õß", "ÈËÊý", "×î¶à", "ËøÒþÅÔ", "»°Ìâ");
     clrtoeol();
     resetcolor();
     update_endline();
@@ -1338,7 +1338,7 @@ static int room_list_show(struct _select_def *conf, int i)
     int j = room_get(i-1);
     if(j!=-1) {
         r=rooms+j;
-        prints("  %3d  %-14s %-12s %4s %3d  %3d   %2s  %-20s", i, r->name, r->creator, "É±ÈË", r->people, r->maxpeople, (r->flag&ROOM_LOCKED)?"ÊÇ":"·ñ", r->title);
+        prints("  %3d  %-14s %-12s %3d  %3d  %2s%2s%2s %-20s", i, r->name, r->creator, r->people, r->maxpeople, (r->flag&ROOM_LOCKED)?"¡ñ":"", (r->flag&ROOM_SECRET)?"¡ñ":"", (!(r->flag&ROOM_DENYSPEC))?"¡ñ":"", r->title);
     }
     return SHOW_CONTINUE;
 }
