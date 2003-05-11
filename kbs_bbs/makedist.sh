7c026d394a7bed5af933d770c6a4f12f77f6aa5c
kcn <kcn@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2003-05-11 06:28:59 +0000

    加入makedist.sh到dist中
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@3550 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/makedist.sh
+++ kbs_bbs/makedist.sh
@@ -24,3 +24,13 @@ for i in A B C D E F G H I J K L M N O P R S T U V W X Y Z; do
 md $1/home $i
 md $1/mail $i
 done
+md $1/vote
+md $1/boards
+md $1/boards/_attach
+md $1/tmp
+md $1/tmp/forward
+md $1/tmp/bbs-gsend
+md $1/tmp/email
+md $1/tmp/clip
+md $1/tmp/bbs-internet-gw
+md $1/var
