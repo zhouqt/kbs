f1f89de197309e6d6b6d42f61d3355b370d8d11d
binxun <binxun@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2003-05-14 10:28:56 +0000

    用户注册php
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@3674 de4889a5-8ad4-4bf3-bd87-57997c21d515

---
+++ kbs_bbs/bbs2www/html/bbsreg.php
@@ -0,0 +1,54 @@
+<?php
+	/**
+	 * This file registry a new id, work with bbsreg.html
+	 * by binxun 2003.5
+	 */
+	require("funcs.php");
+    html_init("gb2312");
+
+	@$userid=$_POST["userid"];
+	@$pass1=$_POST["pass1"];
+    @$pass2=$_POST["pass2"];
+	@$nickname=$_POST["username"];
+
+	if(strcmp($pass1,$pass2))
+        html_error_quit("两次密码输入不一样");
+    else if(strlen($pass1) < 5 || !strcmp($pass1,$userid))
+        html_error_quit("密码长度太短或者和用户名相同!");
+
+    $ret=bbs_createnewid($userid,$pass1,$username);
+	switch($ret)
+	{
+	case 0:
+			break;
+	case 1:
+			html_error_quit("用户名有非数字字母字符或者首字符不是字母!");
+			break;
+	case 2:
+			html_error_quit("用户名至少为两个字母!");
+			break;
+	case 3:
+			html_error_quit("系统用字或不雅用字!");
+			break;
+	case 4:
+			html_error_quit("该用户名已经被使用!");
+			break;
+	case 5:
+			html_error_quit("用户名太长,最长12个字符!");
+			break;
+	case 6:
+			html_error_quit("密码太长,最长39个字符!");
+			break;
+	case 10:
+			html_error_quit("系统错误,请与系统管理员SYSOP联系.");
+			break;
+	default:
+			html_error_quit("未知的错误!");
+			break;
+	}
+?>
+<body>
+申请BBS水木清华ID成功,现在还没有通过身份认证,只有最基本的权限,不能发文,发信,聊天等,请两天后到个人工具箱中填写注册单.<br>
+注册单通过审核后,你将获得合法用户权限！<br/><a href="http://www.smth.edu.cn">现在登录进站</a>
+</body>
+</html>
