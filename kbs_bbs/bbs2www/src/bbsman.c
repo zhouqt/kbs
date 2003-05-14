7e3fe7e6dc973ecbf461d2973f4fc3892f80ea92
stiger <stiger@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2003-05-14 08:30:58 +0000

    收录精华
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@3661 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/bbs2www/src/bbsman.c
+++ kbs_bbs/bbs2www/src/bbsman.c
@@ -5,10 +5,11 @@
 
 char genbuf[1024];
 char currfile[STRLEN];
+char im_path[MAXPATH];
 
 int main()
 {
-    int i, total = 0, mode;
+    int i, total = 0, mode,num;
     char board[80], *ptr;
     char buf[STRLEN];
     bcache_t *brd;
@@ -24,8 +25,22 @@ int main()
     strcpy(board, brd->filename);
     if (!has_BM_perm(currentuser, board))
         http_fatal("你无权访问本页");
-    if (mode <= 0 || mode > 5)
+    if (mode <= 0 || mode > 6)
         http_fatal("错误的参数");
+	if( mode == 6 ){
+		num = atoi(getparm("num"));
+		if( num < 0 || num >= ANNPATH_NUM )
+			http_fatal("丝路参数错误");
+		char *i_path[ANNPATH_NUM];
+		char *i_title[ANNPATH_NUM];
+		int i_time = 0;
+		int i_select = 0;
+		load_import_path(i_path,i_title,&i_time,&i_select);
+		strcpy(im_path,i_path[num]);
+		free_import_path(i_path,i_title,&i_time);
+		if(strncmp(im_path,"0Announce/groups/",17))
+			http_fatal("丝路错误");
+	}
     printf("<table>");
     for (i = 0; i < parm_num && i < 40; i++) {
         if (!strncmp(parm_name[i], "box", 3)) {
@@ -40,6 +55,8 @@ int main()
                 do_set(board, atoi(parm_name[i] + 3), FILE_NOREPLY_FLAG);
             if (mode == 5)
                 do_set(board, atoi(parm_name[i] + 3), FILE_DING_FLAG);
+			if (mode == 6)
+				do_import(board,atoi(parm_name[i]+3));
         }
        	else if (!strncmp(parm_name[i], "boz", 3)) {
             total++;
@@ -53,6 +70,8 @@ int main()
                 do_set_zd(board, atoi(parm_name[i] + 3), FILE_NOREPLY_FLAG);
             if (mode == 5)
                 do_del_zd(board, atoi(parm_name[i] + 3));
+			if (mode == 6)
+				do_import(board,atoi(parm_name[i]+3));
         }
     }
     printf("</table>");
@@ -85,8 +104,63 @@ int do_del(char *board, int id)
 	}
 	return;
     }
-
+	close(fd);
     printf("<tr><td></td><td></td><td>文件不存在.</td></tr>\n");
+}
+
+int bbsman_import(char *board,struct fileheader *f)
+{
+	MENU pm;
+	char buf[PATHLEN],bname[PATHLEN];
+	char fname[STRLEN];
+	int ret;
+	int i;
+
+	bzero(&pm, sizeof(pm));
+	pm.path = im_path;
+	a_loadnames(&pm);
+
+	ann_get_postfilename(fname,f,&pm);
+	sprintf(bname,"%s/%s",pm.path,fname);
+	sprintf(buf,"%-38.38s %s",f->title,currentuser->userid);
+	a_additem(&pm,buf,fname,NULL,0,f->attachment);
+	if(a_savenames(&pm) == 0){
+		sprintf(buf,"boards/%s/%s",board,f->filename);
+		f_cp(buf,bname,0);
+		/* change flag */
+		ret = 1;
+	}else 
+		ret = 0;
+
+	for(i=0;i<pm.num;i++)
+		free(pm.item[i]);
+	return ret;
+}
+
+
+int do_import(char *board, int id)
+{
+	struct fileheader f;
+	int ent;
+	int fd;
+	char dir[256];
+
+	sprintf(dir,"boards/%s/.DIR",board);
+	fd = open(dir,O_RDWR,0644);
+	if(fd < 0)
+		http_fatal("打开文件错误");
+	if(get_records_from_id(fd,id,&f,1,&ent)){
+		close(fd);
+		if(bbsman_import(board,&f))
+            printf("<tr><td>%s  </td><td>标题:%s </td><td>收录成功.</td></tr>\n", f.owner, nohtml(f.title));
+		else
+            printf("<tr><td>%s  </td><td>标题:%s </td><td>收录不成功.</td></tr>\n", f.owner, nohtml(f.title));
+	}
+	else{
+		close(fd);
+    	printf("<tr><td></td><td></td><td>文件不存在.</td></tr>\n");
+	}
+
 }
 
 int do_del_zd(char *board, int id)
