b3b29435a0d267c2982136dd87f228f428503119
bad <bad@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2003-05-14 05:23:47 +0000

    可以下载查询结果
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@3652 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/service/dict.c
+++ kbs_bbs/service/dict.c
@@ -4,15 +4,98 @@
 #if HAVE_MYSQL == 1
 #include <mysql.h>
 
+void print_res(char * r)
+{
+    char * p;
+    for (p=r;*p!=0;p++) {
+        if (insound) {
+            if (*p==25) insound=0;
+//		else prints("%c",*p);
+        } else
+        switch (*p) {
+            case 1: prints("n.");break;
+            case 2: prints("v.");break;
+            case 3: prints("a.");break;
+            case 4: prints("ad.");break;
+            case 5: prints("vi.");break;
+            case 6: prints("vt.");break;
+            case 8: prints("vbl.");break;
+            case 9: prints("int.");break;
+            case 10: prints("art.");break;
+            case 11: prints("aux.");break;
+            case 12: prints("num.");break;
+            case 13: prints("conj.");break;
+            case 14: prints("prep.");break;
+            case 15: prints("pron.");break;
+            case 16: prints("st.");break;
+            case 17: prints("adj.");break;
+            case 18: prints("adv.");break;
+            case 19: prints("pr.");break;
+            case 20: prints("abbr.");break;
+            case 21: break;
+            case 22: break;
+            case 25: insound=1; break;
+            case 26: break;
+            case 30: break;
+            case 31: p++; p++; break;
+            case 59: prints("\n"); break;
+            default: prints("%c",*p);
+        }
+    }
+}
+
+void prinf_res(FILE* fp, char * r)
+{
+    char * p;
+    for (p=r;*p!=0;p++) {
+        if (insound) {
+            if (*p==25) insound=0;
+        } else
+        switch (*p) {
+            case 1: fprintf(fp, "n.");break;
+            case 2: fprintf(fp, "v.");break;
+            case 3: fprintf(fp, "a.");break;
+            case 4: fprintf(fp, "ad.");break;
+            case 5: fprintf(fp, "vi.");break;
+            case 6: fprintf(fp, "vt.");break;
+            case 8: fprintf(fp, "vbl.");break;
+            case 9: fprintf(fp, "int.");break;
+            case 10: fprintf(fp, "art.");break;
+            case 11: fprintf(fp, "aux.");break;
+            case 12: fprintf(fp, "num.");break;
+            case 13: fprintf(fp, "conj.");break;
+            case 14: fprintf(fp, "prep.");break;
+            case 15: fprintf(fp, "pron.");break;
+            case 16: fprintf(fp, "st.");break;
+            case 17: fprintf(fp, "adj.");break;
+            case 18: fprintf(fp, "adv.");break;
+            case 19: fprintf(fp, "pr.");break;
+            case 20: fprintf(fp, "abbr.");break;
+            case 21: break;
+            case 22: break;
+            case 25: insound=1; break;
+            case 26: break;
+            case 30: break;
+            case 31: p++; p++; break;
+            case 59: fprintf(fp, "\n"); break;
+            default: fprintf(fp, "%c",*p);
+        }
+    }
+}
+
 int dict_main()
 {
     MYSQL s;
     MYSQL_RES *res;
     MYSQL_ROW row;
+    int oldmode;
     int i;
     char sql[600], word[300];
+    FILE* fp;
+    char fn[80];
 
-    scrint=false;
+    oldmode = uinfo.mode;
+    modify_user_mode(DICT);
     clear();
     mysql_init(&s);
     if (!mysql_real_connect(&s, 
@@ -23,89 +106,104 @@ int dict_main()
 			    sysconf_eval("MYSQLPORT",1521), sysconf_str("MYSQLSOCKET"), 0)) {
         prints("%s\n", mysql_error(&s));
         pressanykey();
-        scrint=true;
-	redoscr();
+        modify_user_mode(oldmode);
         return;
     }
     while (1) {
-    char table[8];
-    move(2,0);
-    getdata(2, 0, "请输入单词:  ", word, 70, true, NULL, true);
-    clear();
-    move(3,0);
-    if (word[0]==0) break;
-    if (strchr(word,'\'')) break;
-    if (word[0]&0x80)
-        strcpy(table, "cedict");
-    else
-        strcpy(table, "ecdict");
-#ifdef SMTH
-    if (!HAS_PERM(currentuser,PERM_SYSOP))
-        sprintf(sql, "SELECT * FROM %s WHERE word='%s' and dictid=0", table, word);
-    else
-#endif
-    sprintf(sql, "SELECT * FROM %s WHERE word='%s'", table, word);
-    if (mysql_real_query(&s, sql, strlen(sql))) {
-        prints("%s\n", mysql_error(&s));
-        pressanykey();
-        return;
-    }
-    res = mysql_store_result(&s);
-    row = mysql_fetch_row(res);
-    if (row==NULL)
-        prints("没有这个词！");
-    else{
-        prints("\x1b[1;32;4m%s\x1b[m\n", row[1]);
-	strcpy(word,row[1]);
-    while (row != NULL) {
-	char* p;
-	int insound=0;
-	if (strcmp(word,row[1]))
-        prints("\x1b[1;32;4m%s\x1b[m\n", row[1]);
-	for (p=row[2];*p!=0;p++) {
-            if (insound) {
-                if (*p==25) insound=0;
-//		else prints("%c",*p);
-            } else
-            switch (*p) {
-            case 1: prints("n.");break;
-            case 2: prints("v.");break;
-	    case 3: prints("a.");break;
-	    case 4: prints("ad.");break;
-	    case 5: prints("vi.");break;
-	    case 6: prints("vt.");break;
-	    case 8: prints("vbl.");break;
-	    case 9: prints("int.");break;
-	    case 10: prints("art.");break;
-	    case 11: prints("aux.");break;
-	    case 12: prints("num.");break;
-	    case 13: prints("conj.");break;
-	    case 14: prints("prep.");break;
-	    case 15: prints("pron.");break;
-	    case 16: prints("st.");break;
-	    case 17: prints("adj.");break;
-	    case 18: prints("adv.");break;
-	    case 19: prints("pr.");break;
-	    case 20: prints("abbr.");break;
-            case 21: break;
-            case 22: break;
-            case 25: insound=1; break;
-            case 26: break;
-            case 30: break;
-            case 31: p++; p++; break;
-            case 59: prints("\n"); break;
-            default: prints("%c",*p);
+        char table[8];
+        int len=0;
+        move(2,0);
+        word[0]=0;
+        while(1) {
+            char ch;
+            move(2,0);
+            prints("请输入单词:  %s", word);
+            clrtoeol();
+            ch = igetkey();
+            clear();
+            if(ch==13||ch==10) break;
+            else if((ch==KEY_DEL||ch==8)&&len) {
+                len--; word[len]=0;
             }
-	}
-	prints("\n");
+            else if(ch==KEY_TAB&&len) {
+            }
+            else if(isprint2(ch)) {
+                word[len]=ch;
+                len++;
+                word[len]=0;
+            }
+
+            if(word[0]) {
+                int i=0;
+                if (word[0]&0x80)
+                    strcpy(table, "cedict");
+                else
+                    strcpy(table, "ecdict");
+                sprintf(sql, "SELECT * FROM %s WHERE word like '%s*' and dictid=0", table, word);
+                if (mysql_real_query(&s, sql, strlen(sql))) {
+                    prints("%s\n", mysql_error(&s));
+                    pressanykey();
+                    return;
+                }
+                res = mysql_store_result(&s);
+                row = mysql_fetch_row(res);
+                while(row!=NULL) {
+                    row = mysql_fetch_row(res);
+                    move(3+i, 0);
+                    prints("%s ", row[1]);
+                    print_res(row[2]);
+                    i++;
+                    if(i>=20) break;
+                }
+            }
+        }
+        if (word[0]==0) break;
+        if (strchr(word,'\'')) break;
+        if (word[0]&0x80)
+            strcpy(table, "cedict");
+        else
+            strcpy(table, "ecdict");
+#ifdef SMTH
+        if (!HAS_PERM(currentuser,PERM_SYSOP))
+            sprintf(sql, "SELECT * FROM %s WHERE word='%s' and dictid=0", table, word);
+        else
+#endif
+        sprintf(sql, "SELECT * FROM %s WHERE word='%s'", table, word);
+        if (mysql_real_query(&s, sql, strlen(sql))) {
+            prints("%s\n", mysql_error(&s));
+            pressanykey();
+            return;
+        }
+        res = mysql_store_result(&s);
         row = mysql_fetch_row(res);
-    }
-    }
+        if (row==NULL) {
+            clear();
+            move(3, 0);
+            prints("没有这个词！");
+        }
+        else{
+            char title[80];
+            sprintf(title, "单词%s的含义", word);
+            sprintf(fn, "tmp/%s.%d.dict", currentuser->userid, rand());
+            fp = fopen(fn, "w");
+            fprintf(fp, "\x1b[1;32;4m%s\x1b[m\n", row[1]);
+            strcpy(word,row[1]);
+           while (row != NULL) {
+            	char* p;
+            	int insound=0;
+            	if (strcmp(word,row[1]))
+                    fprintf(fp, "\x1b[1;32;4m%s\x1b[m\n", row[1]);
+            	prinf_res(fp, row[2]);
+              fprintf(fp, "\n");
+              row = mysql_fetch_row(res);
+            }
+           fclose(fp);
+           ansimore_withzmodem(fn, true, title);
+        }
     //i = igetkey();
     }
     mysql_close(&s);
-    scrint=true;
-    redoscr();
+    modify_user_mode(oldmode);
 }
 #endif /* HAVE_MSYQL == 1*/
 #endif
