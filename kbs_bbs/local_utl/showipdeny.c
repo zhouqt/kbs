8eae8f0f61353d0a3dcbed74cde868b3d6d4e429
bad <bad@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2003-05-17 12:15:03 +0000

    bugs fix
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@3792 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/local_utl/showipdeny.c
+++ kbs_bbs/local_utl/showipdeny.c
@@ -6,6 +6,7 @@ int main(int argc, char **argv)
 {
     FILE *fp, *fp2;
     char fn[80],fn2[80];
+    char * p;
     int i,j,ip[4],t,k;
     time_t tt;
     chdir(BBSHOME);
@@ -19,10 +20,12 @@ int main(int argc, char **argv)
         while(!feof(fp)) {
             if(fscanf(fp, "%d %ld %d.%d.%d.%d %d %d", &i, &j, &ip[0], &ip[1], &ip[2], &ip[3], &t, &k)<=0) break;
             tt=(time_t) j;
+            p = ctime(&tt);
+            p[19]=0; p+=4;
             if(i==0)
-                fprintf(fp2, "%s 来自%d.%d.%d.%d两次连接时间太短.一小时内共连接%d次.\n", ctime(tt), ip[0],ip[1],ip[2],ip[3],t);
+                fprintf(fp2, "%s 来自 %d.%d.%d.%d 两次连接时间太短.一小时内共连接%d次.\n", p, ip[0],ip[1],ip[2],ip[3],t+1);
             else
-                fprintf(fp2, "%s 来自%d.%d.%d.%d连接过于频繁.一小时内共连接%d次.\n", ctime(tt), ip[0],ip[1],ip[2],ip[3],t);
+                fprintf(fp2, "%s 来自 %d.%d.%d.%d 连接过于频繁.    一小时内共连接%d次.\n", p, ip[0],ip[1],ip[2],ip[3],t);
         }
         fclose(fp);
         fclose(fp2);
@@ -33,7 +36,10 @@ int main(int argc, char **argv)
         strcpy(deliveruser.username, "自动发信系统");
         currentuser = &deliveruser;
         strcpy(fromhost, "天堂");
-        sprintf(fn, "%s的犯罪记录", ctime(time(0)));
+        tt = time(0);
+        p = ctime(&tt);
+        p[19]=0; p+=4;
+        sprintf(fn, "%s的犯罪记录", p);
         post_file(&deliveruser, NULL, fn2, "system_dev", fn, 0, 1);
     }
     return 0;
