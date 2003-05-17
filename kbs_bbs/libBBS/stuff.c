125d0ef37ea00bd0e8fac5758af99a9898af6d20
bad <bad@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2003-05-17 09:59:09 +0000

    logs
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@3782 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/libBBS/stuff.c
+++ kbs_bbs/libBBS/stuff.c
@@ -1174,6 +1174,7 @@ int check_IP_lists(char *IP)
 {
     int fd;
     int i,j,k;
+    FILE* fp;
     struct stat st;
     struct flock ldata;
     struct ip_struct{
@@ -1209,14 +1210,22 @@ int check_IP_lists(char *IP)
         }
         if(ip[0]==ips[i].ip[0]&&ip[1]==ips[i].ip[1]&&ip[2]==ips[i].ip[2]&&ip[3]==ips[i].ip[3]){
             if((double)(now-ips[i].last)<=CON_THRESHOLD2) {
-                bbslog("user", "too short connection %d.%d.%d.%d", ip[0],ip[1],ip[2],ip[3]);
+                fp=fopen(".IPdenys", "a");
+                if(fp){
+                    fprintf(fp, "0 %ld %d.%d.%d.%d\n", (unsigned int)now, ip[0],ip[1],ip[2],ip[3]);
+                    fclose(fp);
+                }
                 ret = 1;
             }
             found=1;
             ips[i].last = now;
             ips[i].t++;
             if(ips[i].t/(double)(ips[i].last-ips[i].first)>=CON_THRESHOLD) {
-                bbslog("user", "too many connection %d.%d.%d.%d", ip[0],ip[1],ip[2],ip[3]);
+                fp=fopen(".IPdenys", "a");
+                if(fp){
+                    fprintf(fp, "1 %ld %d.%d.%d.%d\n", (unsigned int)now, ip[0],ip[1],ip[2],ip[3]);
+                    fclose(fp);
+                }
                 ret = 1;
             }
             break;
