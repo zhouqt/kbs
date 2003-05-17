9add910261596480d40f569cf949f642be8bd82d
stiger <stiger@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2003-05-17 13:38:06 +0000

    转移定义到calltime.h
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@3797 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/src/calltime.c
+++ kbs_bbs/src/calltime.c
@@ -1,24 +1,5 @@
 #include "bbs.h"
 
-#define CLOCK_TYPE_NORMAL 1
-#define CLOCK_TYPE_DAY 2
-#define CLOCK_TYPE_WEEK 3
-#define CLOCK_TYPE_MONTH 4
-#define CLOCK_TYPE_YEAR 5
-#define CLOCK_TYPE_IMP_NOT 6
-#define CLOCK_TYPE_IMP_HAD 7
-#define CLOCK_TYPE_LOGIN 8
-#define CLOCK_TYPE_LOGIN_HAD 9
-
-#define MAX_CLOCK_DEF 100
-
-struct clock_struct
-{
-	int type;
-	time_t clock_time;
-	char memo[40];
-};
-
 struct clock_struct * clock_data = NULL;
 int clock_total = 0;
 
