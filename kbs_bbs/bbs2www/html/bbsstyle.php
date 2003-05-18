5ab0fc1f5dff2d3d2cda12499711091b4ac73fee
flyriver <flyriver@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2003-05-18 16:44:50 +0000

    新增 bbsstyle.php 页面，用于修改界面风格。
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@3825 de4889a5-8ad4-4bf3-bd87-57997c21d515

---
+++ kbs_bbs/bbs2www/html/bbsstyle.php
@@ -0,0 +1,38 @@
+<?php
+	if (isset($_GET["s"]))
+	{
+		$style = $_GET["s"];
+		settype($style, "integer");
+		switch ($style)
+		{
+		case 1:
+			setcookie("STYLE",1);
+			$colwidth="170,*";
+			break;
+		case 0:
+		default:
+			setcookie("STYLE",0);
+			$colwidth="144,*";
+		}
+		$charset = "gb2312";
+?>
+<?xml version="1.0" encoding="<?php echo $charset; ?>"?>
+<!DOCTYPE html
+     PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=<?php echo $charset; ?>"/>
+</head>
+<body>
+<script language="javascript">
+parent.mainframe.cols = "<?php echo $colwidth; ?>";
+top.f2.navigate('/cgi-bin/bbs/bbsleft');
+top.f3.navigate(top.f3.location);
+top.f4.navigate(top.f4.location);
+</script>
+</body>
+</html>
+<?php
+	}
+?>
