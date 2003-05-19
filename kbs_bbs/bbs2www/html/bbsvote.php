66acb45f15ebc5f6202a1eba13cb936a61defcaa
stiger <stiger@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2003-05-19 15:15:48 +0000

    投票的php
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@3878 de4889a5-8ad4-4bf3-bd87-57997c21d515

---
+++ kbs_bbs/bbs2www/html/bbsvote.php
@@ -0,0 +1,56 @@
+<?php
+
+	require("funcs.php");
+	if ($loginok != 1 || ($currentuser["userid"] == "guest") )
+		html_nologin();
+	else
+	{
+		html_init("gb2312");
+
+		if(isset($_GET["board"]))
+			$board = $_GET["board"];
+		else
+			html_error_quit("讨论区错误");
+
+		if(isset($_GET["num"]))
+			$num = $_GET["num"];
+		else
+			html_error_quit("参数错误1");
+
+		$brdarr = array();
+		$brdnum = bbs_getboard($board,$brdarr);
+		if($brdnum == 0)
+			html_error_quit("错误的讨论区");
+
+		if(bbs_checkreadperm($currentuser["index"],$brdnum)==0)
+			html_error_quit("您没有权限");
+
+		$votearr = array();
+		$retnum = bbs_get_vote_from_num($board,$votearr,$num);
+
+		if($retnum <= 0)
+			html_error_quit("该投票不存在");
+?>
+<body>
+<center><p><?php echo BBS_FULL_NAME; ?> -- [投票列表] [用户:<?php echo $currentuser["userid"];?>] 
+<?php echo $board; ?>版投票<br></p>
+<hr class="default"/>
+<?php
+		$descdir = "vote/".$board."/desc.".$votearr[0]["DATE"] ;
+		bbs_printansifile($descdir);
+?>
+<hr class="default"/>
+<table width="613">
+<tr><td>序号</td><td><?php echo $num;?></tr>
+<tr><td>标题</td><td><?php echo $votearr[0]["TITLE"];?></tr>
+<tr><td>类型</td><td><?php echo $votearr[0]["TYPE"];?></tr>
+<tr><td>开启者</td><td><?php echo $votearr[0]["USERID"];?></tr>
+<tr><td>开启日期</td><td><?php echo date("r",$votearr[0]["DATE"]);?></tr>
+<tr><td>投票天数</td><td><?php echo $votearr[0]["MAXDAY"];?></tr>
+</table>
+<hr class="default"/>
+<?php
+		html_normal_quit();
+
+	}
+?>
