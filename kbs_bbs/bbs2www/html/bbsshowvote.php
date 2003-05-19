5b68ca083e185b120565ed1f8219bb245dc51538
stiger <stiger@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2003-05-19 13:19:32 +0000

    显示一个版面所有投票的php
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@3861 de4889a5-8ad4-4bf3-bd87-57997c21d515

---
+++ kbs_bbs/bbs2www/html/bbsshowvote.php
@@ -0,0 +1,56 @@
+<?php
+
+	require("funcs.php");
+	if ($loginok != 1 || ($currentuser["userid"] == "guest") )
+		html_nologin();
+	else
+	{
+		html_init("gb2312");
+
+		if(isset($_GET["board"]))
+			$board = $_GET["board"];
+		else
+			html_error_quit("讨论区错误");
+
+		$brdarr = array();
+		$brdnum = bbs_getboard($board,$brdarr);
+		if($brdnum == 0)
+			html_error_quit("错误的讨论区");
+
+		if(bbs_checkreadperm($currentuser["index"],$brdnum)==0)
+			html_error_quit("您没有权限");
+
+		$votearr = array();
+		$retnum = bbs_get_votes($board,$votearr);
+?>
+<body>
+<center><p><?php echo BBS_FULL_NAME; ?> -- [投票列表] [用户:<?php echo $currentuser["userid"];?>] 
+版面<?php echo $board; ?>共有<?php echo $retnum;?>个投票<br></p>
+<hr class="default"/>
+<table width="613">
+<tr><td>序号</td><td>标题</td><td>类型</td><td>开启者</td><td>开启日期</td><td>投票天数</td></tr>
+<?php
+		for($i = 0; $i < $retnum; $i++ ){
+?>
+<tr><td>
+<?php echo $i+1;?>
+</td><td>
+<?php echo $votearr[$i]["TITLE"];?>
+</td><td>
+<?php echo $votearr[$i]["TYPE"];?>
+</td><td>
+<?php echo $votearr[$i]["USERID"];?>
+</td><td>
+<?php echo date("r",$votearr[$i]["DATE"]);?>
+</td><td>
+<?php echo $votearr[$i]["MAXDAY"];?>
+</td></tr>
+<?php
+		}
+?>
+</table>
+<?php
+		html_normal_quit();
+
+	}
+?>
