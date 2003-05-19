fe19fadee54fd26a428eb9122cf94d2d7efb4e3e
binxun <binxun@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2003-05-19 01:31:02 +0000

    错误处理
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@3831 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/bbs2www/html/bbsfillform.php
+++ kbs_bbs/bbs2www/html/bbsfillform.php
@@ -4,37 +4,40 @@
 	 * by binxun 2003.5
 	 */
 	require("funcs.php");
-    html_init("gb2312");
+	html_init("gb2312");
 
 	if ($loginok != 1)
 		html_nologin();
 	else
 	{
-	    @$realname=$_POST["realname"];
-	    @$dept=$_POST["dept"];
-        @$address=$_POST["address"];
+		@$realname=$_POST["realname"];
+		@$dept=$_POST["dept"];
+		@$address=$_POST["address"];
 		@$year=$_POST["year"];
 		@$month=$_POST["month"];
 		@$day=$_POST["day"];
-	    @$email=$_POST["email"];
+		@$email=$_POST["email"];
 		@$phone=$_POST["phone"];
 
 
 	if(!strcmp($currentuser["userid"],"guest"))
-	    html_error_quit("请申请另外的帐号填写注册单!");
+		html_error_quit("请申请另外的帐号填写注册单!");
 
 	//用户已经通过注册
 	//未满等待时间
 
-    $ret=bbs_createregform($realname,$dept,$address,$year,$month,$day,$email,$phone);
+	$ret=bbs_createregform($realname,$dept,$address,$year,$month,$day,$email,$phone,FALSE); //手工填写注册单
 
 	switch($ret)
 	{
 	case 0:
-
+		break;	
+	case 1:
+		html_error_quit("您的注册单还没有处理，请耐心等候");
+		break;
 	default:
-			html_error_quit("未知的错误!");
-			break;
+		html_error_quit("未知的错误!");
+		break;
 	}
 ?>
 <body>
