1e6781029b3098a05ff397c53705a0360f1498f5
stiger <stiger@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2003-05-20 16:38:46 +0000

    是非/单选/复选都OK了
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@3903 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/bbs2www/html/bbsvote.php
+++ kbs_bbs/bbs2www/html/bbsvote.php
@@ -25,8 +25,63 @@
 		if(bbs_checkreadperm($currentuser["index"],$brdnum)==0)
 			html_error_quit("您没有权限");
 
+		$oldvote = array();
 		$votearr = array();
-		$retnum = bbs_get_vote_from_num($board,$votearr,$num);
+		$uservotearr = array();
+
+		for( $i=0; $i<32; $i++){
+			$oldvote[$i] = 0;
+		}
+
+		if( $_GET["submit"] ){
+
+			if(isset($_GET["type"]))
+				$votetype = $_GET["type"];
+			else
+				html_error_quit("参数错误2");
+
+			$votevalueint = 0;
+
+			if( $votetype == "单选" || $votetype == "是非" ){
+				if(isset($_GET["ITEM"]))
+					$itemvalue = $_GET["ITEM"];
+				else
+					html_error_quit("参数错误3");
+
+				settype($itemvalue,"integer");
+				if( $itemvalue < 0 || $itemvalue > 31 )
+					html_error_quit("参数错误4");
+
+				if( $votetype == "是非" && ($itemvalue < 0 || $itemvalue > 2) )
+					html_error_quit("参数错误5");
+
+				$votevalueint = ( 1 << $itemvalue );
+
+			}else if( $votetype == "复选" ){
+
+				for($i = 0; $i < 32; $i++){
+					$itemstr = "ITEM".($i+1);
+					if(isset($_GET[$itemstr]) && $_GET[$itemstr]=="on"){
+						$votevalueint += ( 1 << $i );
+					}
+				}
+
+			}else
+				html_error_quit("参数错误5");
+
+			$retnum = bbs_vote_num($board,$num,$votevalueint);
+			if($retnum <= 0)
+				html_error_quit("投票错误".$retnum);
+			else{
+?>
+投票成功
+<?php
+				html_normal_quit();
+
+			}
+		}
+
+		$retnum = bbs_get_vote_from_num($board,$votearr,$num,$uservotearr);
 
 		if($retnum <= 0)
 			html_error_quit("该投票不存在");
@@ -47,21 +102,44 @@
 <tr><td>开启者</td><td><?php echo $votearr[0]["USERID"];?></tr>
 <tr><td>开启日期</td><td><?php echo date("r",$votearr[0]["DATE"]);?></tr>
 <tr><td>投票天数</td><td><?php echo $votearr[0]["MAXDAY"];?></tr>
+<tr><td><?php if($uservotearr[0]["USERID"]){?>您已经投票，现在可以更改<?php }else{?>您尚未投票<?php }?></td><td></td></tr>
 </table>
 <hr class="default"/>
 <form action="/bbsvote.php" method="get">
 <table width="613">
 <?php
+		if($uservotearr[0]["USERID"]){
+			for( $i =0; $i < $votearr[0]["TOTALITEMS"]; $i++){
+				if( $uservotearr[0]["VOTED"] & (1 << $i) )
+					$oldvote[$i] = 1;
+			}
+		}
 		if( $votearr[0]["TYPE"] == "复选" ){
 
 			for( $i=0; $i < $votearr[0]["TOTALITEMS"]; $i++){
 				$itemstr = "ITEM".($i+1);
 ?>
 <tr><td><?php echo $i+1;?></td>
-<td><input type="checkbox" name="<?php echo $itemstr;?>"></td>
+<td><input type="checkbox" name="<?php echo $itemstr;?>" <?php if($oldvote[$i]) echo "checked";?>></td>
 <td><?php echo $votearr[0][$itemstr];?></td></tr>
 <?php
 			}
+?>
+<input type="hidden" name="type" value="复选">
+<?php
+		}else if( $votearr[0]["TYPE"] == "单选" || $votearr[0]["TYPE"] == "是非" ){
+
+			for( $i=0; $i < $votearr[0]["TOTALITEMS"]; $i++){
+				$itemstr = "ITEM".($i+1);
+?>
+<tr><td><?php echo $i+1;?></td>
+<td><input type="radio" name="ITEM" value="<?php echo $i;?>" <?php if($oldvote[$i]) echo "checked";?>></td>
+<td><?php echo $votearr[0][$itemstr];?></td></tr>
+<?php
+			}
+?>
+<input type="hidden" name="type" value="<?php echo $votearr[0]["TYPE"];?>">
+<?php
 		}
 ?>
 </table>
