93c3cd0ddcda89ee51e6796d214fa5d26994a088
kcn <kcn@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2003-05-22 16:54:48 +0000

    查找
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@3967 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/bbs2www/src/bbsfind.c
+++ kbs_bbs/bbs2www/src/bbsfind.c
@@ -7,6 +7,7 @@ char day[20], user[20], title[80];
 int main()
 {
     char user[32], title3[80], title[80], title2[80];
+    char board[BOARDNAMELEN+1];
     int day;
 
     init_all();
@@ -14,6 +15,7 @@ int main()
     strsncpy(title, getparm("title"), 50);
     strsncpy(title2, getparm("title2"), 50);
     strsncpy(title3, getparm("title3"), 50);
+    strsncpy(board, getparm("board"), BOARDNAMELEN+1);
     day = atoi(getparm("day"));
     if (day == 0) {
         printf("%s -- 站内文章查询<hr color=green>\n", BBSNAME);
@@ -25,12 +27,12 @@ int main()
         printf("查找最近: <input maxlength=5 size=5 type=text name=day value=7> 天以内的文章<br><br>\n");
         printf("<input type=submit value=提交查询>\n");
     } else {
-        search(user, title, title2, title3, day * 86400);
+        search(board,user, title, title2, title3, day * 86400);
     }
     http_quit();
 }
 
-int search(char *id, char *pat, char *pat2, char *pat3, int dt)
+int search(char* boardname,char *id, char *pat, char *pat2, char *pat3, int dt)
 {
     FILE *fp;
     char board[256], dir[256], buf2[150];
@@ -46,9 +48,12 @@ int search(char *id, char *pat, char *pat2, char *pat3, int dt)
         printf("和 '%s' ", nohtml(pat2));
     if (pat3[0])
         printf("不含 '%s'", nohtml(pat3));
-    printf("时间: %d 天<br><hr color=green>\n", dt / 86400);
+    if (dt>0)
+        printf("时间: %d 天<br><hr color=green>\n", dt / 86400);
     for (i = 0; i < MAXBOARD; i++) {
         total = 0;
+        if (boardname[0]&&strcasemp(bc[i].filename,boardname))
+            continue;
         strcpy(board, bc[i].filename);
         if (!has_read_perm(currentuser, board))
             continue;
@@ -67,7 +72,7 @@ int search(char *id, char *pat, char *pat2, char *pat3, int dt)
                 continue;
             if (pat[0] && !strcasestr(x.title, pat))
                 continue;
-            if (abs(now - t) > dt)
+            if ((dt>0) &&(abs(now - t) > dt))
                 continue;
             if (pat2[0] && !strcasestr(x.title, pat2))
                 continue;
