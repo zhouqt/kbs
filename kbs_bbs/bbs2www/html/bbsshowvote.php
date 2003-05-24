4e636fc98f58d7943a8dc54f652ce52222946894
stiger <stiger@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2003-05-24 01:37:50 +0000

    匿名投票返回错误不让用户登陆
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@4012 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/bbs2www/html/bbsshowvote.php
+++ kbs_bbs/bbs2www/html/bbsshowvote.php
@@ -1,9 +1,10 @@
 <?php
 
 	require("funcs.php");
-	if ($loginok != 1 || ($currentuser["userid"] == "guest") )
-		html_nologin();
-	else
+	if ($loginok != 1 || ($currentuser["userid"] == "guest") ){
+		html_init("gb2312");
+		html_error_quit("匿名用户没有投票权限");
+	}else
 	{
 		html_init("gb2312");
 
