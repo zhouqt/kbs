c12df8981df3837b1b3a7de55b07d351081b65e9
bad <bad@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2003-05-26 05:31:22 +0000

    effsize
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@4049 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/local_utl/calc_effsize.c
+++ kbs_bbs/local_utl/calc_effsize.c
@@ -10,7 +10,8 @@ int calcboard(struct boardheader * bh, void * arg)
     char * ptr;
     int size=sizeof(struct fileheader);
     setbdir(0, fn, bh->filename);
-    if ((fd = open(fn, O_RDONLY, 0664)) == -1) {
+    printf("%s:\n", bh->filename);
+    if ((fd = open(fn, O_RDWR, 0664)) == -1) {
         bbslog("user", "%s", "recopen err");
         return 0;      /* 创建文件发生错误*/
     }
@@ -22,7 +23,7 @@ int calcboard(struct boardheader * bh, void * arg)
     fcntl(fd, F_SETLKW, &ldata);
     total = buf.st_size / size;
 
-    if ((i = safe_mmapfile_handle(fd, O_RDONLY, PROT_READ, MAP_SHARED, (void **) &ptr, (size_t*)&buf.st_size)) != 1) {
+    if ((i = safe_mmapfile_handle(fd, O_RDWR, PROT_READ|PROT_WRITE, MAP_SHARED, (void **) &ptr, (size_t*)&buf.st_size)) != 1) {
         if (i == 2)
             end_mmapfile((void *) ptr, buf.st_size, -1);
         ldata.l_type = F_UNLCK;
@@ -38,6 +39,7 @@ int calcboard(struct boardheader * bh, void * arg)
         int j;
         size_t fsize;
 
+        printf("\r%d", i);
         setbfile(ffn, bh->filename, ptr1->filename);
         {
             int k,abssize=0,entercount=0,ignoreline=0;
