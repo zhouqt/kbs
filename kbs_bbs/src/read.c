502f01d670e6b20d74c0923fd6b7cfeab97ef996
bad <bad@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2003-06-02 06:19:08 +0000

    i_read_key的bug
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@4105 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/src/read.c
+++ kbs_bbs/src/read.c
@@ -712,16 +712,21 @@ static int i_read_key(int cmdmode, struct one_key *rcmdlist, struct keeploc *loc
     default:
         for (i = 0; rcmdlist[i].fptr != NULL; i++) {
             if (rcmdlist[i].key == ch) {
+                if (cmdmode != RMAIL && cmdmode != GMENU
+                     && (digestmode==DIR_MODE_NORMAL||digestmode==DIR_MODE_THREAD)) {
 		    /* add by stiger */
-		if( POSTFILE_BASENAME(((fileheader *)(pnt+(locmem->crs_line-locmem->top_line)*ssize))->filename)[0]=='Z' ){
-		    if(ch=='D' || ch=='b' || ch=='B') return DONOTHING;
-		    else if(ch=='s')
-                    mode = (*(rcmdlist[i].fptr)) (locmem->crs_line - get_num_records(currdirect, ssize), &pnt[(locmem->crs_line - locmem->top_line) * ssize], currdirect );
-		    else
-                    mode = (*(rcmdlist[i].fptr)) (locmem->crs_line - get_num_records(currdirect, ssize), &pnt[(locmem->crs_line - locmem->top_line) * ssize], ding_direct );
-		}
-		else
-                    mode = (*(rcmdlist[i].fptr)) (locmem->crs_line, &pnt[(locmem->crs_line - locmem->top_line) * ssize], currdirect);
+        		if( POSTFILE_BASENAME(((fileheader *)(pnt+(locmem->crs_line-locmem->top_line)*ssize))->filename)[0]=='Z' ){
+        		    if(ch=='D' || ch=='b' || ch=='B') return DONOTHING;
+        		    else if(ch=='s')
+                            mode = (*(rcmdlist[i].fptr)) (locmem->crs_line - get_num_records(currdirect, ssize), &pnt[(locmem->crs_line - locmem->top_line) * ssize], currdirect );
+        		    else
+                            mode = (*(rcmdlist[i].fptr)) (locmem->crs_line - get_num_records(currdirect, ssize), &pnt[(locmem->crs_line - locmem->top_line) * ssize], ding_direct );
+        		}
+        		else
+                            mode = (*(rcmdlist[i].fptr)) (locmem->crs_line, &pnt[(locmem->crs_line - locmem->top_line) * ssize], currdirect);
+                }
+                else
+                      mode = (*(rcmdlist[i].fptr)) (locmem->crs_line, &pnt[(locmem->crs_line - locmem->top_line) * ssize], currdirect);
                 break;
             }
         }
