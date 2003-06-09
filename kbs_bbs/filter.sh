dc39c8031a370ea18855962c99f7d350996a0e8e
kcn <kcn@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2003-06-09 07:10:40 +0000

    加入对#else的filter
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@4164 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/filter.sh
+++ kbs_bbs/filter.sh
@@ -1,16 +1,27 @@
 #!/bin/sh
 awk 'BEGIN { definenum=0;infilter=0; } \
-  $0~/#ifdef[ \t]*FILTER/ { if (infilter==0) {infilter=1;definenum=0;} } \
-  $0~/#ifdef[ \t]*SMTH/ { if (infilter==0) {infilter=1;definenum=0;} } \
+  $0~/#ifdef[ \t]*FILTER/ { if (infilter==0) {infilter=1;definenum=0;output=0;} } \
+  $0~/#ifdef[ \t]*SMTH/ { if (infilter==0) {infilter=1;definenum=0;output=0;} } \
   { if (infilter==1) {  \
       if (match($0,"#endif")) {\
          definenum--; \
-         if (definenum==0) \
+         if (definenum==0) {\
          infilter=0; \
-      } \
-      if (match($0,"#if")) {\
-         definenum++; \
-      } \
+         output=0; }\
+      } else {\
+        if (match($0,"#if")) {\
+          definenum++; \
+          output=0; \
+        } else {\
+          if (match($0,"#else")) {\
+           if (definenum==1) \
+             output=1; \
+           else  output=0; \
+          } else {\
+            if (output==1) print $0; \
+          } \
+        }\
+      }\
     } else \
      print $0; \
   }' $1 > /tmp/filtersource
