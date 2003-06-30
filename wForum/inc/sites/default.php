e5c54c1240c1cad26d8a8f422d75be3d2c622dc5
roy <roy@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2003-06-30 07:05:01 +0000

    修改站点定义文件存放路径
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@4346 de4889a5-8ad4-4bf3-bd87-57997c21d515

---
+++ wForum/inc/sites/default.php
@@ -0,0 +1,7 @@
+<?php
+$Version="Powered by£ºSmth bbs2WWW Version 0.0.1";
+$Copyright="Copyright ?2000 - 2002 <font face=Verdana, Arial, Helvetica, sans-serif size=1><b>Roy<font color=#CC0000>@zixia.net</font></b></font> ";
+
+
+
+?>
