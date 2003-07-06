c3720d1d7ee55e4541d8eca8798971a0164bfb0e
flyriver <flyriver@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2003-07-06 18:05:13 +0000

    Added a typedef.
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@4408 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/src/output.h
+++ kbs_bbs/src/output.h
@@ -1,7 +1,10 @@
 #ifndef __OUTPUT_H__
 #define __OUTPUT_H__
 
+struct __buffered_output;
+
 typedef int (*output_handler_t)(char *buf, size_t buflen, void *arg);
+typedef void (*flush_handler_t)(struct __buffered_output *output);
 
 typedef struct __buffered_output
 {
@@ -9,7 +12,7 @@ typedef struct __buffered_output
 	size_t buflen;
 	char *outp;
 	output_handler_t output;
-	void (*flush)(struct __buffered_output *output);
+	flush_handler_t flush;
 } buffered_output_t;
 
 #endif /* __OUTPUT_H__ */
