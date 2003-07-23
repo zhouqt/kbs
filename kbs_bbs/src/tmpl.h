e7999509d6b01e2e89e44a59cfb8dd0c2d89c198
stiger <stiger@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2003-07-23 10:16:15 +0000

    摸板的结构定义
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@4520 de4889a5-8ad4-4bf3-bd87-57997c21d515

---
+++ kbs_bbs/src/tmpl.h
@@ -0,0 +1,32 @@
+#ifndef _TEMPLATE_H
+#define _TEMPLATE_H
+
+#define TEMPLATE_DIR ".templ"
+#define MAX_TEMPLATE 10
+#define MAX_CONTENT 20
+#define TMPL_BM_FLAG 0x1
+#define MAX_CONTENT_LENGTH 555
+#define TMPL_NOW_VERSION 1
+
+struct s_content{
+	char text[50];
+	int length;
+};
+
+struct s_template{
+	char title[50];
+	char title_prefix[20];
+	int content_num;
+	char filename[STRLEN];
+	int flag;
+	int version;
+	char unused[16];
+	char title_tmpl[STRLEN];
+};
+
+struct a_template{
+	struct s_template * tmpl;
+	struct s_content * cont;
+};
+
+#endif
