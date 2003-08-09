426a5087d85832c21f1be1da2d0236968465471f
roy <roy@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2003-08-09 04:06:16 +0000

    版权信息
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@4716 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- wForum/inc/sites/default.php
+++ wForum/inc/sites/default.php
@@ -1,6 +1,6 @@
 <?php
-$Version="Powered by：wForum Version 0.0.1";
-$Copyright="<a href='http://www.aka.cn/' target=_blank>阿卡信息技术(北京)有限公司</a> 版权所有 2000 - 2003 <font face=Verdana, Arial, Helvetica, sans-serif size=1><b>Roy<font color=#CC0000>@zixia.net</font></b></font> ";
+$Version="<a href='http://wforum.aka.cn' target=_blank>Powered by：wForumVersion 0.0.1</a>";
+$Copyright="<a href='http://www.aka.cn/' target=_blank>阿卡信息技术(北京)有限公司</a> & <a href='http://www.smth.cn'>水木清华BBS</a> 版权所有 2000 - 2003 <br><font face=Verdana, Arial, Helvetica, sans-serif size=1><b>Roy<font color=#CC0000>@zixia.net</font></b></font> ";
 
 $AnnounceBoard="Announce";
 
