761f22d5c5f6e60e6bf3495035f7664a65c19e8f
zixia <zixia@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2003-08-15 04:59:37 +0000

    check
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@4760 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- wForum/inc/sites/default.php
+++ wForum/inc/sites/default.php
@@ -1,6 +1,6 @@
 <?php
 $Version="Powered by wForum Version 1.0";
-$Copyright="<a href='http://www.aka.cn/' target=_blank>阿卡信息技术(北京)有限公司</a> & <a href='http://www.smth.cn'>水木清华BBS</a> 版权所有 2000 - 2003 <br><font face=Verdana, Arial, Helvetica, sans-serif size=1><b>Roy<font color=#CC0000>@zixia.net</font></b></font> ";
+$Copyright="<a href='http://www.aka.cn/' target=_blank>阿卡信息技术(北京)有限公司</a> & <a href='http://www.smth.cn'>水木清华BBS</a> 版权所有 1994 - 2008 <br><font face=Verdana, Arial, Helvetica, sans-serif size=1><b>Roy<font color=#CC0000>@zixia.net</font></b></font> ";
 
 $AnnounceBoard="Announce";
 
