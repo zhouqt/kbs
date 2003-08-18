9d09e508dd97f28eb1688a4bfa314a02be823962
stiger <stiger@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2003-08-18 07:33:51 +0000

    libBBS里用prints要#ifdef BBS_MAIN呀
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@4828 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/libBBS/record.c
+++ kbs_bbs/libBBS/record.c
@@ -223,7 +223,9 @@ int size;
      * if((fd = open(filename,O_WRONLY|O_CREAT,0644)) == -1) { 
      */
     if ((fd = open(filename, O_WRONLY | O_CREAT, 0664)) == -1) {        /* Leeward 98.04.27: 0664->Enable write access of WWW-POST programe */
+#ifdef BBS_MAIN
         perror(filename);
+#endif
         return -1;
     }
     flock(fd, LOCK_EX);
