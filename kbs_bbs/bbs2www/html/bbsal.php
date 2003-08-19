0fb9851ccc33376060573fc38b492ab6644d3118
stiger <stiger@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2003-08-19 13:44:51 +0000

    增加编辑功能,web通讯录完工
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@4839 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/bbs2www/html/bbsal.php
+++ kbs_bbs/bbs2www/html/bbsal.php
@@ -2,11 +2,39 @@
         
     require("funcs.php");
 
-    $hostname=bbs_sysconf_str("MYSQLHOST");
-    $username=bbs_sysconf_str("MYSQLUSER");
-    $password=bbs_sysconf_str("MYSQLPASSWORD");
-    $dbname=bbs_sysconf_str("MYSQLDATABASE");
+	$hostname=bbs_sysconf_str("MYSQLHOST");
+	$username=bbs_sysconf_str("MYSQLUSER");
+	$password=bbs_sysconf_str("MYSQLPASSWORD");
+	$dbname=bbs_sysconf_str("MYSQLDATABASE");
 
+function al_print_form($al){
+
+?>
+<table border="0">
+<tr><td>姓名:</td><td><input name="t_name" maxlength=13 size=13 value=<?php echo htmlspecialchars($al[2]);?>></td></tr>
+<tr><td>分组:</td><td><input name="t_group" maxlength=13 size=13 value=<?php echo htmlspecialchars($al[15]);?>></td></tr>
+<tr><td>bbsid:</td><td><input name="t_bbsid" maxlength=13 size=13 value=<?php echo htmlspecialchars($al[3]);?>></td></tr>
+<tr><td>学校:</td><td><input name="t_school" size=50  value=<?php echo htmlspecialchars($al[4]);?>></td></tr>
+<tr><td>邮政编码:</td><td><input name="t_zipcode" maxlength=6 size=6  value=<?php echo htmlspecialchars($al[5]);?>></td></tr>
+<tr><td>家庭住址:</td><td><input name="t_homeaddr" size=50 value=<?php echo htmlspecialchars($al[6]);?>></td></tr>
+<tr><td>工作地址:</td><td><input name="t_companyaddr" size=50 value=<?php echo htmlspecialchars($al[7]);?>></td></tr>
+<tr><td>家庭电话:</td><td><input name="t_tel_h" maxlength=20 size=20 value=<?php echo htmlspecialchars($al[8]);?>></td></tr>
+<tr><td>工作电话:</td><td><input name="t_tel_o" maxlength=20 size=20 value=<?php echo htmlspecialchars($al[9]);?>></td></tr>
+<tr><td>手机号码:</td><td><input name="t_mobile" maxlength=13 size=13 value=<?php echo htmlspecialchars($al[10]);?>></td></tr>
+<tr><td>email:</td><td><input name="t_email" maxlength=30 size=30 value=<?php echo htmlspecialchars($al[11]);?>></td></tr>
+<tr><td>qq:</td><td><input name="t_qq" maxlength=10 size=10 value=<?php echo htmlspecialchars($al[12]);?>></td></tr>
+<tr><td>生日:</td><td><input name="t_birth_year" maxlength=4 size=4 value=<?php echo substr($al[13],0,4);?>>年<input name="t_birth_month" maxlength=2 size=2 value=<?php echo substr($al[13],5,2);?>>月<input name="t_birth_day" maxlength=2 size=2 value=<?php echo substr($al[13],8,2);?>>日</td></tr>
+<tr><td>备注:</td><td><input name="t_memo" size=50 value=<?php echo htmlspecialchars($al[14]);?>></td></tr>
+</table>
+<center>
+<input type=submit name=submit value="提交">
+<input type=reset name=reset value="重置">
+</center>
+</form>
+
+<?php
+
+}
     if($loginok !=1)
     	html_nologin(); 
     else
@@ -98,19 +126,19 @@
 </center>
 <hr class=default>
 <table border="1" width="613" align="center" cellpadding="0" cellspacing="0">
-<tr><td>姓名</td><td><?php echo $row[2];?></td></tr>
-<tr><td>bbsid</td><td><?php echo $row[3];?></td></tr>
-<tr><td>分组</td><td><?php echo $row[15];?></td></tr>
-<tr><td>生日</td><td><?php echo $row[13];?></td></tr>
-<tr><td>学校</td><td><?php echo $row[4];?></td></tr>
-<tr><td>邮政编码</td><td><?php echo $row[5];?></td></tr>
-<tr><td>家庭地址</td><td><?php echo $row[6];?></td></tr>
-<tr><td>家庭电话</td><td><?php echo $row[9];?></td></tr>
-<tr><td>公司地址</td><td><?php echo $row[7];?></td></tr>
-<tr><td>公司电话</td><td><?php echo $row[8];?></td></tr>
-<tr><td>手机</td><td><?php echo $row[10];?></td></tr>
-<tr><td>email</td><td><?php echo $row[11];?></td></tr>
-<tr><td>qq</td><td><?php echo $row[12];?></td></tr>
+<tr><td>姓名</td><td><?php echo htmlspecialchars($row[2]);?></td></tr>
+<tr><td>bbsid</td><td><?php echo htmlspecialchars($row[3]);?></td></tr>
+<tr><td>分组</td><td><?php echo htmlspecialchars($row[15]);?></td></tr>
+<tr><td>生日</td><td><?php echo htmlspecialchars($row[13]);?></td></tr>
+<tr><td>学校</td><td><?php echo htmlspecialchars($row[4]);?></td></tr>
+<tr><td>邮政编码</td><td><?php echo htmlspecialchars($row[5]);?></td></tr>
+<tr><td>家庭地址</td><td><?php echo htmlspecialchars($row[6]);?></td></tr>
+<tr><td>家庭电话</td><td><?php echo htmlspecialchars($row[9]);?></td></tr>
+<tr><td>公司地址</td><td><?php echo htmlspecialchars($row[7]);?></td></tr>
+<tr><td>公司电话</td><td><?php echo htmlspecialchars($row[8]);?></td></tr>
+<tr><td>手机</td><td><?php echo htmlspecialchars($row[10]);?></td></tr>
+<tr><td>email</td><td><?php echo htmlspecialchars($row[11]);?></td></tr>
+<tr><td>qq</td><td><?php echo htmlspecialchars($row[12]);?></td></tr>
 <tr><td>备注</td><td><pre><?php echo $row[14];?></pre></td></tr>
 </table>
 <center>
@@ -120,7 +148,21 @@
 <?php
 				html_normal_quit();
 			}
-		}else if(isset($action) && $action=="add"){
+		}else if(isset($action) && ( $action=="add" || $action=="edit" ) ){
+			if( $action == "edit" ){
+				if( $_GET["id"] ){
+					$edit_id = $_GET["id"];
+				}else if ($_POST["id"]){
+					$edit_id = $_POST["id"];
+				}
+				else
+					html_error_quit("参数错误");
+
+				settype($edit_id, "integer");
+				if($edit_id < 0)
+					html_error_quit("参数错误7");
+			}
+
 			if( $_GET["submit"] || $_POST["submit"] ){
 				if( $_POST["t_name"] ){
 					$t_name = $_POST["t_name"];
@@ -253,8 +295,11 @@
 					$t_memo="";
 				}
 
-				$sqlstr = "INSERT INTO addr VALUES (NULL, '".$currentuser["userid"]."', '".addslashes($t_name)."','".addslashes($t_bbsid)."','".addslashes($t_school)."','".addslashes($t_zipcode)."','".addslashes($t_homeaddr)."','".addslashes($t_companyaddr)."','".addslashes($t_tel_o)."','".addslashes($t_tel_h)."','".addslashes($t_mobile)."','".addslashes($t_email)."','".addslashes($t_qq)."',\"".$t_birth_year."-".$t_birth_month."-".$t_birth_day."\",'".addslashes($t_memo)."','".addslashes($t_group)."' );";
-
+				if( $action == "add" ){
+					$sqlstr = "INSERT INTO addr VALUES (NULL, '".$currentuser["userid"]."', '".addslashes($t_name)."','".addslashes($t_bbsid)."','".addslashes($t_school)."','".addslashes($t_zipcode)."','".addslashes($t_homeaddr)."','".addslashes($t_companyaddr)."','".addslashes($t_tel_o)."','".addslashes($t_tel_h)."','".addslashes($t_mobile)."','".addslashes($t_email)."','".addslashes($t_qq)."',\"".$t_birth_year."-".$t_birth_month."-".$t_birth_day."\",'".addslashes($t_memo)."','".addslashes($t_group)."' );";
+				}else{
+					$sqlstr = "UPDATE addr SET name='".addslashes($t_name)."',bbsid='".addslashes($t_bbsid)."',school='".addslashes($t_school)."',zipcode='".addslashes($t_zipcode)."',homeaddr='".addslashes($t_homeaddr)."',companyaddr='".addslashes($t_companyaddr)."',tel_o='".addslashes($t_tel_o)."',tel_h='".addslashes($t_tel_h)."',mobile='".addslashes($t_mobile)."',email='".addslashes($t_email)."',qq='".addslashes($t_qq)."',birthday=\"".$t_birth_year."-".$t_birth_month."-".$t_birth_day."\",memo='".addslashes($t_memo)."',groupname='".addslashes($t_group)."' WHERE id=$edit_id AND userid='".$currentuser["userid"]."' ;";
+				}
 				//echo $sqlstr;
 
 				$result = mysql_query($sqlstr) or die(mysql_error());
@@ -263,43 +308,41 @@
 					html_error_quit("失败");
 				}else{
 ?>
-<a href="/bbsal.php?start=<?php echo $startnum;?>&count=<?php echo $count;?>&order=<?php echo $order;?>&desc=<?php echo $desc;?>">添加成功，返回</a>
+<a href="/bbsal.php?start=<?php echo $startnum;?>&count=<?php echo $count;?>&order=<?php echo $order;?>&desc=<?php echo $desc;?>"><?php if($action == "add") echo "添加"; else echo "修改"; ?>成功，返回</a>
 <?php
 					html_normal_quit();
 				}
-			}else{
+			}else if($action == "add"){
 ?>
-<body>
 <center><p><?php echo BBS_FULL_NAME; ?> -- 通讯录条目增加 [用户: <?php echo $currentuser["userid"]; ?>]</p>
 </center>
 <hr class=default>
 <form action="/bbsal.php?start=<?php echo $startnum;?>&count=<?php echo $count;?>&order=<?php echo $order;?>&desc=<?php echo $desc;?>&action=add" method=post>
-<table border="0">
-<tr><td>姓名:</td><td><input name="t_name" maxlength=13 size=13></td></tr>
-<tr><td>分组:</td><td><input name="t_group" maxlength=13 size=13></td></tr>
-<tr><td>bbsid:</td><td><input name="t_bbsid" maxlength=13 size=13></td></tr>
-<tr><td>学校:</td><td><input name="t_school" size=50></td></tr>
-<tr><td>邮政编码:</td><td><input name="t_zipcode" maxlength=6 size=6></td></tr>
-<tr><td>家庭住址:</td><td><input name="t_homeaddr" size=50></td></tr>
-<tr><td>工作地址:</td><td><input name="t_companyaddr" size=50></td></tr>
-<tr><td>家庭电话:</td><td><input name="t_tel_h" maxlength=20 size=20></td></tr>
-<tr><td>工作电话:</td><td><input name="t_tel_o" maxlength=20 size=20></td></tr>
-<tr><td>手机号码:</td><td><input name="t_mobile" maxlength=13 size=13></td></tr>
-<tr><td>email:</td><td><input name="t_email" maxlength=30 size=30></td></tr>
-<tr><td>qq:</td><td><input name="t_qq" maxlength=10 size=10></td></tr>
-<tr><td>生日:</td><td><input name="t_birth_year" maxlength=4 size=4>年<input name="t_birth_month" maxlength=2 size=2>月<input name="t_birth_day" maxlength=2 size=2>日</td></tr>
-<tr><td>备注:</td><td><input name="t_memo" size=50></td></tr>
-</table>
-<center>
-<input type=submit name=submit value="增加">
-<input type=reset name=reset value="重置">
-</center>
-</form>
-</body>
-
 <?php
+				al_print_form($al);
 
 				html_normal_quit();
+			}else{
+
+				$sqlstr = "SELECT * FROM addr WHERE userid=\"".$currentuser["userid"]."\" AND id=".$edit_id;
+
+				$result = mysql_query($sqlstr) or die(mysql_error());
+
+				if( ! $result ){
+					html_error_quit("数据错误");
+				}else{
+					$row = mysql_fetch_row($result);
+					if( ! $row )
+						html_error_quit("数据错误1");
+?>
+<center><p><?php echo BBS_FULL_NAME; ?> -- 通讯录条目修改 [用户: <?php echo $currentuser["userid"]; ?>]</p>
+</center>
+<hr class=default>
+<form action="/bbsal.php?start=<?php echo $startnum;?>&count=<?php echo $count;?>&order=<?php echo $order;?>&desc=<?php echo $desc;?>&action=edit&id=<?php echo $edit_id;?>" method=post>
+<?php
+					al_print_form($row);
+					html_normal_quit();
+				}
 			}
 		}
 
@@ -324,11 +367,14 @@
 ?>
 <tr>
 <td><?php echo $startnum+$i+1;?></td>
-<td><a href="/bbsal.php?start=<?php if($i==0 && $startnum > 0) echo ($startnum-1); else echo $startnum;?>&count=<?php echo $count;?>&action=detail&id=<?php echo $row[0];?>&order=<?php echo $order;?>&desc=<?php echo $desc;?>"><?php echo $row[2];?></a></td>
-<td><?php echo $row[3];?></td>
-<td><?php echo $row[15];?></td>
+<td><a href="/bbsal.php?start=<?php echo $startnum;?>&count=<?php echo $count;?>&action=detail&id=<?php echo $row[0];?>&order=<?php echo $order;?>&desc=<?php echo $desc;?>"><?php echo htmlspecialchars($row[2]);?></a></td>
+<td><?php echo htmlspecialchars($row[3]);?></td>
+<td><?php echo htmlspecialchars($row[15]);?></td>
 <td><pre><?php echo $row[14];?></pre></td>
-<td><a onclick="return confirm('你真的要删除这条记录吗?')" href="/bbsal.php?start=<?php if($i==0 && $startnum > 0) echo ($startnum-1); else echo $startnum;?>&count=<?php echo $count;?>&action=del&id=<?php echo $row[0];?>&order=<?php echo $order;?>&desc=<?php echo $desc;?>">删除</a></td>
+<td>
+<a onclick="return confirm('你真的要删除这条记录吗?')" href="/bbsal.php?start=<?php if($i==0 && $startnum > 0) echo ($startnum-1); else echo $startnum;?>&count=<?php echo $count;?>&action=del&id=<?php echo $row[0];?>&order=<?php echo $order;?>&desc=<?php echo $desc;?>">删除</a>
+<a href="/bbsal.php?start=<?php echo $startnum;?>&count=<?php echo $count;?>&action=edit&id=<?php echo $row[0];?>&order=<?php echo $order;?>&desc=<?php echo $desc;?>">编辑</a>
+</td>
 </tr>
 <?php
 				$i++;
