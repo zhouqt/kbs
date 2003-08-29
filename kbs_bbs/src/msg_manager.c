0827cba8d51a426c4dfbc1279799281934d46d14
stiger <stiger@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2003-08-29 07:14:48 +0000

    收发搞反了
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@4942 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/src/msg_manager.c
+++ kbs_bbs/src/msg_manager.c
@@ -36,7 +36,7 @@ static int set_smsg_select(struct _select_def *conf)
 {
 	clear();
 	move(0,0);
-	prints("%s信人:%s   时间:%s", s_m[conf->pos-conf->page_pos].type?"发":"收", s_m[conf->pos-conf->page_pos].dest, s_m[conf->pos-conf->page_pos].time);
+	prints("%s信人:%s   时间:%s", s_m[conf->pos-conf->page_pos].type?"收":"发", s_m[conf->pos-conf->page_pos].dest, s_m[conf->pos-conf->page_pos].time);
 	prints("\n内容:\n%s", s_m[conf->pos-conf->page_pos].context);
 	pressanykey();
 
