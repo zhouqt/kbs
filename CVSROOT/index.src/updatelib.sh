cbd23cea76d688d4b848c19ba5716baff3277bdd
bad <bad@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2003-09-20 03:59:50 +0000

    bug
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@5034 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- CVSROOT/index.src/updatelib.sh
+++ CVSROOT/index.src/updatelib.sh
@@ -2,8 +2,9 @@
 gcc -g -o newindex newindex.c
 gcc -g -o newqueryd newqueryd.c -lm
 mkdir /home/bbs
-cp -f newindex /home/bbs/newindex
-cp -f newqueryd /home/bbs/newqueryd
+cp -f newindex /home/bbs
+cp -f newqueryd /home/bbs
+cp -f word.txt /home/bbs
 cd /home/bbs
 mkdir /home/bbs/bk
 ./newindex
