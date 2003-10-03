bc1218a29e7375c7424e3ab379a79bfc933bf7a1
flyriver <flyriver@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2003-10-03 13:29:00 +0000

    New output routines.
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@5105 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/libBBS/output.c
+++ kbs_bbs/libBBS/output.c
@@ -1,28 +1,10 @@
 #include "bbs.h"
 #include "output.h"
 
-static void default_flush_buffer(buffered_output_t *output)
+static int default_write_func(const char *str, size_t len)
 {
-	*(output->outp) = '\0'; 
-	printf("%s", output->buf);
-	output->outp = output->buf;
-}
-
-static int default_buffered_output(char *buf, size_t buflen, void *arg)
-{
-	buffered_output_t *output = (buffered_output_t *)arg;
-	if (output->buflen <= buflen)
-	{
-		output->flush(output);
-		printf("%s", buf);
-		return 0;
-	}
-	if ((output->buflen - (output->outp - output->buf) - 1) <= buflen) 
-		output->flush(output);
-	strncpy(output->outp, buf, buflen); 
-	output->outp += buflen;
-
-	return 0;
+	fwrite(str, 1, len, stderr);
+	return len;
 }
 
 buffered_output_t* alloc_output(size_t buflen)
@@ -31,15 +13,16 @@ buffered_output_t* alloc_output(size_t buflen)
 
 	if ((ptr = (buffered_output_t *)malloc(sizeof(buffered_output_t))) == NULL)
 		return NULL;
-	if ((ptr->buf = (char *)malloc(buflen)) == NULL)
+	if ((ptr->buf = (char *)malloc(buflen+1)) == NULL)
 	{
 		free(ptr);
 		return NULL;
 	}
 	ptr->outp = ptr->buf;
 	ptr->buflen = buflen;
-	ptr->output = default_buffered_output;
-	ptr->flush = default_flush_buffer;
+	/*ptr->output = default_buffered_output;
+	ptr->flush = default_flush_buffer;*/
+	ptr->write = default_write_func;
 
 	return ptr;
 }
@@ -53,21 +36,12 @@ void free_output(buffered_output_t *out)
 	}
 }
 
-output_handler_t 
-override_default_output(buffered_output_t *out, output_handler_t output)
+output_write_func_t 
+override_default_write(buffered_output_t *out, output_write_func_t write_func)
 {
-	output_handler_t old_output = out->output;
-	out->output = output;
+	output_write_func_t old_write = out->write;
+	out->write = write_func;
 
-	return old_output;
-}
-
-flush_handler_t 
-override_default_flush(buffered_output_t *out, flush_handler_t flush)
-{
-	flush_handler_t old_flush = out->flush;
-	out->flush = flush;
-
-	return old_flush;
+	return old_write;
 }
 
--- kbs_bbs/src/output.h
+++ kbs_bbs/src/output.h
@@ -5,14 +5,38 @@ struct __buffered_output;
 
 typedef int (*output_handler_t)(char *buf, size_t buflen, void *arg);
 typedef void (*flush_handler_t)(struct __buffered_output *output);
+typedef int (*output_write_func_t)(const char *str, size_t len);
 
 typedef struct __buffered_output
 {
 	char *buf;
 	size_t buflen;
 	char *outp;
-	output_handler_t output;
-	flush_handler_t flush;
+	/*output_handler_t output;
+	flush_handler_t flush;*/
+	output_write_func_t write;
 } buffered_output_t;
+
+#define BUFFERED_FLUSH(o) \
+{ \
+	if ((o)->outp - (o)->buf > 0) \
+	{ \
+		(o)->write((o)->buf, (o)->outp - (o)->buf); \
+		(o)->outp = (o)->buf; \
+	} \
+}
+
+#define BUFFERED_OUTPUT(o,p,l) \
+{ \
+	if ((o)->buflen < l) \
+	{ \
+		BUFFERED_FLUSH(o); \
+		(o)->write(p, l); \
+	} \
+	if ((o)->buflen - ((o)->outp - (o)->buf) < l) \
+		BUFFERED_FLUSH(o); \
+	memcpy((o)->outp, p, l); \
+	(o)->outp += l; \
+}
 
 #endif /* __OUTPUT_H__ */
