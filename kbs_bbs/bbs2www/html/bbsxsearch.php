88b9beeba0bffee905e55f6b8117bb32d023148c
bad <bad@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2003-10-17 18:45:43 +0000

    hoho
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@5145 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/bbs2www/html/bbsxsearch.php
+++ kbs_bbs/bbs2www/html/bbsxsearch.php
@@ -15,22 +15,24 @@
 ?>
 <style type="text/css">A {color: #0000f0}</style>
 <body>
-<center><?php echo BBS_FULL_NAME; ?> -- 精华区全文检索
-<form action="bbsxsearch.php" method="post" name="qf0">
+<center><?php echo BBS_FULL_NAME; ?> -- 令狐冲精华区全文检索
+<form action="bbsxsearch.php" name="qf0">
+<table width="640" border="0" align="center" cellpadding="0" cellspacing="0">
 <tr> 
-<td width="205"><a href="/bbsxsearch.php"><img src="images/ycul-logo-small.gif" width="205" height="75" border="0"></a></td>
+<td width="74"><a href="/bbsxsearch.php"><img src="images/xsearch.jpg" border="0"></a></td>
 <td align="center" valign="middle">
-  <input type="text" name="q" value="<?php echo $select; ?>" style="width: 368"><br>
-  <select name="area" size="1" style="width: 130" onChange="changeBoard0();">
+  <input type="text" name="q" value="<?php echo $select; ?>" style="width: 500"><br>
+  <select name="area" size="1" style="width: 160" onChange="changeBoard0();">
   <option value="" selected>--- 全部的分类 ---</option>
   <option value="1">BBS系统  -- [系统]</option>
   </select>
-  <select name="board" style="width: 145px" size="1">
+  <select name="board" style="width: 245px" size="1">
   <option value="" selected>--- 全部版面 ---</option>
   </select>
   <input type="submit" style="width: 80px" value="<?php if ($select=="") echo "开始搜索"; else echo "重新搜索"; ?>">
 </td>
 </tr>
+</table>
 </form>
 
 </center>
