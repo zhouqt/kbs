f324b6a74e93fe75dc01cd038c38994e0a828aa9
stiger <stiger@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2003-11-03 14:12:03 +0000

    搜索描述
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@5244 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/bbs2www/src/bbssel.c
+++ kbs_bbs/bbs2www/src/bbssel.c
@@ -31,14 +31,14 @@ int main()
         }
         printf("%s -- 选择讨论区<hr color=green>\n", BBSNAME);
         printf("找不到这个讨论区, ", board);
-        printf("标题中含有'%s'的讨论区有: <br><br>\n", board);
+        printf("标题或者描述中含有'%s'的讨论区有: <br><br>\n", board);
         printf("<table>");
         for (i = 0; i < MAXBOARD; i++) {
             board1 = bc[i].filename;
             title = bc[i].title;
             if (!check_read_perm(currentuser, &bc[i]))
                 continue;
-            if (strcasestr(board1, board) || strcasestr(title, board)) {
+            if (strcasestr(board1, board) || strcasestr(title, board) || strcasestr(bc[i].des, board)) {
                 total++;
                 printf("<tr><td>%d", total);
                 printf("<td><a href=/bbsdoc.php?board=%s>%s</a><td>%s<br>\n", board1, board1, title + 7);
