586d87cd191a9c185fa3439c73987bbcafe7c8d7
kcn <kcn@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2003-11-21 09:30:45 +0000

    这个现在还没有
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@5369 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/local_utl/settoptitle.c
+++ kbs_bbs/local_utl/settoptitle.c
@@ -1,12 +1,15 @@
 #include "bbs.h"
 
+
 int settop(struct boardheader* bh,void* arg)
 {
+#ifdef DEBUG_NEW_READ
   char buf[MAXPATH];
   int i;
   setbdir(DIR_MODE_ZHIDING,buf,bh->filename);
    i=get_num_records(buf, sizeof(struct fileheader));
    board_update_toptitle(bh,i);
+#endif
 //   printf("%s %d\n",bh->filename,i);
 }
 
