aa1e925db8db6594a8327a737ab80c6ed219e2ac
stiger <stiger@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2003-11-28 17:09:43 +0000

    祝福·校内
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@5414 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/src/bbs.c
+++ kbs_bbs/src/bbs.c
@@ -1500,7 +1500,7 @@ int read_hot_info(int ent, struct fileheader *fileinfo, char *direct)
     char ans[4];
     move(t_lines - 1, 0);
     clrtoeol();
-    getdata(t_lines - 1, 0, "选择阅读: 1)本日十大新闻 2)本日十大祝福 3)近期热点 4)系统热点 5)日历日记[1]: ", ans, 3, DOECHO, NULL, true);
+    getdata(t_lines - 1, 0, "选择阅读: 1)本日十大新闻 2)祝福·校内 3)近期热点 4)系统热点 5)日历日记[1]: ", ans, 3, DOECHO, NULL, true);
     switch (ans[0])
 	{
     case '2':
