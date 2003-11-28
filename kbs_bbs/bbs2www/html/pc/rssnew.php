2c5241aa57653ea4955246f43080f96dbc9bfe6b
windinsn <windinsn@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2003-11-28 07:41:57 +0000

    最新文章 => xml
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@5409 de4889a5-8ad4-4bf3-bd87-57997c21d515

---
+++ kbs_bbs/bbs2www/html/pc/rssnew.php
@@ -0,0 +1,71 @@
+<?php
+	/*
+	** @id:windinsin nov 29,2003
+	*/
+	require("rsstool.php");
+	
+	$link = pc_db_connect();
+	$rss = array();
+	$rss[channel] = array(
+			"siteaddr" => "http://".$pcconfig["SITE"],
+			"title" => BBS_FULL_NAME."即时Blog文章列表" ,
+			"pcaddr" => "http://".$pcconfig["SITE"],
+			"desc" => BBS_FULL_NAME."最新".$pcconfig["NEWS"]."篇文章",
+			"email" => BBS_FULL_NAME,
+			"publisher" => BBS_FULL_NAME,
+			"creator" => BBS_FULL_NAME,
+			"rights" => BBS_FULL_NAME,
+			"date" => date("Y-m-d"),
+			"updatePeriod" => "即时更新",
+			"updateFrequency" => "最新的".$pcconfig["NEWS"]."篇Blog文章",
+			"updateBase" => date("Y-m-d H:i:s"),
+			
+			);
+	
+	$query = "SELECT * FROM nodes WHERE `access` = 0 ORDER BY `nid` DESC LIMIT 0 , ".$pcconfig["NEWS"]." ; ";
+	$result = mysql_query($query,$link);
+	$j = 0;
+	$bloguser = array();
+	while($rows=mysql_fetch_array($result))
+	{
+		if(!$bloguser[$rows[uid]])
+		{
+			$query = "SELECT `corpusname`,`theme`,`username` FROM users WHERE `uid` = '".$rows[uid]."' LIMIT 0 , 1 ;";
+			$result0 = mysql_query($query);
+			$rows0 = mysql_fetch_array($result0);
+			$bloguser[$rows[uid]] = array(
+						"USER" => $rows0[username],
+						"NAME" => $rows0[corpusname],
+						"THEM" => $rows0[theme]
+						);
+		}
+		
+		$body = "<br>\n".
+			"来自: ".html_format($bloguser[$rows[uid]]["NAME"])."<br>\n".
+			"主题: ".html_format($bloguser[$rows[uid]]["THEM"])."<br>\n".
+			"作者: ".$bloguser[$rows[uid]]["USER"]."<br>\n".
+			"发信站: ".BBS_FULL_NAME."<br>\n".
+			"时间: ".time_format($rows[created])."<br>\n".
+			"<hr size=1>\n".
+			html_format($rows[body],TRUE).
+			"<hr size=1>\n".
+			"(<a href=\"http://".$pcconfig["SITE"]."/pc/pccon.php?id=".$rows[uid]."&amp;nid=".$rows[nid]."&amp;tid=".$rows[tid]."&s=all\">浏览全文</a>\n".
+			"<a href=\"http://".$pcconfig["SITE"]."/pc/pccom.php?act=pst&nid=330\">发表评论</a>)<br>\n".
+			"<a href=\"http://".$pcconfig["SITE"]."/pc/rss.php?userid=".$bloguser[$rows[uid]]["USER"]."\"><img src=\"http://".$pcconfig["SITE"]."/pc/images/xml.gif\" border=\"0\" align=\"absmiddle\" alt=\"XML\">Blog地址：http://".$pcconfig["SITE"]."/pc/rss.php?userid=".$bloguser[$rows[uid]]["USER"]."</a>";
+		$rss[useretems][$j] = array(
+					"addr" => "http://".$pcconfig["SITE"]."/pc/pccon.php?id=".$rows[uid]."&amp;nid=".$rows[nid]."&amp;tid=".$rows[tid],
+					"title" => htmlspecialchars(stripslashes($rows[subject])),
+					"desc" => $body,
+					"publisher" => BBS_FULL_NAME,
+					"creator" => $bloguser[$rows[uid]]["USER"],
+					"rights" => $bloguser[$rows[uid]]["USER"].".bbs@".$pcconfig["SITE"]
+					);
+		$j ++;
+	}
+	mysql_free_result($result);
+	
+	pc_db_close($link);
+	header("Content-Type: text/xml");
+	header("Content-Disposition: inline;filename=userrss.xml");
+	@pc_rss_user($rss);
+?>
---
+++ kbs_bbs/bbs2www/html/pc/rsstool.php
@@ -0,0 +1,149 @@
+<?php
+	$needlogin=0;
+	require("pcfuncs.php");
+	
+	function pc_rss_init()
+	{
+?>
+<?xml version="1.0" encoding="gb2312"?>
+<rdf:RDF 
+	xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+	xmlns:dc="http://purl.org/dc/elements/1.1/" 
+	xmlns:sy="http://purl.org/rss/1.0/modules/syndication/"
+	xmlns:co="http://purl.org/rss/1.0/modules/company/"
+	xmlns:ti="http://purl.org/rss/1.0/modules/textinput/"
+	xmlns="http://purl.org/rss/1.0/"
+>
+<?php	
+	}
+	
+	function pc_rss_end()
+	{
+?>
+</rdf:RDF>
+<?php
+	}
+	
+	function pc_rss_channel($rss)
+	{
+?>
+	<channel rdf:about="<?php echo $rss[channel][siteaddr]; ?>">
+		<title><?php echo $rss[channel][title]; ?></title>
+		<link><?php echo $rss[channel][pcaddr]; ?></link>
+		<description><?php echo $rss[channel][desc]; ?></description>
+		<dc:language>gb2312</dc:language> 
+<?php
+		if($rss[channel][publisher])
+		{
+?>
+		<dc:publisher><?php echo $rss[channel][publisher]; ?></dc:publisher> 
+<?php
+		}
+		if($rss[channel][email])
+		{
+?>
+		<dc:creator><?php echo $rss[channel][email]; ?></dc:creator> 
+<?php
+		}
+		if($rss[channel][rights])
+		{
+?>
+		<dc:rights>Copyright <?php echo $rss[channel][rights]; ?></dc:rights> 
+<?php
+		}
+		if($rss[channel][date])
+		{
+?>
+		<dc:date><?php echo $rss[channel][date]; ?></dc:date>
+<?php
+		}
+	 	if($rss[channel][updatePeriod])
+	 	{
+?>
+		<sy:updatePeriod><?php echo $rss[channel][updatePeriod]; ?></sy:updatePeriod>
+<?php
+		}
+		if($rss[channel][updateFrequency])
+		{
+?>
+		<sy:updateFrequency><?php echo $rss[channel][updateFrequency]; ?></sy:updateFrequency>
+<?php
+		}
+		if($rss[channel][updateBase])
+		{
+?>
+		<sy:updateBase><?php echo $rss[channel][updateBase]; ?></sy:updateBase> 
+<?php
+		}
+		if($rss[channel][logoimg])
+		{
+?>
+		<image rdf:resource="<?php echo $rss[channel][logoimg]; ?>" />
+<?php
+		}
+?>
+		<items>
+			<rdf:Seq>
+				<rdf:li resource="<?php echo $rss[channel][siteaddr]; ?>" /> 
+				<rdf:li resource="<?php echo $rss[channel][pcaddr]; ?>" /> 
+			</rdf:Seq>
+		</items>
+	</channel>
+<?php
+	}
+	
+	function pc_rss_etem($etem)
+	{
+?>
+	<item rdf:about="<?php echo $etem[etemaddr]; ?>">
+		<title><?php echo $etem[etemtitle]; ?></title> 
+		<link><?php echo $etem[etemaddr]; ?></link>
+		<dc:creator><?php echo $etem[etemauth]; ?></dc:creator> 
+		<dc:date><?php echo $etem[etemtime]; ?></dc:date> 
+		<description>
+			<![CDATA[
+				<?php echo $etem[etemdesc]; ?>
+			]]> 
+		</description>
+	</item>
+<?php
+	}
+
+	function pc_rss_user_etem($useretem)
+	{
+?>	
+		<item rdf:about="<?php echo $useretem[addr]; ?>">
+		<title><?php echo $useretem[title]; ?></title> 
+		<link><?php echo $useretem[addr]; ?></link> 
+		<description>
+		<![CDATA[
+			<?php echo $useretem[desc]; ?>
+		]]> 
+		</description>
+		<dc:publisher><?php echo $useretem[publisher]; ?></dc:publisher> 
+		<dc:creator><?php echo $useretem[creator]; ?></dc:creator> 
+		<dc:rights><?php echo $useretem[rights]; ?></dc:rights> 
+		<dc:subject><?php echo $useretem[subject]; ?></dc:subject> 
+		</item>	
+<?php		
+	}
+	
+	function pc_rss_user($rss)
+	{
+		pc_rss_init();
+		pc_rss_channel($rss);
+		foreach($rss[useretems] as $useretem)
+			pc_rss_user_etem($useretem);
+		pc_rss_end();
+	}
+	
+	function pc_rss_output($rss)
+	{
+		pc_rss_init();
+		pc_rss_channel($rss);
+		foreach($rss[etems] as $etem)
+			pc_rss_etem($etem);
+		pc_rss_end();
+	}
+	
+?>
