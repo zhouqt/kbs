69e7c104eae8aed7a7b279f6df2466c8a5b917ca
stiger <stiger@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2003-12-01 08:06:43 +0000

    new推荐
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@5461 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/local_utl/genhot.c
+++ kbs_bbs/local_utl/genhot.c
@@ -3,20 +3,29 @@
 #include "bbs.h"
 #ifdef COMMEND_ARTICLE
 
-#define	MAX_COMMEND 15
+#define	MAX_COMMEND 5
 
 int gen_commend_xml()
 {
-	int fd,dirfd;
+	int dirfd;
 	FILE *fp;
+	FILE *fp1;
 	struct fileheader dirfh;
 	struct fileheader fh;
 	char dirpath[STRLEN];
 	char dirfile[STRLEN];
-	int show=0;
 	char xml_buf[256];
+	char buf[256];
+	struct stat st;
+	int numrecords;
+	int i;
 
 	setbfile(dirpath, COMMEND_ARTICLE, DIGEST_DIR);
+    if (stat(dirpath, &st) < 0)
+        return -1;
+	numrecords = st.st_size / sizeof(struct fileheader) ;
+	if (numrecords <= 0)
+		return -1;
 
     if ((fp = fopen("xml/commend.xml", "w")) == NULL) 
 		return -1;
@@ -26,32 +35,34 @@ int gen_commend_xml()
 
 	dirfd = open(dirpath, O_RDONLY);
 	if( dirfd >= 0 ){
+		if( numrecords > MAX_COMMEND )
+			lseek(dirfd, sizeof(struct fileheader)*(numrecords - MAX_COMMEND), SEEK_SET);
+
 		while(read(dirfd, &dirfh, sizeof(dirfh)) >= sizeof(dirfh) ){
 
-			if(show > MAX_COMMEND)
-				break;
+			setbfile(dirfile, COMMEND_ARTICLE, dirfh.filename);
 
-			setbdir(DIR_MODE_NORMAL, dirfile, dirfh.o_board);
-
-			if ((fd = open(dirfile, O_RDWR, 0644)) < 0)
+			if(( fp1=fopen(dirfile, "r"))==NULL )
 				continue;
-
-    		if(get_records_from_id(fd, dirfh.o_id, &fh, 1, NULL) == 0 )
-			{
-				close(fd);
-				continue;
-			}
-			close(fd);
 
 			fprintf(fp, "<hotsubject>\n");
-			fprintf(fp, "<title>%s</title>\n", encode_xml(xml_buf, fh.title, 
-						sizeof(xml_buf)));
-			fprintf(fp, "<author>%s</author>\n", fh.owner);
-			fprintf(fp, "<board>%s</board>\n", dirfh.o_board);
-			//fprintf(fp, "<time>%d</time>\n", top[i].date);
-			//fprintf(fp, "<number>%d</number>\n", top[i].number);
-			fprintf(fp, "<groupid>%d</groupid>\n", fh.id);
-			fprintf(fp, "</hotsubject>\n");
+			fprintf(fp, "<title>%s</title>\n", encode_xml(xml_buf, dirfh.title, sizeof(xml_buf)));
+			fprintf(fp, "<author>%s</author>\n", dirfh.owner);
+			fprintf(fp, "<board>%s</board>\n", COMMEND_ARTICLE);
+			fprintf(fp, "<o_board>%s</o_board>\n", dirfh.o_board);
+			fprintf(fp, "<o_id>%d</o_id>\n", dirfh.o_id);
+			fprintf(fp, "<o_groupid>%d</o_groupid>\n<brief>", dirfh.o_groupid);
+			for(i=0;i<4;i++) fgets(buf, 255, fp1);
+			for(i=0;i<4;i++){
+				if(fgets(buf, 255, fp1) ){
+					buf[255]=0;
+					fprintf(fp, "%s", encode_xml(xml_buf, buf, sizeof(xml_buf)) );
+				}else
+					break;
+			}
+			fprintf(fp, "</brief>\n</hotsubject>\n");
+
+			fclose(fp1);
         }
 		close(dirfd);
     }
