4a63af27ec44f54fd5feffbe82daf6d70bb2c05e
stiger <stiger@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2003-12-02 06:57:38 +0000

    好看
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@5475 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/bbs2www/xml/mainpage.php
+++ kbs_bbs/bbs2www/xml/mainpage.php
@@ -1,7 +1,5 @@
 <?php
 require("site.php");
-if (BUILD_PHP_EXTENSION==0)
-    @dl("libphpbbslib.so");
 if (!bbs_ext_initialized())
 	bbs_init_ext();
 
@@ -322,8 +320,10 @@ while($board = array_shift($boards))
 ?>
 <tr>
 <td valign="top" class="MainContentText"><LI class=default><a href="/bbsrecon.php?id=<?php echo $commend_id;?>"><?php echo htmlspecialchars(iconv("UTF-8", "GBK", $commend_title));?></a>&nbsp;&lt;<a href="/bbsdoc.php?board=<?php echo $brd_encode;?>"><?php echo htmlspecialchars($brdarr["DESC"]);?></a>&gt;
-<dl style="MARGIN-TOP: 5px;MARGIN-BOTTOM: 5px; MARGIN-LEFT: 25px;"><dt>
-<?php echo htmlspecialchars(iconv("UTF-8","GBK",$commend_brief));?>&nbsp;&nbsp;<a href="/cgi-bin/bbs/bbstcon?board=<?php echo $brd_encode;?>&gid=<?php echo $commend_o_groupid;?>">同主题阅读原版原文</a></dl>
+&nbsp;&nbsp;[<a href="/cgi-bin/bbs/bbstcon?board=<?php echo $brd_encode;?>&gid=<?php echo $commend_o_groupid;?>">同主题阅读原版原文</a>]
+<dl style="MARGIN-TOP: 1px;MARGIN-BOTTOM: 5px; MARGIN-LEFT: 25px;"><dt>
+<?php echo htmlspecialchars(iconv("UTF-8","GBK",$commend_brief));?>
+</dl>
 </td>
 </tr>
 <?php
