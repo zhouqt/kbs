a67df5f8782c9016d8fe53ea34a0fddf79e8bb1e
windinsn <windinsn@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2003-12-11 04:22:43 +0000

    use index.php
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@5526 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/bbs2www/html/pc/pcsearch.php
+++ kbs_bbs/bbs2www/html/pc/pcsearch.php
@@ -88,8 +88,8 @@
 			$themekey = urlencode(stripslashes($rows[theme]));
 			echo "<tr>\n<td class=t3>".($startno + $i + 1)."</td>\n".
 				"<td class=t4><a href=\"/bbsqry.php?userid=".html_format($rows[username])."\">".html_format($rows[username])."</a></td>\n".
-				"<td class=t3>&nbsp;<a href=\"pcdoc.php?userid=".$rows[username]."\">".html_format($rows[corpusname])."</a></td>\n".
-				"<td class=t5>&nbsp;<a href=\"pcdoc.php?userid=".$rows[username]."\">".html_format($rows[description])."</a></td>\n".
+				"<td class=t3>&nbsp;<a href=\"index.php?id=".$rows[username]."\">".html_format($rows[corpusname])."</a></td>\n".
+				"<td class=t5>&nbsp;<a href=\"index.php?id=".$rows[username]."\">".html_format($rows[description])."</a></td>\n".
 				"<td class=t3>&nbsp;<a href=\"pcsearch.php?exact=0&key=t&keyword=".$themekey."\">".html_format($rows[theme])."</a></td>\n".
 				"<td class=\"t4\">".$rows[nodescount]."</a>".
 				"<td class=\"t3\">".$rows[visitcount]."</a>".
