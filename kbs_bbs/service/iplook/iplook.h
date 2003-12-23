637f5e4f1eb2c80e40128136ebce6b2dd0987af8
kxn <kxn@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2003-12-23 04:00:21 +0000

    Added API description....
    K core is great!
    
    
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@5733 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/service/iplook/iplook.h
+++ kbs_bbs/service/iplook/iplook.h
@@ -4,6 +4,17 @@
 #ifdef __cplusplus
 extern "C" {
 #endif 
+/* 
+ *  *
+ *  *   IP 查询接口， kxn@smth
+ *  *    
+ *  *     @param  const char *ipstr      IP 字符串
+ *  *     @param  char ** area           接受 area 的字符串指针
+ *  *     @param  char ** location       接受 location 的字符串指针
+ *  *     
+ *  *     @result int                    0 表查询成功， -1 查找不到
+ *  * 
+ *  * */
 	
 int LookIP(const char *ipstr, char **area, char **location);
 
