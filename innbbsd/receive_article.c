baf2e90039244d2ae204abea2530651de96ad3d2
stiger <stiger@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2003-12-26 08:41:47 +0000

    转信文章计算字节数
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@5796 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- innbbsd/receive_article.c
+++ innbbsd/receive_article.c
@@ -855,6 +855,7 @@ char *pathname, *firstpath;
     chdir(BBSHOME);
     resolve_boards();
     linkflag = find_thread(&threadfh, board, header.title);
+    header.eff_size = get_effsize(article);
     ret = after_post(NULL, &header, board, linkflag ? &threadfh : NULL, 0);
     if ((ret != 0) && (ret != 2)) {
         innbbslog(":Err:after_post Unable to post.\n");
