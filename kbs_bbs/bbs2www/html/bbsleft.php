10c37cac8c469e474a75c50dfb0c03beab19bd73
flyriver <flyriver@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2004-01-01 04:30:08 +0000

    修改顺序。
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@5856 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/bbs2www/html/bbsleft.php
+++ kbs_bbs/bbs2www/html/bbsleft.php
@@ -394,39 +394,64 @@
 		</tr>
 		<tr>
 			<td width="16"><img src="/images/open.gif" border="0"></td>
-			<td><a href="/cgi-bin/bbs/bbs0an" target="f3"><img src="/images/t3.gif" border="0" alt="精华公布栏" align="absmiddle"> 精华公布栏</a></td>
+			<td><a href="/cgi-bin/bbs/bbs0an" target="f3"><img src="/images/t3.gif" border="0" alt="精华区" align="absmiddle"> 精华区</a></td>
 		</tr>
 		<tr>
 			<td width="16">
-				<DIV class="r" id="divexpa">
-				<a href='javascript:changemn("exp");'><img id="imgexp" src="/images/close.gif" border="0"></a>
+				<DIV class="r" id="divboarda">
+				<a href='javascript:changemn("board");'><img id="imgboard" src="/images/close.gif" border="0"></a>
 				</DIV>
 			</td>
-			<td>
-			<a href='javascript:changemn("exp");'>
-			<img src="/images/t7.gif" border="0" alt="水木特刊Web版" align="absmiddle"> 水木特刊Web版
-			</a>
-			</td>
+			<td><a href="/bbssec.php" target="f3"><img src="/images/folder4.gif" border="0" alt="分类讨论区" align="absmiddle"> 分类讨论区</a></td>
 		</tr>
 		<tr>
 			<td> </td>
 			<td>
-				<DIV class="s" id="divexp">
-					&nbsp;
-					<img src="/images/line.gif" border="0" align="absmiddle">
-					<a href="/express/1103/smth_express.htm" target="f3">2003年11月号</a><br>
-					&nbsp;
-					<img src="/images/line.gif" border="0" align="absmiddle">
-					<a href="/express/0903/smth_express.htm" target="f3">2003年9月号</a><br>
-					&nbsp;
-					<img src="/images/line.gif" border="0" align="absmiddle">
-					<a href="/express/0703/smth_express.htm" target="f3">2003年7月号</a><br>
-					&nbsp;
-					<img src="/images/line1.gif" border="0" align="absmiddle">
-					<a href="/express/0603/smth_express.htm" target="f3">2003年6月号</a><br>
+				<DIV class="s" id="divboard">
+<?php
+	display_board_list($section_names,$section_nums);
+?>
 				</DIV>
 			</td>
 		</tr>
+		<tr>
+			<td width="16"><img src="/images/open.gif" border="0"></td>
+			<form action="cgi-bin/bbs/bbssel" target="f3">
+			<td>
+			<img src="/images/t8.gif" border="0" alt="搜索讨论区" align="absmiddle">
+			<input name="board" type="text" class="f2" value="搜索讨论区" size="12" onclick="this.value=''" /> 
+<input name="submit" type="submit" value="GO" style="width:25px;height:20px;font-size: 12px;color: #ffffff;border-style: none;background-color: #718BD6;" />
+			</td>
+			</form>
+		</tr>
+<?php
+	if($currentuser["userid"]!="guest"){
+?>
+		<tr>
+			<td width="16">
+				<DIV class="r" id="divfava">
+				<a href='javascript:changemn("fav");'><img id="imgfav" src="/images/close.gif" border="0"></a>
+				</DIV>
+			</td>
+			<td><a href="bbsfav.php?select=-1" target="f3"><img src="/images/folder3.gif" border="0" alt="我的收藏夹" align="absmiddle"> 我的收藏夹</a></td>
+		</tr>
+		<tr>
+			<td> </td>
+			<td>
+				<DIV class="s" id="divfav">
+<?php
+	display_my_favorite();
+?>
+				</DIV>
+			</td>
+		</tr>
+<?php
+		}
+?>
+<?php
+	if (defined(HAVE_PC))
+	{
+?>
 		<tr>
 			<td width="16">
 				<DIV class="r" id="divpca">
@@ -467,6 +492,9 @@
 		</tr>
 		
 <?php
+	} // defined(HAVE_PC)
+?>
+<?php
 	if($currentuser["userid"]!="guest"){
 		if (bbs_getmailnum($currentuser["userid"],$total,$unread, 0, 0)) {
 			if( $unread != 0 ){
@@ -478,60 +506,11 @@
 ?>
 		<tr>
 			<td width="16">
-				<DIV class="r" id="divtoola">
-				<a href='javascript:changemn("tool");'><img id="imgtool" src="/images/close.gif" border="0"></a>
-				</DIV>
-			</td>
-			<td>
-			<a href='javascript:changemn("tool");'>
-			<img src="/images/t4.gif" border="0" alt="个人工具箱" align="absmiddle"> 个人工具箱
-			</a>
-			</td>
-		</tr>
-		<tr>
-			<td> </td>
-			<td>
-				<DIV class="s" id="divtool">
-					&nbsp;
-					<img src="/images/line.gif" border="0" align="absmiddle">
-					<a href="/bbsfillform.html" target="f3">填写注册单</a><br>
-					&nbsp;
-					<img src="/images/line.gif" border="0" align="absmiddle">
-					<a href="/bbsinfo.php" target="f3">个人资料</a><br>
-					&nbsp;
-					<img src="/images/line.gif" border="0" align="absmiddle">
-					<a href="bbsplan.php" target="f3">改说明档</a><br>
-					&nbsp;
-					<img src="/images/line.gif" border="0" align="absmiddle">
-					<a href="bbssig.php" target="f3">改签名档</a><br>
-					&nbsp;
-					<img src="/images/line.gif" border="0" align="absmiddle">
-					<a href="cgi-bin/bbs/bbspwd" target="f3">修改密码</a><br>
-					&nbsp;
-					<img src="/images/line.gif" border="0" align="absmiddle">
-					<a href="cgi-bin/bbs/bbsparm" target="f3">修改个人参数</a><br>
-					&nbsp;
-					<img src="/images/line.gif" border="0" align="absmiddle">
-					<a href="/bbsal.php" target="f3">通讯录</a><br>
-					&nbsp;
-					<img src="/images/line.gif" border="0" align="absmiddle">
-					<a href="/bbsrsmsmsg.php" target="f3">短信管理器</a><br>
-					&nbsp;
-					<img src="/images/line.gif" border="0" align="absmiddle">
-					<a href="/bbsnick.php" target="f3">临时改昵称</a><br>
-					&nbsp;
-					<img src="/images/line1.gif" border="0" align="absmiddle">
-					<a href="/bbsfall.php" target="f3">设定好友</a><br>
-				</DIV>
-			</td>
-		</tr>
-		<tr>
-			<td width="16">
 				<DIV class="r" id="divmaila">
 				<a href='javascript:changemn("mail");'><img id="imgmail" src="/images/close.gif" border="0"></a>
 				</DIV>
 			</td>
-			<td><a href="/bbsmail.php" target="f3"><img src="/images/t5.gif" border="0" alt="处理信件" align="absmiddle"> 处理信件</a></td>
+			<td><a href="/bbsmail.php" target="f3"><img src="/images/t5.gif" border="0" alt="我的信箱" align="absmiddle"> 我的信箱</a></td>
 		</tr>
 		<tr>
 			<td> </td>
@@ -540,48 +519,6 @@
 <?php
 	display_mail_menu($currentuser["userid"]);
 ?>					
-				</DIV>
-			</td>
-		</tr>
-<?php
-		}
-?>
-		<tr>
-			<td width="16">
-				<DIV class="r" id="divboarda">
-				<a href='javascript:changemn("board");'><img id="imgboard" src="/images/close.gif" border="0"></a>
-				</DIV>
-			</td>
-			<td><a href="/bbssec.php" target="f3"><img src="/images/folder4.gif" border="0" alt="分类讨论区" align="absmiddle"> 分类讨论区</a></td>
-		</tr>
-		<tr>
-			<td> </td>
-			<td>
-				<DIV class="s" id="divboard">
-<?php
-	display_board_list($section_names,$section_nums);
-?>
-				</DIV>
-			</td>
-		</tr>
-<?php
-	if($currentuser["userid"]!="guest"){
-?>
-		<tr>
-			<td width="16">
-				<DIV class="r" id="divfava">
-				<a href='javascript:changemn("fav");'><img id="imgfav" src="/images/close.gif" border="0"></a>
-				</DIV>
-			</td>
-			<td><a href="bbsfav.php?select=-1" target="f3"><img src="/images/folder3.gif" border="0" alt="个人定制区" align="absmiddle"> 个人定制区</a></td>
-		</tr>
-		<tr>
-			<td> </td>
-			<td>
-				<DIV class="s" id="divfav">
-<?php
-	display_my_favorite();
-?>
 				</DIV>
 			</td>
 		</tr>
@@ -638,6 +575,62 @@
 				</DIV>
 			</td>
 		</tr>
+<?php
+	if($currentuser["userid"]!="guest")
+	{
+?>
+		<tr>
+			<td width="16">
+				<DIV class="r" id="divtoola">
+				<a href='javascript:changemn("tool");'><img id="imgtool" src="/images/close.gif" border="0"></a>
+				</DIV>
+			</td>
+			<td>
+			<a href='javascript:changemn("tool");'>
+			<img src="/images/t4.gif" border="0" alt="个人参数设置" align="absmiddle"> 个人参数设置
+			</a>
+			</td>
+		</tr>
+		<tr>
+			<td> </td>
+			<td>
+				<DIV class="s" id="divtool">
+					&nbsp;
+					<img src="/images/line.gif" border="0" align="absmiddle">
+					<a href="/bbsfillform.html" target="f3">填写注册单</a><br>
+					&nbsp;
+					<img src="/images/line.gif" border="0" align="absmiddle">
+					<a href="/bbsinfo.php" target="f3">个人资料</a><br>
+					&nbsp;
+					<img src="/images/line.gif" border="0" align="absmiddle">
+					<a href="bbsplan.php" target="f3">改说明档</a><br>
+					&nbsp;
+					<img src="/images/line.gif" border="0" align="absmiddle">
+					<a href="bbssig.php" target="f3">改签名档</a><br>
+					&nbsp;
+					<img src="/images/line.gif" border="0" align="absmiddle">
+					<a href="cgi-bin/bbs/bbspwd" target="f3">修改密码</a><br>
+					&nbsp;
+					<img src="/images/line.gif" border="0" align="absmiddle">
+					<a href="cgi-bin/bbs/bbsparm" target="f3">修改个人参数</a><br>
+					&nbsp;
+					<img src="/images/line.gif" border="0" align="absmiddle">
+					<a href="/bbsal.php" target="f3">通讯录</a><br>
+					&nbsp;
+					<img src="/images/line.gif" border="0" align="absmiddle">
+					<a href="/bbsrsmsmsg.php" target="f3">短信管理器</a><br>
+					&nbsp;
+					<img src="/images/line.gif" border="0" align="absmiddle">
+					<a href="/bbsnick.php" target="f3">临时改昵称</a><br>
+					&nbsp;
+					<img src="/images/line1.gif" border="0" align="absmiddle">
+					<a href="/bbsfall.php" target="f3">设定好友</a><br>
+				</DIV>
+			</td>
+		</tr>
+<?php
+	}
+?>
 <!--
 		<tr>
 			<td width="16">
@@ -662,14 +655,35 @@
 		</tr>
 -->
 		<tr>
-			<td width="16"><img src="/images/open.gif" border="0"></td>
-			<form action="cgi-bin/bbs/bbssel" target="f3">
-			<td>
-			<img src="/images/t8.gif" border="0" alt="选择讨论区" align="absmiddle">
-			<input name="board" type="text" class="f2" value="选择讨论区" size="12" onmouseover="this.focus()" onfocus="this.select()" /> 
-<input name="submit" type="submit" value=">>GO" style="width:30px;height:20px;font-size: 12px;color: #ffffff;border-style: none;background-color: #718BD6;" />
+			<td width="16">
+				<DIV class="r" id="divexpa">
+				<a href='javascript:changemn("exp");'><img id="imgexp" src="/images/close.gif" border="0"></a>
+				</DIV>
 			</td>
-			</form>
+			<td>
+			<a href='javascript:changemn("exp");'>
+			<img src="/images/t7.gif" border="0" alt="水木特刊Web版" align="absmiddle"> 水木特刊Web版
+			</a>
+			</td>
+		</tr>
+		<tr>
+			<td> </td>
+			<td>
+				<DIV class="s" id="divexp">
+					&nbsp;
+					<img src="/images/line.gif" border="0" align="absmiddle">
+					<a href="/express/1103/smth_express.htm" target="f3">2003年11月号</a><br>
+					&nbsp;
+					<img src="/images/line.gif" border="0" align="absmiddle">
+					<a href="/express/0903/smth_express.htm" target="f3">2003年9月号</a><br>
+					&nbsp;
+					<img src="/images/line.gif" border="0" align="absmiddle">
+					<a href="/express/0703/smth_express.htm" target="f3">2003年7月号</a><br>
+					&nbsp;
+					<img src="/images/line1.gif" border="0" align="absmiddle">
+					<a href="/express/0603/smth_express.htm" target="f3">2003年6月号</a><br>
+				</DIV>
+			</td>
 		</tr>
 		<tr>
 			<td width="16">
@@ -679,7 +693,7 @@
 			</td>
 			<td>
 			<a href='javascript:changemn("ser");'>
-			<img src="/images/t9.gif" border="0" alt="系统资讯和服务" align="absmiddle"> 系统资讯和服务
+			<img src="/images/t9.gif" border="0" alt="文件下载及其他" align="absmiddle"> 文件下载及其他
 			</a>
 			</td>
 		</tr>
