e5fd0dcf75c790904b8b049b844831a030d55fbf
flyriver <flyriver@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2004-01-03 05:07:59 +0000

    继续添加说明。
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@5874 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/contrib/build-RH9.sh
+++ kbs_bbs/contrib/build-RH9.sh
@@ -2,8 +2,14 @@
 # $Id$
 
 # 修改这里的参数定制您的站点
+# BBSHOME 表示您的 BBS 所在的目录
 BBSHOME=/usr/local/bbs
-BBSSITE=devel
+
+# BBSSITE 表示您采用的站点定制文件
+BBSSITE=fb2k
+
+# WWWROOT 表示您的 WWW 所在的目录
+# 注意：这个目录不是 httpd.conf 里面的 DocumentRoot 参数。
 WWWROOT=/var/www
 
 # 以下的代码请不要修改，除非您能确定自己在干什么
