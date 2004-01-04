c98757ff5fd955e5e27327b2e02fab414936b367
flyriver <flyriver@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2004-01-04 09:49:03 +0000

    *** empty log message ***
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@5887 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/bbs2www/sites/devel/site.php
+++ kbs_bbs/bbs2www/sites/devel/site.php
@@ -7,6 +7,8 @@ function getattachtmppath($userid,$utmpnum)
 
 define("ATTACHMAXSIZE","1048576");
 define("ATTACHMAXCOUNT","20");
+define("MAINPAGE_FILE","mainpage.html");
+
 $section_nums = array("0", "1", "2", "3", "4", "5", "6", "7", "8", "9","A","B","C");
 $section_names = array(
     array("BBS ÏµÍ³", "[Õ¾ÄÚ]"),
