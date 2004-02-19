4749b48bd77704b5971f76fe7f141cec68f4eda2
stiger <stiger@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2004-02-19 11:07:10 +0000

    初步整理完毕
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@5987 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/local_utl/repass.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-gcc -o repass repass.c
-gcc -o rehome rehome.c rehome.o
-cp /home/bbs/.PASSWDS /home/bbs/.PASSWDS.old
-repass
-cp /home/bbs/.PASSWDS.tmp /home/bbs/.PASSWDS
-chown bbs.bbs /home/bbs/.PASSWDS
-rehome
