169095ca3f528f361a17bf05610b0565e7bf4d97
flyriver <flyriver@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2004-02-22 07:48:47 +0000

    Added some define.
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@6013 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/bbs2www/sites/happy/site.php
+++ kbs_bbs/bbs2www/sites/happy/site.php
@@ -7,17 +7,19 @@ function getattachtmppath($userid,$utmpnum)
 
 define("ATTACHMAXSIZE","1048576");
 define("ATTACHMAXCOUNT","20");
+define("HAVE_PC", 1); // 支持个人文集
+define("MAINPAGE_FILE","mainpage.php"); // 不使用静态 mainpage 页面
 $section_nums = array("0", "1", "3", "4", "5", "6", "7", "8", "9");
 $section_names = array(
-    array("BBS 系统", "[本站]"),
-    array("快乐聚会", "[院系][协会][团体]"),
-    array("电脑技术", "[电脑][系统][网路]"),
-    array("电脑游戏", "[游戏]"),
-    array("艺术文化", "[文艺][艺术][学术]"),
-    array("转信专区", "[转信]"),
-    array("体育休闲", "[体育][星座][音乐]"),
-    array("知性感性", "[闲聊][感性]"),
-    array("新闻时事", "[新闻][特快][信息]")
+	array("本站系统", "[HAPPYBBS系统区]"),
+	array("网络服务", "[WWW][FTP][校园网]"),
+	array("群体组织", "[院系][协会][团体]"),
+	array("个人空间", "[你] [我] [他] [她]"),
+	array("艺术文化", "[文艺][艺术][学术]"),
+	array("电脑技术", "[电脑][系统][网路]"),
+	array("休闲娱乐", "[流行][星座][游戏]"),
+	array("知性感性", "[闲聊][感性]"),
+	array("其他版面", "[未分类]")
 );
 
 $order_articles = TRUE;
