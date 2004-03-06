ee3d075c149f5a4450c495a7754ea05e989188a3
flyriver <flyriver@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2004-03-06 09:18:18 +0000

    size_t -> off_t
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@6082 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/local_utl/calc_effsize.c
+++ kbs_bbs/local_utl/calc_effsize.c
@@ -30,7 +30,7 @@ int calcboard(struct boardheader * bh, void * arg)
     fcntl(fd, F_SETLKW, &ldata);
     total = buf.st_size / size;
 
-    if ((i = safe_mmapfile_handle(fd, PROT_READ|PROT_WRITE, MAP_SHARED, (void **) &ptr, (off_t*)&buf.st_size)) != 1) {
+    if ((i = safe_mmapfile_handle(fd, PROT_READ|PROT_WRITE, MAP_SHARED, (void **) &ptr, &buf.st_size)) != 1) {
         if (i == 2)
             end_mmapfile((void *) ptr, buf.st_size, -1);
         ldata.l_type = F_UNLCK;
@@ -44,7 +44,7 @@ int calcboard(struct boardheader * bh, void * arg)
         char* p,*op;
         char ffn[80];
         int j;
-        size_t fsize;
+        off_t fsize;
 
         printf("\r%d", i);
         setbfile(ffn, bh->filename, ptr1->filename);
@@ -52,7 +52,7 @@ int calcboard(struct boardheader * bh, void * arg)
             int k,abssize=0,entercount=0,ignoreline=0;
             char * attach;
             long attach_len;
-            j = safe_mmapfile(ffn, O_RDONLY, PROT_READ, MAP_SHARED, (void **) &p, (off_t *)&fsize, NULL);
+            j = safe_mmapfile(ffn, O_RDONLY, PROT_READ, MAP_SHARED, (void **) &p, &fsize, NULL);
             op = p;
             if(j) {
                 k=fsize;
