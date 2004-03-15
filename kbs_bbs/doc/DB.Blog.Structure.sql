5cafd5c6d7ef3429d367fd0d8827b5616742cc4c
windinsn <windinsn@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2004-03-15 04:27:48 +0000

    blog数据库表结构
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@6129 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/doc/DB.Blog.Structure.sql
+++ kbs_bbs/doc/DB.Blog.Structure.sql
@@ -1,10 +1,11 @@
-CREATE TABLE `blogthemes` (
-  `btid` int(10) unsigned zerofill NOT NULL auto_increment,
-  `englishname` varchar(20) NOT NULL default '',
-  `chinesename` varchar(20) NOT NULL default '',
-  `secid` int(10) NOT NULL default '0',
-  PRIMARY KEY  (`btid`)
-) TYPE=MyISAM COMMENT='blog类别' AUTO_INCREMENT=1 ;
+CREATE TABLE `blacklist` (
+  `userid` varchar(12) NOT NULL default '',
+  `uid` int(10) unsigned NOT NULL default '0',
+  `manager` varchar(12) NOT NULL default '',
+  `hostname` varchar(20) NOT NULL default '',
+  `addtime` timestamp(14) NOT NULL,
+  KEY `userid` (`userid`,`uid`)
+) TYPE=MyISAM COMMENT='blog黑名单';
 
 CREATE TABLE `comments` (
   `cid` int(10) unsigned NOT NULL auto_increment,
@@ -25,29 +26,20 @@ CREATE TABLE `comments` (
   FULLTEXT KEY `body` (`body`)
 ) TYPE=MyISAM COMMENT='评论表' AUTO_INCREMENT=21 ;
 
-CREATE TABLE `directories` (
-  `did` int(10) unsigned NOT NULL auto_increment,
-  `pid` int(10) unsigned NOT NULL default '0',
-  `subject` varchar(200) NOT NULL default '',
-  `uid` int(10) NOT NULL default '0',
-  `created` timestamp(14) NOT NULL,
-  `CHANGED` timestamp(14) NOT NULL,
-  `hostname` varchar(20) NOT NULL default '',
-  PRIMARY KEY  (`did`)
-) TYPE=MyISAM COMMENT='用户表' AUTO_INCREMENT=1 ;
 
 CREATE TABLE `logs` (
   `lid` int(10) unsigned NOT NULL auto_increment,
   `username` varchar(20) binary NOT NULL default '',
   `hostname` varchar(20) binary NOT NULL default '',
-  `ACTION` varchar(20) NOT NULL default '',
+  `ACTION` varchar(100) NOT NULL default '',
   `pri_id` int(10) unsigned default NULL,
   `sec_id` int(10) unsigned default NULL,
   `COMMENT` varchar(200) default NULL,
   `logtime` timestamp(14) NOT NULL,
   PRIMARY KEY  (`lid`),
   KEY `username` (`username`,`hostname`,`ACTION`,`pri_id`,`sec_id`,`logtime`)
-) TYPE=MyISAM COMMENT='日志表' AUTO_INCREMENT=1 ;
+) TYPE=MyISAM COMMENT='日志表' AUTO_INCREMENT=10 ;
+
 
 CREATE TABLE `nodes` (
   `nid` int(10) unsigned NOT NULL auto_increment,
@@ -69,12 +61,33 @@ CREATE TABLE `nodes` (
   `htmltag` int(1) NOT NULL default '0',
   `trackback` int(1) NOT NULL default '1',
   `trackbackcount` int(10) NOT NULL default '0',
+  `recommend` int(1) NOT NULL default '0',
+  `updatetime` timestamp(14) NOT NULL,
   PRIMARY KEY  (`nid`),
   KEY `pid` (`pid`),
   KEY `created` (`changed`),
   KEY `changed` (`created`),
   FULLTEXT KEY `body` (`body`)
 ) TYPE=MyISAM COMMENT='文章表' AUTO_INCREMENT=100 ;
+
+
+CREATE TABLE `recommend` (
+  `rid` int(10) unsigned NOT NULL auto_increment,
+  `nid` int(10) unsigned NOT NULL default '0',
+  `hostname` varchar(20) NOT NULL default '',
+  `created` timestamp(14) NOT NULL,
+  `uid` int(10) default NULL,
+  `subject` varchar(200) NOT NULL default 'null',
+  `body` text,
+  `emote` int(2) NOT NULL default '0',
+  `htmltag` int(1) NOT NULL default '0',
+  `recuser` varchar(12) NOT NULL default '',
+  `state` int(1) NOT NULL default '1',
+  PRIMARY KEY  (`rid`),
+  KEY `rid` (`rid`),
+  FULLTEXT KEY `body` (`body`)
+) TYPE=MyISAM COMMENT='推荐文章' AUTO_INCREMENT=102 ;
+
 
 CREATE TABLE `topics` (
   `tid` int(10) NOT NULL auto_increment,
@@ -84,6 +97,7 @@ CREATE TABLE `topics` (
   `sequen` int(2) NOT NULL default '0',
   KEY `tid` (`tid`)
 ) TYPE=MyISAM COMMENT='用户文集分类' AUTO_INCREMENT=12 ;
+
 
 CREATE TABLE `trackback` (
   `tbid` int(10) unsigned NOT NULL auto_increment,
@@ -115,37 +129,13 @@ CREATE TABLE `users` (
   `modifytime` timestamp(14) NOT NULL,
   `links` tinytext NOT NULL,
   `htmleditor` int(1) NOT NULL default '0',
-  `indexnodes` int(1) NOT NULL default '5',
   `indexnodechars` int(5) NOT NULL default '600',
+  `indexnodes` int(1) NOT NULL default '5',
   `useremail` varchar(200) NOT NULL default '',
   `favmode` int(1) NOT NULL default '0',
+  `updatetime` timestamp(14) NOT NULL,
+  `userinfor` text NOT NULL,
   PRIMARY KEY  (`uid`),
   UNIQUE KEY `username` (`username`),
   KEY `corpusname` (`corpusname`,`createtime`)
 ) TYPE=MyISAM COMMENT='用户表' AUTO_INCREMENT=2 ;
-
-CREATE TABLE `vote_topics` (
-  `vtid` int(10) unsigned zerofill NOT NULL auto_increment,
-  `creator` varchar(12) NOT NULL default '',
-  `address` varchar(15) NOT NULL default '',
-  `changed` timestamp(14) NOT NULL,
-  `creattime` timestamp(14) NOT NULL,
-  `subject` varchar(50) NOT NULL default '',
-  `description` varchar(255) NOT NULL default '',
-  `anonymousvote` int(1) unsigned zerofill NOT NULL default '0',
-  `votesperip` int(2) NOT NULL default '3',
-  `options` text NOT NULL,
-  `results` longtext NOT NULL,
-  PRIMARY KEY  (`vtid`),
-  KEY `vtid_2` (`vtid`)
-) TYPE=MyISAM COMMENT='投票项' AUTO_INCREMENT=1 ;
-
-CREATE TABLE `vote_users` (
-  `vuid` int(10) unsigned zerofill NOT NULL auto_increment,
-  `username` varchar(12) NOT NULL default '',
-  `changed` timestamp(14) NOT NULL,
-  `votetime` timestamp(14) NOT NULL,
-  `address` varchar(15) NOT NULL default '',
-  `result` text NOT NULL,
-  PRIMARY KEY  (`vuid`)
-) TYPE=MyISAM COMMENT='用户投票记录' AUTO_INCREMENT=1 ;
