450df2e413c9eb0f6c10be0650480022b723fca8
windinsn <windinsn@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2004-03-27 08:45:14 +0000

    opml
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@6190 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/bbs2www/html/pc/opml.php
+++ kbs_bbs/bbs2www/html/pc/opml.php
@@ -2,11 +2,14 @@
 require("pcfuncs.php");
 require("pcstat.php");
 
-function pc_opml_init()
+function pc_opml_init($opmlTitle)
 {
 ?>
 <?xml version="1.0" encoding="gb2312" ?> 
 <opml>
+<head>
+<title><?php echo htmlspecialchars($title); ?></title>
+</head>
 <body>
 <?php	
 }
@@ -19,10 +22,11 @@ function pc_opml_quit()
 <?php	
 }
 
-function pc_opml_outline($title,$htmlUrl,$xmlUrl)
+function pc_opml_outline($title,$htmlUrl,$xmlUrl,$description,$text="")
 {
+	if(!$text) $text = $title;
 ?>
-<outline title="<?php echo htmlspecialchars($title); ?>" htmlUrl="<?php echo htmlspecialchars($htmlUrl); ?>" xmlUrl="<?php echo htmlspecialchars($xmlUrl); ?>" /> 
+<outline text="<?php echo htmlspecialchars($text); ?>" title="<?php echo htmlspecialchars($title); ?>" type="rss" version="RSS" htmlUrl="<?php echo htmlspecialchars($htmlUrl); ?>" xmlUrl="<?php echo htmlspecialchars($xmlUrl); ?>" description="<?php echo htmlspecialchars($description); ?>" /> 
 <?php	
 }
 
@@ -33,21 +37,31 @@ if(pc_update_cache_header())
 $type = intval( $_GET["t"]);
 $link = pc_db_connect();
 if($type == 2) //新用户
+{
 	$blogs = getNewUsers($link,50);
+	$opmlTitle = $pcconfig["BBSNAME"] . "BLOG新用户组";
+}
 elseif($type == 1) //最高访问
+{
 	$blogs = getMostVstUsers($link,50);
+	$opmlTitle = $pcconfig["BBSNAME"] . "BLOG热门用户组";
+}
 else //最近更新
+{
 	$blogs = getLastUpdates($link,50);
+	$opmlTitle = $pcconfig["BBSNAME"] . "BLOG更新用户组";
+}
 pc_db_close($link);
 header("Content-Type: text/xml");
 header("Content-Disposition: inline;filename=opml.xml");
-pc_opml_init();
+pc_opml_init($opmlTitle);
 foreach($blogs as $blog)
 {
 	$title = stripslashes($blog[corpusname]);
 	$htmlUrl = pc_personal_domainname($blog[username]);
 	$xmlUrl = "http://".$pcconfig["SITE"] . "/pc/rss.php?userid=" . $blog[username];
-	pc_opml_outline($title,$htmlUrl,$xmlUrl);
+	$description = stripslashes($blog[description]);
+	pc_opml_outline($title,$htmlUrl,$xmlUrl,$description);
 }
 pc_opml_quit();
 ?>
