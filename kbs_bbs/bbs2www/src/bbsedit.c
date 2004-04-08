1d6751d0e3e7bd11f968cff9a940d450ecd2e9f1
atppp <atppp@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2004-04-08 18:27:24 +0000

    WWW修改文章隐藏IP
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@6279 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/bbs2www/src/bbsedit.c
+++ kbs_bbs/bbs2www/src/bbsedit.c
@@ -135,7 +135,7 @@ int update_form(char *board, char *file)
         fprintf(fout, "%s", buf2);
     }
     fprintf(fout, "%s", unix_string(buf));
-    fprintf(fout, "\033[36m※ 修改:·%s 於 %s 修改本文·[FROM: %s]\033[m\n", currentuser->userid, wwwCTime(time(0)) + 4, fromhost);
+    fprintf(fout, "\033[36m※ 修改:·%s 於 %s 修改本文·[FROM: %s]\033[m\n", currentuser->userid, wwwCTime(time(0)) + 4, SHOW_USERIP(currentuser, fromhost));
     while (fgets(buf2, sizeof(buf2), fin) != NULL) {
         if (Origin2(buf2)) {
             fprintf(fout, "%s", buf2);
