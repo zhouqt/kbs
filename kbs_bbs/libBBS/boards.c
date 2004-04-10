358b222f30edb5c08efe5054d450dc5f8afa6e54
stiger <stiger@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2004-04-10 17:07:57 +0000

    test
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@6317 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/libBBS/boards.c
+++ kbs_bbs/libBBS/boards.c
@@ -87,14 +87,14 @@ void load_favboard(int dohelp,int mode)
 	if(mode!=2){
 		favbrd_list=mybrd_list;
 		favbrd_list_count=&mybrd_list_t;
-		if(favbrd_list_t > 0)
-			return;
+//		if(favbrd_list_t > 0)
+//			return;
 	}else{
 		favbrd_list=bdirshm->allbrd_list;
 		favbrd_list_count = &bdirshm->allbrd_list_t;
 		return;
 	}
-	if(favbrd_list_t>0) return;
+//	if(favbrd_list_t>0) return;
 
 	bzero(favbrd_list, sizeof(struct favbrd_struct)*FAVBOARDNUM);
     if ((fd = open(fname, O_RDONLY, 0600)) != -1) {
