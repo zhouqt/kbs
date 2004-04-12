36e8e37b7302cdbff75a3b187292b7b655c2c6ca
windinsn <windinsn@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2004-04-12 04:12:36 +0000

    重新发送激活码
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@6340 de4889a5-8ad4-4bf3-bd87-57997c21d515

---
+++ kbs_bbs/bbs2www/html/bbssendacode.php
@@ -0,0 +1,111 @@
+<?php
+require("funcs.php");
+require("reg.inc.php");
+html_init("gb2312");
+
+if ($loginok != 1)
+	html_nologin();
+else
+{
+	$new_reg_mail = $_POST["newemail"];
+	
+	if(!strcmp($currentuser["userid"],"guest"))
+	{
+		html_error_quit("请先登录!");
+		exit();
+	}
+	//检查激活码
+	$activation = "";
+	$userid = $currentuser["userid"];
+	$ret = bbs_getactivation($userid,$activation);
+	
+	if($ret == -1 || $ret == -10)
+		html_error_quit("系统错误，请联系管理员!");
+		
+	if($ret == 0)
+	{
+		if(bbs_reg_haveactivated($activation))
+			html_error_quit("您的帐户已激活");	
+		$reg_email = bbs_reg_getactivationemail($activation);
+	}
+	else
+		$reg_email = "";
+	
+?>
+<body>
+<br /><br />
+<?php
+		if($new_reg_mail)
+		{
+			$new_activation = bbs_reg_newactivation(bbs_create_activation(),$new_reg_mail);
+			$ret = bbs_setactivation($userid,$new_activation);
+			if($ret != 0)
+				html_error_quit("系统错误");
+			
+			$mailbody="
+<?xml version=\"1.0\" encoding=\"gb2312\">
+<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\">
+<html>
+<body><P class=MsoNormal><FONT size=2><SPAN 
+style=\"FONT-FAMILY: 宋体; mso-ascii-font-family: 'Times New Roman'; mso-hansi-font-family: 'Times New Roman'\">  " . $realname  . "欢迎您来到</SPAN><SPAN 
+style=\"FONT-FAMILY: 宋体; mso-ascii-font-family: 'Times New Roman'; mso-hansi-font-family: 'Times New Roman'\">" . BBS_FULL_NAME . "。</SPAN></FONT></P>
+<P class=MsoNormal><FONT size=2><SPAN 
+style=\"FONT-FAMILY: 宋体; mso-ascii-font-family: 'Times New Roman'; mso-hansi-font-family: 'Times New Roman'\">您的注册信息是：</SPAN></FONT></P>
+<P class=MsoNormal><FONT size=2><SPAN 
+style=\"FONT-FAMILY: 宋体; mso-ascii-font-family: 'Times New Roman'; mso-hansi-font-family: 'Times New Roman'\">用户名：" . $userid . "</SPAN></FONT></P>
+<P class=MsoNormal><FONT size=2><SPAN 
+style=\"FONT-FAMILY: 宋体; mso-ascii-font-family: 'Times New Roman'; mso-hansi-font-family: 'Times New Roman'\">昵称：" . $nickname . "<SPAN></FONT></P>
+<P class=MsoNormal><FONT size=2><SPAN 
+style=\"FONT-FAMILY: 宋体; mso-ascii-font-family: 'Times New Roman'; mso-hansi-font-family: 'Times New Roman'\">密码：" . $password . "</SPAN></FONT></P>
+<P class=MsoNormal><FONT size=2><SPAN lang=EN-US>email</SPAN><SPAN 
+style=\"FONT-FAMILY: 宋体; mso-ascii-font-family: 'Times New Roman'; mso-hansi-font-family: 'Times New Roman'\">：" . $reg_email . "</SPAN></FONT></P>
+<P class=MsoNormal><FONT size=2><A 
+href=\"https://www.smth.edu.cn/bbsact.php?userid=".$userid."&acode=".$activation."<SPAN 
+style=\"FONT-FAMILY: 宋体; mso-ascii-font-family: 'Times New Roman'; mso-hansi-font-family: 'Times New Roman'\">点击这里激活您在" . BBS_FULL_NAME . "的新帐号</SPAN>
+<br /><br />
+<P class=MsoNormal><FONT size=2><SPAN 
+style=\"FONT-FAMILY: 宋体; mso-ascii-font-family: 'Times New Roman'; mso-hansi-font-family: 'Times New Roman'\"></SPAN></FONT></P>
+</body>
+</html>
+";
+/* To send HTML mail, you can set the Content-type header. */
+			$headers  = "MIME-Version: 1.0\r\n";
+			$headers .= "Content-type: text/html; charset=gb2312\r\n";
+			
+			/* additional headers */
+			$headers .= "From: BBS水木清华站 <https://www.smth.edu.cn>\r\n";
+			
+			if(!mail($new_reg_mail, "welcome to " . BBS_FULL_NAME, $mailbody,$headers))
+			{		
+?>
+<p align="center">
+激活码发送失败！请重新设置您的注册Email。
+</p><p align="center">
+<a href="/bbssendacode.php">[重新发送激活码]</a>
+</p>
+<?php
+			}
+			else
+			{
+?>
+<p align="center">
+激活码发送成功！请查收信件并激活您的帐号。
+</p><p align="center">
+<a href-"/bbsfillform.html">[填写注册单]</a>
+</p>
+<?php
+			}
+		}
+		else
+		{
+?>
+<form action="bbssendacode.php" method="post">
+Emai地址：
+<input type="text" name="newemail" size="20" maxlength="100" value="<?php echo $reg_email; ?>" />
+<input type="submit" value="发送激活码" />
+</form>
+<?php
+		}
+		html_normal_quit();
+	}
+?>
