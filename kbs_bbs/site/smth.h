89c6b320e2f42756fced980cc27988c26182fd85
kcn <kcn@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2004-04-17 03:50:15 +0000

    modify
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@6412 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/site/smth.h
+++ kbs_bbs/site/smth.h
@@ -18,7 +18,7 @@
 #define MANUAL_DENY		0       /*是否允许手动解封 */
 #define BBS_SERVICE_DICT	1
 #define HAVE_TSINGHUA_INFO_REGISTER 1   /* 允许从清华信息系统注册 */
-#define HAVE_PERSONAL_DNS	1       /*个人域名服务 */
+#define HAVE_PERSONAL_DNS	0       /*个人域名服务 */
 #define HAVE_CUSTOM_USER_TITLE	1       /*定义用户特定的title */
 
 #define BUILD_PHP_EXTENSION 1   /*将php lib编成php extension */
@@ -55,7 +55,7 @@
 #define SYSMAIL_BOARD       "sysmail"
 #define BLESS_BOARD "Blessing"
 
-#define MAXUSERS  		300000
+#define MAXUSERS  		350000
 #define MAXCLUB			128
 #define MAXBOARD  		1024
 #define MAXACTIVE 		12000
