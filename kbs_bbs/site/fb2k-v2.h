475010c97869cc40f6d385899e37f558fd28e264
atppp <atppp@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2004-05-15 00:43:54 +0000

    555, mistake
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@6708 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/site/fb2k-v2.h
+++ kbs_bbs/site/fb2k-v2.h
@@ -43,8 +43,8 @@
 #define IDLE_TIMEOUT    (60*20) 
 #define MONITOR_TIMEOUT (60*20) 
 
-#define BBSUID 			9999
-#define BBSGID 			9999
+#define BBSUID 			501
+#define BBSGID 			501
 
 /* for bbs2www, by flyriver, 2001.3.9 */
 #define SECNUM 13
