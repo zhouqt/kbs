7fb35b75ca8049cdd1946cb1203ba916542d782a
atppp <atppp@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2004-05-17 06:50:19 +0000

    quote 10 lines
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@6734 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/bbs2www/sites/zixia/site.php
+++ kbs_bbs/bbs2www/sites/zixia/site.php
@@ -10,6 +10,7 @@ function getattachtmppath($userid,$utmpnum)
 define("ATTACHMAXSIZE","1048576");
 define("ATTACHMAXCOUNT","20");
 define("MAINPAGE_FILE", "bbssec.php");
+define("QUOTED_LINES","10");
 
 $section_nums = array("0", "1", "2", "3", "4", "5", "6", "7", "8", "9");
 $section_names = array(
