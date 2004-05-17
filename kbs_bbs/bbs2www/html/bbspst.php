404be0117bd91f1c57deb4ca1511074ee2e10fdc
windinsn <windinsn@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2004-05-17 11:13:56 +0000

    cgi2php: bbsccc -> bbsccc.php
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@6740 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/bbs2www/html/bbspst.php
+++ kbs_bbs/bbs2www/html/bbspst.php
@@ -125,7 +125,7 @@ function dosubmit() {
 	        $nowtitle = "";
 	    }
 ?>
-标&nbsp;&nbsp;题: <input class="sb1" type="text" name="title" size="40" maxlength="100" value="<?php echo $nowtitle; ?>  " /><br />
+标&nbsp;&nbsp;题: <input class="sb1" type="text" name="title" size="40" maxlength="100" value="<?php echo $nowtitle?$nowtitle." ":""; ?>" /><br />
 <?php
 		if (bbs_is_attach_board($brdarr))
 		{
