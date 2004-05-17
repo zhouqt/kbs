5cadfb939a5d73474855fb731e0acc78b88f1f44
stiger <stiger@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2004-05-17 14:39:14 +0000

    版面数据统计也用数据库喽,from zls
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@6744 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/local_utl/bonlinelog.c
+++ kbs_bbs/local_utl/bonlinelog.c
@@ -7,14 +7,39 @@ crontab:  2 * * * * /home/bbs/bin/bonlinelog
 
 ******/
 
+/* mysql 
+ *
+ * +-------+-------------+------+-----+------------+----------------+
+ * | Field | Type        | Null | Key | Default    | Extra          |
+ * +-------+-------------+------+-----+------------+----------------+
+ * | id    | int(10)     |      | PRI | NULL       | auto_increment |
+ * | bname | varchar(20) |      |     |            |                |
+ * | users | int(11)     |      |     | 0          |                |
+ * | nowid | int(11)     |      |     | 0          |                |
+ * | bdate | date        |      | MUL | 0000-00-00 |                |
+ * | bhour | int(2)      |      |     | 0          |                |
+ * +-------+-------------+------+-----+------------+----------------+
 
+
+ CREATE TABLE bonline (
+ id int(10) NOT NULL auto_increment,
+ bname varchar(20) NOT NULL default '',
+ users int(11) NOT NULL default '0',
+ nowid int(11) NOT NULL default '0',
+ bdate date NOT NULL default '0000-00-00',
+ bhour int(2) NOT NULL default '0',
+ PRIMARY KEY  (id),
+ KEY bdate (bdate)
+ ) TYPE=MyISAM COMMENT='bonline';
+ */
+ 
 #include <time.h>
 #include <stdio.h>
 #include "bbs.h"
 #include "config.h"
 
-
-FILE *fp;
+MYSQL s;
+struct tm t;
 
 int fillbcache(struct boardheader *fptr,int idx,void* arg)
 {
@@ -23,15 +48,19 @@ int fillbcache(struct boardheader *fptr,int idx,void* arg)
 	int bnum;
     struct BoardStatus * bs;
     struct userec normaluser;
+	char sql[500];
 
-    if (check_see_perm(NULL, fptr)==0|| strlen(fptr->filename) == 0)
+    if (check_see_perm(NULL, fptr)==0 || strlen(fptr->filename) == 0)
         return;
 
     bnum = getboardnum(fptr->filename,&bp);
     bs = getbstatus(bnum);
 
-	fprintf(fp, "%-15.15s %-40.40s %5d %d\n", fptr->filename, fptr->title+13, bs->currentusers, bs->nowid);
-
+	sprintf(sql, "INSERT INTO bonline VALUES ( NULL, '%s', '%d', '%d', \"%d-%d-%d\", '%d' );",
+				fptr->filename, bs->currentusers, bs->nowid, 
+				t.tm_year+1900, t.tm_mon+1, t.tm_mday, t.tm_hour);
+    if( mysql_real_query( &s, sql, strlen(sql) ))
+        printf("%s\n",mysql_error(&s));
     return 0;
 }
 
@@ -40,42 +69,23 @@ int fillboard()
     apply_record(BOARDS, (APPLY_FUNC_ARG)fillbcache, sizeof(struct boardheader), NULL, 0,false);
 }
 
-#define BONLINE_LOGDIR "/home/bbs/bonlinelog"
-
 main()
 {
-	char path[256];
-	struct stat st;
 	time_t now;
-	struct tm t;
-
-	now = time(0);
-	localtime_r( &now, &t);
 
     chdir(BBSHOME);
-	if( stat( BONLINE_LOGDIR, &st) < 0 ){
-		if(mkdir(BONLINE_LOGDIR, 0755) < 0)
-			exit(0);
-	}
-	sprintf(path, "%s/%d", BONLINE_LOGDIR, t.tm_year+1900);
-	if( stat(path, &st) < 0){
-		if(mkdir(path, 0755) < 0)
-			exit(0);
-	}
-	sprintf(path, "%s/%d/%d", BONLINE_LOGDIR, t.tm_year+1900, t.tm_mon+1);
-	if( stat(path, &st) < 0){
-		if(mkdir(path, 0755) < 0)
-			exit(0);
-	}
+	now = time(0) - 3600;
+	localtime_r( &now, &t);
 
-	sprintf(path, "%s/%d/%d/%d_%d.log", BONLINE_LOGDIR, t.tm_year+1900, t.tm_mon+1, t.tm_mday, t.tm_hour);
-	if((fp=fopen(path, "w"))==NULL){
-		printf("cannot open log file\n");
-		exit(0);
-	}
-
+	mysql_init(&s);
+    if (!my_connect_mysql(&s) ){
+        printf("Mysql connect error: %s\n",mysql_error(&s));
+        return 0;
+    }   
+	
     resolve_boards();
     fillboard();
 
-    fclose(fp);
+    mysql_close(&s);
+    return 1;
 }
--- kbs_bbs/local_utl/post_brdlog.c
+++ kbs_bbs/local_utl/post_brdlog.c
@@ -3,7 +3,7 @@ stiger:    2004.2
 
 记录版面在线人数到日志文件，用于以后统计方便吧
 
-crontab:  2 * * * * /home/bbs/bin/bonlinelog
+crontab:  10 0 * * * /home/bbs/bin/post_brdlog
 
 ******/
 
@@ -23,6 +23,8 @@ struct _brdlog
 } x[MAXBOARD];
 
 int n = 0;
+MYSQL s;
+struct tm t;
 
 int putout(char *path)
 {
@@ -55,42 +57,6 @@ int id_cmp(struct _brdlog *b, struct _brdlog *a)
     return ( (a->nowid - a->yesid) - (b->nowid - b->yesid) );
 }
 
-
-int load_data(int day, char *path)
-{
-	FILE *fp;
-	char buf[256];
-	char board[STRLEN];
-	char title[STRLEN];
-	int online;
-	int id;
-	int i;
-
-	if((fp=fopen(path,"r"))==NULL)
-		return;
-
-	while(fgets(buf,255,fp)){
-		if(strlen(buf) < 55)
-			continue;
-		if( sscanf(buf,"%s %s",board, title) != 2)
-			continue;
-		if( sscanf(buf+55, "%d %d",&online, &id) != 2)
-			continue;
-		for(i=0;i<n;i++){
-			if(!strcmp(x[i].filename, board))
-				break;
-		}
-		if(i==n)
-			continue;
-		x[i].online += online;
-		if(day==0)
-			x[i].nowid = id;
-		if(day==23)
-			x[i].yesid = id;
-	}
-	fclose(fp);
-}
-
 int fillbcache(struct boardheader *fptr,int idx,void* arg)
 {
 
@@ -98,6 +64,9 @@ int fillbcache(struct boardheader *fptr,int idx,void* arg)
 	int bnum;
     struct BoardStatus * bs;
     struct userec normaluser;
+	char sql[256];
+    MYSQL_RES *res;
+    MYSQL_ROW row;
 
 	if(fptr->filename[0]==0 || !normal_board(fptr->filename))
 		return;
@@ -107,71 +76,68 @@ int fillbcache(struct boardheader *fptr,int idx,void* arg)
 
 	strcpy(x[n].filename, fptr->filename);
 	strcpy(x[n].title, fptr->title+13);
-	x[n].yesid=0;
-	x[n].nowid=0;
-	x[n].online=0;
-	n++;
 
+	sprintf(sql, "SELECT MIN(nowid),MAX(nowid),SUM(users) FROM bonline WHERE bdate=\"%d-%d-%d\" AND bname LIKE '%s' ;", t.tm_year+1900, t.tm_mon+1, t.tm_mday, fptr->filename);
+    if( mysql_real_query( &s, sql, strlen(sql) ))
+        printf("%s\n",mysql_error(&s));
+	else{
+	    res = mysql_store_result(&s);
+	    row = mysql_fetch_row(res);
+
+		if(row && row[0] && row[0][0]){
+			x[n].yesid=atoi(row[0]);
+			x[n].nowid=atoi(row[1]);
+			x[n].online=atoi(row[2]);
+		}
+		mysql_free_result(res);
+	}
+	n++;
     return 0;
 }
 
 int fillboard()
 {
+	bzero(x, MAXBOARD * sizeof(struct _brdlog));
     apply_record(BOARDS, (APPLY_FUNC_ARG)fillbcache, sizeof(struct boardheader), NULL, 0,false);
 }
 
-#define BONLINE_LOGDIR "/home/bbs/bonlinelog"
-
-main()
+main(int argc,char **argv)
 {
 	char path[256];
 	char title[256];
-	struct stat st;
 	time_t now;
-	struct tm t;
-	int i;
+	int before;
 
+	if(argc > 1)
+		before = atoi(argv[1]);
+	else
+		before = 0;
+
+	chdir(BBSHOME);
 
     if (init_all()) {
         printf("init data fail\n");
         return -1;
     }
 
-	if( stat( BONLINE_LOGDIR, &st) < 0 ){
-		if(mkdir(BONLINE_LOGDIR, 0755) < 0)
-			exit(0);
-	}
-	sprintf(path, "%s/%d", BONLINE_LOGDIR, t.tm_year+1900);
-	if( stat(path, &st) < 0){
-		if(mkdir(path, 0755) < 0)
-			exit(0);
-	}
-	sprintf(path, "%s/%d/%d", BONLINE_LOGDIR, t.tm_year+1900, t.tm_mon+1);
-	if( stat(path, &st) < 0){
-		if(mkdir(path, 0755) < 0)
-			exit(0);
-	}
+	mysql_init(&s);
 
-	if( stat( path, &st) < 0)
-		exit(0);
+    if (! my_connect_mysql(&s) ){
+        printf("Mysql connect error: %s\n",mysql_error(&s));
+        return 0;
+    }
+
+	now=time(0) - 3600 - 86400*before;
+	localtime_r( &now, &t);
 
     fillboard();
 
-	for(now=time(0),i=0;i<24;i++,now-=3600){
-
-		localtime_r( &now, &t);
-		sprintf(path, "%s/%d/%d/%d_%d.log", BONLINE_LOGDIR, t.tm_year+1900, t.tm_mon+1, t.tm_mday, t.tm_hour);
-
-		load_data(i,path);
-	}
+	mysql_close(&s);
 
     qsort(x, n, sizeof(x[0]), online_cmp);
 
 	sprintf(path,"tmp/brdlog.%d.out",getpid());
 	putout(path);
-
-	now=time(0);
-	localtime_r( &now, &t);
 
     sprintf(title, "%d年%2d月%2d日版面统计数据(在线排序)", t.tm_year + 1900, t.tm_mon + 1, t.tm_mday);
     post_file(NULL, "", path, "SysTrace", title, 0, 1, getSession());
@@ -182,11 +148,7 @@ main()
 	sprintf(path,"tmp/brdlog.%d.out",getpid());
 	putout(path);
 
-	now=time(0);
-	localtime_r( &now, &t);
-
     sprintf(title, "%d年%2d月%2d日版面统计数据(文章数排序)", t.tm_year + 1900, t.tm_mon + 1, t.tm_mday);
     post_file(NULL, "", path, "SysTrace", title, 0, 1, getSession());
 	unlink(path);
-
 }
