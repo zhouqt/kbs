55a1ecd95c46358c0f1d49fee60d38c552c890a5
atppp <atppp@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2004-05-17 06:19:14 +0000

    save save for 20040515 update
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@6732 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/bbs2www/sites/zixia/frames.php
+++ kbs_bbs/bbs2www/sites/zixia/frames.php
@@ -1,3 +1,10 @@
+<?php
+	if (!isset($_GET['boardid'])){
+		$url='bbssec.php';
+	} else {
+		$url='bbsdoc.php?board='.$_GET['boardid'];
+	}
+?>
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=gb2312">
@@ -7,15 +14,15 @@
 <script language=javascript>
 if(is_ie) document.write(""+
 "  <frameset name=mainframe frameborder=0 border=0 cols=\"180,*\">"+
-        "<frame name=menu scrolling=no src=/bbsleft.php>"+
+        "<frame name=menu scrolling=no src=menu.html>"+
         "<frameset name=viewfrm rows=*,20>"+
-              "<frame framespacing=\"2\" name=\"f3\" src=\"/bbssec.php\">"+
+              "<frame framespacing=\"2\" name=\"f3\" src=\"/<?php echo $url; ?>\">"+
               "<frame scrolling=no marginwidth=4 marginheight=\"1\" framespacing=\"1\" name=\"f4\" src=\"bbsguestfoot.php\">");
 else document.write(""+
 "  <frameset cols=\"180,*\" frameborder=0 border=0 >"+
-    "<frame name=\"f2\" framespacing=\"0\" src=\"/bbsleft.php\">"+
+    "<frame name=\"f2\" framespacing=\"0\" src=\"/cgi-bin/bbs/bbsleft\">"+
     "<frameset name=\"viewfrm\" rows=\"*, 20\">"+
-      "<frame framespacing=\"2\" name=\"f3\" src=\"/bbssec.php\">"+
+      "<frame framespacing=\"2\" name=\"f3\" src=\"/<?php echo $url; ?>\">"+
       "<frame scrolling=\"no\" marginwidth=\"4\" marginheight=\"1\" framespacing=\"1\" name=\"f4\" src=\"bbsguestfoot.php\">");
 </script>
     </frameset>
