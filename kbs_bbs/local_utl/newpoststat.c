2b3193f18e8aec18eb0be2ac0222c4a474af5242
stiger <stiger@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2004-05-25 17:01:50 +0000

    newpoststat
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@6880 de4889a5-8ad4-4bf3-bd87-57997c21d515

---
+++ kbs_bbs/local_utl/newpoststat.c
@@ -0,0 +1,627 @@
+/* 使用mysql的十大统计功能， 2004.5.25 stiger */
+
+/*
+   select bname,threadid,count(distinct userid) as count from postlog WHERE time<NOW() GROUP BY bname,threadid order by count desc;
+*/
+#include <stdio.h>
+#include <time.h>
+#include "bbs.h"
+#include "urlencode.c"
+
+#ifdef NEWPOSTSTAT
+
+#define	DELETE
+
+char *myfile[] = { "day", "week", "month", "year", "bless" };
+int mytop[] = { 10, 50, 100, 100, 10 };
+char *mytitle[] = { "日十大热门话题",
+    "周五十大热门话题",
+    "月百大热门话题",
+    "年度百大热门话题",
+    "日十大衷心祝福"
+};
+
+#define TOPCOUNT 100
+#define SECTOPCOUNT 10
+#define MAXCMP 10000
+#ifdef BLESS_BOARD
+const char *surfix_bless[23] = {
+	" ",
+    "  \x1b[1;34m◆  ",
+    "\x1b[1;32m┏\x1b[0;32m┴\x1b[1m┓",
+    "\x1b[0;32m│\x1b[1m深\x1b[0;32m│",
+    "\x1b[0;32m│\x1b[1m情\x1b[0;32m│",
+    "\x1b[1;32m┗\x1b[0;32m┬\x1b[1m┛",
+    "  \x1b[0;32m│  ",
+    "\x1b[1;32m┏\x1b[0;32m┴\x1b[1m┓",
+    "\x1b[0;32m│\x1b[1m祝\x1b[0;32m│",
+    "\x1b[0;32m│\x1b[1m福\x1b[0;32m│",
+    "\x1b[1;32m┗\x1b[0;32m┬\x1b[1m┛",
+    "  \x1b[1;34m◆  ",
+    "  \x1b[1;34m◆  ",
+    "\x1b[1;32m┏\x1b[0;32m┴\x1b[1m┓",
+    "\x1b[0;32m│\x1b[1m校\x1b[0;32m│",
+    "\x1b[0;32m│\x1b[1m内\x1b[0;32m│",
+    "\x1b[1;32m┗\x1b[0;32m┬\x1b[1m┛",
+    "  \x1b[32m│  ",
+    "\x1b[1;32m┏\x1b[0;32m┴\x1b[1m┓",
+    "\x1b[0;32m│\x1b[1m热\x1b[0;32m│",
+    "\x1b[0;32m│\x1b[1m点\x1b[0;32m│",
+    "\x1b[1;32m┗\x1b[0;32m┬\x1b[1m┛",
+    "  \x1b[1;34m◆  "
+};
+#endif
+
+struct postrec {
+	char userid[IDLEN+1];
+    char board[BOARDNAMELEN];      /* board name */
+	unsigned int groupid;
+    time_t date;                /* last post's date */
+    int number;                 /* post number */
+	char title[81];
+};
+
+struct postrec top[TOPCOUNT];
+struct postrec sectop[SECNUM][SECTOPCOUNT];
+
+int sectopnum[SECNUM];
+int sectopnumtotal=0;
+
+int topnum=0;
+#ifdef SMTH
+int topnum1=0;
+#endif
+
+#define INTERVAL 20
+
+static char * get_file_title(char *boardname, int threadid, char *title, char *userid)
+{
+
+	char dirfile[256];
+	int fd;
+	struct fileheader fh;
+
+            sprintf(dirfile, "boards/%s/.DIR", boardname);
+			if ((fd = open(dirfile, O_RDWR, 0644)) < 0)
+				return NULL;
+
+    		if( get_records_from_id(fd, threadid, &fh, 1, NULL) == 0 ){
+				close(fd);
+				return NULL;
+			}
+			close(fd);
+
+			strncpy(title, fh.title, 80);
+			title[80]=0;
+
+			strncpy(userid, fh.owner, IDLEN);
+			userid[IDLEN]='\0';
+
+			return title;
+}
+
+extern const char seccode[SECNUM][5];
+static int get_seccode_index(char prefix)
+{
+    int i;
+
+    for (i = 0; i < SECNUM; i++) {
+        if (strchr(seccode[i], prefix) != NULL)
+            return i;
+    }
+    return -1;
+}
+
+/***********
+  根据type得到十大列表,已经经过排序等一系列检查,可以直接输出
+  type!=4的时候还得到分区十大
+  *******/
+int get_top(int type)
+{
+
+	MYSQL s;
+	MYSQL_RES *res;
+	MYSQL_ROW row;
+	char sqlbuf[500];
+	char cmptime[100];
+	int start=0;
+	int i,secid;
+	int threadid;
+	char title[81];
+	char userid[IDLEN+1];
+	int m,n;
+#ifdef SMTH
+	struct boardheader *bh;
+#endif
+
+	topnum = 0;
+	topnum1 = 0;
+
+	if(type < 0 || type > 4)
+		return 0;
+
+	mysql_init (&s);
+
+	if (! my_connect_mysql(&s) ){
+		return 0;;
+	}
+
+	if(type==0 || type==4){
+		sprintf(cmptime,"YEAR(time)=YEAR(CURDATE()) AND MONTH(time)=MONTH(CURDATE()) AND DAYOFMONTH(time)=DAYOFMONTH(CURDATE())");
+	}else if(type==1){
+		sprintf(cmptime,"YEAR(time)=YEAR(CURDATE()) AND WEEK(time)=WEEK(CURDATE())");
+	}else if(type==2){
+		sprintf(cmptime,"YEAR(time)=YEAR(CURDATE()) AND MONTH(time)=MONTH(CURDATE())");
+	}else if(type==3){
+		sprintf(cmptime,"YEAR(time)=YEAR(CURDATE())");
+	}
+
+	bzero(top, TOPCOUNT * sizeof(struct postrec));
+	bzero(sectop, TOPCOUNT * sizeof(struct postrec));
+
+	for(i=0;i<SECNUM;i++) sectopnum[i]=0;
+
+	while(1){
+		if(type==4){
+#ifdef SMTH
+			if(topnum>=5 && topnum1>=5)
+				break;
+#else
+			if(topnum>=mytop[type])
+				break;
+#endif
+		}else{
+			if(topnum>=mytop[type] && sectopnumtotal>=SECNUM*SECTOPCOUNT)
+				break;
+		}
+
+		if(start > MAXCMP)
+			break;
+
+		sprintf(sqlbuf,"SELECT bname,threadid,MAX(time) AS maxtime,count(DISTINCT userid) AS count FROM postlog WHERE %s GROUP BY bname,threadid ORDER BY count desc LIMIT %d,%d;", cmptime, start, INTERVAL);
+		
+		if( mysql_real_query( &s, sqlbuf, strlen(sqlbuf) )){
+			mysql_close(&s);
+			
+			return topnum;
+		}
+
+		res = mysql_store_result(&s);
+	
+		while(1){
+			row = mysql_fetch_row(res);
+			if(row==NULL)
+				break;
+printf("%s:%s:%s\n",row[0],row[1],row[2]);
+			/***检查是否该计算十大***/
+#ifdef BLESS_BOARD
+			bh = getbcache(row[0]);
+			if(bh==NULL || bh->flag & BOARD_POSTSTAT){
+				continue;
+			}
+			if(type==0){
+				if ( ! strcasecmp(row[0], BLESS_BOARD) 
+#ifdef SMTH
+						|| bh->group==503 || bh->group==552
+#endif
+								){
+					continue;
+				}
+			}else if(type==4){
+				if ( strcasecmp(row[0], BLESS_BOARD) 
+#ifdef SMTH
+						&& bh->group!=503 && bh->group!=552
+#endif
+								){
+					continue;
+				}
+			}
+#endif
+			threadid = atoi(row[1]);
+			if(get_file_title(row[0], threadid, title, userid) == NULL){
+				continue;
+			}
+
+			secid= get_seccode_index(bh->title[0]);
+/**一个版面最多3个十大**/
+#ifndef NINE_BUILD
+			if(type!=4){
+                m = 0;
+                for (n = 0; n < topnum; n++) {
+                    if (!strcmp(row[0], top[n].board))
+                        m++;
+                }
+				if(m>3)
+					continue;
+
+				/***分区十大里一个版面也最多3个***/
+				if(secid!=-1){
+                	m = 0;
+                	for (n = 0; n < sectopnum[i]; n++) {
+                    	if (!strcmp(row[0], sectop[secid][n].board))
+                        	m++;
+                	}
+					if(m>3)
+						continue;
+				}
+			}else{
+#ifdef SMTH
+				if(strcasecmp(row[0], BLESS_BOARD)){
+                	m = 0;
+                	for (n = 0; n < topnum1; n++) {
+                    	if (!strcmp(row[0], top[n].board))
+                        	m++;
+                	}
+					if(m>1)
+						continue;
+				}
+#endif
+			}
+#endif
+
+			/***先记录正常十大的值***/
+#ifdef SMTH
+			if(type==4 && strcasecmp(row[0], BLESS_BOARD) ){
+				if(topnum1>=5)
+					continue;
+			strncpy(top[5+topnum1].board, row[0], BOARDNAMELEN);
+			top[5+topnum1].board[BOARDNAMELEN-1]='\0';
+			top[5+topnum1].groupid = threadid;
+			strncpy(top[5+topnum1].title, title, 80);
+			top[5+topnum1].title[80]='\0';
+			strncpy(top[5+topnum1].userid, userid, IDLEN);
+			top[5+topnum1].userid[IDLEN]='\0';
+			top[5+topnum1].date = timestamp2tt(row[2]);
+			top[5+topnum1].number = atoi(row[3]);
+
+			topnum1++;
+			}else{
+				if(type==4 && topnum>=5)
+					continue;
+#endif
+			if(topnum < mytop[type]){
+
+			strncpy(top[topnum].board, row[0], BOARDNAMELEN);
+			top[topnum].board[BOARDNAMELEN-1]='\0';
+			top[topnum].groupid = threadid;
+			strncpy(top[topnum].title, title, 80);
+			top[topnum].title[80]='\0';
+			strncpy(top[topnum].userid, userid, IDLEN);
+			top[topnum].userid[IDLEN]='\0';
+			top[topnum].date = timestamp2tt(row[2]);
+			top[topnum].number = atoi(row[3]);
+
+			topnum++;
+
+			}
+#ifdef SMTH
+			}
+#endif
+
+			/***计算分区十大***/
+			i=secid;
+			if(i!=-1){
+				if( sectopnum[i] < SECTOPCOUNT){
+
+			strncpy(sectop[i][sectopnum[i]].board, row[0], BOARDNAMELEN);
+			sectop[i][sectopnum[i]].board[BOARDNAMELEN-1]='\0';
+			sectop[i][sectopnum[i]].groupid = threadid;
+			strncpy(sectop[i][sectopnum[i]].title, title, 80);
+			sectop[i][sectopnum[i]].title[80]='\0';
+			strncpy(sectop[i][sectopnum[i]].userid, userid, IDLEN);
+			sectop[i][sectopnum[i]].userid[IDLEN]='\0';
+			sectop[i][sectopnum[i]].date = timestamp2tt(row[2]);
+			sectop[i][sectopnum[i]].number = atoi(row[3]);
+
+					sectopnum[i]++;
+					sectopnumtotal++;
+				}
+			}
+
+			if(type==4){
+#ifdef SMTH
+				if(topnum>=5 && topnum1>=5)
+					break;
+#else
+				if(topnum>=mytop[type])
+					break;
+#endif
+			}else{
+				if(topnum >= mytop[type] && sectopnumtotal >= SECNUM*SECTOPCOUNT)
+					break;
+			}
+
+		}
+
+		mysql_free_result(res);
+
+		start += INTERVAL;
+
+	}
+	
+	mysql_close(&s);
+
+	return topnum;
+
+}
+
+/*
+ * mytype 0 本日
+ *        1 本周
+ *        2 本月
+ *        3 本年
+ *        4 祝福榜
+ */
+void writestat(int mytype)
+{
+	int i;
+	char buf[256];
+	char *p;
+	char curfile[256];
+	FILE *fp;
+
+    sprintf(curfile, "etc/posts/%s", myfile[mytype]);
+    if ((fp = fopen(curfile, "w")) != NULL) {
+#ifdef SMTH
+	if( mytype == 4) {
+        fprintf(fp, "              \x1b[1;33m── \x1b[31m☆\x1b[33m☆\x1b[32m☆ \x1b[41;32m  \x1b[33m本日五大衷心祝福  \x1b[m\x1b[1;32m ☆\x1b[31m☆\x1b[33m☆ ──\x1b[m               %s\x1b[m\n", surfix_bless[0]);
+
+        for (i = 0; i < topnum; i++) {
+
+            strcpy(buf, ctime(&top[i].date));
+            buf[20] = NULL;
+            p = buf + 4;
+
+            fprintf(fp,
+                        "                                            \x1b[33m%s \x1b[1;31m%4d\x1b[0;37m人      %s\x1b[m\n"
+                        "\x1b[1m第\x1b[31m%2d \x1b[37m名 \x1b[4%dm %-51.51s\x1b[m \x1b[1;33m%-12s%s\x1b[m\n",
+                        p, top[i].number, surfix_bless[(i) * 2 + 1], i+1, (i) / 2 + 1, top[i].title, top[i].userid, surfix_bless[(i+1) * 2]);
+
+		}
+		for( ; i < 5; i++){
+            fprintf(fp,
+                        "                                                                         %s\x1b[m\n"
+                        "                                                                         %s\x1b[m\n",
+                        surfix_bless[i * 2 +1], surfix_bless[(i+1) * 2 ] );
+		}
+        fprintf(fp, "                                                                         %s\x1b[m\n", surfix_bless[11]);
+
+        fprintf(fp, "              \x1b[1;33m── \x1b[31m☆\x1b[33m☆\x1b[32m☆ \x1b[41;32m  \x1b[33m本日五大校内话题  \x1b[m\x1b[1;32m ☆\x1b[31m☆\x1b[33m☆ ──\x1b[m               %s\x1b[m\n", surfix_bless[12]);
+
+        for (i = 0; i < topnum1; i++) {
+
+            strcpy(buf, ctime(&top[5+i].date));
+            buf[20] = NULL;
+            p = buf + 4;
+
+            fprintf(fp,
+                        "        \x1b[33m%-20.20s                %s \x1b[1;31m%4d\x1b[0;37m人      %s\x1b[m\n"
+                        "\x1b[1m第\x1b[31m%2d \x1b[37m名 \x1b[4%dm %-51.51s\x1b[m \x1b[1;33m%-12s%s\x1b[m\n",
+                        top[5+i].board, p, top[5+i].number, surfix_bless[(i + 6) * 2 + 1], i+1, (i) / 2 + 1, top[5+i].title, top[5+i].userid, surfix_bless[(i+7) * 2 ]);
+
+		}
+		for( ; i < 5; i++){
+            fprintf(fp,
+                        "                                                                         %s\x1b[m\n"
+                        "                                                                         %s\x1b[m\n",
+                        surfix_bless[(i+6) * 2 + 1], surfix_bless[(i+7) * 2 ] );
+		}
+
+		fclose(fp);
+		return;
+	}
+#endif
+
+#ifdef BLESS_BOARD
+        if (mytype == 4)
+            fprintf(fp, "              \x1b[1;33m── \x1b[31m☆\x1b[33m☆\x1b[32m☆ \x1b[41;32m  \x1b[33m本日十大衷心祝福  \x1b[m\x1b[1;32m ☆\x1b[31m☆\x1b[33m☆ ──\x1b[m\n\n");
+        else
+#endif
+            fprintf(fp, "                \033[34m-----\033[37m=====\033[41m 本%s \033[m=====\033[34m-----\033[m\n\n", mytitle[mytype]);
+
+        for (i = 0; i < topnum; i++) {
+
+            strcpy(buf, ctime(&top[i].date));
+            buf[20] = NULL;
+            p = buf + 4;
+
+
+#ifdef BLESS_BOARD
+            if (mytype == 4)
+                fprintf(fp,
+                        "                                            %s \x1b[1;31m%4d\x1b[0;37m人      %s\x1b[m\n"
+                        "\x1b[1m第\x1b[31m%2d \x1b[37m名 \x1b[4%dm %-51.51s\x1b[m \x1b[1;33m%-12s%s\x1b[m\n",
+                        p, top[i].number, surfix_bless[(i) * 2], i+1, (i) / 2 + 1, top[i].title, top[i].userid, surfix_bless[(i) * 2 + 1]);
+            else
+#endif
+                fprintf(fp,
+                        "\033[37m第\033[31m%3d\033[37m 名 \033[37m信区 : \033[33m%-16s\033[37m【\033[32m%s\033[37m】\033[36m%4d \033[37m人\033[35m%16s\n"
+                        "     \033[37m标题 : \033[44m\033[37m%-60.60s\033[m\n", i + 1, top[i].board, p, top[i].number, top[i].userid, top[i].title);
+        }
+
+#ifdef BLESS_BOARD
+        if (mytype == 4)
+            fprintf(fp, "                                                                         %s\x1b[m", surfix_bless[20]);
+#endif
+        fclose(fp);
+    }
+}
+
+void gen_sec_hot_subjects_xml(int mytype, int secid)
+{
+	FILE *fp;
+	char curfile[256];
+	char xml_buf[256];
+	char url_buf[256];
+	int i;
+
+    sprintf(curfile, "xml/%s_sec%d.xml", myfile[mytype], secid);
+    if ((fp = fopen(curfile, "w")) != NULL) 
+	{
+		fprintf(fp, "<?xml version=\"1.0\" encoding=\"GBK\"?>\n");
+		fprintf(fp, "<hotsubjects>\n");
+
+        for (i = 0; i < sectopnum[secid]; i++) 
+		{
+
+			fprintf(fp, "<hotsubject>\n");
+			fprintf(fp, "<title>%s</title>\n", encode_url(url_buf,encode_xml(xml_buf, sectop[secid][i].title, 
+						sizeof(xml_buf)),sizeof(url_buf)));
+			fprintf(fp, "<author>%s</author>\n", encode_url(url_buf,sectop[secid][i].userid,sizeof(url_buf)));
+			fprintf(fp, "<board>%s</board>\n", encode_url(url_buf,sectop[secid][i].board,sizeof(url_buf)));
+			fprintf(fp, "<time>%d</time>\n", sectop[secid][i].date);
+			fprintf(fp, "<number>%d</number>\n", sectop[secid][i].number);
+			fprintf(fp, "<groupid>%d</groupid>\n", sectop[secid][i].groupid);
+			fprintf(fp, "</hotsubject>\n");
+        }
+		fprintf(fp, "</hotsubjects>\n");
+
+        fclose(fp);
+    }
+}
+
+void gen_secs_hot_subjects_xml(int mytype)
+{
+	int i;
+
+	for(i=0; i<SECNUM; i++){
+		gen_sec_hot_subjects_xml(mytype, i);
+	}
+}
+
+void gen_blessing_list_xml()
+{
+	FILE *fp;
+	char curfile[256];
+	char xml_buf[256];
+	char url_buf[256];
+	int i;
+
+    sprintf(curfile, "xml/%s.xml", myfile[4]);
+    if ((fp = fopen(curfile, "w")) != NULL) 
+	{
+		fprintf(fp, "<?xml version=\"1.0\" encoding=\"GBK\"?>\n");
+		fprintf(fp, "<hotsubjects>\n");
+
+        for (i = 0; i < topnum; i++) 
+		{
+			fprintf(fp, "<hotsubject>\n");
+			fprintf(fp, "<title>%s</title>\n", encode_url(url_buf,encode_xml(xml_buf, top[i].title, 
+						sizeof(xml_buf)),sizeof(url_buf)));
+			fprintf(fp, "<author>%s</author>\n", encode_url(url_buf,top[i].userid,sizeof(url_buf)));
+			fprintf(fp, "<board>%s</board>\n", encode_url(url_buf,top[i].board,sizeof(url_buf)));
+			fprintf(fp, "<time>%d</time>\n", top[i].date);
+			fprintf(fp, "<number>%d</number>\n", top[i].number);
+			fprintf(fp, "<groupid>%d</groupid>\n", top[i].groupid);
+			fprintf(fp, "</hotsubject>\n");
+        }
+        for (i = 5; i < topnum1+5; i++) 
+		{
+			fprintf(fp, "<hotsubject>\n");
+			fprintf(fp, "<title>%s</title>\n", encode_url(url_buf,encode_xml(xml_buf, top[i].title, 
+						sizeof(xml_buf)),sizeof(url_buf)));
+			fprintf(fp, "<author>%s</author>\n", encode_url(url_buf,top[i].userid,sizeof(url_buf)));
+			fprintf(fp, "<board>%s</board>\n", encode_url(url_buf,top[i].board,sizeof(url_buf)));
+			fprintf(fp, "<time>%d</time>\n", top[i].date);
+			fprintf(fp, "<number>%d</number>\n", top[i].number);
+			fprintf(fp, "<groupid>%d</groupid>\n", top[i].groupid);
+			fprintf(fp, "</hotsubject>\n");
+        }
+		fprintf(fp, "</hotsubjects>\n");
+
+        fclose(fp);
+    }
+}
+
+void gen_hot_subjects_xml(int mytype)
+{
+	FILE *fp;
+	char curfile[256];
+	char xml_buf[256];
+	char url_buf[256];
+	int i;
+
+	if(mytype==4)
+		return gen_blessing_list_xml();
+
+    sprintf(curfile, "xml/%s.xml", myfile[mytype]);
+    if ((fp = fopen(curfile, "w")) != NULL) 
+	{
+		fprintf(fp, "<?xml version=\"1.0\" encoding=\"GBK\"?>\n");
+		fprintf(fp, "<hotsubjects>\n");
+
+        for (i = 0; i < topnum; i++) 
+		{
+			fprintf(fp, "<hotsubject>\n");
+			fprintf(fp, "<title>%s</title>\n", encode_url(url_buf,encode_xml(xml_buf, top[i].title, 
+						sizeof(xml_buf)),sizeof(url_buf)));
+			fprintf(fp, "<author>%s</author>\n", encode_url(url_buf,top[i].userid,sizeof(url_buf)));
+			fprintf(fp, "<board>%s</board>\n", encode_url(url_buf,top[i].board,sizeof(url_buf)));
+			fprintf(fp, "<time>%d</time>\n", top[i].date);
+			fprintf(fp, "<number>%d</number>\n", top[i].number);
+			fprintf(fp, "<groupid>%d</groupid>\n", top[i].groupid);
+			fprintf(fp, "</hotsubject>\n");
+        }
+		fprintf(fp, "</hotsubjects>\n");
+
+        fclose(fp);
+    }
+}
+
+void poststat(int mytype)
+{
+	get_top(mytype);
+	writestat(mytype);
+	gen_hot_subjects_xml(mytype);
+	if(mytype!=4)
+		gen_secs_hot_subjects_xml(mytype);
+}
+
+
+int main(int argc, char **argv)
+{
+    time_t now;
+    struct tm ptime;
+    int i;
+    char buf[80], curfile[80] ;
+
+	if (init_all()) {
+		printf("init data fail\n");
+		return -1;
+	}
+    time(&now);
+    ptime = *localtime(&now);
+    if (argc == 2) {
+        i = atoi(argv[1]);
+        if (i != 0 || argv[1][0]=='0') {
+            poststat(i);
+            return 0;
+        }
+    }
+
+    if (ptime.tm_hour == 0) {
+		if (ptime.tm_yday == 1)
+			poststat(3);
+        if (ptime.tm_mday == 1)
+            poststat(2);
+        if (ptime.tm_wday == 0)
+            poststat(1);
+    }
+
+    poststat(0);
+    poststat(4);
+
+    if (ptime.tm_hour == 23) {
+        char fname[STRLEN];
+
+        sprintf(fname, "%d年%2d月%2d日十大热门话题", ptime.tm_year + 1900, ptime.tm_mon + 1, ptime.tm_mday);
+        post_file(NULL, "", "etc/posts/day", "BBSLists", fname, 0, 1, getSession());
+        if (ptime.tm_wday == 6) {
+            sprintf(fname, "%d年%2d月%2d日本周五十大热门话题", ptime.tm_year + 1900, ptime.tm_mon + 1, ptime.tm_mday);
+            post_file(NULL, "", "etc/posts/week", "BBSLists", fname, 0, 1, getSession());
+        }
+    }
+    return 0;
+}
+
+#endif
