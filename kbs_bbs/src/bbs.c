e1f6fe57f0da8870a11a2378cda423079e56686b
atppp <atppp@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2004-06-03 05:31:00 +0000

    这里字符有点错位，不是故意的吧...
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@6935 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/src/bbs.c
+++ kbs_bbs/src/bbs.c
@@ -727,7 +727,7 @@ void readtitle(struct _select_def* conf)
     setfcolor(WHITE, DEFINE(getCurrentUser(), DEF_HIGHCOLOR));
     setbcolor(BLUE);
     clrtoeol();
-    prints(" 编号   %-12s %6s %s", "刊 登 者", "日  期", " 文章标题");
+    prints("  编号   %-12s %6s %s", "刊 登 者", "日  期", " 文章标题");
     sprintf(title, "在线:%4d [%4s模式]", bs->currentusers, readmode);
     move(2, -strlen(title)-1);
     prints("%s", title);
