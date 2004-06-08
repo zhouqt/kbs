82ceed35b006ee2b643b541473ef26ddf379c156
atppp <atppp@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2004-06-08 07:28:12 +0000

    树形回复结构测试，把roy写的三个核心函数换了，不会被打pp吧...
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@6992 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- wForum/loadtree.php
+++ wForum/loadtree.php
@@ -25,6 +25,8 @@ if (bbs_checkreadperm($usernum, $boardID) == 0) {
 }
 bbs_set_onboard($boardID,1);
 
+/* wforum's original implementaion. going to remove */
+/*
 $articles = bbs_get_article($boardName, $articleID);
 @$article=$articles[0];
 if ($article==NULL) {
@@ -34,6 +36,11 @@ $threadNum=bbs_get_thread_article_num($boardName,intval($article['ID']));
 $total=$threadNum+1;
 $threads=bbs_get_thread_articles($boardName, intval($article['ID']),0,$total);
 $total=count($threads);
+*/
+$num = bbs_get_threads_from_gid($boardID, $articleID, 0 , $threads , $haveprev );
+if ($num==0) {
+	exit(0);
+}
 
 //showTree($boardName,$boardID,$articleID,$article,$threads,$total);
 ?>
@@ -41,7 +48,7 @@ $total=count($threads);
 <script language="javascript" type="text/javascript" src="inc/browser.js"></script>
 <script language="javascript">
 	oTd=getParentRawObject("followTd<?php echo $articleID; ?>");
-	oTd.innerHTML='<TABLE border=0 cellPadding=0 cellSpacing=0 width="100%" align=center><TBODY><?php showTree($boardName,$boardID,$articleID,$article,$threads,$total);?></TBODY></TABLE>';
+	oTd.innerHTML='<TABLE border=0 cellPadding=0 cellSpacing=0 width="100%" align=center><TBODY><?php showTree($boardName,$boardID,$articleID,$threads,count($threads));?></TBODY></TABLE>';
 </script>
 </body>
 
@@ -77,6 +84,7 @@ function showTree($boardName,$boardID,$articleID,$article,$threads,$threadNum) {
 
 }
 */
+/* rem by atppp 2004.06.07
 function showTree($boardName,$boardID,$articleID,$article,$threads,$threadNum) {
 	$IDs=array();
 	$nodes=array();
@@ -91,12 +99,12 @@ function showTree($boardName,$boardID,$articleID,$article,$threads,$threadNum) {
 	$bottom++;
 	while($head<$bottom) {
 		if ($head==0) 
-			showTreeItem($boardName,$articleID,$article,0,$start, 0);
+			showTreeItem($boardName,$articleID,$article, 0, 0);
 		else 
-			showTreeItem($boardName,$articleID,$threads[$threadNum-$nodes[$head]],$nodes[$head], $level[$head]);
+			showTreeItem($boardName,$articleID,$threads[$nodes[$head]],$nodes[$head], $level[$head]);
 		for ($i=1;$i<=$threadNum;$i++){
-			if ( (!isset($printed[$i])) && ($threads[$threadNum-$i]['REID']==$IDs[$head]) ) {
-				$IDs[$bottom]=intval($threads[$threadNum-$i]['ID']);
+			if ( (!isset($printed[$i])) && ($threads[$i]['REID']==$IDs[$head]) ) {
+				$IDs[$bottom]=intval($threads[$i]['ID']);
 				$level[$bottom]=$level[$head]+1;
 				$printed[$i]=1;
 				$nodes[$bottom]=$i;
@@ -106,18 +114,86 @@ function showTree($boardName,$boardID,$articleID,$article,$threads,$threadNum) {
 		$head++;
 	}
 }
+*/
 
-function showTreeItem($boardName,$articleID,$thread,$threadID,$level){
+
+class TreeNode {
+	var $data;
+	var $index;
+	var $first_child;
+	var $last_child;
+	var $next_sibling;
+	
+	function TreeNode($data, $index) {
+		$this->data = &$data;
+		$this->index = $index;
+		$this->first_child = $this->last_child = $this->next_sibling = null;
+	}
+	
+	function addChild(&$node) { /* here it's very important to assign by reference */
+		if ($this->first_child == null) $this->first_child = &$node;
+		if ($this->last_child != null) {
+			$this->last_child->next_sibling = &$node;
+		}
+		$this->last_child = &$node;
+	}
+}
+
+function showTree($boardName,$boardID,$articleID,$threads,$threadNum) {
+	$more = ($threadNum > 30);
+	if ($more) $threadNum = 30; //最多显示30个，先这样吧。
+	
+	/* 产生回复树结构 */
+	$treenodes = array();
+	for($i=0; $i < $threadNum; $i++) {
+		$treenodes[$i] = new TreeNode($threads[$i], $i);
+	}
+	for($i=1; $i < $threadNum; $i++) {
+		for ($j=0; $j < $threadNum; $j++) {
+			if ($i == $j) continue;
+			if ($threads[$i]['REID'] == $threads[$j]['ID']) {
+				$treenodes[$j]->addChild($treenodes[$i]);
+				break;
+			}
+		}
+	}
+	
+	$lastflag = array(); //$lastflag[level]: 0: no more follows at this level; 1: more follows at this level;
+	showTreeRecursively($boardName, $boardID, $articleID, $treenodes, 0, 0, $lastflag);
+	
+	if ($more) echo '<tr><td class=TableBody1 width="100%" height=25 style="color:red"> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;... 还有更多 ...</td></tr>';
+}
+
+function showTreeRecursively($boardName, $boardID, $articleID, &$treenodes, $index, $level, &$lastflag) {
+	showTreeItem($boardName,$articleID, $treenodes[$index]->data, $index, $level, $lastflag);
+	$cur = &$treenodes[$index]->first_child;
+	while($cur != null) {
+		$temp = &$cur->next_sibling;
+		$lastflag[$level] = ($temp != null);
+		showTreeRecursively($boardName, $boardID, $articleID, $treenodes, $cur->index, $level+1, $lastflag);
+		$cur = &$temp;
+	}
+}
+
+function showTreeItem($boardName,$articleID,$thread,$threadID,$level, $lastflag){
 	echo '<TR><TD class=TableBody1 width="100%" height=25>';
 	for ($i=0;$i<$level;$i++) {
-		echo "&nbsp;&nbsp;";
+		if ($lastflag[$i]) {
+			if ($i == $level - 1) echo '<img src="pic/line2.gif">'; // |-
+			else echo '<img src="pic/line.gif">';                   // |
+		} else {
+			if ($i == $level - 1) echo '<img src="pic/line1.gif">'; // \
+			else echo "&nbsp;&nbsp;";                               // nothing
+		}
 	}
 	echo '<img src="pic/nofollow.gif"><a href="disparticle.php?boardName='.$boardName.'&ID='.$articleID.'&start='.$threadID.'&listType=1">';
-	if (strLen($thread['TITLE'])>22) {
+	/* 再多也多不过 ARTICLE_TITLE_LEN，别省略了吧 - atppp */
+/*	if (strLen($thread['TITLE'])>22) { 
 		echo htmlspecialchars(substr($thread['TITLE'],0,22),ENT_QUOTES).'...';
 	} else {
 		echo htmlspecialchars($thread['TITLE'],ENT_QUOTES);
-	}
+	} */
+	echo htmlspecialchars($thread['TITLE'],ENT_QUOTES);
 	echo '</a> -- <a href="dispuser.php?id='.$thread['OWNER'].'">'.$thread['OWNER'].'</a></td></tr>';
 }
 ?>
