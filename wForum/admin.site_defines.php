728516481e56eaa496e79715b02ea8a982f8edd7
atppp <atppp@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2004-06-19 07:41:36 +0000

    搞个能看配置的，一步一步来
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@7094 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- wForum/admin.site_defines.php
+++ wForum/admin.site_defines.php
@@ -16,9 +16,14 @@ main();
 show_footer();
 
 function preprocess() {
-	global $userid;
-	if (isset($userid) && ($userid == "SYSOP")) return; //以后在改吧
+	global $currentuser;
+	if ($currentuser["userlevel"] & BBS_PERM_SYSOP) return;
 	foundErr("不许看 :)");
+}
+
+function show_value($value, $is_boolean) {
+	if ($is_boolean) return ($value?"是":"否");
+	else return htmlspecialchars($value);
 }
 
 function main() {
@@ -28,13 +33,10 @@ function main() {
 <tr> 
 	  <th colspan="3">站点配置参数显示</th>
 </tr>
-<tr> 
-	  <th>参数说明</th><th>当前值</th><th>默认值</th>
-</tr>
 <?php
 	$ccc = count($site_defines);
 	for ($i = 0; $i < $ccc; $i++) {
-		if ($site_defines[$i] === false) return;
+		if ($site_defines[$i] === false) break;
 		if (is_string($site_defines[$i])) {
 ?>
 <tr>
@@ -42,29 +44,37 @@ function main() {
 </tr>
 <?php
 		} else {
+			$is_boolean = ($site_defines[$i][2] == "b");
+			$is_constant = ($site_defines[$i][0] == 0);
+			$cur_var = $site_defines[$i][1];
+			$default = $site_defines[$i][3];
+			if ($is_constant) {
+				$cur = constant($cur_var);
+			} else {
+				$cur = $GLOBALS[$cur_var];
+			}
+			$is_default = ($default == $cur);
 ?>
 <tr>
 	<td class=TableBody1>
-		<b><?php echo htmlspecialchars($site_defines[$i][4]); ?></b>
+<?php
+			if (!$is_default) echo "<b>";
+			echo htmlspecialchars($site_defines[$i][4]);
+			if (!$is_default) echo "</b>&nbsp;&nbsp;[默认值：".show_value($default, $is_boolean)."]";
+?>
 	</td>
 	<td class=TableBody1>
 <?php
-			$varname = $site_defines[$i][1];
-			if ($site_defines[$i][0]) { //变量
-				$var = $GLOBALS[$varname];
-			} else { //常量
-				$var = constant($varname);
-			}
-			if ($site_defines[$i][2] == "b") echo ($var?"是":"否");
-			else echo htmlspecialchars($var);
+			if (!$is_default) echo "<b>";
+			echo show_value($cur, $is_boolean);
+			if (!$is_default) echo "</b>";
 ?>	
 	</td>
 	<td class=TableBody1>
 <?php
-	$var = $site_defines[$i][3];
-	if ($site_defines[$i][2] == "b") echo ($var?"是":"否");
-	else echo htmlspecialchars($var);
-?>
+			echo $is_constant?"常量":"<b>变量</b>";
+			echo " 名称：".htmlspecialchars($cur_var);
+?>	
 	</td>
 </tr>
 <?php
