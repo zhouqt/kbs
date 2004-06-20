90de5e86015753af301606e4c55d2ed692686865
atppp <atppp@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2004-06-20 07:55:16 +0000

    site defines
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@7104 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- wForum/inc/sites/default.php
+++ wForum/inc/sites/default.php
@@ -2,10 +2,73 @@
 $Version="Powered by wForum Version 0.9";
 $Copyright="<a href='http://www.aka.cn/' target=_blank>阿卡信息技术(北京)有限公司</a> & <a href='http://www.smth.cn'>水木清华BBS</a> 版权所有 1994 - 2008 <br><font face=Verdana, Arial, Helvetica, sans-serif size=1><b>Roy<font color=#CC0000>@zixia.net</font></b></font> ";
 
-if (!isset($AnnounceBoard)) { //公告版面
-	$AnnounceBoard="Announce";
+
+/*
+ * 可配置参数总列表
+ *
+ * 格式
+ * 0. integer: 是常数(0)还是变量(1)，一般站点定义参数都应该是常数
+ * 1. string:  常量或变量名称
+ * 2. string:  常量或变量类型：integer(i), string(s), boolean(b)
+ * 3. mixed:   默认值
+ * 4. string:  参数说明
+ */
+$site_defines = array(
+"网站基本配置",
+array(1, "SiteURL", 's', "http://localhost/", "站点 URL，也就是本站首页地址"),
+array(1, "SiteName", 's', "测试站", "站点名称"),
+array(1, "Banner", 's', "pic/ws.jpg", "页面左上角显示的站点标题图片"),
+array(0, "MAINTITLE", 's', "<IMG SRC=\"bar/title.jpg\">", "页面正上方显示的站点标题"),
+array(0, "OLD_REPLY_STYLE", 'b', false, "是否使用传统 telnet re 文方式"),
+array(0, "SHOW_REGISTER_TIME", 'b', false, "是否显示用户注册时间"),
+array(1, "AnnounceBoard", 's', "Announce", "系统公告版面英文名称"),
+array(0, "SERVERTIMEZONE", 's', '北京时间', "服务器时区"),
+"页面参数",
+array(0, "ANNOUNCENUMBER", 'i', 5, "首页滚动显示的公告数量"),
+array(0, "ARTICLESPERPAGE", 'i', 30, "目录列表下每页显示的主题数"),
+array(0, "USERSPERPAGE", 'i', 20, "在线用户/好友等列表每页显示的人数"),
+array(0, "THREADSPERPAGE", 'i', 10, "同主题阅读每页文章数"),
+"附件相关",
+array(0, "ATTACHMAXSIZE", 's', '524288', "最大附件尺寸，需要同时修改php.ini里的upload_max_size ,单位byte"),
+array(0, "ATTACHMAXTOTALSIZE", 's', '2097152', "一篇文章中所有附件的总大小限制, 单位byte"),
+array(0, "ATTACHMAXCOUNT", 's', '5', "一篇文章中最多可上载的附件数量"),
+"自定义头像相关",
+array(0, "USER_FACE", 'b', true, "是否允许自定义头像"),
+array(0, "MYFACEDIR", 's', "uploadFace/", "自定义头像上传保存位置"),
+array(0, "MYFACEMAXSIZE", 's', "524288", "自定义头像最大文件大小, 单位byte"),
+"数据库相关",
+array(1, "dbhost", 's', "localhost", "数据库服务器地址"),
+array(1, "dbuser", 's', "", "数据库服务器用户"),
+array(1, "dbpasswd", 's', "", "数据库服务器密码"),
+array(1, "dbname", 's', "", "数据库表"),
+"其他附加功能",
+array(0, "SHOWTELNETPARAM", 'b', true, "是否允许配置 telnet 下专用的个人参数"),
+array(0, "SHOWREPLYTREE", 'b', true, "是否用树图显示回复结构"),
+array(0, "ALLOWMULTIQUERY", 'b', false, "是否允许全站/多版面查询"),
+array(0, "SMS_SUPPORT", 'b', false, "是否允许手机短信"),
+array(0, "AUDIO_CHAT", 'b', false, "是否显示语音聊天室的 link"),
+false
+);
+
+/* site.php 没有定义过的参数都定义一下 */
+$ccc = count($site_defines);
+for ($i = 0; $i < $ccc; $i++) {
+	if ($site_defines[$i] === false) break;
+	if (is_string($site_defines[$i])) continue;
+	
+	$default = $site_defines[$i][3];
+	$cur_var = $site_defines[$i][1];
+	if ($site_defines[$i][0] == 0) {
+		if (!defined($cur_var)) define($cur_var, $default);
+	} else {
+		if (!isset($$cur_var)) $$cur_var = $default;
+	}
 }
 
+
+
+
+/* 还有一些遗留下来的可配置参数，以后看情况要不要放入 site_defines */
 $HTMLCharset = "GB2312"; //这个我觉得肯定是 gb2312 吧，以后要变的话再做成可自定义的参数
 
 if (!isset($HTMLTitle)) {
@@ -20,87 +83,18 @@ if (!defined('BOARDLISTSTYLE')) {
 	define('BOARDLISTSTYLE','default');
 }
 
-if (!defined('ANNOUNCENUMBER')) {   //首页滚动显示的公告数量
-	define('ANNOUNCENUMBER',5);
-}
-
-if (!defined('ARTICLESPERPAGE')) {
-	define('ARTICLESPERPAGE',30); //目录列表下每页显示的主题数
-}
-
-if (!defined('USERSPERPAGE')) {
-    define("USERSPERPAGE", 20); //在线用户/好友等列表每页显示的人数
-}
-
-if (!defined('THREADSPERPAGE')) { //同主题阅读每页文章数
-	define('THREADSPERPAGE',10);
-}
-
-if (!defined('MAINTITLE')) { //标题图片
-	define ('MAINTITLE','<IMG SRC="bar/title.jpg">');
-}
-
-if (!defined('ATTACHMAXSIZE')) { //最大附件尺寸，需要同时修改php.ini里的upload_max_size ,单位byte
-	define('ATTACHMAXSIZE','524288');
-}
-
-if (!defined('ATTACHMAXTOTALSIZE')) { //一篇文章中所有附件的总大小限制, 单位byte
-	define('ATTACHMAXTOTALSIZE','2097152');
-}
-
-if (!defined('ATTACHMAXCOUNT')) { //一篇文章中最多可上载的附件数量
-	define('ATTACHMAXCOUNT','5');
-}
-
-if (!defined('SERVERTIMEZONE')) { //服务器时区 - 暂时这样搞吧 - atppp
-	define('SERVERTIMEZONE','北京时间');
-}
-
 if (!defined('USEBROWSCAP')) { //是否使用 browscap 函数来更准确地判断浏览器和操作系统类型，需要配置 PHP - atppp
-	define('USEBROWSCAP',0); //默认 OFF. 设置成 ON 虽然会准确判断出非 IE 浏览器，但是会大大降低出首页的速度，慎用！//ToDo: 这个没有放入 site_defines.php
+	define('USEBROWSCAP',0); //默认 OFF. 设置成 ON 虽然会准确判断出非 IE 浏览器，但是会大大降低出首页的速度，慎用！
 }
 
-if (!defined('SHOWTELNETPARAM')) { //是否允许配置 telnet 下专用的个人参数 - atppp
-	define('SHOWTELNETPARAM',1); //默认 ON，保持和以前的兼容性
-}
 
-if (!defined('MYFACEDIR')) { //自定义头像上传保存位置
-	define('MYFACEDIR','uploadFace/');
-}
 
-if (!defined('MYFACEMAXSIZE')) { //自定义头像最大文件大小, 单位byte
-	define('MYFACEMAXSIZE','524288');
-}
-
-if (!defined('SHOWREPLYTREE')) { //是否用树图显示回复结构
-	define('SHOWREPLYTREE', 1);  //默认用树图显示
-}
-
-if (!defined('ALLOWMULTIQUERY')) { //是否允许全站/多版面查询
-	define('ALLOWMULTIQUERY', 0); //默认关闭
-}
-
-if (!defined('SMS_SUPPORT')) { //是否允许手机短信，ToDo: 其实最好是 phplib 里面输出 #define SMS_SUPPORT 的参数
-	define('SMS_SUPPORT', 0); //默认关闭
-}
-
-if (!defined('USER_FACE')) { //是否允许自定义头像
-	define('USER_FACE', 1); //默认开启
-}
-
-if (!defined('AUDIO_CHAT')) { //是否显示语音聊天室的 link
-	define('AUDIO_CHAT', 0); //默认关闭
-}
-
-if (!defined('OLD_REPLY_STYLE')) { //是否使用传统 telnet re 文方式
-	define('OLD_REPLY_STYLE', true); //默认关闭
-}
-
-if (!defined('SHOW_REGISTER_TIME')) { //是否显示用户注册时间
-	define('SHOW_REGISTER_TIME', false); //默认关闭
-}
-
-/* 格式：每项五个参数，第一个 0 表示 telnet 下专用参数；第二个是参数名称；第三个是参数具体解释；第四五是参数 ON 和 OFF 所代表的具体含义 */
+/*
+ * 用户自定义参数
+ *
+ * 格式：每项五个参数，第一个 0 表示 telnet 下专用参数；第二个是参数名称；
+ *                     第三个是参数具体解释；第四五是参数 ON 和 OFF 所代表的具体含义
+ */
 if (!isset($user_define)) {
 	$user_define=array(array(0,"显示活动看版", "在telnet方式下是否显示活动看板","显示","不显示"), /* DEF_ACBOARD */
 	    array(0,"使用彩色", "在telnet方式下是否使用彩色显示", "使用", "不使用"),                /* DEF_COLOR */
