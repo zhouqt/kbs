cf6bf87c7b287315b5c17cdd0b8d0887e1fee6da
atppp <atppp@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2004-06-26 17:58:08 +0000

    bug and indent
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@7140 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/bbs2www/html/bbssig.php
+++ kbs_bbs/bbs2www/html/bbssig.php
@@ -1,43 +1,41 @@
 <?php
-	require("funcs.php");
+    require("funcs.php");
 login_init();
-	if ($loginok != 1)
-		html_nologin();
-	else
-	{
-		html_init("gb2312");
-		$filename=bbs_sethomefile($currentuser["userid"],"signatures");
-		if (isset($_GET['type']))
-    		if ($_GET["type"]=="1") {
-    			if(!strcmp($currentuser["userid"],"guest"))
-            		html_error_quit("不能给guest设置签名档!");
-    		    $fp=@fopen($filename,"w+");
+    if ($loginok != 1)
+        html_nologin();
+    else
+    {
+        html_init("gb2312");
+        $filename=bbs_sethomefile($currentuser["userid"],"signatures");
+        if (isset($_GET['type']))
+            if ($_GET["type"]=="1") {
+                if(!strcmp($currentuser["userid"],"guest"))
+                    html_error_quit("不能给guest设置签名档!");
+                $fp=@fopen($filename,"w+");
                         if ($fp!=false) {
-    		    fwrite($fp,str_replace("\r\n", "\n", $_POST["text"]));
-    		    fclose($fp);
+                fwrite($fp,str_replace("\r\n", "\n", $_POST["text"]));
+                fclose($fp);
                         }
-    			bbs_recalc_sig();
-    		}
+                bbs_recalc_sig();
+            }
 ?>
 <body>
 <center><?php echo BBS_FULL_NAME; ?> -- 设置签名档 [使用者: <?php echo $currentuser["userid"]; ?>]</center><hr />
 <form name=form1 method="post" action="bbssig.php?type=1">
 签名档每6行为一个单位, 可设置多个签名档.<table width="610" border="1"><tr><td><textarea name="text"  onkeydown='if(event.keyCode==87 && event.ctrlKey) {document.form1.submit(); return false;}'  onkeypress='if(event.keyCode==10) return document.form1.submit()' rows="20" cols="100" wrap="physical">
 <?php
-if (isset($_GET['type'])) {
-    if ($_GET["type"]!="1") {
-        $fp = @fopen ($filename, "r");
-        if ($fp!=false) {
+if (!isset($_GET['type']) || ($_GET["type"]!="1")) {
+    $fp = @fopen ($filename, "r");
+    if ($fp!=false) {
         while (!feof ($fp)) {
             $buffer = fgets($fp, 300);
             echo $buffer;
         }
         fclose ($fp);
-        }
     }
-    else {
-        echo $_POST["text"];
-    }
+}
+else {
+    echo $_POST["text"];
 }
 ?>
 </textarea></table>
@@ -46,5 +44,5 @@ if (isset($_GET['type'])) {
 </body>
 </html>
 <?php
-	}
+    }
 ?>
