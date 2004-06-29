4f3955555e50351f310d5f2861cbebc257c5bb59
flyriver <flyriver@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2004-06-29 15:48:14 +0000

    跟进一下。
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@7156 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/site/fb2k-v2.h
+++ kbs_bbs/site/fb2k-v2.h
@@ -23,7 +23,16 @@
 #define BUILD_PHP_EXTENSION 1   /*½«php lib±à³Éphp extension */
 /*#define USE_SEM_LOCK 1*/
 
+#ifdef HAVE_MYSQL
 #define PERSONAL_CORP
+#else
+#undef PERSONAL_CORP
+#endif
+
+#ifndef HAVE_LIBESMTP
+#define OWNSENDMAIL "/usr/lib/sendmail"
+#endif
+
 #define HAVE_WFORUM	1
 #define RAW_ARTICLE 0
 #define FB2000			1
