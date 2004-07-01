9dc2038a5b82878893e3cdf6f8605339f76c7c39
stiger <stiger@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2004-07-01 10:20:20 +0000

    genbplot
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@7166 de4889a5-8ad4-4bf3-bd87-57997c21d515

---
+++ kbs_bbs/local_utl/genbplot.c
@@ -0,0 +1,55 @@
+#include "bbs.h"
+
+#define BONLINE_LOGDIR "/home/bbs/bonlinelog"
+int generate_board(struct boardheader *bh,void* arg)
+{
+	char buf[256];
+
+	if(bh==NULL || bh->filename[0]=='\0')
+		return 0;
+
+    if (check_see_perm(NULL, bh)==0)
+		return 0;
+
+	sprintf(buf,"%s/boards/%s.all",BONLINE_LOGDIR, bh->filename);
+
+	if(!dashf(buf))
+		return 0;
+
+	sprintf(buf,"sed -e \"s/BOARD/%s/g\" boards.plt > boardstmp.plt", bh->filename);
+	system(buf);
+	system("gnuplot boardstmp.plt");
+
+	return 0;
+}
+
+int generate_all()
+{
+	apply_boards(generate_board,NULL);
+    return 0;
+}
+
+int main(int argc,char** argv)
+{
+	struct boardheader bh;
+
+	chdir(BBSHOME);
+	resolve_boards();
+
+	if(argc < 2){
+		printf("./genbplot [-a|boardname]\n");
+		exit(0);
+	}
+	if(argv[1][0]!='-'){
+    	strncpy(bh.filename,argv[1],STRLEN);
+    	generate_board(&bh,NULL);
+	}else{
+		if(!strcmp(argv[1],"-a")){
+    		generate_all();
+		}else{
+			printf("./genbplot [-a|boardname]\n");
+		}
+	}
+
+    return 0;
+}
