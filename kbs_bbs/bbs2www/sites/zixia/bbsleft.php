02cf5538882d7c0a47ee28065a5fe35cfa45a1e9
atppp <atppp@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2004-07-05 00:02:34 +0000

    update
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@7207 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/bbs2www/sites/zixia/bbsleft.php
+++ kbs_bbs/bbs2www/sites/zixia/bbsleft.php
@@ -2,6 +2,7 @@
 	/*This file shows user tools. windinsn Oct 27,2003*/
 	
 	require("funcs.php");
+login_init();
 	
         $img_subdir = "/images/menuspring/";
         
@@ -9,6 +10,7 @@
 	{
 ?>
 <table width="100%" border="0" cellspacing="0" cellpadding="1" class="b1">
+<col width="16px"/><col align="left"/>
 <?php
 		$i = 0;
 		foreach ($section_names as $secname)
@@ -19,7 +21,7 @@
 			$level = 0;
 ?>
 <tr>
-<td align="right" width="16">
+<td align="right">
 <a href="javascript:submenu(0,0,<?php echo $group; ?>,0,0)">
 <img id="submenuimg_brd_<?php echo $group; ?>_0" src="/images/close.gif" border="0">
 </a>
@@ -38,7 +40,7 @@
 		}
 ?>
 <tr>
-<td align="right" width="16">
+<td align="right">
 <img src="/images/open.gif" border="0">
 </td>
 <td>
@@ -53,8 +55,9 @@
 	{
 ?>
 <table width="100%" border="0" cellspacing="0" cellpadding="0" class="b1">
+<col width="16px"/><col align="left"/>
 <?php
- 		$select = -1; 
+ 		$select = 0; 
  		$yank = 0;
  		 		
                 if( bbs_load_favboard($select)!=-1 && $boards = bbs_fav_boards($select, 1)) 
@@ -71,7 +74,7 @@
 				{
 ?>
 <tr>
-<td align="right" width="16">
+<td align="right">
 <a href="javascript:submenu(1,<?php echo $brd_bid[$j]; ?>,0,0,0)">
 <img id="submenuimg_fav_<?php echo $brd_bid[$j]; ?>" src="/images/close.gif" border="0">
 </a>
@@ -92,7 +95,7 @@
 				{
 ?>
 <tr>
-<td width="16" align="right">
+<td align="right">
 <?php			  		
 			  		$brd_link="/bbsdoc.php?board=" . urlencode($brd_name[$j]);
 
@@ -115,7 +118,6 @@
 <?php
 				}
 			}
-                        bbs_release_favboard(); 
                         
                 }
 ?>
@@ -164,7 +166,8 @@
 		$db["PASS"]=bbs_sysconf_str("MYSQLPASSWORD");
 		$db["NAME"]=bbs_sysconf_str("MYSQLSMSDATABASE");
 		
-		@$link = mysql_connect($db["HOST"],$db["USER"],$db["PASS"]) or die("无法连接到服务器!");
+		@$link = mysql_connect($db["HOST"],$db["USER"],$db["PASS"]) ;
+		if (!$link) return;
 		@mysql_select_db($db["NAME"],$link);
 		
 		$query = "SELECT `uid` FROM `users` WHERE `username` = '".$userid."' AND `createtime`  > ".date("YmdHis",$userfirstlogin)." LIMIT 0,1 ;";
@@ -211,7 +214,10 @@
 <A href="/pc/pcdoc.php?userid=<?php echo $userid; ?>&tag=7" target="f3">参数设定</A><BR>
 &nbsp;
 <img src="images/line.gif" width="11" height="16" align="absmiddle">
-<A href="/pc/pcmanage.php?userid=<?php echo $userid; ?>&act=post&tag=0&pid=0" target="f3">添加文章</A><BR>
+<A href="/pc/pcfile.php?userid=<?php echo $userid; ?>" target="f3">个人空间</A><BR>
+&nbsp;
+<img src="images/line.gif" width="11" height="16" align="absmiddle">
+<A href="/pc/pcmanage.php?userid=<?php echo $userid; ?>&act=post&tag=0&pid=0" target="_blank">添加文章</A><BR>
 <?php		
 		}	
 	}
@@ -223,6 +229,20 @@
 
 ?>
 <script src="bbsleft.js"></script>
+<script language="javascript">
+function bbs_auto_reload() {
+    top.f4.location.reload();
+<?php
+    if($currentuser["userid"]!="guest"){
+?>
+    top.fmsg.location.reload();
+<?php
+    }
+?>
+    setTimeout('bbs_auto_reload()',540000);
+}
+setTimeout('bbs_auto_reload()',540000);
+</script>
 <body  TOPMARGIN="0" leftmargin="0">
 <iframe id="hiddenframe" name="hiddenframe" width="0" height="0"></iframe>
 <table width="100%" border="0" cellspacing="0" cellpadding="0">
@@ -231,8 +251,8 @@
 		<table width="100%" border="0" cellspacing="0" cellpadding="0">
 		<tr>
 			<td colspan="2">
-			<!--<img src="/images/t1.gif" border="0"> -->
-<span style="font: bold 10pt 'Comic Sans MS',Arial;">&nbsp;BBS.<span style="color:hotpink">ZIXIA</span>.NET</span>
+			<!--<img src="/images/t1.gif" border="0">-->
+			<span style="font: bold 10pt 'Comic Sans MS',Arial;">&nbsp;BBS.<span style="color:hotpink">ZIXIA</span>.NET</span>
 			</td>
 		</tr>
 <?php
@@ -285,7 +305,7 @@
 ?>
 		<tr>
 			<td colspan="2" class="t2">
-		<!--	<img src="/images/t2.gif" border="0">-->
+			<!--<img src="/images/t2.gif" border="0">-->
 			</td>
 		</tr>
 		</table>
@@ -297,16 +317,17 @@
 <tr>
 	<td align="center">
 		<table width="90%" border="0" cellspacing="0" cellpadding="1" class="b1">
+		<col width="16px"/><col align="left"/>
 		<tr>
-			<td width="16"><img src="/images/open.gif" border="0"></td>
+			<td><img src="/images/open.gif" border="0"></td>
 			<td><a href="<?php echo MAINPAGE_FILE; ?>" target="f3"><img src="<?php echo $img_subdir; ?>m1.gif" border="0" alt="首页" align="absmiddle"> 首页导读</a></td>
 		</tr>
 		<tr>
-			<td width="16"><img src="/images/open.gif" border="0"></td>
-			<td><a href="/cgi-bin/bbs/bbs0an" target="f3"><img src="<?php echo $img_subdir; ?>m2.gif" border="0" alt="精华区" align="absmiddle"> 精华区</a></td>
+			<td><img src="/images/open.gif" border="0"></td>
+			<td><a href="/bbs0an.php" target="f3"><img src="<?php echo $img_subdir; ?>m2.gif" border="0" alt="精华区" align="absmiddle"> 精华区</a></td>
 		</tr>
 		<tr>
-			<td width="16">
+			<td>
 				<DIV class="r" id="divboarda">
 				<a href='javascript:changemn("board");'><img id="imgboard" src="/images/close.gif" border="0"></a>
 				</DIV>
@@ -324,11 +345,11 @@
 			</td>
 		</tr>
 		<tr>
-			<td width="16"><img src="/images/open.gif" border="0"></td>
-			<form action="cgi-bin/bbs/bbssel" target="f3">
+			<td><img src="/images/open.gif" border="0"></td>
+			<form action="/bbssel.php" method="get" target="f3">
 			<td><nobr>
 			<img src="<?php echo $img_subdir; ?>m5.gif" border="0" alt="搜索讨论区" align="absmiddle">
-			<input name="board" type="text" class="f2" value="搜索讨论区" size="12" onclick="this.value=''" /> 
+			<input name="board" type="text" class="f2" value="搜索讨论区" size="12" onmouseover="this.focus()" onfocus="this.select()" /> 
 <input name="submit" type="submit" value="GO" style="width:25px;height:20px;font-size: 12px;color: #ffffff;border-style: none;background-color: #718BD6;" />
 			</nobr></td>
 			</form>
@@ -337,12 +358,12 @@
 	if($currentuser["userid"]!="guest"){
 ?>
 		<tr>
-			<td width="16">
+			<td>
 				<DIV class="r" id="divfava">
 				<a href='javascript:changemn("fav");'><img id="imgfav" src="/images/close.gif" border="0"></a>
 				</DIV>
 			</td>
-			<td><a href="bbsfav.php?select=-1" target="f3"><img src="<?php echo $img_subdir; ?>mfolder3.gif" border="0" alt="我的收藏夹" align="absmiddle"> 我的收藏夹</a></td>
+			<td><a href="bbsfav.php?select=0" target="f3"><img src="<?php echo $img_subdir; ?>mfolder3.gif" border="0" alt="我的收藏夹" align="absmiddle"> 我的收藏夹</a></td>
 		</tr>
 		<tr>
 			<td> </td>
@@ -362,7 +383,7 @@
 	{
 ?>
 		<tr>
-			<td width="16">
+			<td>
 				<DIV class="r" id="divpca">
 				<a href='javascript:changemn("pc");'><img id="imgpc" src="/images/close.gif" border="0"></a>
 				</DIV>
@@ -389,7 +410,7 @@
 					<a href="/pc/pc.php" target="f3">用户列表</a><br>
 					&nbsp;
 					<img src="/images/line.gif" border="0" align="absmiddle">
-					<a href="/pc/pcrec.php" target="f3">推荐文章</a><br>
+					<a href="/pc/pcreco.php" target="f3">推荐文章</a><br>
 					&nbsp;
 					<img src="/images/line.gif" border="0" align="absmiddle">
 					<a href="/pc/pclist.php" target="f3">热门排行</a><br>
@@ -398,16 +419,16 @@
 					<a href="/pc/pcsec.php" target="f3">分类目录</a><br>
 					&nbsp;
 					<img src="/images/line.gif" border="0" align="absmiddle">
-					<a href="/pc/pcnew.php" target="f3">最新文章</a><br>
+					<a href="/pc/pcnew.php" target="f3">最新日志</a><br>
 					&nbsp;
 					<img src="/images/line.gif" border="0" align="absmiddle">
 					<a href="/pc/pcnew.php?t=c" target="f3">最新评论</a><br>
 					&nbsp;
 					<img src="/images/line.gif" border="0" align="absmiddle">
-					<a href="/pc/pcsearch2.php" target="f3">文集搜索</a><br>
+					<a href="/pc/pcsearch2.php" target="f3">博客搜索</a><br>
 					&nbsp;
 					<img src="/images/line.gif" border="0" align="absmiddle">
-					<a href="/pc/pcnsearch.php" target="f3">文章搜索</a><br>
+					<a href="/pc/pcnsearch.php" target="f3">日志搜索</a><br>
 					&nbsp;
 					<img src="/images/line.gif" border="0" align="absmiddle">
 					<a href="/bbsdoc.php?board=SMTH_blog" target="f3">Blog论坛</a><br>
@@ -425,7 +446,7 @@
 	if($currentuser["userid"]!="guest"){
 ?>
 		<tr>
-			<td width="16">
+			<td>
 				<DIV class="r" id="divmaila">
 				<a href='javascript:changemn("mail");'><img id="imgmail" src="/images/close.gif" border="0"></a>
 				</DIV>
@@ -446,7 +467,7 @@
 		}
 ?>
 		<tr>
-			<td width="16">
+			<td>
 				<DIV class="r" id="divchata">
 				<a href='javascript:changemn("chat");'><img id="imgchat" src="/images/close.gif" border="0"></a>
 				</DIV>
@@ -461,10 +482,14 @@
 			<td> </td>
 			<td>
 				<DIV class="s" id="divchat">
+<?php
+    if (!defined("SITE_SMTH")) { // Smth不提供在线用户列表 add by windinsn, May 5,2004
+?>
 					&nbsp;
 					<img src="/images/line.gif" border="0" align="absmiddle">
 					<a href="bbsuser.php" target="f3">在线用户</a><br>
 <?php
+        }
 	if($currentuser["userid"]=="guest"){
 ?>					
 					&nbsp;
@@ -495,12 +520,16 @@
 				</DIV>
 			</td>
 		</tr>
+		<tr>
+			<td><img src="/images/open.gif" border="0"></td>
+			<td><a href="/bbsstyle0.php" target="f3"><img src="<?php echo $img_subdir; ?>m6.gif" border="0" alt="界面方案" align="absmiddle"> 界面方案</a>
+		</tr>
 <?php
 	if($currentuser["userid"]!="guest")
 	{
 ?>
 		<tr>
-			<td width="16">
+			<td>
 				<DIV class="r" id="divtoola">
 				<a href='javascript:changemn("tool");'><img id="imgtool" src="/images/close.gif" border="0"></a>
 				</DIV>
@@ -515,9 +544,24 @@
 			<td> </td>
 			<td>
 				<DIV class="s" id="divtool">
+<?php
+	if(!($currentuser["userlevel"]&BBS_PERM_LOGINOK) )
+	{
+?>					
+					&nbsp;
+					<img src="/images/line.gif" border="0" align="absmiddle">
+					<a href="/bbsnew.php" target="f3">新用户须知</a><br>
+<!--
+					&nbsp;
+					<img src="/images/line.gif" border="0" align="absmiddle">
+					<a href="/bbssendacode.php" target="f3">发送激活码</a><br>
+-->
 					&nbsp;
 					<img src="/images/line.gif" border="0" align="absmiddle">
 					<a href="/bbsfillform.html" target="f3">填写注册单</a><br>
+<?php
+	}
+?>
 					&nbsp;
 					<img src="/images/line.gif" border="0" align="absmiddle">
 					<a href="/bbsinfo.php" target="f3">个人资料</a><br>
@@ -529,10 +573,10 @@
 					<a href="bbssig.php" target="f3">改签名档</a><br>
 					&nbsp;
 					<img src="/images/line.gif" border="0" align="absmiddle">
-					<a href="cgi-bin/bbs/bbspwd" target="f3">修改密码</a><br>
+					<a href="/bbspwd.php" target="f3">修改密码</a><br>
 					&nbsp;
 					<img src="/images/line.gif" border="0" align="absmiddle">
-					<a href="cgi-bin/bbs/bbsparm" target="f3">修改个人参数</a><br>
+					<a href="/bbsparm.php" target="f3">修改个人参数</a><br>
 					&nbsp;
 					<img src="/images/line.gif" border="0" align="absmiddle">
 					<a href="/bbsal.php" target="f3">通讯录</a><br>
@@ -553,30 +597,7 @@
 ?>
 <!--
 		<tr>
-			<td width="16">
-				<DIV class="r" id="divstylea">
-				<a href='javascript:changemn("style");'><img id="imgstyle" src="/images/close.gif" border="0"></a>
-				</DIV>
-			</td>
-			<td><img src="<?php echo $img_subdir; ?>m7.gif" border="0" alt="界面风格" align="absmiddle"> 界面风格</td>
-		</tr>
-		<tr>
-			<td> </td>
 			<td>
-				<DIV class="s" id="divstyle">
-					&nbsp;
-					<img src="/images/line.gif" border="0" align="absmiddle">
-					<a href="/bbsstyle.php?s=0">大字体</a><br>
-					&nbsp;
-					<img src="/images/line1.gif" border="0" align="absmiddle">
-					<a href="/bbsstyle.php?s=1">小字体</a><br>
-				</DIV>
-			</td>
-		</tr>
--->
-<!--
-		<tr>
-			<td width="16">
 				<DIV class="r" id="divexpa">
 				<a href='javascript:changemn("exp");'><img id="imgexp" src="/images/close.gif" border="0"></a>
 				</DIV>
@@ -587,7 +608,6 @@
 			</a>
 			</td>
 		</tr>
--->
 		<tr>
 			<td> </td>
 			<td>
@@ -608,7 +628,7 @@
 			</td>
 		</tr>
 		<tr>
-			<td width="16">
+			<td>
 				<DIV class="r" id="divsera">
 				<a href='javascript:changemn("ser");'><img id="imgser" src="/images/close.gif" border="0"></a>
 				</DIV>
@@ -619,6 +639,7 @@
 			</a>
 			</td>
 		</tr>
+-->
 		<tr>
 			<td> </td>
 			<td>
@@ -626,6 +647,15 @@
 					&nbsp;
 					<img src="/images/line.gif" border="0" align="absmiddle">
 					<a href="/games/index.html" target="f3">休闲娱乐</a><br>
+<?php
+    if (defined("SERVICE_QUIZ")) {
+?>
+                    &nbsp;
+					<img src="/images/line.gif" border="0" align="absmiddle">
+					<a href="/games/quiztop.php" target="f3">开心辞典</a><br>
+<?php
+    }
+?>
 					&nbsp;
 					<img src="/images/line.gif" border="0" align="absmiddle">
 					<a href="/data/fterm-smth.zip" target="_blank">Fterm下载</a><br>
@@ -638,17 +668,26 @@
 			</td>
 		</tr>
 		<tr>
-			<td width="16"><img src="/images/open.gif" border="0"></td>
+			<td><img src="/images/open.gif" border="0"></td>
 			<td><a href="telnet:bbs.zixia.net"><img src="<?php echo $img_subdir; ?>m6.gif" border="0" alt="telnet登录" align="absmiddle"> Telnet登录</a>
 		</tr>
+<?php
+    if (defined("SITE_SMTH")) {
+        if ($currentuser["userlevel"]&BBS_PERM_SYSOP) {
+?>
+<script src="/bbsleftmenu.js"></script>
+<?php
+        }
+    }
+?>
 <!--
 		<tr>
-			<td width="16"><img src="/images/open.gif" border="0"></td>
+			<td><img src="/images/open.gif" border="0"></td>
 			<td><img src="/images/t10.gif" border="0" alt="查看帮助信息" align="absmiddle"> 查看帮助
 		</tr>
 -->
 		<tr>
-			<td width="16"><img src="/images/open.gif" border="0"></td>
+			<td><img src="/images/open.gif" border="0"></td>
 			<td><a href="/bbslogout.php" target="_top"><img src="<?php echo $img_subdir; ?>m7.gif" border="0" alt="离开本站" align="absmiddle"> 离开本站</a>
 		</tr>
 		
@@ -657,8 +696,12 @@
 </tr>
 </table>
 <script type="text/javascript" src="/bbsleftad.js"> </script>
+<P aling="center">
 <?php
-		
+		powered_by_smth();
+?>
+</P>
+<?php
 		html_normal_quit();
 		}
 ?>
