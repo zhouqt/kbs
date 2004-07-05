20d1e9e0ebb38998acb5a4b0919603d399ec190e
stiger <stiger@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2004-07-05 18:18:02 +0000

    new
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@7217 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/service/winmine.c
+++ kbs_bbs/service/winmine.c
@@ -14,9 +14,19 @@ char userid[20]="unknown.", fromhost[20]="unknown.";
 int topT[20], gameover=0;
 double topS[20];//add by stiger
 int marknum=0;
-char ganyingshi='N';
+int ganyingshi=0;
+char recfile[20];
 
 int winmine_main() {
+	strcpy(recfile,"mine.rec");
+    strcpy(userid,getCurrentUser()->userid);
+	modify_user_mode(WINMINE);
+    winmine();
+}
+
+int winmineg_main() {
+	ganyingshi=1;
+	strcpy(recfile,"mine2.rec");
     strcpy(userid,getCurrentUser()->userid);
 	modify_user_mode(WINMINE);
     winmine();
@@ -25,14 +35,14 @@ int winmine_main() {
 int show_mines() {
  int x, y;
  for(x=1; x<=30; x++)
-      for(y=3; y<=18; y++)
+   for(y=3; y<=18; y++)
       {
-       if (a[x][y]) {
+      if (a[x][y]) {
            move(y-1,x*2-2);
            prints("[1;31mÀ×[m");
-          }
-         }
-        return;
+      }
+   }
+ return;
 }
 
 int winmine() {
@@ -41,9 +51,9 @@ int winmine() {
     clear();
     refresh();
     while(1) {
-	totallei=0;//add by stiger
-	stiger=0;//add by stiger
-	steps=0;//add by stiger
+		totallei=0;//add by stiger
+		stiger=0;//add by stiger
+		steps=0;//add by stiger
         opened=0;
         clear();
         for (x=0;x<=31;x++)
@@ -83,7 +93,6 @@ int num_mark_beside(int x1, int y1) {
 int wininit(int x1, int y1) {
     int n, x, y;
     int i,j;//add by stiger
-    char bufff[50];
     srandom(time(0));
     for(n=1; n<=99; n++) {
         do {
@@ -100,8 +109,7 @@ int wininit(int x1, int y1) {
         }
     }
     move(20,45);
-    sprintf(bufff,"¹²ÓÐÀ×:%d",totallei);
-    prints(bufff);
+    prints("¹²ÓÐÀ×:%d",totallei);
     refresh();
 }
 
@@ -179,7 +187,6 @@ if(!f[x][y] && (o[x][y] || m[x][y])){
 int winloop()
 {
     int x, y, c, marked, t0, inited;
-    char buf[100];
     x= 10;
     y= 8;
     inited= 0; 
@@ -196,15 +203,12 @@ int winloop()
         if((c==KEY_LEFT||c=='h')&&x>1) x--;
         if((c==KEY_RIGHT||c=='l')&&x<30) x++;
         move(20,0);
-        sprintf(buf, "Ê±¼ä: %d ", time(0)-t0);
-        prints(buf);
+        prints("Ê±¼ä: %d ", time(0)-t0);
         move(21,0);
-        sprintf(buf, "×ø±ê: %3d, %3d", x, y);
-        prints(buf);
+        prints("×ø±ê: %3d, %3d", x, y);
         move(y-1,x*2-2);
 //modified by stiger,2001,6,12,¸ÄÕýmarkedÍ³¼ÆµÄ´íÎó
-	if(x==10&&y==9&&c=='f') ganyingshi='p';
-        if(ganyingshi=='p'||ganyingshi=='P')
+        if(ganyingshi)
         {
         	marknum=o[x-1][y-1]+o[x-1][y]+o[x-1][y+1]+o[x][y-1]+o[x][y+1]+o[x+1][y-1]+o[x+1][y]+o[x+1][y+1];
         	if((m[x][y]!=1)&&(num_mine_beside(x, y)==8-marknum)) {
@@ -221,8 +225,7 @@ int winloop()
         }
 //add by stiger,2001,6,12,¸ÄÕýmarkedÏÔÊ¾µÄÎÊÌâ
         move(20,30);
-        sprintf(buf, "±ê¼Ç: %d ", marked);
-        prints(buf);
+        prints("±ê¼Ç: %d ", marked);
 //add end
         if(c=='H') winhelp();
         if(c=='d'|| c=='D') winrefresh();
@@ -243,19 +246,17 @@ int winloop()
              }
              winsh(x, y);
 //add by stiger,2001,6,12
-        move(20,30);
-        sprintf(buf, "±ê¼Ç: %d ", marked);
-        prints(buf);
+        	move(20,30);
+        	prints("±ê¼Ç: %d ", marked);
 //add end
         }
         if(checkwin()==1) {
             /*add by stiger*/
             stiger=steps*1.0/(time(0)-t0);
             move(22,0);
-            sprintf(buf,"×£ºØÄã£¡Äã³É¹¦ÁË£¡Æ½¾ùÃ¿Ãë%lf²½",stiger);
-            prints(buf);
+            prints("×£ºØÄã£¡Äã³É¹¦ÁË£¡Æ½¾ùÃ¿Ãë%lf²½",stiger);
             gameover= 0;
- 	    win_checkrec(time(0)-t0);/* added by soff ½øÐÐÅÅÐÐ¼ì²é */
+ 	    	win_checkrec(time(0)-t0);/* added by soff ½øÐÐÅÅÐÐ¼ì²é */
             return 1;
         }
         if(gameover) {
@@ -291,8 +292,7 @@ int winrefresh() {
     int x, y;
     clear();
     move(22,0);
-    prints("[1;32m¡î¼üÅÌÉ¨À×¡î[0;1m [[35m°ïÖú: H[37m] [[36mÍË³ö: Q[37m] [[35m´ò¿ª: F[37m] [[36m±êÀ×: S][m\n\r");
-    prints("[v1.00 by zhch.nju 00.3] press \'[1;32mH[m\' to get help, \'[1;32mCtrl+C[m\' to exit.");
+    prints("[1;32m¡î¼üÅÌÉ¨À×¡î[0;1m [[35m°ïÖú: H[37m] [[36mÍË³ö: Ctrl+C[37m] [[35m´ò¿ª: f[37m] [[36m±êÀ×: s][m\n");
     for(y=1; y<=16; y++) { 
         move(y-1,0);
         for(x=1; x<=30; x++)
@@ -303,13 +303,12 @@ int winrefresh() {
 
 int winhelp() {
     clear();
-    prints("==»¶Ó­À´Íæ¼üÅÌÉ¨À×ÓÎÏ·== \r\n(nju BBS Õ¾³¤ zhch Éè¼Æ) \r\n---------------------------------\r\n\r\n");
+    prints("==»¶Ó­À´Íæ¼üÅÌÉ¨À×ÓÎÏ·== \r\n---------------------------------\r\n\r\n");
     prints("Íæ·¨ºÜ¼òµ¥£¬ºÍ[1;34mwindows[mÏÂµÄÊó±êÉ¨À×²î²»¶à.\r\n");
         prints("  '[1;32mF[m'¼üµÄ×÷ÓÃÏàµ±ÓÚÊó±êµÄ×ó¼ü¼°Ë«»÷µÄ×÷ÓÃ£¬ ³ÌÐò¸ù¾ÝÄãµã»÷µÄÎ»ÖÃ\r\n");
         prints("  ×Ô¶¯ÅÐ¶ÏÒª½øÐÐÄÄÖÖ²Ù×÷¡£\r\n");
         prints("  '[1;32mS[m'¼üÔòÏàµ±ÓÚÊó±êÓÒ¼üµÄ¹¦ÄÜ, ¿ÉÓÃÀ´±êÀ×.\r\n");
         prints("  '[1;32mH[m'¼üÓÃÀ´ÏÔÊ¾±¾°ïÖúÐÅÏ¢.\r\n");
-        prints("  'ÔÚ(10,9)×ø±êÏÂ°´Ë«»÷¼ü(F)¼´½øÈë¸ÐÓ¦Ê½É¨À×Ä£Ê½\r\n");
         prints("  '[1;32mQ[m'¼üÍË³öÓÎÏ·.\r\n");
         prints("  µ±ÆÁÄ»ÂÒµôÊ±£¬¿ÉÓÃ'[1;32mD[m'¿ÉÓÃÀ´Ë¢ÐÂÆÁÄ»¡£\r\n");
         prints("µÚÒ»´Îµã»÷Ò»¶¨»á¿ªÒ»Æ¬£¬ºÜÊæ·þ°É¡£\r\n");
@@ -324,10 +323,10 @@ int win_loadrec() {
     for(n=0; n<=19; n++) {
         strcpy(topID[n], "null.");
         topT[n]=999;
-	topS[n]=0;
+		topS[n]=0;
         strcpy(topFROM[n], "unknown.");
     }
-    fp=fopen("mine2.rec", "r");
+    fp=fopen(recfile, "r");
     if(fp==NULL) {
         win_saverec();
         return;
@@ -340,7 +339,7 @@ int win_loadrec() {
 int win_saverec() {
     FILE *fp;
     int n;
-    fp=fopen("mine2.rec", "w");
+    fp=fopen(recfile, "w");
     for(n=0; n<=19; n++) {
         fprintf(fp, "%s %d %s %lf\n", topID[n], topT[n], topFROM[n],topS[n]);
     }
@@ -348,18 +347,17 @@ int win_saverec() {
 }
 
 int win_showrec() {
-    char buf[200];
     int n;
+	char buf[256];
     win_loadrec();
     clear();
     prints("[44;37m                      --==    É¨À×ÅÅÐÐ°ñ    ==--                             \r\n[m");
-    prints("[41m No.          ID        TIME                         FROM    Steps/sec   [m\r\n");
+    prints("[41m No.          ID        TIME       Steps/sec                            [m\r\n");
     for(n=0; n<=19; n++) {
-        sprintf(buf, "[1;37m%3d[32m%13s[0;37m%12d[m%29s     %lf\r\n", n+1, topID[n], topT[n], topFROM[n],topS[n]);
-        prints(buf);
+        sprintf(buf,"[1;37m%3d[32m%13s[0;37m%12d[m     %lf \r\n", n+1, topID[n], topT[n], topS[n]);
+		prints(buf);
     }
-    sprintf(buf, "[41m                                                                               [m\r\n");
-    prints(buf);
+    prints("[41m                                                                               [m\r\n");
     pressanykey();
 }
 
