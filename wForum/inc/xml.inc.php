bb689ece5f67a7f0d2ff895199702bd2f416b47e
atppp <atppp@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2004-07-08 06:04:30 +0000

    hehe
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@7245 de4889a5-8ad4-4bf3-bd87-57997c21d515

---
+++ wForum/inc/xml.inc.php
@@ -0,0 +1,44 @@
+<?php
+/* most code copied from bbs2www code */
+
+function undo_html_format($str)
+{
+	$str = preg_replace("/&gt;/i", ">", $str);
+	$str = preg_replace("/&lt;/i", "<", $str);
+	$str = preg_replace("/&quot;/i", "\"", $str);
+	$str = preg_replace("/&amp;/i", "&", $str);
+	return $str;
+}
+
+# iterate through an array of nodes
+# looking for a text node
+# return its content
+function get_content($parent)
+{
+    $nodes = $parent->child_nodes();
+    while($node = array_shift($nodes))
+        if ($node->node_type() == XML_TEXT_NODE)
+            return $node->node_value();
+    return "";
+}
+
+# get the content of a particular node
+function find_content($parent,$name)
+{
+    $nodes = $parent->child_nodes();
+    while($node = array_shift($nodes))
+        if ($node->node_name() == $name)
+            return undo_html_format(urldecode(get_content($node)));
+    return "";
+}
+
+# get an attribute from a particular node
+function find_attr($parent,$name,$attr)
+{
+    $nodes = $parent->child_nodes();
+    while($node = array_shift($nodes))
+        if ($node->node_name() == $name)
+            return $node->get_attribute($attr);
+    return "";
+}
+?>
