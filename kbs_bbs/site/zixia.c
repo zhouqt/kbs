c8068753d147dd512cbfdc5f0e85adcbbb57d14b
atppp <atppp@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2004-07-11 06:23:06 +0000

    bug
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@7267 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/site/zixia.c
+++ kbs_bbs/site/zixia.c
@@ -175,7 +175,7 @@ uleveltochar( char* buf, struct userec *lookupuser )
 	// buf[10], buf 最多 4 个汉字 + 1 byte （\0结尾）
 	//根据 level
 	//
-	if( !( lvl | PERM_POST ) ) strcpy( buf, "哑巴" ); 
+	if( !( lvl & PERM_POST ) ) strcpy( buf, "哑巴" ); 
       	else if ( ! (lvl & PERM_BASIC) ) strcpy( buf, "瞎子" ); 
 	else if( lvl < PERM_DEFAULT ) strcpy( buf, "书生" );
     	//else if( lvl & PERM_SYSOP ) strcpy(buf,"帮主");
