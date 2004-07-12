1de527331395944cac7e9a141fa0b8e5571ffb83
atppp <atppp@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2004-07-12 16:15:40 +0000

    --with-dom done at zixia
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@7282 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- wForum/topten.php
+++ wForum/topten.php
@@ -4,27 +4,30 @@ $setboard=1;
 
 require("inc/funcs.php");
 require("inc/user.inc.php");
-
-show_nav();
+require("inc/xml.inc.php");
 
 setStat("十大热门话题");
+
+show_nav();
 
 showUserMailBoxOrBR();
 head_var();
 
-showTopTen();
+showTopTenXML();
 
 show_footer();
 
-/*--------------- function defines ------------------*/
+function showTopTenXML() {
+	
+	$hotsubject_file = BBS_HOME . "/xml/day.xml";
+	$doc = domxml_open_file($hotsubject_file);
+	if (!$doc)
+		return;
 
-function showTopTen(){
-	$top_file = get_bbsfile("/etc/posts/day");
-	$fp = fopen($top_file, "r");
-	if ($fp == FALSE) {
-		foundErr("无法读取十大话题数据");
-	}
-	$modifytime=filemtime($top_file);
+	$root = $doc->document_element();
+	$boards = $root->child_nodes();
+
+	$boardArr = array();
 ?>
 <table cellspacing=1 cellpadding=0 align=center width="97%" class=TableBorder1>
 <thead><tr><th align="center" colspan=5 height=25>今日十大热门话题</th></tr></thead>
@@ -33,32 +36,35 @@ function showTopTen(){
 <table cellspacing=1 cellpadding=0 align=center width="97%" class=TableBorder1>
 <thead><tr><th height="25" width=50>名次</td><th>讨论区</td><th>标题</td><th>作者</td><th>人数</td></tr></thead>
 <?php
-	fgets($fp, 256);
-	fgets($fp, 256);
-	for ($i = 0; $i < 10 && !feof($fp); $i++)
+	$i = 0;
+	while($board = array_shift($boards))
 	{
-		$one_line = fgets($fp, 256);
-		if ($one_line == FALSE)
-			break;
-		$r_board = trim(substr($one_line, 41, 16));
-		$r_num = trim(substr($one_line, 97, 4));
-		$r_id = trim(substr($one_line, 118, 12));
-		$one_line = fgets($fp, 256);
-		if ($one_line == FALSE)
-			break;
-		$r_title = trim(substr($one_line, 27, 60));
-		$class='TableBody'.($i%2 +1);
+		if ($board->node_type() == XML_TEXT_NODE)
+			continue;
+		
+		$r_title = find_content($board, "title");
+		$r_author = find_content($board, "author");
+		$r_board = find_content($board, "board");
+		$r_time = find_content($board, "time");
+		$r_number = find_content($board, "number");
+		$r_groupid = find_content($board, "groupid");
+		
+		$brdnum = bbs_getboard($r_board, $boardArr);
+		if ($brdnum == 0)
+			continue;
+		$r_board = urlencode($boardArr["NAME"]);
+		$i++;
+		$class = 'TableBody'.($i%2 +1);
 ?>
 <tr>
-<td class=<?php echo $class; ?>  align=center><?php echo $i+1; ?></td>
+<td height="25" class=<?php echo $class; ?>  align=center><?php echo $i; ?></td>
 <td class=<?php echo $class; ?> >&nbsp;<a href="board.php?name=<?php echo $r_board; ?>"><?php echo $r_board; ?></a></td>
-<td class=<?php echo $class; ?> >&nbsp;<a href="queryresult.php?boardNames=<?php echo $r_board; ?>&title=<?php echo urlencode($r_title); ?>"><?php echo $r_title; ?></a></td>
-<td class=<?php echo $class; ?> >&nbsp;<a href="dispuser.php?id=<?php echo $r_id; ?>"><?php echo $r_id; ?></a></td>
-<td class=<?php echo $class; ?> >&nbsp;<?php echo $r_num; ?></td>
+<td class=<?php echo $class; ?> >&nbsp;<a href="disparticle.php?boardName=<?php echo $r_board; ?>&ID=<?php echo $r_groupid; ?>"><?php echo htmlspecialchars($r_title); ?></a></td>
+<td class=<?php echo $class; ?> align="center"><a href="dispuser.php?id=<?php echo $r_author; ?>"><?php echo $r_author; ?></a></td>
+<td class=<?php echo $class; ?> align="center">&nbsp;<?php echo $r_number; ?></td>
 </tr>
 <?php
-		}
-		fclose($fp);
+	}
 ?>
 </table>
 <?php
