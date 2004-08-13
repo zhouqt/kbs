e8abedf378221e7567a6fb2d523360b250a67f65
atppp <atppp@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2004-08-13 05:55:11 +0000

    发帖前预览支持附件以及上传的插入到文中的图片.警告:这两天的wforum部分代码不稳定
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@7440 de4889a5-8ad4-4bf3-bd87-57997c21d515

---
+++ wForum/bbsuploadcon.php
@@ -0,0 +1,44 @@
+<?php
+	require("inc/funcs.php");
+	require("inc/attachment.inc.php");
+	
+	@$attachnum = $_GET["ap"];
+	if ($attachnum == 0) {
+		echo 1;
+		exit;
+	}
+	
+	if ($loginok != 1) {
+		echo 2;
+		exit;
+	}
+
+	$attachdir=bbs_getattachtmppath($currentuser["userid"] ,$utmpnum);
+
+	if (($fp=@fopen($attachdir . "/.index", "r")) == FALSE) {
+		echo 3;
+		exit;
+	}
+	$id = 0;
+	while (!feof($fp)) {
+		$buf = fgets($fp);
+		$id++;
+		if ($id != $attachnum) continue;
+		
+		$buf=substr($buf,0,-1); //remove "\n"
+		if ($buf=="") {
+			echo 4;
+			break;
+		}
+		$file = substr($buf,0,strpos($buf,' '));
+		if ($file == "")  {
+			echo 5;
+			break;
+		}
+		$name = strstr($buf,' ');
+		$name = substr($name,1);
+		output_attachment($file, 0, $name);
+		break;
+	}
+	fclose($fp);
+?>
--- wForum/preview.php
+++ wForum/preview.php
@@ -8,16 +8,17 @@ setStat("ЬћзгдЄРР");
 
 html_init();
 ?>
+<script src="inc/funcs.js"  language="javascript"></script>
 <body>
-<table cellpadding=3 cellspacing=1 align=center class=TableBorder1>
-	<TR><Th height=25>ЬћзгдЄРР</Th></TR>
-	<TR><TD class=TableBody1 
-height=24><b><?php echo htmlspecialchars($_POST["title"]); ?></b></TD></TR>
-</TABLE><br>
-<table cellpadding=3 cellspacing=1 align=center class=TableBorder1>
-<TR><TD 
-class=TableBody1><?php echo dvbcode(str_replace("\n", "<br />", htmlspecialchars(str_replace("\r", "", $_POST["body"]))), 0, "TableBody2"); ?></TD></TR>
-</TABLE>
+<table cellpadding="3" cellspacing="1" align="center" class="TableBorder1">
+	<tr><th height="25">ЬћзгдЄРР</th></tr>
+	<tr><td class="TableBody1" 
+height="24"><b><?php echo htmlspecialchars($_POST["title"]); ?></b></td></tr>
+</table><br/>
+<table cellpadding="3" cellspacing="1" align="center" class="TableBorder1">
+<tr><td 
+class="TableBody1"><?php echo dvbcode(bbs_printansifile($_POST["body"],1,'bbsuploadcon.php?s',0,1), 0, "TableBody2"); ?></td></tr>
+</table>
 <?php
 	show_footer(false);
 ?>
