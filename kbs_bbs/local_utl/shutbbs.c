f822b5df8b1863cd28f77554fcad041e1951f2b8
flyriver <flyriver@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2004-08-17 15:20:13 +0000

    shutdown bbs program for Cygwin.
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@7444 de4889a5-8ad4-4bf3-bd87-57997c21d515

---
+++ kbs_bbs/local_utl/shutbbs.c
@@ -0,0 +1,41 @@
+#include "bbs.h"
+#include <windows.h>
+#include <sys/cygwin.h>
+
+typedef struct external_pinfo external_pinfo;
+
+void kill_process(const char *prog_name)
+{
+	external_pinfo *p;
+	cygwin_getinfo_types query = CW_GETPINFO;
+	char *s;
+	char pname[MAX_PATH];
+	int pid;
+	
+	for (pid = 0;
+		(p = (external_pinfo *)cygwin_internal(query, pid | CW_NEXTPID));
+		pid = p->pid)
+	{
+		pname[0] = '\0';
+		cygwin_conv_to_posix_path(p->progname, pname);
+		s = strchr(pname, '\0') - 4;
+		if (s > pname && strcasecmp(s, ".exe") == 0)
+			*s = '\0';
+		if (strcasecmp(pname, prog_name) == 0)
+			kill(p->pid, SIGTERM);
+	}
+}
+
+int main()
+{
+	cygwin_internal(CW_LOCK_PINFO, 1000);
+	kill_process(BBSHOME "/bin/innbbsd");
+	kill_process(BBSHOME "/bin/sshbbsd");
+	kill_process(BBSHOME "/bin/bbsd");
+	kill_process(BBSHOME "/bin/bbslogd");
+	kill_process(BBSHOME "/bin/miscd");
+	kill_process("/usr/bin/ipc-daemon2");
+	cygwin_internal(CW_UNLOCK_PINFO);
+
+	return 0;
+}
