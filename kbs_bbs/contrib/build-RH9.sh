c0ee6e2b80b836bd3ef52f87f1b9612a8bed309a
atppp <atppp@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2004-09-02 05:39:12 +0000

    主分支小小的跟进一下flyriver的修改
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@7471 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/contrib/build-RH9.sh
+++ kbs_bbs/contrib/build-RH9.sh
@@ -14,44 +14,45 @@ WWWROOT=/var/www
 
 # 以下的代码请不要修改，除非您能确定自己在干什么
 if [ -f Makefile ]; then
-	make distclean
+  make distclean
 fi
 
-cvs up -d
-
-aclocal; autoheader; automake -a; autoconf
+if [ -d CVS ]; then
+  cvs up -d
+  aclocal; autoheader; automake -a; autoconf
+fi
 
 if [ -d bbs2www ]; then
-	cd bbs2www
-
-	aclocal; autoheader; automake -a; autoconf
-
-	cd ..
-	WWWCONFIG="--with-www=$WWWROOT --with-php=/usr/include/php"
+  if [ -d CVS ]; then
+    cd bbs2www
+    aclocal; autoheader; automake -a; autoconf
+    cd ..
+  fi
+  WWWCONFIG="--with-www=$WWWROOT --with-php=/usr/include/php"
 else
-	WWWCONFIG=--disable-www
+  WWWCONFIG=--disable-www
 fi
 
 if [ -d sshbbsd ]; then
-	cd sshbbsd
-
-	aclocal; autoheader; automake -a; autoconf
-
-	cd ..
-	SSHCONFIG=--enable-ssh
+  if [ -d CVS ]; then
+    cd sshbbsd
+    aclocal; autoheader; automake -a; autoconf
+    cd ..
+  fi
+  SSHCONFIG=--enable-ssh
 else
-	SSHCONFIG=--disable-ssh
+  SSHCONFIG=--disable-ssh
 fi
 
 if [ -d innbbsd ]; then
-	cd innbbsd
-
-	aclocal; autoheader; automake -a; autoconf
-
-	cd ..
-	INNCONFIG=--enable-innbbsd
+  if [ -d CVS ]; then
+    cd innbbsd
+    aclocal; autoheader; automake -a; autoconf
+    cd ..
+  fi
+  INNCONFIG=--enable-innbbsd
 else
-	INNCONFIG=--disable-innbbsd
+  INNCONFIG=--disable-innbbsd
 fi
 
 ./configure --prefix=$BBSHOME --enable-site=$BBSSITE \
