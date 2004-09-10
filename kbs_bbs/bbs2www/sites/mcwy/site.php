2ce9364a6616ea2ece8144810fb49c1c0d3df334
atppp <atppp@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2004-09-10 06:44:07 +0000

    先开个头，站点自定义这个问题需要看看了
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@7483 de4889a5-8ad4-4bf3-bd87-57997c21d515

---
+++ kbs_bbs/bbs2www/sites/mcwy/site.php
@@ -0,0 +1,21 @@
+<?php
+
+define("ATTACHMAXSIZE","8388608");
+define("ATTACHMAXCOUNT","20");
+//define("HAVE_BRDENV", 1); //支持版面导读
+define("HAVE_PC", 1);
+define("QUOTED_LINES","10");
+define("MAINPAGE_FILE","mainpage.php");
+//define("ENABLE_ABOARDS" , 1);//web首页使用活动看板
+define("SITE_ZIXIA",1); //目前主要涉及新注册用户认证的相关代码 - atppp
+define("PWD_HTTP",1);
+
+$section_nums = array("0", "1", "2", "3", "4");
+$section_names = array(
+    array("本站系统", "[本站]"),
+    array("人在美国", "[校园][创业]"),
+    array("大话西游", "[留学][学校][团体]"),
+    array("学术研讨", "[学术][科学][电脑]"),
+    array("感性生活", "[休闲][生活][感性]")
+);
+?>
