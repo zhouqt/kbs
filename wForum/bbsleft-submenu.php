567b68fbd6c5ac60fad28e971f79c3945704d00a
atppp <atppp@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2004-10-03 07:25:30 +0000

    ci base version from bbs2www
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@7524 de4889a5-8ad4-4bf3-bd87-57997c21d515

---
+++ wForum/bbsleft-submenu.php
@@ -0,0 +1,93 @@
+<?php
+require_once("funcs.php");
+login_init();
+if(isset($_GET["fav"]))
+{
+	$boardList = FALSE;
+	$select = intval($_GET["select"]);
+	if( bbs_load_favboard($select)==-1 ) exit();
+	$subMenu = "submenu_fav_".$select;
+	$boards = bbs_fav_boards($select, 1);
+}
+else
+{
+	$boardList = TRUE;
+	$group = intval($_GET["group"]);
+	$group2 = intval($_GET["group2"]);
+	$yank = intval($_GET["yank"]);
+	if ($yank) $yank = 1;
+	$subMenu = "submenu_brd_".$group."_".$group2;
+	if($group < 0 || $group > sizeof($section_nums) )
+		exit();
+	$boards = bbs_getboards($section_nums[$group], $group2, $yank | 2);
+}
+
+$brd_name = $boards["NAME"]; // 英文名
+$brd_desc = $boards["DESC"]; // 中文描述
+$brd_flag = $boards["FLAG"]; //flag
+$brd_bid = $boards["BID"]; //flag
+$brd_num = sizeof($brd_name);
+
+$innerHtml = "<table width=100% border=0 cellpadding=0 cellspacing=0>";
+for($j = 0 ; $j < $brd_num ; $j ++ )
+{
+	if ($brd_flag[$j]&BBS_BOARD_GROUP)
+	{
+		if($boardList)
+		{
+			$innerHtml .= "<tr><td align='right' width='16'>".
+				"<a href='javascript:submenu(0,0,".$group.",".$brd_bid[$j].",0)'>".
+				"<img id='submenuimg_brd_".$group."_".$brd_bid[$j]."' src='/images/close.gif' border='0'>".
+				"</a></td><td align='left'>".
+				"<a href='/bbsboa.php?group=".$group."&group2=".$brd_bid[$j]."' target='f3'>".
+				"<img src='/images/kfolder1.gif' border='0'>".$brd_desc[$j]."</a>".
+				"</td></tr>".
+				"<tr id='submenu_brd_".$group."_".$brd_bid[$j]."' style='display:none'>".
+				"<td background='/images/line3.gif'> </td>".
+				"<td id='submenu_brd_".$group."_".$brd_bid[$j]."_td'><DIV></DIV>".
+				"</td></tr>";
+		}
+		elseif( bbs_load_favboard($brd_bid[$j])!=-1 && bbs_fav_boards($brd_bid[$j], 1) && $brd_bid[$j]!= -1)
+		{
+			$innerHtml .= "<tr><td align='right' width='16'>".
+				"<a href='javascript:submenu(1,".$brd_bid[$j].",0,0,0)'>".
+				"<img id='submenuimg_fav_".$brd_bid[$j]."' src='/images/close.gif' border='0'>".
+				"</a></td><td align='left'>".
+				"<a href='/bbsfav.php?select=".$brd_bid[$j]."&up=".$select."' target='f3'>".
+				"<img src='/images/kfolder1.gif' border='0'>".$brd_desc[$j]."</a>".
+				"</td></tr>".
+				"<tr id='submenu_fav_".$brd_bid[$j]."' style='display:none'>".
+				"<td background='/images/line3.gif'> </td>".
+				"<td id='submenu_fav_".$brd_bid[$j]."_td'><DIV></DIV>".
+				"</td></tr>";
+	        }
+		else
+		{
+			
+		}
+	}
+	else
+	{
+		$brd_link="/bbsdoc.php?board=" . urlencode($brd_name[$j]);
+		$innerHtml .= "<tr><td width='16' align='right'>";
+		if( $j != $brd_num - 1 )
+			$innerHtml .= "<img src='/images/line.gif' align='absmiddle'>";
+		else
+			$innerHtml .= "<img src='/images/line1.gif' align='absmiddle'>";
+
+		$innerHtml .= "</td><td><A href='".$brd_link."' target='f3'>".$brd_desc[$j]."</A></td></tr>";
+	}
+}
+$innerHtml .= "</table>";
+if(!$boards || !$brd_num) $innerHtml = "<font color=#eeeeee>-空目录-</font>"; 
+?>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=gb2312">
+</head>
+<script src="/bbsleft.js"></script>
+<body>
+<script language="javascript">
+parent.document.getElementById('<?php echo $subMenu; ?>_td').innerHTML = "<?php echo $innerHtml; ?>" ; 
+</script>
+</body></html>
---
+++ wForum/frames.php
@@ -0,0 +1,12 @@
+<?php
+    require("inc/funcs.php");
+?>
+<html>
+<head>
+<title><?php echo $HTMLTitle; ?></title>
+</head>
+<frameset name="mainframe" frameborder="0" border="1" cols="120,*">
+    <frame name="menu" marginwidth="0" marginheight="0" src="bbsleft.php">
+    <frame name="main" marginwidth="0" marginheight="0" src="index.php">
+</frameset>
+</html>
