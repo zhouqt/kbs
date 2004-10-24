e33e38ae799222dbb0f42f60e94e90dd3157d51e
atppp <atppp@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2004-10-24 21:34:04 +0000

    en
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@7583 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- wForum/inc/sites/zixia.php
+++ wForum/inc/sites/zixia.php
@@ -20,6 +20,7 @@ define("ATTACHMAXCOUNT","20");
 
 define('MYFACEMAXSIZE','102400');
 
+define('ALLOW_SELF_MULTIQUERY', true);
 define('SUPPORT_TEX', true);
 
 $section_nums = array("0", "1", "2", "3", "4", "5", "6", "7", "8", "9");
