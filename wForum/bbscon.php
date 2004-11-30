ccd259a8e2ccbedb4e9c7853e35942ea78f71f7f
atppp <atppp@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2004-11-30 05:02:56 +0000

    轻量级文章阅读，仅显示文章但保留全部格式包括mathml
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@7658 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- wForum/bbscon.php
+++ wForum/bbscon.php
@@ -5,23 +5,22 @@
 		$nocookie = 1;
 	}
 	require("inc/funcs.php");
-	require("inc/attachment.inc.php");
 
 	if (!$loginok && $attachpos!=0) bbs_setguest_nologin();
 	
 	$brdarr = array();
 	if( isset( $_GET["bid"] ) ){
-		$brdnum = $_GET["bid"] ;
-		if( $brdnum == 0 ){
+		$boardID = intval($_GET["bid"]);
+		if( $boardID == 0 ){
 			echo 2;
 			exit(0);
 		}
-		$board = bbs_getbname($brdnum);
+		$board = bbs_getbname($boardID);
 		if( !$board ){
 			echo 3;
 			exit(0);
 		}
-		if( $brdnum != bbs_getboard($board, $brdarr) ){
+		if( $boardID != bbs_getboard($board, $brdarr) ){
 			echo 4;
 			exit(0);
 		}
@@ -32,7 +31,7 @@
 	if (!($isnormalboard = bbs_normalboard($board))) {
 		if($loginok == 1) {
 			$usernum = $currentuser["index"];
-			if (bbs_checkreadperm($usernum, $brdnum) == 0) {
+			if (bbs_checkreadperm($usernum, $boardID) == 0) {
 				echo 6;
 				exit(0);
 			}
@@ -43,7 +42,7 @@
 	}
 
 	if (isset($_GET["id"])) {
-		$id = $_GET["id"];
+		$id = intval($_GET["id"]);
 	} else {
 		echo 8;
 		exit(0);
@@ -56,7 +55,7 @@
 			$ftype = $dir_modes["NORMAL"];
 	} else
 		$ftype = $dir_modes["NORMAL"];
-	$total = bbs_countarticles($brdnum, $ftype);
+	$total = bbs_countarticles($boardID, $ftype);
 	if ($total <= 0) {
 		echo 9;
 		exit(0);
@@ -69,28 +68,61 @@
 		return;
 	}
 
+	$boardName = $brdarr["NAME"];
 	if ($attachpos==0) {
-		// jump to the article, this link must be generated from telnet session. added by atppp
-		$gid = $articles[1]["GROUPID"];
-		$boardName = $brdarr["NAME"];
-		$haveprev = 0;
-		$num = bbs_get_threads_from_gid($brdnum, $gid, $gid, $articles, $haveprev );
-		if ($num > 0) {
-			for ($i = 0; $i < $num; $i++) { //可以用二分查找加速，以后再说吧
-				if ($id == $articles[$i]["ID"]) {
-					header("Location: disparticle.php?boardName=$boardName&ID=$gid&start=$i&listType=1");
-					return;
+		if (!isset($_GET["lw"])) {
+			// jump to the article, this link must be generated from telnet session. added by atppp
+			$gid = $articles[1]["GROUPID"];
+			$haveprev = 0;
+			$num = bbs_get_threads_from_gid($boardID, $gid, $gid, $articles, $haveprev );
+			if ($num > 0) {
+				for ($i = 0; $i < $num; $i++) { //可以用二分查找加速，以后再说吧
+					if ($id == $articles[$i]["ID"]) {
+						header("Location: disparticle.php?boardName=$boardName&ID=$gid&start=$i&listType=1");
+						return;
+					}
 				}
 			}
+			echo 1;
+		} else {
+			viewArticle($articles[1], $boardID, $boardName);
 		}
-		echo 1;
 	} else {
-		$filename=bbs_get_board_filename($brdarr["NAME"], $articles[1]["FILENAME"]);
+		$filename=bbs_get_board_filename($boardName, $articles[1]["FILENAME"]);
 		if ($isnormalboard) {
 			if (cache_header("public",filemtime($filename),300)) {
 				exit(0);
 			}
 		}
+		require("inc/attachment.inc.php");
 		output_attachment($filename, $attachpos);
 	}
+	
+function viewArticle($article, $boardID, $boardName) {
+	global $SiteURL;
+	require("inc/ubbcode.php");
+
+	$is_tex = SUPPORT_TEX ? $article["IS_TEX"] : 0;
+	setStat(htmlspecialchars($article['TITLE'] ,ENT_QUOTES) . " " );
+	html_init("","","",$is_tex);
+	$this_link = 'bbscon.php?bid='.$boardID.'&amp;id='.$article['ID'];
+	$full_link = 'disparticle.php?boardName='.$boardName.'&amp;ID='.$article["GROUPID"];
+?>
+<script src="inc/funcs.js"  language="javascript"></script>
+<body>
+<table cellpadding="0" cellspacing="0" class="TableBorder1" style="table-layout:fixed;word-break:break-all">
+<tr><td class="TableBody1"><?php
+	$filename = bbs_get_board_filename($boardName, $article["FILENAME"]);
+	$str = bbs_printansifile($filename,1,$this_link,$is_tex,0);
+	echo DvbTexCode($str, 0, "TableBody2", $is_tex);
+?></td></tr>
+</table>
+<p>
+原文链接：<a href="<?php echo $this_link; ?>"><?php echo $SiteURL.$this_link; ?></a><br />
+主题链接：<a href="<?php echo $full_link; ?>"><?php echo $SiteURL.$full_link; ?></a>
+</p>
+</body>
+</html>
+<?php
+}
 ?>
--- wForum/preview.php
+++ wForum/preview.php
@@ -14,7 +14,7 @@ html_init("","","",$is_tex);
 	<tr><td class="TableBody1" 
 height="24"><b><?php echo htmlspecialchars($_POST["title"]); ?></b></td></tr>
 </table><br/>
-<table cellpadding="3" cellspacing="1" align="center" class="TableBorder1">
+<table cellpadding="3" cellspacing="1" align="center" class="TableBorder1" style="table-layout:fixed;word-break:break-all">
 <tr><td 
 class="TableBody1"><?php
     echo DvbTexCode(bbs_printansifile($_POST["body"],1,'bbsuploadcon.php?s',$is_tex,1), 0, "TableBody2", $is_tex); ?></td></tr>
