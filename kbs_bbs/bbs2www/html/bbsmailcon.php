f51caa73bae268fb9244746d58441d753205b22a
atppp <atppp@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2004-12-26 05:37:25 +0000

    谢谢czz kxn修服务器。准备一起处理附件输出的warning
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@7776 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/bbs2www/html/bbsmailcon.php
+++ kbs_bbs/bbs2www/html/bbsmailcon.php
@@ -5,49 +5,6 @@
 	 */
 	require("funcs.php");
 login_init();
-function get_mimetype($name)
-{
-	$dot = strrchr($name, '.');
-	if ($dot == $name)
-		return "text/plain; charset=gb2312";
-	if (strcasecmp($dot, ".html") == 0 || strcasecmp($dot, ".htm") == 0)
-		return "text/html; charset=gb2312";
-	if (strcasecmp($dot, ".jpg") == 0 || strcasecmp($dot, ".jpeg") == 0)
-		return "image/jpeg";
-	if (strcasecmp($dot, ".gif") == 0)
-		return "image/gif";
-	if (strcasecmp($dot, ".png") == 0)
-		return "image/png";
-	if (strcasecmp($dot, ".pcx") == 0)
-		return "image/pcx";
-	if (strcasecmp($dot, ".css") == 0)
-		return "text/css";
-	if (strcasecmp($dot, ".au") == 0)
-		return "audio/basic";
-	if (strcasecmp($dot, ".wav") == 0)
-		return "audio/wav";
-	if (strcasecmp($dot, ".avi") == 0)
-		return "video/x-msvideo";
-	if (strcasecmp($dot, ".mov") == 0 || strcasecmp($dot, ".qt") == 0)
-		return "video/quicktime";
-	if (strcasecmp($dot, ".mpeg") == 0 || strcasecmp($dot, ".mpe") == 0)
-		return "video/mpeg";
-	if (strcasecmp($dot, ".vrml") == 0 || strcasecmp($dot, ".wrl") == 0)
-		return "model/vrml";
-	if (strcasecmp($dot, ".midi") == 0 || strcasecmp($dot, ".mid") == 0)
-		return "audio/midi";
-	if (strcasecmp($dot, ".mp3") == 0)
-		return "audio/mpeg";
-	if (strcasecmp($dot, ".pac") == 0)
-		return "application/x-ns-proxy-autoconfig";
-	if (strcasecmp($dot, ".txt") == 0)
-		return "text/plain; charset=gb2312";
-	if (strcasecmp($dot, ".xht") == 0 || strcasecmp($dot, ".xhtml") == 0)
-		return "application/xhtml+xml";
-	if (strcasecmp($dot, ".xml") == 0)
-		return "text/xml";
-	return "application/octet-stream";
-}
 
 	if ($loginok != 1)
 		html_nologin();
@@ -100,23 +57,8 @@ function get_mimetype($name)
 
 			@$attachpos=$_GET["ap"];//pointer to the size after ATTACHMENT PAD
 			if ($attachpos!=0) {
-				$filep = fopen($filename, "rb");
-				fseek($filep,$attachpos);
-				$attachname='';
-				while (1) {
-					$char=fgetc($filep);
-					if (ord($char)==0) break;
-					$attachname=$attachname . $char;
-				}
-				$str=fread($filep,4);
-				$array=unpack('Nsize',$str);
-				$attachsize=$array["size"];
-				Header("Content-type: " . get_mimetype($attachname));
-				Header("Accept-Ranges: bytes");
-				Header("Content-Length: " . $attachsize);
-				Header("Content-Disposition: filename=" . $attachname);
-				echo fread($filep,$attachsize);
-				fclose($filep);
+				require_once("attachment.php");
+				output_attachment($filename, $attachpos);
 				exit;
 			} else
 			{
