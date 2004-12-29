b385575265e7193f18b9ea6a43d1e466f9dd6311
atppp <atppp@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2004-12-29 19:46:28 +0000

    关掉显示真实用户信息，可以在site.php中打开。
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@7794 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- wForum/inc/sites/site-example.php
+++ wForum/inc/sites/site-example.php
@@ -18,7 +18,7 @@ define('OLD_REPLY_STYLE', true); //使用传统 telnet re 文方式
 
 define("ENABLE_UBB", true); //是否支持 UBB
 
-define('SHOW_REGISTER_TIME', false); //是否显示用户注册时间
+define('SHOW_REGISTER_TIME', false); //是否支持显示真实用户信息（包括用户注册时间）
 
 /* 附件：每个最大尺寸，总最大尺寸，总最大数量 */
 define("ATTACHMAXSIZE","5242880");
