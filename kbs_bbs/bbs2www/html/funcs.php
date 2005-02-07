a2e5332c3d73ff8afa5f673f2f9c3faf46c3bbb1
atppp <atppp@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2005-02-07 08:21:08 +0000

    proxyIPs 统一处理下吧。ansi_convert 函数去掉。dos2unix indent
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@7926 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/bbs2www/html/funcs.php
+++ kbs_bbs/bbs2www/html/funcs.php
@@ -8,7 +8,7 @@ if (!defined('_BBS_FUNCS_PHP_'))
 define('_BBS_FUNCS_PHP_', 1);
 
 if (!isset($topdir))
-    $topdir=".";
+	$topdir=".";
 
 // NOTE: If you want to statically link smth_bbs phpbbslib into php,
 //       you *MUST* set enable_dl variable to Off in php.ini file.
@@ -51,12 +51,12 @@ $dir_modes = array(
 );
 
 $bbsman_modes = array(
-    "DEL"   => 1,
-    "MARK"  => 2,
-    "DIGEST"=> 3,
-    "NOREPLY" => 4,
-    "ZHIDING" => 5
-    );
+	"DEL"   => 1,
+	"MARK"  => 2,
+	"DIGEST"=> 3,
+	"NOREPLY" => 4,
+	"ZHIDING" => 5
+	);
 /**
  * Constants of board flags, packed in an array.
  */
@@ -89,7 +89,7 @@ define("ENCODESTRING","0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ");
 define("ACTIVATIONLEN",15); //激活码长度
 define("WWW_DEFAULT_PARAMS","0"); //默认的WWW参数
 if (!defined ('FAVORITE_NAME'))
-    define ('FAVORITE_NAME', '我的百宝箱');
+	define ('FAVORITE_NAME', '我的百宝箱');
 
 function decodesessionchar($ch)
 {
@@ -100,6 +100,7 @@ $loginok=0;
 
 function set_fromhost()
 {
+	global $proxyIPs;
 	global $fullfromhost;
 	global $fromhost;
 	
@@ -109,11 +110,16 @@ function set_fromhost()
 		$fromhost=$fullfromhost;
 	}
 	else {
-		$str = strrchr($fullfromhost, ",");
-		if ($str!=FALSE)
-			$fromhost=substr($str,1);
-		else
-			$fromhost=$fullfromhost;
+		$ips = explode(",", $fullfromhost);
+		$c = count($ips);
+		if ($c > 0) {
+			$fromhost = trim($ips[$c - 1]);
+			if (isset($proxyIPs)) {
+				if (in_array($fromhost, $proxyIPs) && $c > 1) {
+					$fromhost = $ips[$c - 2];
+				}
+			}
+		} else $fromhost = $fullfromhost;
 	}
 	if ($fromhost=="")  {
 		$fromhost="127.0.0.1"; 
@@ -273,11 +279,11 @@ history.go(-1);
 
 function error_nologin()
 {
-        setcookie("UTMPKEY","",time() - 3600,"");
-        setcookie("UTMPNUM","",time() - 3600,"");
-        setcookie("UTMPUSERID","",time() - 3600,"");
-        setcookie("LOGINTIME","",time() - 3600,"");
-        setcookie("WWWPARAMS","",time() - 3600,"");
+	setcookie("UTMPKEY","",time() - 3600,"");
+	setcookie("UTMPNUM","",time() - 3600,"");
+	setcookie("UTMPUSERID","",time() - 3600,"");
+	setcookie("LOGINTIME","",time() - 3600,"");
+	setcookie("WWWPARAMS","",time() - 3600,"");
 ?>
 <SCRIPT language="javascript">
 window.location="/nologin.html";
@@ -328,22 +334,22 @@ function html_init($charset,$title="",$otherheader="",$new_style=0)
 	global $currentuser;
 	if ($cachemode=="") {
 		cache_header("nocache");
-    }
+	}
 ?>
 <?xml version="1.0" encoding="<?php echo $charset; ?>"?>
 <!DOCTYPE html
-     PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+	 PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=<?php echo $charset; ?>"/>
 <script src="/bbs.js"></script>
 <?php
-        if ( func_num_args() > 1) {
+		if ( func_num_args() > 1) {
 ?>
 <title><?php echo $title; ?></title>
 <?php
-        }
+		}
 	if($new_style==9 ){
 ?>
 <link rel="stylesheet" type="text/css" href="/bbsleft.css"/>
@@ -357,11 +363,6 @@ document.write('<link rel="stylesheet" type="text/css" href="/' + cssFile + '"/>
 </script>
 <?php			
 	}
-/*
-?>
-<link rel="stylesheet" type="text/css" href="/ansi.css"/>
-<?php
-*/	
 	echo($otherheader); 
 ?>
 </head>
@@ -390,11 +391,11 @@ function html_normal_quit()
 
 function html_nologin()
 {
-        setcookie("UTMPKEY","",time() - 3600,"");
-        setcookie("UTMPNUM","",time() - 3600,"");
-        setcookie("UTMPUSERID","",time() - 3600,"");
-        setcookie("LOGINTIME","",time() - 3600,"");
-        setcookie("WWWPARAMS","",time() - 3600,"");
+	setcookie("UTMPKEY","",time() - 3600,"");
+	setcookie("UTMPNUM","",time() - 3600,"");
+	setcookie("UTMPUSERID","",time() - 3600,"");
+	setcookie("LOGINTIME","",time() - 3600,"");
+	setcookie("WWWPARAMS","",time() - 3600,"");
 ?>
 <html>
 <head></head>
@@ -470,113 +471,6 @@ function get_secname_index($secnum)
 	return -1;
 }
 
-function ansi_getfontcode($fgcolor,$bgcolor,$defaultfg,$defaultbg,$highlight,$blink,$underlink, &$head,&$tail)
-{
-    $modify="";
-    if ($fgcolor==-1) 
-      $modify=sprintf(" color=%s",$defaultfg);
-    else
-    if ($fgcolor==-2)
-      $modify=sprintf(" color=%s",$defaultbg);
-    else
-    if ($highlight)
-       $fgcolor+=8;
-    if ($fgcolor<0) $fgcolor=0;
-    if ($bgcolor==-1)
-      $modify .= sprintf(" style='background-color:%s'",$defaultbg);
-    else
-    if ($bgcolor==-2)
-      $modify .= sprintf(" style='background-color:%s'",$defaultfg);
-    if ($bgcolor<0) $bgcolor=0;
-    $head = sprintf("<font class=f%d%02d%s>",$bgcolor,$fgcolor,$modify);
-    if ($underlink) {
-       $head .= "<u>";
-       $tail = "</u>";
-    }
-    $tail .= "</font>";
-}
-
-function ansi_convert( $buf , $defaultfg, $defaultbg)
-{
-    $keyword = preg_split("/\x1b\[([^a-zA-Z]*)([a-zA-Z])/",$buf,-1,PREG_SPLIT_DELIM_CAPTURE);
-    $fgcolor=-1;
-    $bgcolor=-1;
-    $blink=false;
-    $underlink=false;
-    $highlight=false;
-    for ($i=1;$i<count($keyword);$i+=3) {
-        if ($keyword[$i+2]=="")
-            continue;
-        if ($keyword[$i+1]=='m') {
-            $head="";
-            $tail="";
-            if ($keyword[$i]=="") {
-            		// *[;m
-                $fgcolor=-1;
-                $bgcolor=-1;
-                $blink=false;
-                $underlink=false;
-                $highlight=false;
-            } else {
-            	$good=true;
-            	$colorcodes=split(';',$keyword[$i]);
-                foreach ( $colorcodes as $code ) {
-            	    if (preg_match("/[\D]/",$code)) {
-            	    	$good=false;
-            	        break;
-            	    }
-                    if ($code=="") 
-                        $value=0;
-            	    else
-            	        $value=intval($code);
-                    if ($value<=8 && $value>=1) {
-                    	switch ($value) {
-                    	case 0:
-                            $fgcolor=-1;
-                            $bgcolor=-1;
-                            $blink=false;
-                            $underlink=false;
-                            break;
-                    	case 1:
-                    	    $highlight=1;
-                    	    break;
-                    	case 4:
-                    	    $underlink=1;
-                    	    break;
-                    	case 5:
-                    	    $blink=1;
-                    	case 7:
-                    	    $savebg=$bgcolor;
-                    	    if ($fgcolor==-1)
-                    	        $bgcolor=-2;
-                    	    else
-                    	        $bgcolor=$fgcolor;
-                    	    if ($bgcolor==-1)
-                    	        $fgcolor=-2;
-                    	    else
-                    	        $fgcolor=$savebg;
-                        }
-                    } else
-                    if ($value<=37 && $value>=30)
-                        $fgcolor=$value-30;
-                    else
-                    if ($value<=47 && $value>=40)
-                        $bgcolor=$value-40;
-                    else {
-                    	// unsupport code
-            	    	$good=false;
-                        break;
-                    }
-                }
-                if ($good)
-                    ansi_getfontcode($fgcolor,$bgcolor,$defaultfg,$defaultbg,$highlight,$blink,$underlink, $head,$tail);
-            }
-            $final .= $head . htmlspecialchars($keyword[$i+2]) . $tail;
-        } else $final .= htmlspecialchars($keyword[$i+2]);
-    }
-    return $final;
-}
-
 function bbs_is_owner($article, $user)
 {
 	if ($article["OWNER"] == $user["userid"])
@@ -605,7 +499,7 @@ function bbs_can_edit_article($board, $article, $user)
 
 function check_php_setting($name, $value) {
   if (ini_get($name) != $value) {
-    print "<p>Note that the value of PHP's configuration option <code><b>$name</b></code> is incorrect.  It should be set to '$value' for Drupal to work properly.</p>";
+	print "<p>Note that the value of PHP's configuration option <code><b>$name</b></code> is incorrect.  It should be set to '$value' for Drupal to work properly.</p>";
   }
 }
 
@@ -647,9 +541,9 @@ Powered By <A HREF="http://dev.smth.org" TARGET="_blank">SmthBBS</A>
 }
 
 function bbs_add_super_fav ($title, $url='', $type=0) {
-    global $currentuser;
-    if (!$url) $url = $_SERVER['REQUEST_URI'];
-    echo '<a href="/bbssfav.php?act=choose&title='.rawurlencode ($title).'&url='.rawurlencode ($url).'&type='.intval ($type).'">'.FAVORITE_NAME.'</a>';
+	global $currentuser;
+	if (!$url) $url = $_SERVER['REQUEST_URI'];
+	echo '<a href="/bbssfav.php?act=choose&title='.rawurlencode ($title).'&url='.rawurlencode ($url).'&type='.intval ($type).'">'.FAVORITE_NAME.'</a>';
 }
 
 } // !define ('_BBS_FUNCS_PHP_')
