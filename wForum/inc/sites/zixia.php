7d486b46a9414bac0ff6f992d90cb466ada4781c
atppp <atppp@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2005-04-03 06:54:06 +0000

    update
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@7989 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- wForum/inc/sites/zixia.php
+++ wForum/inc/sites/zixia.php
@@ -1,6 +1,6 @@
 <?php
 
-$SiteName="大话西游";
+$SiteName="大话西游 紫霞BBS";
 
 $SiteURL = "http://bbs.zixia.net/";
 
@@ -14,8 +14,8 @@ define('AUDIO_CHAT', true);
 
 define ("MAINTITLE","<img src=\"bar/title.jpg\"/>");
 
-define("ATTACHMAXSIZE","4194304");
-define ("ATTACHMAXTOTALSIZE","20971520");
+define("ATTACHMAXSIZE","512000");
+define ("ATTACHMAXTOTALSIZE","1024000");
 define("ATTACHMAXCOUNT","20");
 
 define('MYFACEMAXSIZE','102400');
@@ -41,4 +41,54 @@ $section_names = array(
 define("DB_ENABLED", true);
 require "dbconn.php";
 require "default.php";
+
+function init_zixia_boards()
+{
+        define ( ZIXIA_BOARD_FILE, "/bbs/www/zixia/wForum/zixia.boards" );
+
+        global $zixia_boards_list;
+        $zixia_boards_list = array();
+
+        if (($fd = fopen(ZIXIA_BOARD_FILE, "r")) === false) {
+                fclose($fout);
+                return false;
+        }
+        $total = 0;
+
+        while (!feof($fd)) {
+                $line = trim(fgets($fd, 4096));
+//echo "$line<br>\n";
+                if (strlen($line) == 0 || $line{0} == "#") continue;
+                $bn_info = preg_split("/\s+/", $line);
+//print_r($bn_info);
+                $bn_e = strtolower($bn_info[0]);
+                $bn_c = $bn_info[1];
+                $zixia_boards_list[$bn_e] = $bn_c;
+//print_r($zixia_boards_list);
+//echo "<hr>\n";
+        }
+        fclose($fd);
+
+//print_r($zixia_boards_list);
+	return true;
+}
+
+function get_zixia_board_info( $bn )
+{
+	global $zixia_boards_list;
+
+	$bn = strtolower($bn);
+	if ( array_key_exists($bn, $zixia_boards_list) ){
+		if ( strlen($zixia_boards_list[$bn])){
+			return $zixia_boards_list[$bn];
+		}else{
+			return true;
+		}
+	}
+
+	return false;
+}
+
+
+
 ?>
