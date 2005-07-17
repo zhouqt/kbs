8d744ab95f13f1cd8e8d0db2bb466246b5078548
atppp <atppp@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2005-07-17 00:56:02 +0000

    to compile with gcc 4.0.1
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@8112 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/src/vars.h
+++ kbs_bbs/src/vars.h
@@ -33,15 +33,16 @@ extern int selboard;            /* THis flag is true if above is active */
 
 extern char genbuf[1024];       /* generally used global buffer */
 
-extern struct commands cmdlist[];       /* main menu command list */
-
 extern jmp_buf byebye;          /* Used for exception condition like I/O error */
 
+#if 0
+extern struct commands cmdlist[];       /* main menu command list */
 extern struct commands xyzlist[];       /* These are command lists for all the */
 extern struct commands talklist[];      /* sub-menus */
 extern struct commands maillist[];
 extern struct commands dellist[];
 extern struct commands maintlist[];
+#endif
 
 extern char save_title[];       /* These are used by the editor when inserting */
 extern int in_mail;
