af16f99419995d266c1a953f9fcfb571e6934ebd
atppp <atppp@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2005-08-06 07:13:31 +0000

    some general article output functions
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@8147 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/bbs2www/phplib/phpbbslib.c
+++ kbs_bbs/bbs2www/phplib/phpbbslib.c
@@ -285,6 +285,10 @@ static PHP_FUNCTION(bbs_unregister_sms);
 static PHP_FUNCTION(bbs_csv_to_al);
 #endif
 
+#ifdef AJAX
+static PHP_FUNCTION(bbs2_readfile);
+static PHP_FUNCTION(bbs2_readfile_text);
+#endif
 
 /*
  * define what functions can be used in the PHP embedded script
@@ -456,6 +460,11 @@ PHP_FE(bbs_getwwwguestnumber,NULL)
     PHP_FE(bbs_init_ext, NULL)
     PHP_FE(bbs_x_search,third_arg_force_ref_001)
     PHP_FE(bbs_read_ann_dir,fourth_arg_force_ref_0111)
+
+#ifdef AJAX
+    PHP_FE(bbs2_readfile, NULL)
+    PHP_FE(bbs2_readfile_text, NULL)
+#endif
     {NULL, NULL, NULL}
 };
 
@@ -9166,4 +9175,277 @@ static PHP_FUNCTION(bbs_get_threads_from_gid)
 	efree(articles);
 	RETURN_LONG(retnum);
 }
+
+#ifdef AJAX
+/*
+ * refer Ecma-262 
+ * '\033'  -> \r (not exactly the same thing, but borrow...)
+ * '\n'    -> \n
+ * '\\'    -> \\
+ * '\''    -> \'
+ * '\"'    -> \"
+ * '\0'    -> possible start of attachment
+ * 0 <= char < 32 -> ignore
+ * others  -> passthrough
+ */
+
+PHP_FUNCTION(bbs2_readfile)
+{
+	char *filename;
+	int filename_len;
+    char *output_buffer;
+    int output_buffer_len, output_buffer_size, j;
+    char c;
+    char *ptr, *cur_ptr;
+    long ptrlen;
+    int fd;
+    int in_chinese = false;
+    int chunk_size = 51200;
+    struct stat st;
+
+    getcwd(old_pwd, 1023);
+    chdir(BBSHOME);
+    old_pwd[1023] = 0;
+
+	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "s", &filename, &filename_len) == FAILURE) {
+		WRONG_PARAM_COUNT;
+	}
+    
+    fd = open(filename, O_RDONLY);
+    if (fd < 0)
+        RETURN_LONG(2);
+    if (fstat(fd, &st) < 0) {
+        close(fd);
+        RETURN_LONG(2);
+    }
+    if (!S_ISREG(st.st_mode)) {
+        close(fd);
+        RETURN_LONG(2);
+    }
+    if (st.st_size <= 0) {
+        close(fd);
+        RETURN_LONG(2);
+    }
+
+    ptr = mmap(NULL, st.st_size, PROT_READ, MAP_SHARED, fd, 0);
+    ptrlen = st.st_size;
+    close(fd);
+    if (ptr == NULL)
+        RETURN_LONG(-1);
+
+    j = ptrlen;
+    if (j > chunk_size) j = chunk_size;
+    output_buffer_size = 2 * j + 16;
+    output_buffer = (char* )emalloc(output_buffer_size);
+    output_buffer_len = 0;
+    cur_ptr = ptr;
+    strcpy(output_buffer + output_buffer_len, "prints('");
+    output_buffer_len += 8;
+    while (1) {
+        for (; j >= 0 ; j--) {
+            c = *cur_ptr;
+            if (c == '\0') { //assume ATTACHMENT_PAD[0] is '\0'
+               if (ptrlen >= ATTACHMENT_SIZE + sizeof(int) + 2) {
+                    if (!memcmp(cur_ptr, ATTACHMENT_PAD, ATTACHMENT_SIZE)) {
+                        ptrlen = -ptrlen;
+                        break;
+                    }
+                }
+                continue;
+            }
+            if (c < 0) {
+                in_chinese = !in_chinese;
+                output_buffer[output_buffer_len++] = c;
+            } else {
+                do {
+                    if (c == '\n') c = 'n';
+                    else if (c == '\033') c = 'r';
+                    else if (c != '\\' && c != '\'' && c != '\"') {
+                        if (c >= 32) {
+                            output_buffer[output_buffer_len++] = c;
+                        }
+                        break;
+                    }
+                    if (in_chinese && c == 'n') {
+                        output_buffer[output_buffer_len++] = ' ';
+                    }
+                    output_buffer[output_buffer_len++] = '\\';
+                    output_buffer[output_buffer_len++] = c;
+                } while(0);
+                in_chinese = false;
+            }
+            ptrlen--; cur_ptr++;
+        }
+        if (ptrlen <= 0) break;
+        j = ptrlen;
+        if (j > chunk_size) j = chunk_size;
+        output_buffer_size += 2 * j;
+   		output_buffer = (char*)erealloc(output_buffer, output_buffer_size);
+        if (output_buffer == NULL) RETURN_LONG(3);
+    }
+    if (in_chinese) {
+        output_buffer[output_buffer_len++] = ' ';
+    }
+    strcpy(output_buffer + output_buffer_len, "');");
+    output_buffer_len += 3;
+    
+    if (ptrlen < 0) { //attachment
+        char *attachfilename, *attachptr;
+        char buf[1024];
+        char *startbufptr, *bufptr;
+        long attach_len, attach_pos, newlen;
+        int l;
+
+        ptrlen = -ptrlen;
+        strcpy(buf, "attach('");
+        startbufptr = buf + strlen(buf);
+        while(ptrlen > 0) {
+            if (((attachfilename = checkattach(cur_ptr, ptrlen, 
+                                  &attach_len, &attachptr)) == NULL)) {
+                break;
+            }
+            attach_pos = attachfilename - ptr;
+            newlen = attachptr - cur_ptr + attach_len;
+            cur_ptr += newlen;
+            ptrlen -= newlen;
+            if (ptrlen < 0) break;
+            bufptr = startbufptr;
+            while(*attachfilename != '\0') {
+                switch(*attachfilename) {
+    				case '\'':
+    				case '\"':
+    				case '\\':
+    					*bufptr++ = '\\';
+    					/* break is missing *intentionally* */
+    				default:
+    					*bufptr++ = *attachfilename++;
+                }
+            }
+            sprintf(bufptr, "', %ld, %ld);", attach_len, attach_pos);
+
+            l = strlen(buf);
+            if (output_buffer_len + l > output_buffer_size) {
+                output_buffer_size = output_buffer_size + sizeof(buf) * 10;
+           		output_buffer = (char*)erealloc(output_buffer, output_buffer_size);
+                if (output_buffer == NULL) RETURN_LONG(3);
+            }
+            strcpy(output_buffer + output_buffer_len, buf);
+            output_buffer_len += l;
+        }
+    }
+    munmap(ptr, st.st_size);
+
+    RETVAL_STRINGL(output_buffer, output_buffer_len, 0);;
+}
+
+PHP_FUNCTION(bbs2_readfile_text)
+{
+	char *filename;
+	int filename_len;
+    long maxchar;
+    long double_escape;
+    char *output_buffer;
+    int output_buffer_len, output_buffer_size, last_return = 0;
+    char c;
+    char *ptr, *cur_ptr;
+    long ptrlen;
+    int in_escape = false;
+    int fd;
+    char escape_seq[4][16];
+    struct stat st;
+
+    getcwd(old_pwd, 1023);
+    chdir(BBSHOME);
+    old_pwd[1023] = 0;
+
+	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "sll", &filename, &filename_len, &maxchar, &double_escape) == FAILURE) {
+		WRONG_PARAM_COUNT;
+	}
+    
+    fd = open(filename, O_RDONLY);
+    if (fd < 0)
+        RETURN_LONG(2);
+    if (fstat(fd, &st) < 0) {
+        close(fd);
+        RETURN_LONG(2);
+    }
+    if (!S_ISREG(st.st_mode)) {
+        close(fd);
+        RETURN_LONG(2);
+    }
+    if (st.st_size <= 0) {
+        close(fd);
+        RETURN_LONG(2);
+    }
+
+    ptr = mmap(NULL, st.st_size, PROT_READ, MAP_SHARED, fd, 0);
+    ptrlen = st.st_size;
+    close(fd);
+    if (ptr == NULL)
+        RETURN_LONG(-1);
+
+    if (!maxchar) {
+        maxchar = ptrlen;
+    } else if (ptrlen > maxchar) {
+        ptrlen = maxchar;
+    }
+    output_buffer_size = 2 * maxchar;
+    output_buffer = (char* )emalloc(output_buffer_size);
+    output_buffer_len = 0;
+    cur_ptr = ptr;
+    if (double_escape) {
+        strcpy(escape_seq[0], "&amp;amp;");
+        strcpy(escape_seq[1], "&amp;lt;");
+        strcpy(escape_seq[2], "&amp;gt;");
+        strcpy(escape_seq[3], "&lt;br/&gt;");
+    } else {
+        strcpy(escape_seq[0], "&amp;");
+        strcpy(escape_seq[1], "&lt;");
+        strcpy(escape_seq[2], "&gt;");
+        strcpy(escape_seq[3], "<br/>");
+    }
+    while (ptrlen > 0) {
+        c = *cur_ptr;
+        if (c == '\0') { //assume ATTACHMENT_PAD[0] is '\0'
+            break;
+        } else if (c == '\033') {
+            in_escape = true;
+        } else if (!in_escape) {
+            if (output_buffer_len + 16 > output_buffer_size) {
+                output_buffer = (char*)erealloc(output_buffer, output_buffer_size += 128);
+            }
+            switch(c) {
+                case '&':
+                    strcpy(output_buffer + output_buffer_len, escape_seq[0]);
+                    output_buffer_len += strlen(escape_seq[0]);
+                    break;
+                case '<':
+                    strcpy(output_buffer + output_buffer_len, escape_seq[1]);
+                    output_buffer_len += strlen(escape_seq[1]);
+                    break;
+                case '>':
+                    strcpy(output_buffer + output_buffer_len, escape_seq[2]);
+                    output_buffer_len += strlen(escape_seq[2]);
+                    break;
+                case '\n':
+                    strcpy(output_buffer + output_buffer_len, escape_seq[3]);
+                    output_buffer_len += strlen(escape_seq[3]);
+                    last_return = output_buffer_len;
+                    break;
+                default:
+                    if (c < 0 || c >= 32)
+                        output_buffer[output_buffer_len++] = c;
+                    break;
+            }
+        } else if (isalpha(c)) {
+            in_escape = false;
+        }
+        ptrlen--; cur_ptr++;
+    }
+
+    munmap(ptr, st.st_size);
+
+	RETVAL_STRINGL(output_buffer, last_return, 0);
+}
+#endif
 
