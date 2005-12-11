a91c3c51e82a305c258f090e54dd12767a7c7ccc
atppp <atppp@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2005-12-11 08:22:45 +0000

    test
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@8361 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- www2/site.php
+++ www2/site.php
@@ -1,8 +1,8 @@
 <?php
-define("ATTACHMAXSIZE","1048576"); //附件总字节数的上限，单位 bytes
-define("ATTACHMAXCOUNT","20");     //附件数目的上限
-define("MAINPAGE_FILE","mainpage.html"); //首页导读的 URL
-define("QUOTED_LINES", BBS_QUOTED_LINES);        //web 回文保留的引文行数
+define("ATTACHMAXSIZE","1048576");        //附件总字节数的上限，单位 bytes
+define("ATTACHMAXCOUNT","20");            //附件数目的上限
+define("MAINPAGE_FILE","mainpage.html");  //首页导读的 URL
+define("QUOTED_LINES", BBS_QUOTED_LINES); //web 回文保留的引文行数
 
 $section_nums = array("0", "1", "2", "3", "4", "5", "6", "7", "8", "9","A","B","C");
 $section_names = array(
