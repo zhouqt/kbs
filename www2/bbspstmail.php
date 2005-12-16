9d47cdcfd52c3178049665505700e561b98a5302
pig2532 <pig2532@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2005-12-16 17:45:30 +0000

    sorry, a small problem :)
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@8389 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- www2/bbspstmail.php
+++ www2/bbspstmail.php
@@ -105,7 +105,7 @@ function dosubmit() {
 <td class="b9">
 寄信人: <?php echo $currentuser["userid"]; ?><br />
 标&nbsp;&nbsp;题: <input class="sb1" type="text" name="title" size="40" maxlength="100" value="<?php echo htmlspecialchars($title,ENT_QUOTES); ?>"><br />
-收信人: <input class="sb1" type="text" name="userid" value="<?php echo $destuserid; ?>">
+收信人: <input class="sb1" type="text" name="userid" value="<?php echo $destuserid; ?>"<?php if($file!=""){echo " readonly";} ?>>
 <input class="sb1" type="hidden" name="file" value="<?php echo $file; ?>"><br />
 
 使用签名档 <select name="signature">
