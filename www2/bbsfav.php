be5f8408413ac2e90e40da48eaaa7766d1c48080
atppp <atppp@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2005-12-18 01:41:31 +0000

    hack to support 新分类讨论区
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@8401 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- www2/bbsfav.php
+++ www2/bbsfav.php
@@ -1,8 +1,13 @@
 <?php
+	$XX = isset($_GET["x"]);
 	require("www2-funcs.php");
 	login_init();
-	assert_login();
-	page_header($currentuser["userid"] . " 的收藏夹");
+	if ($XX) {
+		page_header("新分类讨论区");
+	} else {
+		assert_login();
+		page_header($currentuser["userid"] . " 的收藏夹");
+	}
 
 	if (isset($_GET["select"]))
 		$select = $_GET["select"];
@@ -10,38 +15,41 @@
 		$select = 0;
 	settype($select, "integer");
 
-	if ($select < 0)// || $group > sizeof($section_nums))
+	if ($select < 0)
 		html_error_quit("错误的参数");
-	if(bbs_load_favboard($select)==-1)
-		html_error_quit("错误的参数");
-	if (isset($_GET["delete"]))
-	{
-		$delete_s=$_GET["delete"];
-		settype($delete_s,"integer");
-		bbs_del_favboard($select,$delete_s);
+	if (!$XX) {
+		if(bbs_load_favboard($select)==-1)
+			html_error_quit("错误的参数");
+		if (isset($_GET["delete"]))
+		{
+			$delete_s=$_GET["delete"];
+			settype($delete_s,"integer");
+			bbs_del_favboard($select,$delete_s);
+		}
+		if (isset($_GET["deldir"]))
+		{
+			$delete_s=$_GET["deldir"];
+			settype($delete_s,"integer");
+			bbs_del_favboarddir($select,$delete_s);
+		}
+		if (isset($_GET["dname"]))
+		{
+			$add_dname=trim($_GET["dname"]);
+			if ($add_dname)
+				bbs_add_favboarddir($add_dname);
+		}
+		if (isset($_GET["bname"]))
+		{
+			$add_bname=trim($_GET["bname"]);
+			if ($add_bname)
+				$sssss=bbs_add_favboard($add_bname);
+		}
 	}
-	if (isset($_GET["deldir"]))
-	{
-		$delete_s=$_GET["deldir"];
-		settype($delete_s,"integer");
-		bbs_del_favboarddir($select,$delete_s);
-	}
-	if (isset($_GET["dname"]))
-	{
-		$add_dname=trim($_GET["dname"]);
-		if ($add_dname)
-			bbs_add_favboarddir($add_dname);
-	}
-	if (isset($_GET["bname"]))
-	{
-		$add_bname=trim($_GET["bname"]);
-		if ($add_bname)
-			$sssss=bbs_add_favboard($add_bname);
-	}
-	$boards = bbs_fav_boards($select, 1);
+	$boards = bbs_fav_boards($select, $XX ? 2 : 1);
 	$list_father = bbs_get_father($select);
 	if ($boards === FALSE)
 		html_error_quit("读取版列表失败");
+	$fix = $XX ? "&x" : "";
 ?>
 <table class="main wide adj">
 <col width="2%" class="center"/><col width="2%"/><col width="23%"/><col width="10%" class="center"/><col width="33%"/><col class="center" width="14%"/><col class="right" width="8%"/><col width="6%" class="center"/>
@@ -65,7 +73,7 @@
 <tr>
 <td> </td>
 <td> <script language="javascript">putImage('groupgroup.gif','alt="up" title="回到上一级"');</script></td>
-<td colspan="6"><a href="bbsfav.php?select=<?php echo $list_father; ?>">回到上一级</a></td>
+<td colspan="6"><a href="bbsfav.php?select=<?php echo $list_father.$fix; ?>">回到上一级</a></td>
 </tr>
 <?php
 	}
@@ -80,10 +88,11 @@
 	if ($brd_flag[$i] == -1 ) {
 ?>
 <td> <script language="javascript">putImage('groupgroup.gif','alt="＋" title="版面组"');</script></td>
-<td><a href="bbsfav.php?select=<?php echo $brd_bid[$i];?>"><?php echo $brd_desc[$i];?></a></td>
+<td><a href="bbsfav.php?select=<?php echo $brd_bid[$i].$fix;?>"><?php echo $brd_desc[$i];?></a></td>
 <td>[目录]</td>
-<td colspan="3"> </td>
-<td><a href="bbsfav.php?select=<?php echo $select;?>&deldir=<?php echo $brd_npos[$i];?>">删除</a></td>
+<td colspan="3"> </td><td>
+<?php if (!$XX) { ?><a href="bbsfav.php?select=<?php echo $select;?>&deldir=<?php echo $brd_npos[$i];?>">删除</a><?php } ?>
+</td>
 </tr>
 <?php
 		continue;
@@ -128,12 +137,15 @@
 ?>
 </td>
 <td><?php echo $brd_artcnt[$i]; ?></td>
-<td><a href="bbsfav.php?select=<?php echo $select;?>&delete=<?php echo ($brd_npos[$i]);?>">删除</a></td>
+<td><?php if (!$XX) { ?><a href="bbsfav.php?select=<?php echo $select;?>&delete=<?php echo ($brd_npos[$i]);?>">删除</a><?php } ?></td>
 </tr>
 <?php
 	}
 ?>
 </table>
+<?php
+	if (!$XX) {
+?>
 <div class="oper"><form action=bbsfav.php>增加目录: <input name=dname size=24 maxlength=20 type=text value="">
 <input type=submit value=确定><input type=hidden name=select value=<?php echo $select;?>>
 </form></div>
@@ -141,5 +153,6 @@
 <input type=submit value=确定><input type=hidden name=select value=<?php echo $select;?>>
 </form></div>
 <?php
+	}
 	page_footer();
 ?>
