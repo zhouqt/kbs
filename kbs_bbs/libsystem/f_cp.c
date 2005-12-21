299c05ec5edf6e074b94def14d25ae01995924b8
kcn <kcn@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2005-12-21 03:54:48 +0000

    new f_cp.c
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@8431 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/libsystem/f_cp.c
+++ kbs_bbs/libsystem/f_cp.c
@@ -1,32 +1,37 @@
-#include <fcntl.h>
 #include "system.h"
 
-#define BLK_SIZ 10240
+/**
+ *  copy file src to file dst
+ *  Param:
+ *    src: source file name
+ *    dst: destination file name
+ *    mode: destination file open mode, can be O_EXCL ...
+ *  Return: 0 - success,other - faled
+ **/
+#define READ_BUFFER_SIZE 10240
 
-int f_cp(src, dst, mode)
-    char *src, *dst;
-    int mode;                   /* O_EXCL / O_APPEND / O_TRUNC */
+int f_cp(char* src,char* dst,int mode)
 {
-    int fsrc, fdst, ret;
-
-    ret = 0;
-
-    if ((fsrc = open(src, O_RDONLY)) >= 0) {
-        ret = -1;
-
-        if ((fdst = open(dst, O_WRONLY | O_CREAT | mode, 0600)) >= 0) {
-            char *pool = (char *) malloc(BLK_SIZ);
-
-            src = pool;
+    int sfd,dfd;
+    int ret=0;
+    
+    if ((sfd = open(src,O_RDONLY)) >= 0 ) {
+        if ((dfd = open(dst,O_WRONLY|O_CREAT|O_TRUNC|mode,0600)) >= 0 ) {
+#ifdef BUFFER_IN_STACK
+            char buffer[READ_BUFFER_SIZE];
+#else
+            char* buffer = (char*)malloc(READ_BUFFER_SIZE);
+#endif
             do {
-                ret = read(fsrc, src, BLK_SIZ);
-                if (ret <= 0)
+                if ((ret = read(sfd, buffer, READ_BUFFER_SIZE)) <= 0)
                     break;
-            } while (write(fdst, src, ret) > 0);
-            close(fdst);
-            free(pool);
-        }
-        close(fsrc);
-    }
+            } while (write(dfd, buffer, ret) > 0);
+#ifndef BUFFER_IN_STACK
+            free(buffer);
+#endif
+            close(dfd);
+    	} else ret=-1;
+        close(sfd);
+    } else ret=-1;
     return ret;
 }
