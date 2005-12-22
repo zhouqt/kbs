be8a3544b6a9434457881102e57b2db8de10df9f
atppp <atppp@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2005-12-22 08:31:17 +0000

    貌似这样？
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@8442 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/local_utl/bmerge.c
+++ kbs_bbs/local_utl/bmerge.c
@@ -228,9 +228,10 @@ int main(int argc,char **argv){
     pos1=0;pos2=0;pos=0;
     while(true){
         /*比较时间戳*/
-        if(pos1<size1&&pos2<size2)
-            ret=strcmp(&dir1[pos1].filename[2],&dir2[pos2].filename[2]);
-        else if(pos1<size1)/*源版面2已经处理完成*/
+        if(pos1<size1&&pos2<size2) {
+            //ret=strcmp(&dir1[pos1].filename[2],&dir2[pos2].filename[2]);
+            ret = get_posttime(&dir1[pos1], &dir2[pos2]);
+        } else if(pos1<size1)/*源版面2已经处理完成*/
             ret=-1;
         else if(pos2<size2)/*源版面1已经处理完成*/
             ret=1;
