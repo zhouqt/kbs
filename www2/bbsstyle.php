f9eaab3b9ae3db4b578525aa0d2e833546f0f429
pig2532 <pig2532@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2006-01-07 09:32:45 +0000

    砍掉叉叉
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@8644 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- www2/bbsstyle.php
+++ www2/bbsstyle.php
@@ -38,17 +38,16 @@
 		alert('还没做这个功能呢... 需要吗？不需要吗？您去 sysop 版喊喊？');
 	}
 	var oldCssID = null, nowCssID = null;
-	function setStyleDiv(cssID, setImg) {
+	function setStyleDiv(cssID) {
 		for (var i=0; i<32; i++) {
 			var o = getObj("stylediv" + i);
 			if (o) {
 				o.style.borderColor = (i!=cssID) ? 'white' : 'blue';
-				if (setImg) getObj("stylethumb" + i).src = "images/" + i + "/thumb.jpg";
 			} else break;
 		}
 	}
 	function chkStyle(cssID) {
-		setStyleDiv(cssID, 0);
+		setStyleDiv(cssID);
 		nowCssID = cssID;
 	}
 	function applyStyle() {
@@ -67,8 +66,6 @@
 			var v = c & (1 << i);
 			setInd(n, v);
 		}
-		oldCssID = nowCssID = getCssID();
-		setStyleDiv(nowCssID, 1);
 		if (!isLogin()) {
 			getObj("cmdSave").disabled = true;
 			getObj("idpsave").style.visibility = "hidden";
@@ -133,7 +130,7 @@ div.stylesel img {
 	for($i=0;$i<$stylecount;$i++)
 	{
 		$ret .= "<div id='stylediv{$i}' class='stylesel clickable' onClick='chkStyle($i);'>";
-		$ret .= "<img id='stylethumb{$i}' alt='预览图' src='' /><br/>{$style_names[$i]}</div>";
+		$ret .= "<img id='stylethumb{$i}' alt='预览图' src='images/{$i}/thumb.jpg' /><br/>{$style_names[$i]}</div>";
 	}
 	print($ret);
 ?>
@@ -143,6 +140,10 @@ div.stylesel img {
 	<div class="oper"><input id="cmdSave" type="submit" value="保存设置"/> &nbsp; <input type="button" onclick="history.go(-1);" value="快速返回"/><br/>
 		<span id="idpsave">[ 点 保存设置 以保证每次登录都使用这个设置。]</span></div>
 </form>
+<script type="text/javascript">
+	oldCssID = nowCssID = getCssID();
+	setStyleDiv(nowCssID);
+</script>
 <?php
 	page_footer();
 ?>
