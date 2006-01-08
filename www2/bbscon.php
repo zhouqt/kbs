041036e4a62ac68fb7500881acae76f3da99a9d5
atppp <atppp@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2006-01-08 08:46:57 +0000

    bbscon减少request的修改先rollback 不过这个修改还是很有意义的，将来可能有用， 所以我不删这个版本，而更新到一个新版本吧
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@8652 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- www2/bbscon.php
+++ www2/bbscon.php
@@ -50,29 +50,39 @@
 		html_error_quit("错误的文章号");
 	}
 	settype($id, "integer");
+	// 获取上一篇或下一篇，同主题上一篇或下一篇的指示
+	@$ptr=$_GET["p"];
+	// 同主题的指示在这里处理
+	if ($ptr == "tn")
+	{
+		$articles = bbs_get_threads_from_id($brdnum, $id, $dir_modes["NORMAL"],1);
+		if ($articles == FALSE)
+			$redirt_id = $id;
+		else
+			$redirt_id = $articles[0]["ID"];
+		if (($loginok == 1) && $currentuser["userid"] != "guest")
+			bbs_brcaddread($brdarr["NAME"], $redirt_id);
+		header("Location: " . "bbscon.php?bid=" . $brdnum . "&id=" . $redirt_id);
+		exit;
+	}
+	elseif ($ptr == "tp")
+	{
+		$articles = bbs_get_threads_from_id($brdnum, $id, $dir_modes["NORMAL"],-1);
+		if ($articles == FALSE)
+			$redirt_id = $id;
+		else
+			$redirt_id = $articles[0]["ID"];
+		if (($loginok == 1) && $currentuser["userid"] != "guest")
+			bbs_brcaddread($brdarr["NAME"], $redirt_id);
+		header("Location: " . "bbscon.php?bid=" . $brdnum . "&id=" . $redirt_id);
+		exit;
+	}
 
 	if (isset($_GET["ftype"])){
 		$ftype = intval($_GET["ftype"]);
 	} else {
 		$ftype = $dir_modes["NORMAL"];
 	}
-	
-	$previd = $nextid = $tprevid = $tnextid = $id;
-	// 获取同主题上下篇的ID号
-	if($ftype == $dir_modes["NORMAL"])
-	{
-		$articles = bbs_get_threads_from_id($brdnum, $id, $dir_modes["NORMAL"],1);
-		if ($articles == FALSE)
-			$tnextid = $id;
-		else
-			$tnextid = $articles[0]["ID"];
-		$articles = bbs_get_threads_from_id($brdnum, $id, $dir_modes["NORMAL"],-1);
-		if ($articles == FALSE)
-			$tprevid = $id;
-		else
-			$tprevid = $articles[0]["ID"];
-	}
-	
 	$dir_perm = bbs_is_permit_mode($ftype, 1);
 	if (!$dir_perm) {
 		html_error_quit("错误的模式");
@@ -94,14 +104,6 @@
         $num = bbs_get_records_from_id($brdarr["NAME"], $id, $ftype, $articles);
         if ($num <= 0) html_error_quit("错误的文章号,原文可能已经被删除<script>clearArticleDiv(".$id.");</script>");
         $article = $articles[1];
-	
-	// 获取上下篇文章的ID号
-	if($ftype == $dir_modes["NORMAL"])
-	{
-		$previd = ($articles[0]["ID"]==0) ? $id : $articles[0]["ID"];
-		$nextid = ($articles[2]["ID"]==0) ? $id : $articles[2]["ID"];
-	}
-
     } else {
         $num = @intval($_GET["num"]);
         if (($num <= 0) || ($num > $total)) html_error_quit("错误的文章号,原文可能已经被删除");
@@ -121,18 +123,26 @@
 		exit;
 	}
 
+	if (!$ftype && $ptr == 'p' && $articles[0]["ID"] != 0) {
+		if ($currentuser["userid"] != "guest")
+			bbs_brcaddread($brdarr["NAME"], $articles[0]["ID"]);
+		header("Location: " . "bbscon.php?bid=" . $brdnum . "&id=" . $articles[0]["ID"]);
+		exit;
+	}
+	if (!$ftype && $ptr == 'n' && $articles[2]["ID"] != 0)
+	{
+		if ($currentuser["userid"] != "guest")
+			bbs_brcaddread($brdarr["NAME"], $articles[2]["ID"]);
+		header("Location: " ."bbscon.php?bid=" . $brdnum . "&id=" . $articles[2]["ID"]);
+		exit;
+	}
 	page_header("阅读文章".$dir_name[$ftype], "<a href=\"bbsdoc.php?board=".$brdarr["NAME"]."\">".htmlspecialchars($brdarr["DESC"])."</a>");
 ?>
 <h1><?php echo $brdarr["NAME"]; ?> 版 <?php echo $dir_name[$ftype]; ?></h1>
 <script type="text/javascript"><!--
-<?php
-	$ret = "";
-	$ret .= "var o = new conWriter({$ftype},'" . addslashes($brdarr["NAME"]) . "',{$brdnum},";
-	$ret .= "{$article["ID"]},{$article["GROUPID"]},{$article["REID"]},'{$article["FILENAME"]}','";
-	$ret .= addslashes(bbs_get_super_fav($article["TITLE"], "bbscon.php?bid={$brdnum}&id={$article["ID"]}"));
-	$ret .= "',{$num},{$previd},{$nextid},{$tprevid},{$tnextid});";
-	echo $ret;
-?>
+var o = new conWriter(<?php echo $ftype; ?>, '<?php echo addslashes($brdarr["NAME"]); ?>', <?php echo $brdnum; ?>, <?php
+echo $article["ID"];?>, <?php echo $article["GROUPID"];?>, <?php echo $article["REID"];?>, '<?php echo $article["FILENAME"];?>', '<?php
+echo addslashes(bbs_get_super_fav($article['TITLE'], "bbscon.php?bid=" . $brdnum . "&id=" . $article["ID"]));?>', <?php echo $num; ?>);
 o.h(1);
 attachURL = 'bbscon.php?<?php echo $_SERVER["QUERY_STRING"]; ?>';
 <?php $s = bbs2_readfile($filename); if (is_string($s)) echo $s; ?>
