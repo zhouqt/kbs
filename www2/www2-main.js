bdd90ef1f161285b6725d9f7f4b59510051d4c99
atppp <atppp@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2006-01-19 07:33:26 +0000

    这个涉及到服务器上比较麻烦的改动，先缓缓
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@8724 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- www2/www2-main.js
+++ www2/www2-main.js
@@ -975,7 +975,7 @@ conWriter.prototype.t = function() {
 		ret += '[<a href="bbsedit.php' + qry + '">修改</a>] ';
 		if (!zd) {
 			ret += '[<a href="bbsedittitle.php' + qry + '">修改标题</a>] ';
-			ret += '[<a href="bbsprop.php' + qry + '">属性</a>] ';
+			//ret += '[<a href="bbsprop.php' + qry + '">属性</a>] ';
 			ret += '[<a onclick="return confirm(\'你真的要删除本文吗?\')" href="bbsdel.php?board=' + this.board + '&id=' + this.id + '">删除</a>] ';
 		}
 	}
