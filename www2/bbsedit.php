c188b810ecf30ec28e09471b3076c9edaa0090ca
pig2532 <pig2532@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2006-01-31 11:01:28 +0000

    幸好有备份
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@8755 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- www2/bbsedit.php
+++ www2/bbsedit.php
@@ -24,6 +24,13 @@
 	}
 	if (bbs_is_readonly_board($brdarr))
 		html_error_quit("不能在只读讨论区发表文章");
+	if (isset($_GET["ftype"]))
+		if ($_GET["ftype"] == $dir_modes["ZHIDING"])
+			$ftype = $dir_modes["ZHIDING"];
+		else
+			$ftype = $dir_modes["NORMAL"];
+	else
+		$ftype = $dir_modes["NORMAL"];
 
 	bbs_board_nav_header($brdarr, "修改文章");
 	
@@ -32,10 +39,10 @@
 	else
 		html_error_quit("错误的文编号");
 	$articles = array();
-	$num = bbs_get_records_from_id($brdarr["NAME"], $id,$dir_modes["NORMAL"],$articles);
+	$num = bbs_get_records_from_id($brdarr["NAME"], $id, $ftype, $articles);
 	if ($num == 0)
 		html_error_quit("错误的文编号");
-	$ret = bbs_caneditfile($board,$articles[1]['FILENAME']);
+	/*$ret = bbs_caneditfile($board,$articles[1]['FILENAME']);
 	switch ($ret) {
 		case -1:
 			html_error_quit("讨论区名称错误");
@@ -59,11 +66,49 @@
 			html_error_quit("您的POST权被封");
 			break;
 		default:
-	}
+	}*/
 
 	$brd_encode = urlencode($brdarr["NAME"]);
 	
 	if (isset($_GET['do'])) {
+
+		$ret = bbs_edittitle($board, $id, $_POST["title"], $ftype);
+		if($ret != 0)
+		{
+			switch($ret)
+			{
+				case -1:
+					html_error_quit("错误的讨论区");
+					break;
+				case -2:
+					html_error_quit("对不起，该讨论区不能修改标题");
+					break;
+				case -3:
+					html_error_quit("对不起，该讨论区为只读讨论区");
+					break;
+				case -4:
+					html_error_quit("错误的文章号");
+					break;
+				case -5:
+					html_error_quit("对不起，您已被停止在".$board."版的发文权限");
+					break;
+				case -6:
+					html_error_quit("对不起，您无权修改本文");
+					break;
+				case -7:
+					html_error_quit("标题含有不雅用字");
+					break;
+				case -8:
+					html_error_quit("对不起，当前模式无法修改标题");
+					break;
+				case -9:
+					html_error_quit("标题过长");
+					break;
+				default:
+					html_error_quit("系统错误，请联系管理员");
+			}
+		}
+
 		$ret=bbs_updatearticle($board,$articles[1]['FILENAME'],$_POST['text']);
 		switch ($ret) {
 			case -1:
@@ -75,7 +120,7 @@
 			case 0:
 				html_success_quit("文章修改成功",
 				array("<a href='bbsdoc.php?board=" . $brd_encode . "'>返回 " . $brdarr['DESC'] . "</a>",
-				"<a href='bbscon.php?board=" . $brd_encode . "&id=" . $id . "'>返回《" . htmlspecialchars($articles[1]['TITLE']) . "》</a>"));
+				"<a href='bbscon.php?board=" . $brd_encode . "&id=" . $id . "&ftype='" . $ftype . "'>返回《" . htmlspecialchars($_POST["title"]) . "》</a>"));
 				break;
 			default:
 		}
@@ -83,7 +128,7 @@
 	}
 ?>
 <link rel="stylesheet" type="text/css" href="ansi.css"/>
-<form name="postform" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>?board=<?php echo $brd_encode; ?>&id=<?php echo $id; ?>&do" class="large">
+<form name="postform" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>?board=<?php echo $brd_encode; ?>&id=<?php echo $id; ?>&ftype=<?php echo $ftype; ?>&do" class="large">
 <div class="article">
 <?php
 	$notes_file = bbs_get_vote_filename($brdarr["NAME"], "notes");
@@ -114,8 +159,7 @@
 </div>
 <fieldset><legend>修改文章</legend>
 发信人: <?php echo $articles[1]['OWNER']; ?>, 信区: <?php echo $brd_encode; ?> [<a href="bbsdoc.php?board=<?php echo $brd_encode; ?>">本讨论区</a>]<br/>
-标&nbsp;&nbsp;题: <input readonly type="text" name="title" size="40" maxlength="100" value="<?php echo $articles[1]['TITLE']; ?>" />
-[<a href="bbsedittitle.php?board=<?php echo $brd_encode; ?>&id=<?php echo $id; ?>">修改标题</a>]
+标&nbsp;&nbsp;题: <input type="text" name="title" size="40" maxlength="100" value="<?php echo $articles[1]['TITLE']; ?>" />
 <textarea name="text" onkeydown='return textarea_okd(dosubmit, event);' wrap="physical" id="sfocus">
 <?php
 	bbs_printoriginfile($board,$articles[1]['FILENAME']);
