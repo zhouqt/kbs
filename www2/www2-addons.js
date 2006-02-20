a7a75380b45dbd52240511883fc884c5ade58281
atppp <atppp@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2006-02-20 22:58:16 +0000

    for ajax/ansi decoding/fancy popups
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@8892 de4889a5-8ad4-4bf3-bd87-57997c21d515

---
+++ www2/www2-addons.js
@@ -0,0 +1,10 @@
+/**
+ * Part of the KBS BBS Code
+ * Copyright (c) 2005-2006 KBS Development Team. (http://dev.kcn.cn/)
+ * Source file is subject to the pending KBS License.
+ *
+ * You may use and/or modify the source code only for pure personal study
+ * purpose (e.g. using it in a public website is not acceptable), unless
+ * you get explicit permission by the KBS Development Team.
+ */
+
