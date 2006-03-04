6199ea101fd750dac9cae82153491ad5477a620f
etnlegend <etnlegend@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2006-03-04 09:32:56 +0000

    rebuild digest
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@9018 de4889a5-8ad4-4bf3-bd87-57997c21d515

---
+++ kbs_bbs/local_utl/rebuild_digest.c
@@ -0,0 +1,65 @@
+#include "bbs.h"
+static ssize_t data_write(int fd,const void *data,size_t size){
+    size_t len;
+    ssize_t ret;
+    const void *p;
+    for(ret=0,p=data,len=size;len>0&&ret!=-1;p+=ret,len-=ret)
+        ret=write(fd,p,len);
+    return ret;
+}
+int main(int argc,char **argv){
+    struct stat st;
+    struct fileheader fh,*fptr;
+    char buf[256],fn[256];
+    const char *board;
+    int fd,i,j,count;
+    void *ptr;
+    if(argc!=2||!(board=argv[1]))
+        return __LINE__;
+    if(chdir(BBSHOME)==-1)
+        return __LINE__;
+    setbdir(DIR_MODE_NORMAL,fn,board);
+    if(stat(fn,&st)||!S_ISREG(st.st_mode)||!(st.st_size))
+        return __LINE__;
+    if((fd=open(fn,O_RDONLY,0644))==-1)
+        return __LINE__;
+    if(flock(fd,LOCK_SH)==-1)
+        return __LINE__;
+    ptr=mmap(NULL,st.st_size,PROT_READ,MAP_SHARED,fd,0);
+    flock(fd,LOCK_UN);
+    close(fd);
+    if(ptr==MAP_FAILED)
+        return __LINE__;
+    setbdir(DIR_MODE_DIGEST,fn,board);
+    if((fd=open(fn,O_WRONLY|O_CREAT|O_TRUNC,0644))==-1){
+        munmap(ptr,st.st_size);
+        return __LINE__;
+    }
+    if(flock(fd,LOCK_EX)==-1){
+        close(fd);
+        munmap(ptr,st.st_size);
+        return __LINE__;
+    }
+    for(count=st.st_size/sizeof(struct fileheader),
+        fptr=(struct fileheader*)ptr,j=0,i=0;i<count;i++){
+        if((fptr[i].accessed[0])&FILE_DIGEST){
+            memcpy(&fh,&fptr[i],sizeof(struct fileheader));
+            fh.accessed[0]&=~FILE_DIGEST;
+            if(fh.filename[1]=='/')
+                fh.filename[2]='G';
+            else
+                fh.filename[0]='G';
+            setbfile(fn,board,fptr[i].filename);
+            setbfile(buf,board,fh.filename);
+            if(unlink(buf)==-1||link(fn,buf)==-1)
+                continue;
+            data_write(fd,&fh,sizeof(struct fileheader));
+            j++;
+        }
+    }
+    flock(fd,LOCK_UN);
+    close(fd);
+    munmap(ptr,st.st_size);
+    printf("Rebuild: %-32.32s <%d>!\n",board,j);
+    return 0;
+}
