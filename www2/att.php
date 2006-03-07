a0170f0cba55bdcb974a60f0287dbea4c17d8ab5
atppp <atppp@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2006-03-07 12:29:27 +0000

    pig
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@9051 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- www2/att.php
+++ www2/att.php
@@ -1,5 +1,11 @@
 <?php
 	require("www2-funcs.php");
+	
+	function go_die() {
+		header("Location: images/pig.jpg");
+		die;
+	}
+	
 	login_init(FALSE, TRUE);
 	$query = $_SERVER["QUERY_STRING"];
 	settype($query, "string");
@@ -14,45 +20,45 @@
 		$num = intval($av[4]);
 		$ap = intval($av[5]);
 	} else {
-		die;
+		go_die();
 	}
-	if ($ap <= 0) die;
+	if ($ap <= 0) go_die();
 	$bid = intval($av[1]);
 	$id = intval($av[2]);
 	$brdarr = array();
 	$isnormalboard = bbs_safe_getboard($bid, "", $brdarr);
 	if (is_null($isnormalboard)) {
-		die;
+		go_die();
 	}
 	$board = $brdarr["NAME"];
 	$brdnum = $brdarr["BID"];
 
 	$dir_perm = bbs_is_permit_mode($ftype, 1);
 	if (!$dir_perm) {
-		die;
+		go_die();
 	}
 
 	if($ftype == $dir_modes["DELETED"]) {
 		$usernum = $currentuser["index"];
-		if (!$usernum) die;
-		if (!bbs_is_bm($brdnum, $usernum)) die;
+		if (!$usernum) go_die();
+		if (!bbs_is_bm($brdnum, $usernum)) go_die();
 	}
 	
 	$total = bbs_countarticles($brdnum, $ftype);
 	if ($total <= 0) {
-		die;
+		go_die();
 	}
 
 	$articles = array ();
     if ($dir_perm == 1) { //sorted
         $articles = array ();
         $num = bbs_get_records_from_id($board, $id, $ftype, $articles);
-        if ($num <= 0) die;
+        if ($num <= 0) go_die();
         $article = $articles[1];
     } else {
-        if (($num <= 0) || ($num > $total)) die;
-        if (($articles = bbs_getarticles($board, $num, 1, $ftype)) === false) die;
-        if ($id != $articles[0]["ID"]) die;
+        if (($num <= 0) || ($num > $total)) go_die();
+        if (($articles = bbs_getarticles($board, $num, 1, $ftype)) === false) go_die();
+        if ($id != $articles[0]["ID"]) go_die();
         $article = $articles[0];
     }
 
