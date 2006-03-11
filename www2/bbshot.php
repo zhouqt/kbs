1461a05e88a637810a9deead708efd904a1da5fe
atppp <atppp@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2006-03-11 09:42:43 +0000

    先这样再说
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@9079 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- www2/bbshot.php
+++ www2/bbshot.php
@@ -50,8 +50,9 @@ function bbs_get_hot_threads($board,$num,&$threads,&$err)
 		return false;
 	}
 	$board = $brdarr['NAME'];
-	$now = date('YmdHis');
-	$sql = 'SELECT threadid,userid,title,time AS created,MAX(time) AS changed,count(DISTINCT userid) AS count FROM postlog WHERE YEAR(time)=YEAR('.$now.') AND MONTH(time)=MONTH('.$now.') AND DAYOFMONTH(time)=DAYOFMONTH('.$now.') AND bname = \''.addslashes($board).'\' GROUP BY threadid ORDER BY count DESC , id DESC LIMIT 0 , '.(intval($num)*2).';';
+	$tt = date('Ymd')."000000";
+	$sql = 'SELECT threadid,userid,title,time AS created,MAX(time) AS changed,count(DISTINCT userid) AS count FROM postlog WHERE time>='.$tt.
+	       ' AND bname = \''.addslashes($board).'\' GROUP BY threadid ORDER BY count DESC , id DESC LIMIT 0 , '.(intval($num)*2).';';
 	if (!$db->query($sql,1)) {
 		$err = $db->err;
 		return false;    
