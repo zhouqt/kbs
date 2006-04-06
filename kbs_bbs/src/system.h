d7a2a5371cf04bec7957bccb370239b0c9e1208a
etnlegend <etnlegend@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2006-04-06 09:29:10 +0000

    这堆东西又不下崽, 留着干嘛...
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@9176 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/src/system.h
+++ kbs_bbs/src/system.h
@@ -23,7 +23,6 @@ char* strsep(char **strptr,const char *delim);
 char *strcasestr(const char *haystack, const char *needle);
 #endif
 
-
 #define READ_BUFFER_SIZE 8192
 
 #ifndef HAVE_FLOCK
@@ -52,31 +51,10 @@ int f_rm(const char *path);
 int f_touch(const char *file);
 void uuencode(const char* inbuf,int size,const char *filename,OUTC_FUNC fn);
 
-
 char* bm_strcasestr_rp(const char *string,const char *pattern,size_t *shift,int *init);    /* 字符串多次大小写不敏感匹配函数*/
-#if 0 //atppp 20060404
-char* bm_strstr(const char *string,const char *pattern);                                    /* 字符串匹配函数*/
-char* bm_strstr_rp(const char *string,const char *pattern,size_t *shift,int *init);        /* 字符串多次匹配函数*/
-char* bm_strcasestr(const char *string,const char *pattern);                                /* 字符串大小写不敏感的匹配函数*/
-void *memfind(const void *in_block,         /* 数据块 */
-              const size_t block_size,      /* 数据块长度 */
-              const void *in_pattern,       /* 需要查找的数据 */
-              const size_t pattern_size,    /* 查找数据的长度 */
-              size_t *shift,                /* 移位表，应该是256*size_t的数组 */
-              int *init);                  /* 是否需要初始化移位表 */
-void *txtfind(const void *in_block,         /* 数据块 */
-              const size_t block_size,      /* 数据块长度 */
-              const void *in_pattern,       /* 需要查找的数据 */
-              const size_t pattern_size,    /* 查找数据的长度 */
-              size_t *shift,                /* 移位表，应该是256*size_t的数组 */
-              int *init);                  /* 是否需要初始化移位表 */
-#endif
-
 
 int from64tobits(char *out,const char *in); /* Base64 to Big-Endian, 返回字节记数 */
 void to64frombits(unsigned char *out,const unsigned char *in,int inlen);
-
-
 
 #define read_lock(fd,offset,whence,len)\
 	lock_reg(fd,F_SETLK,F_RDLCK,offset,whence,len)
