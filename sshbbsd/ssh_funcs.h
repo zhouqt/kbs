507a112a228780df3f62d689c93c29626cf9dc1b
etnlegend <etnlegend@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2006-04-11 08:28:17 +0000

    加一个...
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@9293 de4889a5-8ad4-4bf3-bd87-57997c21d515

---
+++ sshbbsd/ssh_funcs.h
@@ -0,0 +1,21 @@
+#ifndef SSH_FUNCS_H_INCLUDED
+#define SSH_FUNCS_H_INCLUDED
+#ifdef SSHBBS
+
+#include <packet.h>
+
+unsigned long ssh_crc32(const unsigned char *buf,unsigned int len);
+int ssh_write(int fd,const void *buf,size_t count);
+int ssh_read(int fd,void *buf,size_t count);
+int ssh_select(int n,fd_set *readfds,fd_set *writefds,fd_set *exceptfds,struct timeval *timeout);
+int ssh_init(void);
+int bbs_entry(void);
+#ifndef HAVE_IPV6_SMTH
+int check_IP_lists(unsigned int IP2);
+#else /* HAVE_IPV6_SMTH */
+int check_IP_lists(struct in6_addr sip);
+#endif /* HAVE_IPV6_SMTH */
+
+
+#endif /* SSHBBS */
+#endif /* SSH_FUNCS_H_INCLUDED */
