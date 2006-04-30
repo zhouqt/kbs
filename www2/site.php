14b4a4de67ceeedfb201d5845b28a25649d97b8f
pig2532 <pig2532@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2006-04-30 02:37:11 +0000

    默认让有吧 不然总会忘记
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@9434 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- www2/site.php
+++ www2/site.php
@@ -7,6 +7,7 @@ define("QUOTED_LINES", BBS_QUOTED_LINES);             //web 回文保留的引文行数
 define("SITE_NEWSMTH", 1);
 define("RUNNINGTIME", 1);                             //底部显示页面运行时间
 define("AUTO_BMP2PNG_THRESHOLD", 100000); // requires ImageMagick and safe_mode off
+define("BBS_HAVE_BLOG", 1);
 
 // web 前端有 squid 或者 apache 的 mod_proxy 等代理的时候请打开这个选项
 //define("CHECK_X_FORWARDED_FOR", 1);
