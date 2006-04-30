931881afb323547bf14800fa335edcf4a2488471
etnlegend <etnlegend@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2006-04-30 19:46:34 +0000

    群信进度... 默认至少 1000, 每 5% ...
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@9442 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/local_utl/mails.c
+++ kbs_bbs/local_utl/mails.c
@@ -14,6 +14,7 @@
  *   -m <mode>      设定发送模式, 有效设定值为 `copy` 和 `link`, `copy` 表示以复制模式发送信件, 
  *                  `link` 表示以链接模式发送信件, 默认模式为 `link`
  *   -s <name>      设定信件的发送者为 <name>, 默认为 `SYSOP`, 发送者的最大有效长度为 [IDLEN + 1] 字节
+ *   -v             显示进度信息, 仅当未启用用户检测并且目标用户多于 [MCV_THRESHOLD] 时有效
  *   -h             显示文档信息
  *
 */
@@ -28,7 +29,10 @@
 #define MC_LIST 0x04                            /* 列表检测标签 */
 #define MC_MODE 0x08                            /* 发送模式标签 */
 #define MC_NAME 0x10                            /* 发送者设定标签 */
+#define MC_VERB 0x20                            /* 显示进度信息 */
 #define MCP_AND_FLAG 0x80000000                 /* 权限检测设定为逻辑与时的标签 */
+#define MCV_SEPS 20                             /* 进度显示更新精度 */
+#define MCV_THRESHOLD 1000                      /* 进度显示目标用户门槛 */
 #define MFR_DELIM " \t\n\r,;:"                  /* 字符串分析间断符 */
 
 static const char* const perm_str=XPERMSTR;     /* 权限符号列表 */
@@ -39,9 +43,11 @@ static unsigned int perm;                       /* 设定权限检测时的被检测权限 */
 static char user[IDLEN+1];                      /* 设定用户检测时的用户名 */
 static char list[MP_LEN];                       /* 设定列表检测时的列表文件路径 */
 static char name[IDLEN+2];                      /* 设定发送者时的发送者名称*/
+static int count;                               /* 进度显示当前记数 */
+static int total;                               /* 进度显示总记数 */
 
 /* 打印文档信息 */
-static int m_usage(void){
+static inline int m_usage(void){
     fprintf(stdout,
         "  命令行:          mails [options] <m_title> <m_path>\n"
         "  参数表:\n"
@@ -56,31 +62,59 @@ static int m_usage(void){
         "    -m <mode>      设定发送模式, 有效设定值为 `copy` 和 `link`, `copy` 表示以复制模式发送信件, \n"
         "                   `link` 表示以链接模式发送信件, 默认模式为 `link`\n"
         "    -s <name>      设定信件的发送者为 <name>, 默认为 `SYSOP`, 发送者的最大有效长度为 [%d] 字节\n"
+        "    -v             显示进度信息, 仅当未启用用户检测并且目标用户多于 [%d] 时有效\n"
         "    -h             显示文档信息\n",
-        MT_LEN,BBSHOME,(IDLEN+1));
+        MT_LEN,BBSHOME,(IDLEN+1),MCV_THRESHOLD);
     return 0;
 }
 
 /* 处理权限检测 */
-static int m_check_perm(const struct userec *user){
+static inline int m_check_perm(const struct userec *user){
     return (!(flag&MC_PERM)?1:(!(perm&MCP_AND_FLAG)?(user->userlevel&perm):(((user->userlevel|MCP_AND_FLAG)&perm)==perm)));
 }
 
 /* 处理发送模式 */
-static int m_check_mode(void){
+static inline int m_check_mode(void){
     return (!(flag&MC_MODE)?BBSPOST_LINK:BBSPOST_COPY);
 }
 
 /* 处理发送者 */
-static int m_set_name(void){
+static inline int m_set_name(void){
     if(!(flag&MC_NAME))
         snprintf(name,IDLEN+2,"%s","SYSOP");
     return 0;
 }
 
+/* 处理记数 */
+static inline int m_count(struct userec *user,void *arg){
+    if(m_check_perm(user))
+        total++;
+    return 0;
+}
+
+/* 处理进度 */
+static inline int m_verb(void){
+    if((flag&MC_VERB)&&!(total<MCV_THRESHOLD)){
+#define M_VERB_OUT(t,n) do{fprintf(stdout,(t),(n));fflush(stdout);return 0;}while(0)
+        if(!count)
+            M_VERB_OUT("%d%%",0);
+        if(count==(total-1))
+            M_VERB_OUT("...%d%%\n",100);
+        if(!(count%(total/MCV_SEPS)))
+            M_VERB_OUT("...%d%%",((100*count)/total));
+#undef M_VERB_OUT
+    }
+    return 0;
+}
+
 /* 处理信件发送 */
-static int m_send(struct userec *user,void *arg){
-    return (!m_check_perm(user)?0:mail_file(name,path,user->userid,title,m_check_mode(),NULL));
+static inline int m_send(struct userec *user,void *arg){
+    if(m_check_perm(user)){
+        mail_file(name,path,user->userid,title,m_check_mode(),NULL);
+        m_verb();
+        count++;
+    }
+    return 0;
 }
 
 int main(int argc,char **argv){
@@ -93,7 +127,7 @@ int main(int argc,char **argv){
     if(!getcwd(cwd,MP_LEN))
         ME_QUIT("get current working directory ...");
     opterr=0;
-    while((ret=getopt(argc,argv,"p:u:l:m:s:h"))!=-1){
+    while((ret=getopt(argc,argv,"p:u:l:m:s:vh"))!=-1){
         switch(ret){
             case 'p':                           /* 权限检测 */
                 flag|=MC_PERM;
@@ -123,6 +157,9 @@ int main(int argc,char **argv){
             case 's':                           /* 设定发送者 */
                 flag|=MC_NAME;
                 snprintf(name,IDLEN+2,"%s",optarg);
+                break;
+            case 'v':
+                flag|=MC_VERB;
                 break;
             case 'h':
                 m_usage();
@@ -169,8 +206,11 @@ int main(int argc,char **argv){
                     m_send(p_user,NULL);
         fclose(fp);
     }
-    else
+    else{
+        if(flag&MC_VERB)
+            apply_users(m_count,NULL);
         apply_users(m_send,NULL);
+    }
     return 0;
 #undef ME_QUIT
 }
