0c81a9d33419e8671bdcf28eef699c4ecf9177dd
pig2532 <pig2532@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2006-09-15 12:48:38 +0000

    read reg list
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@9583 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/bbs2www/phplib/phpbbs.admin.c
+++ kbs_bbs/bbs2www/phplib/phpbbs.admin.c
@@ -1,2 +1,51 @@
 #include "php_kbs_bbs.h"
 
+/* kbs admin function for web
+ * pig2532
+ */
+
+
+/* bbs_admin_getnewreg(array reglist);
+ */
+PHP_FUNCTION(bbs_admin_getnewreg) {
+
+    int ac;
+    long i;
+    zval *reglist, *element;
+    FILE *fp;
+    char buf[256], *p;
+
+    ac = ZEND_NUM_ARGS();
+    if(ac != 1 || zend_parse_parameters(1 TSRMLS_CC, "a", &reglist) == FAILURE) {
+        WRONG_PARAM_COUNT;
+    }
+    if(array_init(reglist) != SUCCESS) {
+        RETURN_LONG(-1);
+    }
+
+    if((fp = fopen("new_register", "r")) == NULL) {
+        RETURN_LONG(0);
+    }
+    MAKE_STD_ZVAL(element);
+    array_init(element);
+    i = 0;
+    while(fgets(buf, 256, fp) != NULL) {
+        if(strncmp(buf, "----", 4) == 0) {
+            zend_hash_index_update(Z_ARRVAL_P(reglist), i, (void*)&element, sizeof(zval*), NULL);
+            i++;
+            MAKE_STD_ZVAL(element);
+            array_init(element);
+        }
+        else {
+            p = strchr(buf, ':');
+            if(p == NULL)
+                continue;
+            *p = '\0';
+            add_assoc_string(element, buf, p + 1, 1);
+        }
+    }
+    fclose(fp);
+    RETURN_LONG(i);
+}
+
+
--- kbs_bbs/bbs2www/phplib/phpbbs.admin.h
+++ kbs_bbs/bbs2www/phplib/phpbbs.admin.h
@@ -2,6 +2,12 @@
 #define PHP_BBS_ADMIN_H
 
 
+PHP_FUNCTION(bbs_admin_getnewreg);
+
+
+#define PHP_BBS_ADMIN_EXPORT_FUNCTIONS \
+    PHP_FE(bbs_admin_getnewreg, NULL)
+
 
 #endif /* PHP_BBS_ADMIN_H */
 
