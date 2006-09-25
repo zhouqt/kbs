352ff148b2a6aa03277be80652b9cc2768f6a491
pig2532 <pig2532@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2006-09-25 07:25:15 +0000

    admin menu page
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@9623 de4889a5-8ad4-4bf3-bd87-57997c21d515

---
+++ www2/admindex.php
@@ -0,0 +1,19 @@
+<?php
+    require("www2-admin.php");
+   
+    $adminmenu = array(
+        "reg" => "设定使用者注册资料",
+        "info" => "修改用户数据",
+        "perm" => "更改使用者权限",
+    );
+   
+    admin_header("管理菜单", "系统管理功能表");
+    $count = 0;
+    foreach($adminperm as $key => $value) {
+        if($currentuser["userlevel"] & $adminperm[$key]) {
+            print("<a href=\"adm{$key}.php\">{$adminmenu[$key]}</a><br>");
+        }
+    }
+    print("<br>");
+    page_footer();
+?>
