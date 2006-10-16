3ceed0ec885d42cc8181b51d160520b3a45018ab
atppp <atppp@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2006-10-16 06:17:34 +0000

    orz
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@9688 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/local_utl/find_rm_lost.c
+++ kbs_bbs/local_utl/find_rm_lost.c
@@ -91,7 +91,7 @@ testPOWERJUNK(char *path, char *fn)
 	char buf[1024];
 	struct stat st;
 	sprintf(buf, "%s/%s", path, fn);
-	if (strncmp(fn, ".AUTHOR", 7) && strncmp(fn, ".TITLE", 6) && strncmp(fn, ".Search", 7))
+	if (strncmp(fn, ".AUTHOR", 7) && strncmp(fn, ".TITLE", 6) && strncmp(fn, ".Search", 7) && strncmp(fn, ".SELF.", 6))
 		return 0;
 	if (stat(buf,&st)!=0)
 		return 0;
