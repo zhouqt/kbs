6798eaaae48d4fa8c89676d056a3f471b3b6c05c
atppp <atppp@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2006-10-24 08:01:16 +0000

    有人以为改成false就可以关掉www2dev
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@9752 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- www2/site.php
+++ www2/site.php
@@ -1,5 +1,5 @@
 <?php
-define("www2dev", true);
+define("www2dev", "DEFINED");
 define("ATTACHMAXSIZE",BBS_MAXATTACHMENTSIZE);        //附件总字节数的上限，单位 bytes
 define("ATTACHMAXCOUNT",BBS_MAXATTACHMENTCOUNT);      //附件数目的上限
 define("MAINPAGE_FILE","mainpage.php");              //首页导读的 URL
