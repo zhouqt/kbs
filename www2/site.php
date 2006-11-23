1f99e6890e22cc67e16b6289685d5e356636ddfb
atppp <atppp@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2006-11-23 10:17:32 +0000

    这个不要了。。。
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@9854 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- www2/site.php
+++ www2/site.php
@@ -3,7 +3,6 @@
 define("ATTACHMAXSIZE",BBS_MAXATTACHMENTSIZE);        //附件总字节数的上限，单位 bytes
 define("ATTACHMAXCOUNT",BBS_MAXATTACHMENTCOUNT);      //附件数目的上限
 define("MAINPAGE_FILE","mainpage.php");              //首页导读的 URL
-define("SITE_NEWSMTH", 1);
 define("RUNNINGTIME", 1);                             //底部显示页面运行时间
 define("AUTO_BMP2PNG_THRESHOLD", 100000); // requires ImageMagick and safe_mode off
 define("HAVE_PC", 1);
