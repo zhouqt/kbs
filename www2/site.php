c28617c2d3816f1c5b5d33b296a31ff3ef725bc7
atppp <atppp@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2006-11-23 10:21:26 +0000

    这个现在也不用了吧
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@9856 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- www2/site.php
+++ www2/site.php
@@ -6,9 +6,6 @@ define("MAINPAGE_FILE","mainpage.php");              //首页导读的 URL
 define("RUNNINGTIME", 1);                             //底部显示页面运行时间
 define("AUTO_BMP2PNG_THRESHOLD", 100000); // requires ImageMagick and safe_mode off
 
-if (!defined("BBS_HAVE_BLOG"))
-	define("BBS_HAVE_BLOG", 1); // pig2532 ymsw
-
 // web 前端有 squid 或者 apache 的 mod_proxy 等代理的时候请打开这个选项
 //define("CHECK_X_FORWARDED_FOR", 1);
 
