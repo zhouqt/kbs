8bbc0c10aa7eda0511df5e95d594297a0b4b5fdd
atppp <atppp@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2007-02-08 09:23:51 +0000

    编码错...
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@9907 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- www2/www2-funcs.php
+++ www2/www2-funcs.php
@@ -456,7 +456,7 @@ function assert_login() {
 	global $currentuser;
 	if(!strcmp($currentuser["userid"],"guest")) {
 		$str = "您还没有登录，或者长时间没有动作，请您重新登录。<br /><br />";
-		$str .= "<form action=\"bbslogin.php?mainurl={$_SERVER["REQUEST_URI"]}\" method=\"post\" target=\"_parent\">";
+		$str .= "<form action=\"bbslogin.php?mainurl=".urlencode($_SERVER["REQUEST_URI"])."\" method=\"post\" target=\"_parent\">";
 		$str .= "用户名：<input type=\"text\" name=\"id\" size=\"15\" /><br />";
 		$str .= "密　码：<input type=\"password\" name=\"passwd\" maxlength=\"39\" size=\"15\" /><br /><br />";
 		$str .= "<input type=\"submit\" value=\"登录\" />&nbsp;<input type=\"button\" value=\"注册\" onclick=\"javascript:top.location.href='bbsreg0.html';\" />";
