d2bcd0488c17fc75e00a2e5585dad13d5dfccabf
pig2532 <pig2532@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2007-05-11 12:45:14 +0000

    refresh reply count
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@9925 de4889a5-8ad4-4bf3-bd87-57997c21d515

---
+++ kbs_bbs/local_utl/refresh_reply.c
@@ -0,0 +1,101 @@
+
+/* count numbers of replys for every topics */
+
+#include "bbs.h"
+
+int refresh_board(char* bname) {
+	int fd, fd2, origincount, acount, i, j, replycount;
+	char dirpath[256], originpath[256];
+	char *head, *ahead;
+	struct fileheader *ptr, *aptr;
+	struct stat buf, abuf;
+
+	setbdir(DIR_MODE_NORMAL, dirpath, bname);
+	setbdir(DIR_MODE_ORIGIN, originpath, bname);
+	
+	fd = open(originpath, O_RDWR, 0644);
+	if(fd == -1) {
+		printf("cannot open %s.\n", originpath);
+		return 0;
+	}
+	fd2 = open(dirpath, O_RDWR, 0644);
+	if(fd2 == -1) {
+		close(fd);
+		printf("cannot open %s.\n", dirpath);
+		return 0;
+	}
+
+	BBS_TRY {
+		if(!safe_mmapfile_handle(fd, PROT_READ | PROT_WRITE, MAP_SHARED, &head, &buf.st_size)) {
+			printf("cannot mmap %s.\n", originpath);
+			close(fd);
+			return 0;
+		}
+		if(!safe_mmapfile_handle(fd2, PROT_READ, MAP_SHARED, &ahead, &abuf.st_size)) {
+			printf("cannot mmap %s.\n", dirpath);
+			close(fd);
+			close(fd2);
+			return 0;
+		}
+		origincount = buf.st_size / sizeof(struct fileheader);
+		acount = abuf.st_size / sizeof(struct fileheader);
+		ptr = (struct fileheader *)head;
+		for(i=0; i<origincount; i++) {
+			replycount = 0;
+			aptr = (struct fileheader *)ahead;
+			for(j=0; j<acount; j++) {
+				if(aptr->groupid == ptr->id) {
+					replycount++;
+				}
+				aptr++;
+			}
+			ptr->replycount = replycount;
+			ptr++;
+		}
+	}
+	BBS_CATCH {
+	}
+	BBS_END;
+	
+	end_mmapfile((void *)head, buf.st_size, -1);
+	end_mmapfile((void *)ahead, abuf.st_size, -1);
+	close(fd);
+	close(fd2);
+
+	printf("board %s is done.\n", bname);
+
+	return 0;
+}
+
+static int do_board(struct boardheader* bh, void* arg) {
+	refresh_board(bh->filename);
+	return 0;
+}
+
+int main(int argc, char** argv) {
+	int doall;
+	
+	doall = 0;
+	if(argc == 1) {
+		doall = 1;
+	}
+	else if(argc == 2) {
+		if(strcmp(argv[1], "--help") == 0) {
+			printf("usage:   %s :  refresh reply count for all boards.\n", argv[0]);
+			printf("         %s boardname :  refresh one board.\n", argv[0]);
+			return 0;
+		}
+	}
+	
+	chdir(BBSHOME);
+	resolve_boards();
+	
+	if(doall == 0) {
+		refresh_board(argv[1]);
+	}
+	else {
+		apply_boards(do_board, NULL);
+	}
+
+	return 0;
+}
