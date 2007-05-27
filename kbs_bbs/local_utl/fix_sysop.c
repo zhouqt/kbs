a3560b830f644cfa2ddce795407d1300ee73fcba
pig2532 <pig2532@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2007-05-27 06:16:50 +0000

    foxban: SYSOP's perm & pwd fixer
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@9948 de4889a5-8ad4-4bf3-bd87-57997c21d515

---
+++ kbs_bbs/local_utl/fix_sysop.c
@@ -0,0 +1,45 @@
+#include "bbs.h"
+
+void p_help();
+
+int main(int argc, char*argv[])
+{
+	struct userec uinfo;
+	int unum,ret;
+	struct userec *lookupuser;
+	char buf[80];
+	char * passwd;
+	
+	chdir(BBSHOME);
+	resolve_boards();
+	load_ucache();
+
+	ret=getopt(argc,argv,"hp");
+	if(ret=='h') {
+		p_help();
+		return 0;
+	}
+	strcpy(buf,"SYSOP");
+	unum = getuser(buf, &lookupuser);
+	uinfo = *lookupuser;
+	if(ret=='p') {
+		passwd = getpass("Input a password:");
+		setpasswd(passwd,&uinfo);
+		printf("SYSOP'S Password restored.\n");
+	}
+	if((ret==-1 && argc<2) || ret=='p'){
+		uinfo.userlevel=0xDAFFDFFF;//»Ö¸´SYSOPÄ¬ÈÏÈ¨ÏÞ
+		printf("SYSOP'S PERM restored.\n");
+		update_user(&uinfo, unum, 1);
+	}else if(ret==-1 && argc>1){
+		p_help();
+		return 0;
+	}
+	return 0;
+}
+
+void p_help()
+{
+	printf("SYSOP Password and PERM restore tool.\n\nusage:fix_sysop [option]\n-h	Print this message.\n-p	restore PERM and Passwd.\n Default action is PERM restore.\n");
+        return;
+}
