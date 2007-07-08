b382b804abc026a2e8a458b945d9f43691e6b63a
pig2532 <pig2532@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2007-07-08 11:30:03 +0000

    判断用户是否存在
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@10014 de4889a5-8ad4-4bf3-bd87-57997c21d515

---
+++ kbs_bbs/local_utl/userexist.c
@@ -0,0 +1,19 @@
+
+#include "bbs.h"
+
+int main(int argc, char* argv[]) {
+	
+	int mode;
+    bool dodel;
+    struct userec user;
+	
+	chdir(BBSHOME);
+	load_ucache();
+
+    if(getuser(argv[1], &user))
+        return 1;
+    else
+  	    return 19;
+}
+
+
