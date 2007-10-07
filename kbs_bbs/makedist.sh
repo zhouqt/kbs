80e2b3752f73e4adda71d1664f5cf0f702f7a2d9
atppp <atppp@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2007-10-07 07:15:14 +0000

    empty dir
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@10048 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/makedist.sh
+++ kbs_bbs/makedist.sh
@@ -36,9 +36,12 @@ md $1/boards
 md $1/boards/_attach
 md $1/cache
 md $1/etc/posts
+md $1/etc/notepad_tmp
 md $1/game
 md $1/game/pipgame
 md $1/game/worker
+md $1/log
+md $1/reclog
 md $1/tmp
 md $1/tmp/forward
 md $1/tmp/bbs-gsend
