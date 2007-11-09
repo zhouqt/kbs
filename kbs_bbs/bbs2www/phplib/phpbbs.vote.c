320bd65d50aa05cf239996ebd31d70ce47dbe136
fancy <fancy@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2007-11-09 18:12:46 +0000

    bugfixes ...
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@10090 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/bbs2www/phplib/phpbbs.vote.c
+++ kbs_bbs/bbs2www/phplib/phpbbs.vote.c
@@ -440,11 +440,10 @@ PHP_FUNCTION(bbs_start_vote)
 		sprintf(buff,"[通知] %s 举办投票: %s",bp->filename,ball.title);
 		fprintf(fp,"%s",buff);
 		fclose(fp);
-		if( !normal_board(bp->filename) ){
-			post_file(getCurrentUser(), "", buf, bp->filename, buff, 0,1, getSession());
-		}else{
+		if( normal_board(bp->filename) ){
 			post_file(getCurrentUser(), "", buf, "vote", buff, 0,1, getSession());
 		}
+		post_file(getCurrentUser(), "", buf, bp->filename, buff, 0,1, getSession());
 		unlink(buf);
 	}
 
