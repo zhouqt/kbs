883fcd5ec326677ff2811ac495df2244fed885c6
atppp <atppp@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2008-01-21 22:44:24 +0000

    no need
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@10182 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/autogen.sh
+++ kbs_bbs/autogen.sh
@@ -2,6 +2,5 @@
 
 # quick & dirty fix for CVS. need better solution. 20051004 atppp
 mkdir -p admin; touch admin/Makefile.in;
-mkdir -p innbbsd; touch innbbsd/Makefile.in; touch innbbsd/linkinnd.sh.in;
 
 aclocal; libtoolize -c --force; autoheader; automake -a; autoconf
