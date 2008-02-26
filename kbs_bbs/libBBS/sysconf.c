9a52cd0b04c95d39f168cd600350f04fa2cc5908
fancy <fancy@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2008-02-26 09:53:13 +0000

    ft, mistake ...
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@10232 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/libBBS/sysconf.c
+++ kbs_bbs/libBBS/sysconf.c
@@ -105,7 +105,7 @@ static void sysconf_addmenu(FILE * fp, char *key)
 {
     struct smenuitem *pm;
     char buf[256];
-    char *cmd, *arg[5], *ptr, *q;
+    char *cmd, *arg[5], *ptr;
     int n;
 
     /*
@@ -113,15 +113,14 @@ static void sysconf_addmenu(FILE * fp, char *key)
      */
     sysconf_addkey(key, "menu", sysconf_menu);
     while (fgets(buf, sizeof(buf), fp) != NULL && buf[0] != '%') {
-        q = buf;
-        cmd = strsep(&q, " \t\n");
+        cmd = strtok(buf, " \t\n");
         if (cmd == NULL || *cmd == '#') {
             continue;
         }
         arg[0] = arg[1] = arg[2] = arg[3] = arg[4] = "";
         n = 0;
         for (n = 0; n < 5; n++) {
-            if (!(ptr = strsep(&q, ",\n")))
+            if ((ptr = strtok(NULL, ",\n")) == NULL)
                 break;
             while (*ptr == ' ' || *ptr == '\t')
                 ptr++;
@@ -197,7 +196,7 @@ static void parse_sysconf(const char *fname)
 {
     FILE *fp;
     char buf[256];
-    char tmp[256], *ptr, *q;
+    char tmp[256], *ptr;
     char *key, *str;
     int val;
 
@@ -209,11 +208,11 @@ static void parse_sysconf(const char *fname)
         ptr = buf;
         while (*ptr == ' ' || *ptr == '\t')
             ptr++;
-        q = ptr;
+
         if (*ptr == '%') {
-            strsep(&q, " \t\n");
+            strtok(ptr, " \t\n");
             if (strcmp(ptr, "%menu" /*菜单 */ ) == 0) {
-                str = strsep(&q, " \t\n");
+                str = strtok(NULL, " \t\n");
                 if (str != NULL)
                     sysconf_addmenu(fp, str);
             } else {            /*其它，如screen设计 */
