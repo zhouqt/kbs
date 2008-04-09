2a1720f3403e7c56c7175497c64e4ecde8e89b06
pig2532 <pig2532@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2008-04-09 06:11:36 +0000

    forget this
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@10297 de4889a5-8ad4-4bf3-bd87-57997c21d515

---
+++ kbs_bbs/src/libauth.h
@@ -0,0 +1,20 @@
+
+#ifndef _LIBAUTH_H_
+#define _LIBAUTH_H_
+
+struct bbsauth {
+	char userid[16];
+	char perm[33];
+};
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+	// return: uid
+int bbs_auth(struct bbsauth *a, const char *userid, const char *passwd, char *fromip);
+#ifdef __cplusplus
+}
+#endif
+
+#endif
