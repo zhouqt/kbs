a4378c5160ae23b0bbd31f2580bc8d8f50c0a152
pig2532 <pig2532@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2008-04-23 15:46:06 +0000

    second site macro
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@10337 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/php/bbslib.c
+++ kbs_bbs/php/bbslib.c
@@ -408,7 +408,9 @@ int www_user_init(int useridx, char *userid, int key, struct userec **x, struct 
 
         strncpy(getSession()->fromhost, (*y)->from, IPLEN);
         getSession()->fromhost[IPLEN] = '\0';
+#ifdef SECONDSITE
         getSession()->anonyindex = ((*y)->utmpkey + (*y)->logintime ) % 65536;
+#endif
     } else {
         /*
          * guest用户处理 
