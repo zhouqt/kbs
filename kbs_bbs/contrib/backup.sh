1c4cd0dde88898d92e939bb42a17135db4715734
kcn <kcn@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2008-06-03 10:28:09 +0000

    backup script
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@10383 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/contrib/backup.sh
+++ kbs_bbs/contrib/backup.sh
@@ -1,36 +1,75 @@
 #!/bin/sh
-DAY=`date +%Y-%m-%d`
-BACKUPHOME=/backup/bbs/Announce/$DAY
-BBSHOME=/home/bbs
 
-if [ ! -d "$BACKUPHOME" ]; then
-	mkdir -p $BACKUPHOME
-	cd $BBSHOME/0Announce/groups
-	for i in `for i in $(grep Path .Names); do echo ${i##*/}; done`; do
-		if [ -d "$i" ]; then
-			cd $i
-			for j in `for j in $(grep Path .Names); do echo ${j##*/}; done`; do
-				if [ -d "$j" ]; then
-					echo "Backup 0Announce/$i/$j..."
-					tar -zcf $BACKUPHOME/$j.tar.gz $j
-				fi
-			done
-			cd ..
-		fi
-	done
+#### backup mail home boards 0Announce
+#### set BBSHOME,BACKUPDIR to correct value before use it!
 
-	find ${BACKUPHOME%/*} -maxdepth 1 -mtime +30 | xargs rm -rf
+BACKUPCMD="tar zcf"
+LOGFILE="/tmp/backup$$.log"
+NOTICECMD="/home/bbs/bin/postfilealt.php"
+
+log() {
+  echo `date +"%Y%m%d %H:%M:%S"`:$1 > $LOGFILE
+}
+
+logerror() {
+  log $2
+  $NOTICECMD $LOGFILE SYSOP "backup $1 error" system_dev
+  cat $LOGFILE
+  rm $LOGFILE
+  exit
+}
+
+execcmd() {
+  log $1 >> $LOGFILE
+  $1 2>&1 >> $LOGFILE
+}
+
+rotatefile() {
+  if [ -f $1 ]; then
+    execcmd "mv -f $1 $1.bak"
+  fi
+}
+
+if [ "x$1" == "x" ] || ([ "$1" != "mail" ] && [ "$1" != "home" ] && [ "$1" != "boards" ] && [ "$1" != "0Announce" ]); then
+  logerror "" "usage:$0 mail|home|boards|0Announce"
 fi
 
-cd $BACKUPHOME
-ftp -n <<end
-open $1 $2
-quote user $3
-quote pass $4
-mkdir $DAY
-cd $DAY
-bin
-prompt
-mput *
-quit
-end
+
+if [ "x${BBSHOME}" == "x" ] || [ ! -d $BBSHOME ]; then
+  logerror $1 "BBSHOME no set or incorrect!"
+fi
+
+if [ "x${BACKUPDIR}" == "x" ] || [ ! -d $BACKUPDIR ]; then
+  logerror $1 "BACKUPDIR no set or incorrect!"
+fi
+
+log "backup $1 begin"
+if [ "$1" == "0Announce" ]; then
+  TODIR=$BACKUPDIR/0Announce/groups
+  SOURCEDIR=$BBSHOME/0Announce/groups
+  execcmd "mkdir -p $TODIR"
+  ## backup Announce without board's.
+  rotatefile $BACKUPDIR/0Announce/all_except_groups.tgz 
+  execcmd "$BACKUPCMD $BACKUPDIR/0Announce/all_except_groups.tgz $BBSHOME/0Announce --exclude $BBSHOME/0Announce/groups"
+else
+  TODIR=$BACKUPDIR/$1
+  SOURCEDIR=$BBSHOME/$1
+  execcmd "mkdir -p $TODIR"
+fi
+
+cd $SOURCEDIR
+DIRS=
+for i in *; do
+  if [ -d $i ]; then
+    rotatefile $TODIR/$i.tgz
+    execcmd "$BACKUPCMD $TODIR/$i.tgz $i"
+    DIRS="$DIRS --exclude $SOURCEDIR/$i"
+  fi
+done
+rotatefile $TODIR/__other.tgz
+execcmd "$BACKUPCMD $TODIR/__other.tgz $SOURCEDIR $DIRS"
+log "backup $1 end" >> $LOGFILE
+
+cat $LOGFILE
+$NOTICECMD $LOGFILE SYSOP "backup $1 success" system_dev
+rm $LOGFILE
