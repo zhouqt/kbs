630d5b20563a54f4f9a1308e58cefbc9d7563998
fancy <fancy@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2009-02-03 16:35:05 +0000

    我怎么能这么弱智。。
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@10475 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/src/bbs.c
+++ kbs_bbs/src/bbs.c
@@ -2594,7 +2594,8 @@ int read_hot_info(struct _select_def* conf,struct fileheader *fileinfo,void* ext
                         char ans[4];
                         clear();
                         move(3, 0);
-                        prints("是否追回%s版治版方针 (Yes/No)? [N] ", ans, 3, DOECHO, NULL, true);
+			prints("%s版治版方针已经通过审核, 批号: %d\n", currboard->filename, st.st_mtime);
+                        getdata(t_lines - 1, 0, "是否追回%s版治版方针 (Yes/No)? [N] ", ans, 3, DOECHO, NULL, true);
                         if (ans[0] == 'y' || ans[0] == 'Y') {
                             int ret;
                             ret = set_board_rule(currboard, 3);
