844573c94a13e125201329e5f379307d01ee4704
fancy <fancy@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2010-03-26 14:39:09 +0000

    skybluee: update bootstrap. allow root to execute bootstrap
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@10636 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/local_utl/bootstrap.c
+++ kbs_bbs/local_utl/bootstrap.c
@@ -2,18 +2,48 @@
 #include "bbs.h"
 
 /* 版面属性，见doc/README.SYSOP 1.2 */
-enum board_t {
+enum boardtype {
     nil,
     V,
-    X,
-    o,
+    X,                  /* 写限制为PERM_SYSOP */
+    o,                  /* 读限制为PERM_BASIC */
 };
 
-/* ${BBSHOME} */
-/* fancy Aug 4 2009, WHY NOT USE BBSHOME MACRO??? */
-char bbshome[PATHLEN] = BBSHOME;
-/* SYSOP的密码 */
-char SYSOP_pass[PASSLEN];
+typedef struct board_s {
+    enum boardtype type;
+    char *name;
+} board_t;
+
+board_t boards[] = {
+    {V, "bbsnet"},
+    {V, "denypost"},
+    {V, "Filter"},
+    {V, "GiveupNotice"},
+    {V, "Goodbye"},
+    {V, "Recommend"},
+    {V, "Registry"},
+    {V, "reject_registry"},
+    {V, "syssecurity"},
+    {V, "undenypost"},
+    {V, "sysmail"},
+    {X, "BBSLists"},
+    {X, "newcomers"},
+    {X, "notepad"},
+    {X, "vote"},
+    {o, "Blessing"},
+    {nil, ""},
+};
+
+void usage(char *fmt, ...)
+{
+    va_list args;
+
+    va_start(args, fmt);
+    vfprintf(stdout, fmt, args);
+    fprintf(stdout, "\n");
+    fflush(stdout);
+    va_end(args);
+}
 
 void error(char *fmt, ...)
 {
@@ -27,56 +57,45 @@ void error(char *fmt, ...)
     exit(EXIT_FAILURE);
 }
 
-void end_bootstrap(void)
+void clean_ipc(void)
 {
     char cmd[PATHLEN];
+    struct passwd *pw;
 
-    strcpy(cmd, "killall bbslogd");
-    system(cmd);
-    strcpy(cmd, bbshome);
-    strcat(cmd, "/bin/miscd flush");
-    system(cmd);
-    strcpy(cmd, "killall miscd");
-    system(cmd);
+    system("killall bbslogd");
+    system(BBSHOME "/bin/miscd flush");
+    system("killall miscd");
 
     /* 清除共享内存 */
-    struct passwd *pw;
-    if (!(pw = getpwuid(BBSUID)))
-        error("%s", strerror(errno));
-    sprintf(cmd, "ipcs -m|awk '$0~/%s/{system(sprintf(\"ipcrm -m %%s\",$2));}'", pw->pw_name);
-    system(cmd);
-    sprintf(cmd, "ipcs -q|awk '$0~/%s/{system(sprintf(\"ipcrm -q %%s\",$2));}'", pw->pw_name);
-    system(cmd);
-}
-
-void sig_usr(int signo)
-{
-    if (signo == SIGINT || signo == SIGTERM) {
-        end_bootstrap();
-        error("receive signal: %d", signo);
+    pw = getpwuid(BBSUID);
+    if (pw) {
+        sprintf(cmd, "ipcs -m|awk '$0~/%s/{system(sprintf(\"ipcrm -m %%s\",$2));}'", pw->pw_name);
+        system(cmd);
+        sprintf(cmd, "ipcs -q|awk '$0~/%s/{system(sprintf(\"ipcrm -q %%s\",$2));}'", pw->pw_name);
+        system(cmd);
     }
 }
 
 /* 清空.PASSWDS文件和.BOARDS文件 */
-void cleanids(void)
+void clean_ids_boards(void)
 {
     char path[PATHLEN];
     int fd;
 
-    strcpy(path, bbshome);
-    strcat(path, "/.PASSWDS");
+    strcpy(path, BBSHOME "/.PASSWDS");
     if ((fd = open(path, O_WRONLY | O_CREAT | O_TRUNC, 0644)) == -1)
         error("clean the .PASSWDS error");
     close(fd);
-    strcpy(path, bbshome);
-    strcat(path, "/.BOARDS");
+    usage("file .PASSWDS is cleaned");
+    strcpy(path, BBSHOME "/.BOARDS");
     if ((fd = open(path, O_WRONLY | O_CREAT | O_TRUNC, 0644)) == -1)
         error("clean the .BOARDS error");
     close(fd);
+    usage("file .BOARDS is cleaned");
 }
 
 /* 创建以username为名，以passwd为密码的用户 */
-void do_makeid(char *username, char *passwd)
+void add_bbsid(char *username, char *passwd)
 {
     struct userec *newuser;
     int unum;
@@ -85,7 +104,7 @@ void do_makeid(char *username, char *passwd)
 
     unum = getnewuserid(username);
     if (unum > MAXUSERS || unum <= 0) {
-        end_bootstrap();
+        clean_ipc();
         error("%s: unum error", username);
     }
     strcpy(buf, username);
@@ -108,193 +127,168 @@ void do_makeid(char *username, char *passwd)
 
     update_user(newuser, unum, 1);
 
-    newbbslog(BBSLOG_USIES, "APPLY: %s uid %d from %s", 
+    newbbslog(BBSLOG_USIES, "APPLY: %s uid %d from %s",
             newuser->userid, unum, "127.0.0.1");
     bbslog("user", "%s", "new account");
+    usage("add user %s", username);
 
     sethomepath(path, newuser->userid);
-    sprintf(buf, "/bin/mv -f %s %s/homeback/%s", path, bbshome, newuser->userid);
+    sprintf(buf, "/bin/mv -f %s %s/homeback/%s", path, BBSHOME, newuser->userid);
     system(buf);
-    sprintf(buf, "/bin/mv -f %s %s/mailback/%s", path, bbshome, newuser->userid);
+    sprintf(buf, "/bin/mv -f %s %s/mailback/%s", path, BBSHOME, newuser->userid);
     system(buf);
-    mail_file(DELIVER, "etc/tonewuser", newuser->userid, 
-            "致新注册用户的信", 0, NULL);
+    if (strcmp(username, "guest"))      /* 不给 guest 发信 */
+        mail_file(DELIVER, "etc/tonewuser", newuser->userid, "致新注册用户的信", 0, NULL);
 }
 
-void makeids(void)
+int wrapped_mkdir(const char *path, mode_t mode)
 {
-    do_makeid("SYSOP", SYSOP_pass);
-    do_makeid("guest", "anypassword");
+    int ret = mkdir(path, mode);
+
+    if (ret == -1 && errno == EEXIST)
+        return 0;
+    return ret;
 }
 
-void do_makeboard(enum board_t type, char *name)
+void add_bbsboard(const board_t *pbt)
 {
     struct boardheader newboard;
     char vbuf[PATHLEN], buf[PATHLEN];
     int bid;
 
+    if (!pbt)
+        return;
     memset(&newboard, 0, sizeof(newboard));
-    strcpy(newboard.filename, name);
+    strcpy(newboard.filename, pbt->name);
     strcpy(newboard.title, "0[待定]      版面中文名称待定");
     if (getbid(newboard.filename, NULL) > 0) {
-        end_bootstrap();
+        clean_ipc();
         error("%s: getbid error", newboard.filename);
     }
     strcpy(vbuf, "vote/");
     strcat(vbuf, newboard.filename);
     setbpath(buf, newboard.filename);
-    /* fancy Aug 4 2009, 非空目录删的掉??
-    remove(buf);
-    remove(vbuf);
-    */
-    f_rm(buf);
-    f_rm(vbuf);
-    if (mkdir(buf, 0755) == -1 || mkdir(vbuf, 0755) == -1) {
-        end_bootstrap();
+    /*
+     * keep old files.
+     * f_rm(buf);
+     * f_rm(vbuf);
+     */
+    if (wrapped_mkdir(buf, 0755) == -1 || wrapped_mkdir(vbuf, 0755) == -1) {
+        clean_ipc();
         error("%s: mkdir error", newboard.filename);
     }
     snprintf(newboard.ann_path, 127, "%s/%s", groups[0], newboard.filename);
     newboard.ann_path[127] = '\0';
 
-    /* 默认权限为: 读限制为PERM_SYSOP */
+    /* 默认权限 */
     newboard.level = PERM_SYSOP;
-    //下面是为各个版设定访问权限的代码, 参见doc/README.SYSOP的推荐设置
-    //use it at your own risk
 
-    if (type == X)
-        /* 写限制为PERM_SYSOP */
+    if (pbt->type == X)
         newboard.level |= PERM_POSTMASK;
-    else if (type == o)
-        /* 读限制为PERM_BASIC */
+    else if (pbt->type == o)
         newboard.level = 0;
 
     if (add_board(&newboard) == -1) {
-        end_bootstrap();
+        clean_ipc();
         error("%s: add_board error", newboard.filename);
     }
     if ((bid = getbid(newboard.filename, NULL)) == 0) {
-        end_bootstrap();
+        clean_ipc();
         error("%s: getbid error", newboard.filename);
     }
 
-    sprintf(buf, "add brd %s", newboard.filename);
+    sprintf(buf, "add board %s", newboard.filename);
     bbslog("user", "%s", buf);
+    usage(buf);
 }
 
-void makeboards(void)
+void add_allboards(void)
 {
-    struct _boards {
-        enum board_t type;
-        char *name;
-    } boards[] = {
-        {V, "bbsnet"},
-        {V, "denypost"},
-        {V, "Filter"},
-        {V, "GiveupNotice"},
-        {V, "Goodbye"},
-        {V, "Recommend"},
-        {V, "Registry"},
-        {V, "reject_registry"},
-        {V, "syssecurity"},
-        {V, "undenypost"},
-        {V, "sysmail"},
-        {X, "BBSLists"},
-        {X, "newcomers"},
-        {X, "notepad"},
-        {X, "vote"},
-        {o, "Blessing"},
-        {nil, ""},
-    };
-    struct _boards *ptr = boards;
-
-    for (; ptr->type != nil; ++ptr) {
-        do_makeboard(ptr->type, ptr->name);
-    }
-
+    board_t *pbt = boards;
     char buf1[PATHLEN], buf2[PATHLEN];
 
-    sprintf(buf1, "%s/boards/sysmail", bbshome);
-    sprintf(buf2, "%s/mail/S/SYSOP", bbshome);
+    for (; pbt && pbt->type != nil; ++pbt)
+        add_bbsboard(pbt);
+
+    sprintf(buf1, "%s/boards/sysmail", BBSHOME);
+    sprintf(buf2, "%s/mail/S/SYSOP", BBSHOME);
     remove(buf1);
     symlink(buf2, buf1);
 }
 
-void do_bootstrap(void)
+void bootstrap(void)
 {
-    char cmd[PATHLEN];
     int ret;
-    int c;
     char *prompt[] = {
-        "Input SYSOP's passwd",
-        "Input SYSOP's passwd again",
+        "Input SYSOP's passwd: ",
+        "Input SYSOP's passwd again: ",
     };
-    char pw[2][PASSLEN];
-    int i;
-    int len;
+    char *pw[2];        /* store SYSOP's password */
 
+    /*
+     * About function getpass():
+     * lots of codes still use the function, so we use it,
+     * though it is obsolete.
+     * XXX: memory leak here is harmless.
+     */
     while (1) {
-        for (i=0; i<2; ++i) {
-            len = 0;
-
-            puts(prompt[i]);
-            while (len < PASSLEN - 1 && (c = getchar()) != EOF && c != '\n' && c != '\r')
-                pw[i][len++] = c;
-
-            /* i == 0时，或者第一次输入时，检测不合理输入，第二次不检测 */
-            if (i == 0 && (len < 4 || !strcmp(pw[i], "SYSOP"))) {
-                printf("密码太短或与使用者代号相同，请重新输入\n");
-                break;
-#if 0
-            } else if (i == 0 && len >= LEN - 100) {      /* make enough space */
-                printf("密码太长，请重新输入\n");
-                break;
-#endif
-            } else if (i == 0 && c == EOF)
-                error("encounter 'EOF' when asking password");
-            else
-                pw[i][len] = '\0';
+        pw[0] = strdup(getpass(prompt[0]));
+        if (strlen(pw[0]) > PASSLEN) {
+            fprintf(stderr, "password too long, reinput\n");
+            continue;
         }
-
-        if (strcmp(pw[0], pw[1]))
-            printf("密码输入错误，请重新输入\n");
-        else {
-            printf("SYSOP's passwd is '%s'\n", pw[0]);
-            strcpy(SYSOP_pass, pw[0]);
-            break;
+        if (strlen(pw[0]) < 4) {
+            fprintf(stderr, "password too short, reinput\n");
+            continue;
         }
+        if (!strcmp(pw[0], "SYSOP")) {
+            fprintf(stderr, "password same as BBS ID, reinput\n");
+            continue;
+        }
+        pw[1] = strdup(getpass(prompt[1]));
+        if (strcmp(pw[0], pw[1])) {
+            fprintf(stderr, "passwords don't match, reinput\n");
+            continue;
+        }
+        break;
     }
 
-    cleanids();
+    clean_ipc();
+    clean_ids_boards();
 
-    if (signal(SIGTERM, sig_usr) == SIG_ERR)
-        error("can't catch SIGTERM");
-    if (signal(SIGINT, sig_usr) == SIG_ERR)
-        error("can't catch SIGINT");
-
-    strcpy(cmd, bbshome);
-    strcat(cmd, "/bin/miscd daemon");
-    if ((ret = system(cmd)) != 0)
+    if ((ret = system(BBSHOME "/bin/miscd daemon")) != 0)
         error("error running miscd, return value = %d", ret);
-    strcpy(cmd, bbshome);
-    strcat(cmd, "/bin/bbslogd");
-    if ((ret = system(cmd)) != 0)
+    if ((ret = system(BBSHOME "/bin/bbslogd")) != 0)
         error("error running bbslogd, return value = %d", ret);
 
-    chdir(bbshome);
+    chdir(BBSHOME);
     resolve_boards();
     resolve_utmp();
     load_ucache();
 
-    makeids();
-    makeboards();
+    add_bbsid("SYSOP", pw[0]);
+    add_bbsid("guest", "anypassword");
+    add_allboards();
 
-    end_bootstrap();
+    clean_ipc();
 }
 
-void bootstrap(void) 
+int main(int argc, char *argv[])
 {
+    uid_t uid = geteuid();
     char buf[STRLEN];
-    int len;
+    size_t len;
+
+    if (uid != BBSUID)
+        if (setgid(BBSGID) == -1 || setuid(BBSUID) == -1)
+            error("Please run %s as BBSUSER or root.", argv[0]);
+
+    if (system("pgrep bbsd > /dev/null") == 0 ||
+            system("pgrep sshbbsd > /dev/null") == 0 ||
+            system("pgrep miscd > /dev/null") == 0
+       )
+            // sshdbbsd or bbsd or miscd is running
+        error("Please shutdown BBS first.");
 
     printf("Danger! This may destroy ALL OF YOUR USER AND BOARD DATA!\n");
     printf("Type `Go ahead!' exactly without quotes and press ENTER to continue\n");
@@ -304,44 +298,8 @@ void bootstrap(void)
     if (buf[len-1] == '\n' || buf[len-1] == '\r')
         buf[len-1] = '\0';
     if (!strcmp("Go ahead!", buf))
-        do_bootstrap();
+        bootstrap();
     else
         printf("User aborted.\n");
+    return 0;
 }
-
-void show_help(void)
-{
-    printf("NO Help Now.\nPlease read the source file or kick skybluee.\n");
-    exit(EXIT_SUCCESS);
-}
-
-int main(int argc, char **argv)
-{
-    int ch;
-    int i;
-
-    while ((ch = getopt(argc, argv, "hd:")) != -1)
-        switch (ch) {
-        case 'd' :
-            if (optarg[0] == '\0')
-                error("invalid path");
-            strncpy(bbshome, optarg, PATHLEN);
-            bbshome[PATHLEN - 1] = '\0';
-            for (i=0; bbshome[i] != '\0'; ++i)
-                ;
-            /* remove the tailing '/' */
-            if (i != 0 && bbshome[i-1] == '/')
-                bbshome[i-1] = '\0';
-            break;
-        case 'h' :
-            show_help();
-            break;
-        default :
-            break;
-        }
-
-    bootstrap();
-
-    exit(EXIT_SUCCESS);
-}
-
